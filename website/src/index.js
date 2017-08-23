386b6c06ab338e9fdc3752a5279b255606eb9302
James Kyle <me@thejameskyle.com>
2017-08-24 09:47:23 +1000

    Init website setup

---
+++ website/src/index.js
@@ -0,0 +1,9 @@
+// @flow
+import * as React from 'react';
+import {render} from 'react-dom';
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+render(<App/>, document.getElementById('root'));
