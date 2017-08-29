a8b75aa060d114b2c79d6665c87e3bcd98554ad1
James Kyle <me@thejameskyle.com>
2017-08-29 16:59:29 +1000

    introduce extract-react-types-loader

--- website/src/index.js
+++ website/src/index.js
@@ -2,8 +2,37 @@
 import * as React from 'react';
 import {render} from 'react-dom';
 
+import types from '!!extract-react-types-loader!../../components/badge/src/components/Badge';
+
+console.log(types);
+
+function renderType(type) {
+  if (type.kind === 'number') {
+    return <span>number</span>
+  } else {
+    throw new Error('wat: ' + type.kind);
+  }
+}
+
+function Types(props) {
+  return (
+    <div>
+      {props.types.classes.map(klass => {
+        return (
+          <div>
+            {klass.props.map(prop => {
+              console.log(prop.value);
+              return <div>{prop.key}: {renderType(prop.value)}</div>;
+            })}
+          </div>
+        );
+      })}
+    </div>
+  )
+}
+
 function App() {
-  return <h1>Hello World</h1>;
+  return <Types types={types}/>;
 }
 
 render(<App/>, document.getElementById('root'));
