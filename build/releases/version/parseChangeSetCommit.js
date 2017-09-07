6fa72615cc4d74bfc22eaf0e9fa0c3072e861471
Tong Li <tli@.atlassian.com>
2017-09-07 11:27:10 +1000

    Update parseChangeSetCommit

--- build/releases/version/parseChangeSetCommit.js
+++ build/releases/version/parseChangeSetCommit.js
@@ -1,11 +1,11 @@
 /**
  *
- * @param {string[]} commitMsg
+ * @param {string} commitMsg
  */
 function parseChangeSetCommit(commitMsg) {
-  const lines = commitMsg.filter(line => typeof line === 'string' && line.trim().length !== 0);
+  const lines = commitMsg.split('\n').filter(line => typeof line === 'string' && line.trim().length !== 0);
 
-  if (lines.length <= 0 && !lines.unshift().startsWith('Version:')) {
+  if (lines.length <= 0 && !lines.unshift().startsWith('CHANGESET:')) {
     // This is not a version commit, I don't care
     return null;
   }
