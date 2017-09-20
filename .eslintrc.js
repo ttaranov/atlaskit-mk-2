e4f80f23fe0b184d60e3f57f28ae5ee3db0eb679
Trey Shugart <treshugart@gmail.com>
2017-09-20 16:28:51 +1000

    Move eslintrc option no-restricted-properties from website to the main eslintrc.

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,6 +4,13 @@ module.exports = {
     'import/no-extraneous-dependencies': ['off', 'vscode'],
     'import/no-unresolved': ['off', 'vscode'],
     'max-len': ['error', 140],
+    'no-restricted-properties': [
+      'off',
+      {
+        object: 'React',
+        property: 'Component',
+      },
+    ],
     'no-restricted-syntax': 'off',
     'no-use-before-define': 'off',
     'react/sort-comp': 'off',
--- website/.eslintrc.js
+++ website/.eslintrc.js
@@ -2,13 +2,6 @@ module.exports = {
   rules: {
     'import/extensions': 'off',
     'import/no-dynamic-require': 'off',
-    'no-restricted-properties': [
-      'off',
-      {
-        object: 'React',
-        property: 'Component'
-      },
-    ],
     'react/no-did-mount-set-state': 'off',
   },
 };
