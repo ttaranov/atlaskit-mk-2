486eca354674d2d8562142092adbce088668f1c8
James Kyle <me@thejameskyle.com>
2017-10-03 15:52:28 +1100

    Fix release build types

--- build/utils/logger.js
+++ build/utils/logger.js
@@ -1,23 +1,23 @@
 // @flow
 const chalk = require('chalk');
 
-function log(message: string) {
+function log(message /*: string */) {
   console.log(message);
 }
 
-function info(message: string) {
+function info(message /*: string */) {
   console.error(chalk.cyan('info'), message);
 }
 
-function warn(message: string) {
+function warn(message /*: string */) {
   console.error(chalk.yellow('warn'), message);
 }
 
-function error(message: string) {
+function error(message /*: string */) {
   console.error(chalk.red('error'), message);
 }
 
-function success(message: string) {
+function success(message /*: string */) {
   console.log(chalk.green('success'), message);
 }
 
