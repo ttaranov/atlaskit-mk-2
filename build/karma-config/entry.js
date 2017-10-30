8f81440879b31fdc4a1c5c787241127caf9714c6
Stanislav Sysoev <ssysoev@atlassian.com>
2017-10-30 16:57:07 +1100

    Add karma setup

---
+++ build/karma-config/entry.js
@@ -0,0 +1,27 @@
+// hack around require.context not being dynamic.
+// we use require.context here to grab all the files in each of the packages that have browser tests
+// since require.context will walk every node in the file tree, if we try do this from the root of
+// packages, it will try walk every node_modules folder, and take hours
+// packages we want to add browserstack tests to will need to be added to this list like below.
+
+const contexts = {
+  'editor-core': require.context(
+    '../../packages/fabric/editor-core/test/browser/',
+    true,
+    /^[^_]*.(js|jsx|ts|tsx)$/
+  ),
+};
+
+Object.keys(contexts).forEach(key => {
+  const ctx = contexts[key];
+
+  ctx.keys().forEach(path => {
+    try {
+      ctx(path);
+    } catch (err) {
+      // eslint-disable-next-line
+      console.error(`Running tests in: ${path}`);
+      throw err;
+    }
+  });
+});
