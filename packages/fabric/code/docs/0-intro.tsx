5ae0397f453dfd8808505378aac82ada521369aa
James Kyle <me@thejameskyle.com>
2017-11-01 16:04:10 +1100

    Stub out .npmignores and docs

--- packages/fabric/code/docs/0-intro.tsx
+++ packages/fabric/code/docs/0-intro.tsx
@@ -3,14 +3,4 @@ import { md, Example } from '@atlaskit/docs';
 
 export default md`
   Renders inline code snippets and code blocks.
-
-  ## Examples
-
-  ${(
-    <Example
-      Component={require('../examples/00-inline-code-basic').default}
-      title="Inline Code"
-      source={require('!!raw-loader!../examples/00-inline-code-basic')}
-    />
-  )}
 `;
