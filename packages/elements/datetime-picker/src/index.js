78d497e0baf00e779ace1a1fb9909ee51832c9b0
Ben Wong <benwong@atlassian.com>
2017-11-01 16:55:07 +1100

    AK-3656 Update main field and export DatePickerStateless and helper components

--- packages/elements/datetime-picker/src/index.js
+++ packages/elements/datetime-picker/src/index.js
@@ -1,4 +1,8 @@
 // @flow
 
-// eslint-disable-next-line import/prefer-default-export
 export { default as DatePicker } from './components/DatePicker';
+export { default as DatePickerStateless } from './components/DatePickerStateless';
+
+export { default as BasePicker } from './components/internal/Picker';
+export { default as DateField } from './components/internal/DateField';
+export { default as DateDialog } from './components/internal/DateDialog';
