a7f0460a05a394b134308f6c6177fa3e964c1380
Luke Batchelor <lbatchelor@atlassian.com>
2017-11-02 11:46:00 +1100

    Build now pulls correct version of bolt

--- build-setup.sh
+++ build-setup.sh
@@ -5,7 +5,12 @@
 curl -o- -L https://yarnpkg.com/install.sh | bash -s -- --version 0.23.2
 export PATH=$HOME/.yarn/bin:$PATH
 
-yarn global add bolt
+
+## Installing bolt - we grab the range specified in the package.json so that we install the latest
+## version that satisfies that
+BOLT_VERSION=`node -e 'console.log(require("./package.json").bolt.version)'`
+echo "Installing bolt@$BOLT_VERSION"
+yarn global add bolt@$BOLT_VERSION
 
 # These are used to sign commits when pushing back to Bitbucket
 # No auth is required as we use ssh from pipelines instead
