abb48914e20543656d8a3b5231a64eb42ee6fdbc
Rifat Nabi <rnabi@atlassian.com>
2017-11-10 15:58:46 +1100

    fix(editor-core/collab): Avatars not in the correct place (overlap buttons)

--- packages/fabric/editor-core/example-helpers/mock-collab-provider.ts
+++ packages/fabric/editor-core/example-helpers/mock-collab-provider.ts
@@ -1,28 +1,111 @@
 import { EventEmitter } from 'events';
-import { CollabEditProvider, CollabEvent } from '../src/editor/plugins/collab-edit/provider';
+import {
+  CollabEditProvider,
+  CollabEvent
+} from '../src/editor/plugins/collab-edit/provider';
+
+interface Participant {
+  sid: string;
+  name: string;
+  src: string;
+}
+
+const participants = {
+  rick: {
+    sid: 'rick',
+    name: 'Rick Sanchez',
+    avatar:
+      'https://pbs.twimg.com/profile_images/897250392022540288/W1T-QjML_400x400.jpg'
+  },
+  morty: {
+    sid: 'morty',
+    name: 'Morty Smith',
+    avatar:
+      'https://pbs.twimg.com/profile_images/685489227082129408/YhGfwW73_400x400.png'
+  },
+  summer: {
+    sid: 'sumsum',
+    name: 'Summer Smith',
+    avatar:
+      'https://pbs.twimg.com/profile_images/878646716328812544/dYdU_OKZ_400x400.jpg'
+  }
+};
+
+const others = (sid: string) =>
+  Object.keys(participants).reduce<Participant[]>(
+    (all, id) => (id === sid ? all : all.concat(participants[id])),
+    []
+  );
+
+class Mediator extends EventEmitter {
+  emit(eventName, data, ...args) {
+    switch (eventName) {
+      case 'init': {
+        const { sid, doc } = data as any;
+        this.emit(`${sid}:init`, { doc });
+        this.emit(`${sid}:connected`, { sid });
+        const collaborators = others(sid);
+        const joined = collaborators.map(({ sid: sessionId, ...rest }) => ({
+          sessionId,
+          ...rest,
+          lastActive: 0
+        }));
+        collaborators.forEach(({ sid: xSid }) => {
+          setTimeout(() => {
+            this.emit(`${xSid}:presence`, { joined });
+          }, 0);
+        });
+        return;
+      }
+      case 'data':
+      case 'telepointer': {
+        const { sid, ...rest } = data as any;
+        others(sid).forEach(({ sid }) => {
+          this.emit(`${sid}:${eventName}`, { ...rest });
+        });
+        return;
+      }
+    }
+    super.emit(eventName, data, ...args);
+  }
+}
+
+const mediator = new Mediator();
 
 export class MockCollabEditProvider implements CollabEditProvider {
   protected getState = () => {};
-  protected eventBus: EventEmitter = new EventEmitter();
+  protected sid;
+  protected eventBus: EventEmitter;
+
+  constructor(eventBus: EventEmitter, sid?: string) {
+    // If there's no sid then it's single user, being used for test
+    if (sid) {
+      this.sid = sid;
+      this.eventBus = eventBus;
+    } else {
+      this.eventBus = new EventEmitter();
+    }
+  }
 
   initialize(getState: () => any) {
     this.getState = getState;
     const doc = {
-      'type': 'doc',
-      'content': [
+      type: 'doc',
+      content: [
         {
-          'type': 'paragraph',
-          'content': [
+          type: 'paragraph',
+          content: [
             {
-              'type': 'text',
-              'text': 'Hello World'
+              type: 'text',
+              text: 'Hello World'
             }
           ]
         }
       ]
     };
 
-    this.eventBus.emit('init', { doc });
+    const { sid } = this;
+    this.eventBus.emit('init', { sid, doc });
 
     return this;
   }
@@ -35,20 +118,33 @@ export class MockCollabEditProvider implements CollabEditProvider {
   }
 
   on(evt: CollabEvent, handler: (...args) => void) {
-    this.eventBus.on(evt, handler);
+    const { sid } = this;
+    if (sid) {
+      this.eventBus.on(`${sid}:${evt}`, handler);
+    } else {
+      this.eventBus.on(evt, handler);
+    }
     return this;
   }
 
-  /**
-   * Only used for testing.
-   */
   emit(evt: CollabEvent, ...args) {
-    this.eventBus.emit(evt, ...args);
+    const { sid } = this;
+    if (sid) {
+      this.eventBus.emit(evt, { sid, ...args });
+    } else {
+      this.eventBus.emit(evt, ...args);
+    }
   }
 
   sendMessage(data: any) {
+    const { sid } = this;
+    if (sid) {
+      this.eventBus.emit(`${data.type}`, { sid, ...data });
+    }
   }
 }
 
-export const collabEditProvider = new MockCollabEditProvider();
-export const collabEditProviderPromise = Promise.resolve(collabEditProvider);
+const getCollabEditProviderFor = <T>(participants: T) => (sid?: keyof T) =>
+  Promise.resolve(new MockCollabEditProvider(mediator, sid));
+
+export const collabEditProvider = getCollabEditProviderFor(participants);
--- packages/fabric/editor-core/src/editor/plugins/collab-edit/index.tsx
+++ packages/fabric/editor-core/src/editor/plugins/collab-edit/index.tsx
@@ -1,11 +1,9 @@
-import * as React from 'react';
 import { EditorPlugin } from '../../types';
 import {
   createPlugin,
   CollabEditProvider,
   pluginKey,
 } from './plugin';
-import Avatars from './ui/avatars';
 
 export {
   CollabEditProvider,
@@ -13,17 +11,11 @@ export {
 };
 
 const collabEditPlugin: EditorPlugin = {
-
   pmPlugins() {
     return [
       { rank: 1000, plugin: (schema, props, dispatch, providerFactory) => createPlugin(dispatch, providerFactory) }
     ];
-  },
-
-  primaryToolbarComponent(editorView, eventDispatcher) {
-    return <Avatars editorView={editorView} eventDispatcher={eventDispatcher} />;
-  },
-
+  }
 };
 
 export default collabEditPlugin;
