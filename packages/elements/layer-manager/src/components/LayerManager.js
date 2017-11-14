67e57b24def8c17de64b62d5a6014c666ce2109d
Michael Blaszczyk <mblaszczyk@atlassian.com>
2017-11-14 11:54:51 +1100

    Add flag support to layer manager

--- packages/elements/layer-manager/src/components/LayerManager.js
+++ packages/elements/layer-manager/src/components/LayerManager.js
@@ -43,6 +43,7 @@ export default class LayerManager extends Component<Props, State> {
         </AppWrapper>
         <GatewayDest name="modal" component={TransitionGroup} />
         <GatewayDest name="spotlight" />
+        <GatewayDest name="flag" />
         <GatewayDest name="tooltip" component={TransitionGroup} />
       </GatewayProvider>
     );
