778868223dcd502ec45d246aab0a87c354976aa4
Eduard Shvedai <eshvedai@atlassian.com>
2017-11-16 07:25:58 +1100

    added InlineMacro test, fixed linting

--- packages/fabric/editor-core/src/ui/Macro/MacroComponent.tsx
+++ packages/fabric/editor-core/src/ui/Macro/MacroComponent.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import { Component } from 'react';
 import { EditorView } from 'prosemirror-view';
 import { EditorState, Transaction } from 'prosemirror-state';
