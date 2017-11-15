3bb04d862d8f8821673b9c10a3192fc44a637e6a
Eduard Shvedai <eshvedai@atlassian.com>
2017-11-16 00:05:41 +1100

    skip backwards compat test

--- packages/fabric/editor-common/__tests__/json-schema/backwards-compat.ts
+++ packages/fabric/editor-common/__tests__/json-schema/backwards-compat.ts
@@ -43,8 +43,7 @@ expect.extend({
   },
 });
 
-// TODO: skip
-describe('JSON schema', () => {
+describe.skip('JSON schema', () => {
   it('should be backwards compatible', async () => {
     let existingSchema: any;
 
