a2458d1f9b2b4fca3817b7eec7f21c8ec7c52675
Eduard Shvedai <eshvedai@atlassian.com>
2017-11-16 07:25:58 +1100

    added InlineMacro test, fixed linting

--- packages/fabric/editor-core/tests/browser/ui/Macro.tsx
+++ packages/fabric/editor-core/tests/browser/ui/Macro.tsx
@@ -4,41 +4,27 @@ import { mount } from 'enzyme';
 import ProviderFactory from '../../../src/providerFactory';
 import Macro from '../../../src/ui/Macro';
 import MacroComponent from '../../../src/ui/Macro/MacroComponent';
-import { doc, panel, p, makeEditor } from '@atlaskit/editor-test-helpers';
 import { macroProviderPromise } from '../../../example-helpers/mock-macro-provider';
 import { inlineMacroData } from '../../../example-helpers/mock-macro-data';
 
 describe('@atlaskit/editor-core/ui/Macro', () => {
-  const editor = (doc: any) => makeEditor({ doc });
   const node = inlineMacroData[0] as any;
 
-  it('should render macro component', async (done) => {
-    const { editorView } = editor(doc(panel(p('te{<>}xt'))));
-    const macro = mount(<Macro editorView={editorView} node={node} />);
+  it('should render macro component', () => {
+    const macro = mount(<Macro editorView={{} as any} node={node} />);
     const macroComponent = macro.find(MacroComponent);
     expect(macroComponent.prop('node')).to.equal(node);
     macro.unmount();
   });
 
-  it('should pass macroProvider into MacroComponent', async (done) => {
-    const { editorView } = editor(doc(panel(p('te{<>}xt'))));
+  it('should pass macroProvider into MacroComponent', () => {
     const providerFactory = new ProviderFactory();
     providerFactory.setProvider('macroProvider', macroProviderPromise);
 
-    const macro = mount(<Macro editorView={editorView} node={node} providerFactory={providerFactory}/>);
+    const macro = mount(<Macro editorView={{} as any} node={node} providerFactory={providerFactory}/>);
     const macroComponent = macro.find(MacroComponent);
 
     expect(macroComponent.prop('macroProvider')).to.equal(macroProviderPromise);
-    macro.unmount();
-  });
-
-  it('should render placeholder image if macroProvider is set', () => {
-    const { editorView } = editor(doc(panel(p('te{<>}xt'))));
-    const providerFactory = new ProviderFactory();
-    providerFactory.setProvider('macroProvider', macroProviderPromise);
-
-    const macro = mount(<Macro editorView={editorView} node={node} providerFactory={providerFactory}/>);
-    expect(macro.find(`img`)).to.have.length(1);
     macro.unmount();
   });
 });
