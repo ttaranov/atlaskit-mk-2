1d25fa2884a418d3885fb99897c7c05f923ab41a
bswierczynski <bswierczynski@atlassian.com>
2017-11-20 15:13:53 +1100

    [WIP] Broken propTypes / flow

--- packages/elements/tree-table/src/components/TreeTable.js
+++ packages/elements/tree-table/src/components/TreeTable.js
@@ -1,18 +1,28 @@
 // @flow
-import React, { Children, PureComponent, Component, type Element } from 'react';
+import React, {
+  Children,
+  PureComponent,
+  Component,
+  type ElementType,
+} from 'react';
 import Subtree from './Subtree';
 
 import { type DataFunction } from './../types';
 
 type Props = {
-  columns: Array<Element>,
-  headers: ?Array<Element<*> | string>,
+  /** This is the columns prop description */
+  columns: Array<ElementType>,
+  // columns: Array<Element>,
+  // headers?: Array<string>,
+  // headerWidths?: Array<string|number>,
+  /** This is the data prop description */
   data: DataFunction | string,
 };
 
-export default class TreeTable extends PureComponent<Props> {
-  props: Props; // eslint-disable-line react/sort-comp
-  // portalElement: HTMLElement;
+export default class TreeTable extends Component<Props> {
+  // static defaultProps = {
+  //   headers: null
+  // }
 
   render() {
     const { data, columns } = this.props;
