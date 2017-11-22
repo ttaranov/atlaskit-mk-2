5f725805584709ba923d6de1fed6c13d75f8391e
Artur Bodera <abodera@atlassian.com>
2017-11-23 10:50:53 +1100

    Improve resolver to follow webpack rules for using "atlaskit:src" main field for sub-packages.

--- resolver.js
+++ resolver.js
@@ -1,20 +1,29 @@
 // @flow
 const fs = require('fs');
 const path = require('path');
-const defaultResolver = require('jest-resolve/build/defaultResolver');
+
+// This is the resolver used by webpack, which we configure similarly
+// to AK website (see ./website/webpack.config.js - "resolve" field)
+const wpResolver = require('enhanced-resolve').ResolverFactory.createResolver({
+  fileSystem: fs,
+  useSyncFileSystemCalls: true,
+  mainFields: ['atlaskit:src', 'browser', 'main'],
+  extensions: ['.js', '.ts', '.tsx'],
+});
 
 module.exports = function resolver(
   modulePath /*: string */,
   params /*: any */,
 ) {
-  // Skip relative modulePath
+  // If resolving relative paths, use default NodeJS resolver
   if (!modulePath.startsWith('.') && !modulePath.startsWith(path.sep)) {
     try {
       return require.resolve(modulePath);
     } catch (e) {} // eslint-disable-line
   }
 
-  let result = defaultResolver(modulePath, params);
+  // Try to resolve to source files of AK packages using webpack resolver
+  let result = wpResolver.resolveSync({}, params.basedir, modulePath);
 
   if (result) {
     // Dereference symlinks to ensure we don't create a separate
