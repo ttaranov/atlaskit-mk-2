e0c9415f8cfd20a18810ca4b7209a23212e9d09d
Luke Batchelor <lbatchelor@atlassian.com>
2017-11-23 17:22:38 +1100

    Moves the git push before the publish step in release to give us a more stable build

--- build/releases/release/index.js
+++ build/releases/release/index.js
@@ -76,21 +76,21 @@ async function run(opts) {
     logger.log('Committing changes...');
     // TODO: Check if there are any unstaged changed before committing and throw
     // , as it means something went super-odd.
-    const committed = await git.commit(publishCommit);
+    await git.commit(publishCommit);
 
-    if (committed) {
-      // bolt will throw if there is an error
-      await bolt.publish({ access: 'public' });
+    // we push back before publishing because it's easier to recover from being ahead of npm than
+    // behind
+    logger.log('Pushing changes back to origin...');
+    await git.rebaseAndPush(maxGitRetries);
 
-      const releasedPackages = releaseObj.releases
-        .map(r => `${r.name}@${r.version}`)
-        .join('\n');
-      logger.success('Successfully published:');
-      logger.log(releasedPackages);
+    // bolt will throw if there is an error
+    await bolt.publish({ access: 'public' });
 
-      logger.log('Pushing changes back to origin...');
-      await git.rebaseAndPush(maxGitRetries);
-    }
+    const releasedPackages = releaseObj.releases
+      .map(r => `${r.name}@${r.version}`)
+      .join('\n');
+    logger.success('Successfully published:');
+    logger.log(releasedPackages);
   }
 }
 
