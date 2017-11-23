989a32902a1dcbb51e075a8ab59198712cbf85ec
Aleksandr Sasha Motsjonov <amotsjonov@atlassian.com>
2017-11-24 08:20:47 +1100

    Introduce media-core

---
+++ packages/fabric/media-core/docs/0-intro.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { md, Example, Props } from '@atlaskit/docs';
+
+export default md`
+  # MediaCore
+
+  This package is required by other Media Components, and should not be used
+  directly.
+
+  It holds shared code between Media Components, such as:
+
+  * models
+  * providers
+  * interfaces
+
+`;
