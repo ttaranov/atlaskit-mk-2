8e3504eceab132391fbcf8f81f1c21357f9c2eed
Aleksandr Sasha Motsjonov <amotsjonov@atlassian.com>
2017-11-24 13:58:37 +1100

    Add tests and examples

--- packages/fabric/media-core/docs/0-intro.tsx
+++ packages/fabric/media-core/docs/0-intro.tsx
@@ -13,4 +13,33 @@ export default md`
   * providers
   * interfaces
 
+  ## Installation
+
+  ~~~sh
+  yarn add @atlaskit/media-core
+  ~~~
+
+  ## Using the component
+
+  \`Context\` is the main object that is created with \`ContextFactory\`. It can
+  be created using either \`token\` and either \`clientId\` or \`asapIssuer\`.
+
+  ~~~js
+  import { Context, ContextConfig, ContextFactory } from '@atlaskit/media-core';
+
+  const authProvider = ({ collectionName }) =>
+    new Promise((resolve, reject) => {
+      resolve({
+        token: 'token-that-was-recieved-in-some-async-way',
+        clientId: 'some-client-id',
+        //  asapIssuer: 'asap-issuer'
+      });
+    });
+  const config: ContextConfig = {
+    serviceHost: 'http://example.com',
+    authProvider,
+  };
+  const context: Context = ContextFactory.create(config);
+  ~~~
+
 `;
