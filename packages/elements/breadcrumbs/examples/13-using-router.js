6aa9d115bfbc9edd5c5fe4adbfbfb4815bacd328
Ben Conolly <bconolly@atlassian.com>
2017-11-27 17:01:59 +1100

    added breadcrumbs to new repo

---
+++ packages/elements/breadcrumbs/examples/13-using-router.js
@@ -0,0 +1,20 @@
+// // @flow
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import TitledPage from './component/TitledPage';
+//
+// function makePage(title) {
+//   return () => <TitledPage title={title} />;
+// }
+//
+// export default () => (
+//   <Router>
+//     <Switch>
+//       <Route component={makePage('Page 1')} path="/page1" />
+//       <Route component={makePage('Page 2')} path="/page2" />
+//       <Route component={makePage('Page 3')} path="/page3" />
+//       <Route component={makePage('Page 4')} path="/page4" />
+//       <Route component={makePage('Container home')} />
+//     </Switch>
+//   </Router>
+// );
