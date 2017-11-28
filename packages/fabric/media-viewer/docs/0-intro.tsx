1be8a2fbaa14b2b1d58ea1ea2b30152e37858e22
Hector Leon Zarco Garcia <hezarco@gmail.com>
2017-11-28 15:28:19 +1100

    Migrate MediaViewer

---
+++ packages/fabric/media-viewer/docs/0-intro.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { md, Example, Props } from '@atlaskit/docs';
+
+export default md`
+  # @atlaskit/media-viewer
+
+  ## Installation
+
+  ~~~sh
+  yarn add @atlaskit/media-viewer
+  ~~~
+
+  ## Using the component
+
+`;
---
+++ packages/fabric/media-viewer/examples/0-basic.tsx
@@ -0,0 +1,1 @@
+export default () => <div>🔥 MediaViewer 🔥</div>;
