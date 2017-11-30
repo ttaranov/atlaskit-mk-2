695f7fd183afee18f3077efa17b28dae0f6b9935
Alex Reardon <areardon@atlassian.com>
2017-11-30 15:20:32 +1100

    adding themed example

---
+++ packages/elements/toggle/examples/3-themed.js
@@ -0,0 +1,36 @@
+// @flow
+import React from 'react';
+import Toggle from '@atlaskit/toggle';
+import Button from '@atlaskit/button';
+import { AtlaskitThemeProvider, gridSize } from '@atlaskit/theme';
+
+console.log('AtlaskitThemeProvider', AtlaskitThemeProvider);
+
+type State = {|
+  mode: 'light' | 'dark',
+|};
+
+export default class Swapper extends React.Component<any, State> {
+  onClick = () => {
+    this.setState({
+      mode: this.state.mode === 'light' ? 'dark' : 'light',
+    });
+  };
+  render() {
+    return (
+      <AtlaskitThemeProvider mode={this.state.mode}>
+        <div>
+          <Toggle />
+          <div style={{ marginTop: gridSize() }}>
+            <Button onClick={this.onClick}>
+              Toggle button{' '}
+              <span role="img" aria-label="irony">
+                😂
+              </span>
+            </Button>
+          </div>
+        </div>
+      </AtlaskitThemeProvider>
+    );
+  }
+}
