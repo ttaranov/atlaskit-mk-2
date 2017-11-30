c9e83f65101dfd2e937146262cd0684e56bf52c3
Luke Batchelor <lbatchelor@atlassian.com>
2017-12-01 08:51:50 +1100

    CHANGESET: Removes broken dist builds and simply exposes individual polyfills
    Summary: Removes broken dist builds and simply exposes individual polyfills
    
    Release notes: <none>
    
    Releases: @atlaskit/polyfills@patch
    
    Dependents: []
    
    ---
    {"summary":"Removes broken dist builds and simply exposes individual polyfills","releases":[{"name":"@atlaskit/polyfills","type":"patch"}],"dependents":[]}
    ---

--- packages/elements/polyfills/index.js
+++ packages/elements/polyfills/index.js
@@ -1,7 +1,16 @@
 // @flow
 /*
-    This file is npmignored and is only used so that locally, babel will find the src/index.js
-   file before looking in package.json:main
+    Note: This package is different to others where the root index file is just used as a workaround
+    The polyfills package needs to expose each individual polyfill as well as a single entrance that
+    pulls them all in
+    e.g require('@atlaskit/polyfills/object-assign') or require('@atlaskit/polyfills')
+
+    It deliberately does not have an atlaskit:src field as this causes issues when we alias pkg's
+    locally.
+
+    This file is also deliberately es5 as we wont be babelifying it.
 */
 
-export * from './src/';
+require('./array-prototype-includes');
+require('./object-assign');
+require('./string-prototype-includes');
