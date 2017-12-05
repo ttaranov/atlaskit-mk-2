d9de7b3ed119dfa6c3deea54b6f9dd35c94c6daf
Charles Lee <clee@atlassian.com>
2017-12-05 17:13:24 +1100

    remove reference to ChildrenType in src/types of tabs package

--- packages/elements/tabs/src/types.js
+++ packages/elements/tabs/src/types.js
@@ -1,13 +1,11 @@
 // @flow
-import type { Element, Node } from 'react';
-import type { PropType } from 'babel-plugin-react-flow-props-to-prop-types'; // eslint-disable-line import/no-extraneous-dependencies
+import type { Node } from 'react';
 
-export type ChildrenType = PropType<Array<Element<any>> | Element<any>, any>;
 export type FunctionType = (...args: Array<any>) => mixed;
 export type StatefulTab = {
-  content?: any,
+  content?: Node,
   defaultSelected?: boolean,
-  label: any,
+  label: string,
   isSelected?: boolean,
 };
 export type StatelessTab = {
