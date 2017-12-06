d5d8e5f13b27af30a3aa2880cf935f87da17486b
Stanislav Sysoev <ssysoev@atlassian.com>
2017-12-06 14:53:12 +1100

    CHANGESET: Add default inline code styles
    Summary: Add default inline code styles
    
    Release notes: <none>
    
    Releases: @atlaskit/editor-core@patch
    
    Dependents: @atlaskit/conversation@none, @atlaskit/editor-bitbucket@none, @atlaskit/editor-cq@none, @atlaskit/editor-jira@none, @atlaskit/editor-test-helpers@none, @atlaskit/renderer@none, @atlaskit/editor-core@none, @atlaskit/editor-bitbucket-transformer@none, @atlaskit/editor-common@none, @atlaskit/editor-cq-transformer@none, @atlaskit/editor-jira-transformer@none, @atlaskit/editor-json-transformer@none
    
    ---
    {"summary":"Add default inline code styles","releases":[{"name":"@atlaskit/editor-core","type":"patch"}],"dependents":[{"name":"@atlaskit/conversation","dependencies":["@atlaskit/editor-core","@atlaskit/renderer"],"type":"none"},{"name":"@atlaskit/editor-bitbucket","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-bitbucket-transformer"],"type":"none"},{"name":"@atlaskit/editor-cq","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-cq-transformer","@atlaskit/editor-common"],"type":"none"},{"name":"@atlaskit/editor-jira","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-json-transformer","@atlaskit/editor-jira-transformer"],"type":"none"},{"name":"@atlaskit/editor-test-helpers","dependencies":["@atlaskit/editor-core","@atlaskit/editor-common"],"type":"none"},{"name":"@atlaskit/renderer","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-common"],"type":"none"},{"name":"@atlaskit/editor-core","dependencies":["@atlaskit/renderer","@atlaskit/editor-test-helpers","@atlaskit/editor-json-transformer","@atlaskit/editor-cq-transformer","@atlaskit/editor-common","@atlaskit/editor-bitbucket-transformer"],"type":"none"},{"name":"@atlaskit/editor-bitbucket-transformer","dependencies":["@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-bitbucket"],"type":"none"},{"name":"@atlaskit/editor-common","dependencies":["@atlaskit/editor-test-helpers"],"type":"none"},{"name":"@atlaskit/editor-cq-transformer","dependencies":["@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-cq"],"type":"none"},{"name":"@atlaskit/editor-jira-transformer","dependencies":["@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-jira"],"type":"none"},{"name":"@atlaskit/editor-json-transformer","dependencies":["@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-bitbucket"],"type":"none"}]}
    ---

--- packages/fabric/editor-core/src/editor/ui/ContentStyles/index.ts
+++ packages/fabric/editor-core/src/editor/ui/ContentStyles/index.ts
@@ -7,7 +7,11 @@ import {
   akEditorTableBorderSelected,
   akEditorTableFloatingControls,
 } from '../../../styles';
-import { akGridSizeUnitless, akColorN80 } from '@atlaskit/util-shared-styles';
+import {
+  akGridSizeUnitless,
+  akColorN80,
+  akColorN20,
+} from '@atlaskit/util-shared-styles';
 import { telepointerStyle } from '../../plugins/collab-edit/styles';
 
 const tableStyle = `
@@ -100,13 +104,8 @@ const ContentStyles = styled.div`
     &::before {
       content: attr(data-text);
       color: ${akColorN80};
+      pointer-events: none;
     }
-  }
-
-  .ProseMirror ul,
-  .ProseMirror ol {
-    padding-left: 30px;
-    cursor: default;
   }
 
   .ProseMirror blockquote {
@@ -135,6 +134,28 @@ const ContentStyles = styled.div`
 
   .ProseMirror pre {
     white-space: pre-wrap;
+  }
+
+  .ProseMirror .code {
+    padding: 2px 1px;
+    background: ${akColorN20};
+    border-radius: 3px;
+    font-family: monospace;
+    white-space: pre-wrap;
+
+    &:before,
+    &:after {
+      vertical-align: text-top;
+      display: inline-block;
+      width: 3px;
+      content: '';
+    }
+  }
+
+  .ProseMirror ul,
+  .ProseMirror ol {
+    padding-left: 30px;
+    cursor: default;
   }
 
   .ProseMirror li {
