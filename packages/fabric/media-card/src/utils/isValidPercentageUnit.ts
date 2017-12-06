299f9704fdbd72e8a040b2f5bb7fcfa5008a791c
Hector Leon Zarco Garcia <hezarco@gmail.com>
2017-12-06 16:06:30 +1100

    Add specs to isValidPercentageUnit

--- packages/fabric/media-card/src/utils/isValidPercentageUnit.ts
+++ packages/fabric/media-card/src/utils/isValidPercentageUnit.ts
@@ -1,7 +1,8 @@
 export const isValidPercentageUnit = (value: string | number): boolean => {
   const valueString = `${value}`;
+
   return (
-    valueString.lastIndexOf('%') === valueString.length - 1 &&
-    !isNaN(parseInt(valueString, 10))
+    valueString[valueString.length - 1] === '%' &&
+    !isNaN(+valueString.slice(0, -1))
   );
 };
