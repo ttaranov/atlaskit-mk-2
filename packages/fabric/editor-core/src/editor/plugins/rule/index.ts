caf2ac040ab0d0b09e5a73763b1084dc5695e676
Stanislav Sysoev <ssysoev@atlassian.com>
2017-12-08 11:40:13 +1100

    CHANGESET: Add rule plugin
    Summary: Add rule plugin
    
    Release notes: <none>
    
    Releases: @atlaskit/editor-core@minor
    
    Dependents: @atlaskit/conversation@patch, @atlaskit/editor-bitbucket@none, @atlaskit/editor-cq@none, @atlaskit/editor-jira@none, @atlaskit/editor-test-helpers@none, @atlaskit/renderer@none, @atlaskit/editor-core@none, @atlaskit/editor-bitbucket-transformer@none, @atlaskit/editor-common@none, @atlaskit/editor-cq-transformer@none, @atlaskit/editor-jira-transformer@none, @atlaskit/editor-json-transformer@none
    
    ---
    {"summary":"Add rule plugin","releases":[{"name":"@atlaskit/editor-core","type":"minor"}],"dependents":[{"name":"@atlaskit/conversation","dependencies":["@atlaskit/editor-core","@atlaskit/renderer"],"type":"patch"},{"name":"@atlaskit/editor-bitbucket","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-bitbucket-transformer"],"type":"none"},{"name":"@atlaskit/editor-cq","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-cq-transformer","@atlaskit/editor-common"],"type":"none"},{"name":"@atlaskit/editor-jira","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-json-transformer","@atlaskit/editor-jira-transformer"],"type":"none"},{"name":"@atlaskit/editor-test-helpers","dependencies":["@atlaskit/editor-core","@atlaskit/editor-common"],"type":"none"},{"name":"@atlaskit/renderer","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-common"],"type":"none"},{"name":"@atlaskit/editor-core","dependencies":["@atlaskit/renderer","@atlaskit/editor-test-helpers","@atlaskit/editor-json-transformer","@atlaskit/editor-cq-transformer","@atlaskit/editor-common","@atlaskit/editor-bitbucket-transformer"],"type":"none"},{"name":"@atlaskit/editor-bitbucket-transformer","dependencies":["@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-bitbucket"],"type":"none"},{"name":"@atlaskit/editor-common","dependencies":["@atlaskit/editor-test-helpers"],"type":"none"},{"name":"@atlaskit/editor-cq-transformer","dependencies":["@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-cq"],"type":"none"},{"name":"@atlaskit/editor-jira-transformer","dependencies":["@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-jira"],"type":"none"},{"name":"@atlaskit/editor-json-transformer","dependencies":["@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-bitbucket"],"type":"none"}]}
    ---

--- packages/fabric/editor-core/src/editor/plugins/rule/index.ts
+++ packages/fabric/editor-core/src/editor/plugins/rule/index.ts
@@ -1,8 +1,13 @@
 import { EditorPlugin } from '../../types';
+import { rule } from '@atlaskit/editor-common';
 import keymapPlugin from '../../../plugins/rule/keymap';
 import inputRulePlugin from '../../../plugins/rule/input-rule';
 
 const rulePlugin: EditorPlugin = {
+  nodes() {
+    return [{ name: 'rule', node: rule, rank: 1000 }];
+  },
+
   pmPlugins() {
     return [
       {
