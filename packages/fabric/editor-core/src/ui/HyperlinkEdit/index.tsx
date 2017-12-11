94b0db2f3a3707a29276156b3a2af072027ed310
Jarred Mack <jmack2@.atlassian.com>
2017-12-11 14:50:57 +1100

    fix:ED-3120 - Fix hyperlink edit to close when hitting esc

--- packages/fabric/editor-core/src/ui/HyperlinkEdit/index.tsx
+++ packages/fabric/editor-core/src/ui/HyperlinkEdit/index.tsx
@@ -162,6 +162,7 @@ export default class HyperlinkEdit extends PureComponent<Props, State> {
           offset={[0, 3]}
           fitWidth={230}
           onPositionCalculated={this.adjustPosition}
+          onCancel={this.handleOnCancel}
           popupsMountPoint={this.props.popupsMountPoint}
           popupsBoundariesElement={this.props.popupsBoundariesElement}
         >
@@ -253,6 +254,14 @@ export default class HyperlinkEdit extends PureComponent<Props, State> {
       }
     }
     this.resetInputActive();
+  };
+
+  private handleOnCancel = () => {
+    this.setState({
+      inputActive: false,
+      editorFocused: false,
+    });
+    this.handleOnBlur();
   };
 
   private handleUnlink = () => {
