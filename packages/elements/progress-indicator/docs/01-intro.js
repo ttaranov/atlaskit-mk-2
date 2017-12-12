4a011bb6195577d2ec33c44d21ea9aaef4bb8435
Ajay Narain Mathur <amathur@atlassian.com>
2017-12-12 17:29:14 +1100

    CHANGESET: migrated page indicator to mk2
    Summary: migrated page indicator to mk2
    
    Release notes: <none>
    
    Releases: @atlaskit/progress-indicator@patch
    
    Dependents: []
    
    ---
    {"summary":"migrated page indicator to mk2","releases":[{"name":"@atlaskit/progress-indicator","type":"patch"}],"dependents":[]}
    ---

---
+++ packages/elements/progress-indicator/docs/01-intro.js
@@ -0,0 +1,28 @@
+// @flow
+import React from 'react';
+import { md, Example, Props } from '@atlaskit/docs';
+
+export default md`
+  The progress dots are visual indicators used when stepping a user through
+  a journey, to allow them to keep track of their progress.
+
+  They are typically accompanied by a carousel or other such UI device.
+
+  ## Examples
+
+  ${(
+    <Example
+      Component={require('../examples/01-basic').default}
+      title="Basic"
+      source={require('!!raw-loader!../examples/01-basic')}
+    />
+  )}
+
+  ${(
+    <Props
+      props={require('!!extract-react-types-loader!../src/components/Dots')}
+    />
+  )}
+
+
+`;
