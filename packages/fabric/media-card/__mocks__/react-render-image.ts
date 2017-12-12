2925a3d4bc62045285ec33532e9b038f3a32845d
James Newell <jnewell@atlassian.com>
2017-12-12 16:11:21 +1100

    inline react-render-image comments

--- packages/fabric/media-card/__mocks__/react-render-image.ts
+++ packages/fabric/media-card/__mocks__/react-render-image.ts
@@ -1,7 +1,15 @@
-let values;
-
-export const __mock__ = v => {
-  values = v;
+let params = {
+  image: undefined,
+  loaded: false,
+  errored: false,
 };
 
-export default ({ children }) => children(values);
+/* set the mock parameters  */
+export const __mock__ = p => {
+  params = p;
+};
+
+/*
+  mock `react-render-image` calling the the Function as a Child with the mock parameters
+*/
+export default ({ children }) => children(params);
