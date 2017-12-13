0686933d64eebb155fa245deb882cc724d544e8e
Hector Leon Zarco Garcia <hezarco@gmail.com>
2017-12-14 10:20:36 +1100

    Add test for non retina case in withDataURI component

--- packages/fabric/media-card/__tests__/withDataURI.spec.tsx
+++ packages/fabric/media-card/__tests__/withDataURI.spec.tsx
@@ -338,8 +338,30 @@ describe('WithDataURI', () => {
         expect(dataURIService.fetchImageDataUri).toBeCalledWith(
           expect.anything(),
           expect.objectContaining({
-            width: 100,
-            height: 200,
+            width: 50 * 2,
+            height: 100 * 2,
+          }),
+        );
+      });
+
+      it('should fetch dimensions with original size for non retina displays', () => {
+        (isRetina as jest.Mock<void>).mockReturnValue(false);
+        const element = shallow<WithDataURIProps, WithDataURIState>(
+          <DemoComponentWithDataURI
+            dataURIService={dataURIService}
+            metadata={metadata}
+            dimensions={dimensions}
+          />,
+        );
+
+        const instance = element.instance() as WithDataURI;
+        instance.updateDataURI({ dataURIService, metadata });
+
+        expect(dataURIService.fetchImageDataUri).toBeCalledWith(
+          expect.anything(),
+          expect.objectContaining({
+            width: 50,
+            height: 100,
           }),
         );
       });
