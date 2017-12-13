265aa4ef3dc065609b47966826bb71c91af2af01
Gautham Renganathan <grenganathan@atlassian.com>
2017-12-13 14:44:42 +1100

    adding confluence page tree

---
+++ packages/fabric/confluence-page-tree/src/types.js
@@ -0,0 +1,15 @@
+// @flow
+
+export type PageData = {
+  id: string,
+  title: string,
+  childTypes: {
+    page: {
+      value: string,
+    },
+  },
+};
+export type PageAttribute = {
+  id: string,
+  history: any,
+};
