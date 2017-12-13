2adca2850ff7db77545bb633dd0374d78bcb511e
bswierczynski <bswierczynski@atlassian.com>
2017-12-13 14:36:10 +1100

    Remove ConfluencePageTree
    It will land in a separate repo.

--- packages/fabric/confluence-page-tree/examples/basic.js
+++
@@ -1,9 +0,0 @@
-// @flow
-import React from 'react';
-
-import ConfluencePageTree from '../src';
-
-export default () => {
-  //TODO: remove hard coded data
-  return <ConfluencePageTree contentId={''} cloudId={''} />;
-};
