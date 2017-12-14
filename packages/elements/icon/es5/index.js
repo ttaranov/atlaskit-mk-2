321e935893d7552e097ded3d958a6fdde9e97693
Ben Conolly <bconolly@atlassian.com>
2017-12-14 14:14:32 +1100

    ad compiled es5 code

---
+++ packages/elements/icon/es5/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.size = undefined;
+
+var _Icon = require('./components/Icon');
+
+var _Icon2 = _interopRequireDefault(_Icon);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = _Icon2.default;
+exports.size = _Icon.size;
