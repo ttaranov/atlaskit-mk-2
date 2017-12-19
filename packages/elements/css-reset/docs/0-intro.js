5cec857cb61a1c07925cc946df8697d8125b911b
Ajay Narain Mathur <amathur@atlassian.com>
2017-12-20 10:35:56 +1100

    removed @name@ and @version@ from docs and added script to compile less files to dist

--- packages/elements/css-reset/docs/0-intro.js
+++ packages/elements/css-reset/docs/0-intro.js
@@ -7,26 +7,22 @@ export default md`
 
   This package exports a CSS file which provides a minimal reset along with base styles for many HTML elements. It is meant to be used as a basis for all styling to be built upon.
 
-  ## Try it out
-
-  Interact with a [live demo of the @NAME@ component](https://aui-cdn.atlassian.com/atlaskit/stories/@NAME@/@VERSION@/).
-
   ## Installation
 
   \`\`\`
-  npm install @NAME@
+  npm install @atlaskit/css-reset
   \`\`\`
 
   ## Using the component
 
   ### Importing
 
-  The \`@NAME@\` package can be consumed via the dist, or in Webpack.
+  The @atlaskit/css-reset package can be consumed via the dist, or in Webpack.
 
   #### Importing in Webpack
 
   \`\`\`
-  import '@NAME@';
+  import '@atlaskit/css-reset';
   \`\`\`
 
   The Webpack style loader should then place the CSS within the HEAD of your HTML element.
@@ -36,7 +32,7 @@ export default md`
   \`\`\`
     <html>
       <head>
-        <link rel="stylesheet" href="node_modules/@NAME@/dist/bundle.css" />
+        <link rel="stylesheet" href="node_modules/@atlaskit/css-reset/dist/bundle.css" />
       </head>
       <body>
         <!-- ... -->
