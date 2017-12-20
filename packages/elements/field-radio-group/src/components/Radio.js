f1af546f851b170429090bf9205b17c046793a49
Mike Gardiner <mgardiner@atlassian.com>
2017-12-20 14:25:44 +1100

    Migrate FieldRadioGroup initial import from old repo

---
+++ packages/elements/field-radio-group/src/components/Radio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ThemeProvider, withTheme } from 'styled-components';
+import Radio from './RadioBase';
+
+const RadioWithTheme = withTheme(Radio);
+
+const emptyTheme = {};
+
+export default function(props) {
+  return (
+    <ThemeProvider theme={emptyTheme}>
+      <RadioWithTheme {...props} />
+    </ThemeProvider>
+  );
+}
