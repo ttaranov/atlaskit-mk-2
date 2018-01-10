59c759edca63e7bb77ecace95604ea704174ecd9
David Richard <drichard@atlassian.com>
2018-01-09 16:38:28 -0800

    XSU-15: Improve docs slightly.

--- packages/fabric/global-search/docs/0-intro.tsx
+++ packages/fabric/global-search/docs/0-intro.tsx
@@ -1,17 +1,22 @@
-import { md } from '@atlaskit/docs';
-
-/* eslint-disable prettier */
+import React from 'react';
+import { md, code } from '@atlaskit/docs';
 
 export default md`
-  A cross-product search component. You should really use it. It's gonna be good.
+  A search component that connects to the Atlassian cross-product search backend.
 
-  ~~~~
-            __     __
-           /  \~~~/  \
-     ,----(     ..    )
-    /      \__     __/
-   /|         (\  |(
-  ^ \   /___\  /\ |
-     |__|   |__|-"
-  ~~~~
+  ## Using the component
+
+  Primary use case for the component is to be displayed in Navigation. Put in the drawers prop of the Navigation component as follows:
+
+  ${code`
+  import { GlobalQuickSearch } from '@atlaskit/global-search';
+
+  <Navigation
+    drawers={[
+      <AkSearchDrawer ...props>
+        <GlobalQuickSearch cloudId="{cloudId} environment="{environment}" />
+      </AkSearchDrawer>,
+    ]}
+  </Navigation>
+  `}
 `;
