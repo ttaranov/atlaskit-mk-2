b022f2214c973a25e68b0c1df7dda4f26fce5750
Charles Lee <clee@atlassian.com>
2018-01-11 09:37:08 +1100

    fix withRenderTarget HoC from using erroneous Prop typing

--- packages/elements/layer-manager/src/components/withRenderTarget.js
+++ packages/elements/layer-manager/src/components/withRenderTarget.js
@@ -1,5 +1,5 @@
 // @flow
-import React, { Component, type ComponentType } from 'react';
+import React, { Component, type ComponentType, type ElementRef } from 'react';
 import PropTypes from 'prop-types';
 
 import { Gateway, GatewayRegistry } from './gateway';
@@ -13,7 +13,7 @@ type Props = {
 
 export default function withRenderTarget(
   { target, withTransitionGroup }: Props,
-  WrappedComponent: ComponentType<*>,
+  WrappedComponent: ComponentType<{}>,
 ) {
   // Access the analytics context types so we can provide them across portal boundaries
   // until we can support React 16 where it can be provided natively
@@ -25,13 +25,13 @@ export default function withRenderTarget(
   const ContextProvider = withContextFromProps(analyticsContextTypes, null);
 
   // eslint-disable-next-line react/prefer-stateless-function
-  return class extends Component<Props> {
-    gatewayOrPortalChildRef: HTMLElement | null;
+  return class extends Component<{}> {
+    gatewayOrPortalChildRef: ElementRef<any> | null;
     static contextTypes = {
       gatewayRegistry: PropTypes.instanceOf(GatewayRegistry),
       ...analyticsContextTypes,
     };
-    getWrappedComponentRef = ref => {
+    getWrappedComponentRef = (ref: ElementRef<any> | null): mixed => {
       this.gatewayOrPortalChildRef = ref;
     };
     render() {
