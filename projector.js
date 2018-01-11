8dce4c6d079e5ea62fd4155849e64efab9c27008
Luke Batchelor <lbatchelor@atlassian.com>
2018-01-11 15:41:49 +1100

    Fixes browserstack to now run in master builds again

--- projector.js
+++ projector.js
@@ -1,13 +1,8 @@
 // @flow
 
-const chalk = require('chalk');
 const release = require('./build/releases/release');
 const karma = require('projector-karma');
-const {
-  getKarmaConfig,
-  getPackagesWithKarmaTests,
-} = require('./build/karma-config');
-const { getChangedPackagesSinceMaster } = require('./build/utils/packages');
+const { getKarmaConfig } = require('./build/karma-config');
 
 exports.release = async () => {
   await release.run({ cwd: __dirname });
@@ -31,21 +26,5 @@ exports.testBrowser = async (
 exports.testBrowserCI = async (
   { watch, browserstack } /*: { watch: boolean, browserstack: boolean }*/,
 ) => {
-  const changedPackages = await getChangedPackagesSinceMaster();
-  const changedPackagesNames = changedPackages.map(pkg => pkg.name);
-  const packagesWithKarmaTests = await getPackagesWithKarmaTests();
-  const changedPackagesWithKarmaTests = packagesWithKarmaTests.filter(
-    pkg => changedPackagesNames.indexOf(pkg) !== -1,
-  );
-
-  if (!changedPackagesWithKarmaTests.length) {
-    // eslint-disable-next-line
-    console.log(
-      chalk.blue(
-        'Skip karma – none of the changed packages has karma tests...',
-      ),
-    );
-  } else {
-    await runKarma({ watch, browserstack });
-  }
+  await runKarma({ watch, browserstack });
 };
