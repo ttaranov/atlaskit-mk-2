344acfe494d213c26d5da4dc5356c6422b5cfad4
Michael James <mjames@atlassian.com>
2018-01-12 17:45:55 +1100

    fix(media-picker): Fix /dist generation
    * Moved /popup folder into /src and flattened it by removing its /src
    * tslint:disable:no-var-requires for importing of svgs and png

---
+++ packages/fabric/media-picker/src/popup/components/views/editor/editorView/toolbar/buttons/lineWidthButton.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { Component } from 'react';
+
+import { GenericButton } from './genericButton';
+import { OptionsIcon } from './optionsIcon';
+import { LineWidthIcon } from './styles';
+
+// The icon is inlined because we need to change its color
+// tslint:disable:no-var-requires
+const svg = require('!raw-loader!./icons/lineWidth.svg');
+
+export interface LineWidthButtonProps {
+  readonly isActive: boolean;
+  readonly onClick: () => void;
+}
+
+export class LineWidthButton extends Component<LineWidthButtonProps> {
+  render() {
+    const { isActive, onClick } = this.props;
+
+    return (
+      <GenericButton isActive={isActive} onClick={onClick}>
+        <LineWidthIcon src={svg} />
+        <OptionsIcon isActive={isActive} />
+      </GenericButton>
+    );
+  }
+}
---
+++ packages/fabric/media-picker/src/popup/components/views/editor/errorView/errorView.tsx
@@ -0,0 +1,88 @@
+import * as React from 'react';
+import { Component } from 'react';
+// import Button from '@atlaskit/button';
+
+import { EscHelper } from '../escHelper';
+import {
+  errorHintRetry,
+  errorHintCritical,
+  errorButtonRetry,
+  errorButtonCancel,
+  errorButtonClose,
+} from '../phrases';
+import { CenterView } from '../styles';
+import {
+  ErrorPopup,
+  ErrorIcon,
+  ErrorMessage,
+  ErrorHint,
+  ErrorButton,
+} from './styles';
+
+// The error icon is inlined because the error message can be shown if there is no network connection.
+// If we had this svg in a separate file, it wouldn't be loaded and the user would see a "no image" box.
+// tslint:disable:no-var-requires
+const errorIcon = require('!raw-loader!../icons/error.svg');
+
+export interface ErrorViewProps {
+  readonly message: string;
+  readonly onCancel: () => void;
+  readonly onRetry?: () => void;
+}
+
+export class ErrorView extends Component<ErrorViewProps> {
+  private escHelper: EscHelper;
+
+  componentDidMount() {
+    this.escHelper = new EscHelper(this.props.onCancel);
+  }
+
+  componentWillUnmount() {
+    this.escHelper.teardown();
+  }
+
+  render(): JSX.Element {
+    return (
+      <CenterView>
+        <ErrorPopup>
+          <ErrorIcon src={errorIcon} />
+          <ErrorMessage>{this.props.message}</ErrorMessage>
+          <ErrorHint>{this.renderHint()}</ErrorHint>
+          {this.renderTryAgainButton()}
+          {this.renderCancelButton()}
+        </ErrorPopup>
+      </CenterView>
+    );
+  }
+
+  private renderHint(): JSX.Element {
+    const { onRetry } = this.props;
+    if (onRetry) {
+      return <span>{errorHintRetry}</span>;
+    }
+
+    return <span>{errorHintCritical}</span>;
+  }
+
+  private renderTryAgainButton(): JSX.Element | null {
+    const { onRetry } = this.props;
+    if (onRetry) {
+      return (
+        <ErrorButton appearance="primary" onClick={onRetry}>
+          {errorButtonRetry}
+        </ErrorButton>
+      );
+    }
+
+    return null;
+  }
+
+  private renderCancelButton(): JSX.Element {
+    const { onCancel, onRetry } = this.props;
+    return (
+      <ErrorButton appearance="subtle" onClick={onCancel}>
+        {onRetry ? errorButtonCancel : errorButtonClose}
+      </ErrorButton>
+    );
+  }
+}
