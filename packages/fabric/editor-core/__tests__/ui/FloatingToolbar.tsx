4cc0d4204086212eea1fd8582df5e9d7e5e84744
David Richard <drichard@atlassian.com>
2018-01-11 17:02:46 -0800

    ED-3482: Fixes height issue for hyperlink toolbar.

--- packages/fabric/editor-core/__tests__/ui/FloatingToolbar.tsx
+++ packages/fabric/editor-core/__tests__/ui/FloatingToolbar.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { mount } from 'enzyme';
 import { Popup } from '@atlaskit/editor-common';
-import FloatingToolbar, { DEFAULT_HEIGHT } from '../../src/ui/FloatingToolbar';
+import FloatingToolbar from '../../src/ui/FloatingToolbar';
 import { Container } from '../../src/ui/FloatingToolbar/styles';
 
 describe('FloatingToolbar', () => {
@@ -27,12 +27,7 @@ describe('FloatingToolbar', () => {
     expect(wrapper.find(Popup).props().fitHeight).toBe(DEFAULT_HEIGHT);
   });
 
-  it('defaults container height', () => {
-    const wrapper = mount(<FloatingToolbar target={target} />);
-    expect(wrapper.find(Container).props().height).toBe(DEFAULT_HEIGHT);
-  });
-
-  it('passes height to container', () => {
+  it('passes height to popup', () => {
     const wrapper = mount(<FloatingToolbar target={target} fitHeight={30} />);
     expect(wrapper.find(Popup).props().fitHeight).toBe(30);
   });
