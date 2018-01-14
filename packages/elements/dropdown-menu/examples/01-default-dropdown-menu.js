9b19048852110500e9f961219a4fce61b48251ce
Ajay Narain Mathur <amathur@atlassian.com>
2018-01-15 10:52:42 +1100

    updated examples to be in container with padding to stay visible

--- packages/elements/dropdown-menu/examples/01-default-dropdown-menu.js
+++ packages/elements/dropdown-menu/examples/01-default-dropdown-menu.js
@@ -4,16 +4,18 @@ import React from 'react';
 import DropdownMenu, { DropdownItemGroup, DropdownItem } from '../src';
 
 export default () => (
-  <DropdownMenu
-    trigger="Choices"
-    triggerType="button"
-    shouldFlip={false}
-    position="right middle"
-    onOpenChange={e => console.log('dropdown opened', e)}
-  >
-    <DropdownItemGroup>
-      <DropdownItem>Sydney</DropdownItem>
-      <DropdownItem>Melbourne</DropdownItem>
-    </DropdownItemGroup>
-  </DropdownMenu>
+  <div style={{ margin: '20px' }}>
+    <DropdownMenu
+      trigger="Choices"
+      triggerType="button"
+      shouldFlip={false}
+      position="right middle"
+      onOpenChange={e => console.log('dropdown opened', e)}
+    >
+      <DropdownItemGroup>
+        <DropdownItem>Sydney</DropdownItem>
+        <DropdownItem>Melbourne</DropdownItem>
+      </DropdownItemGroup>
+    </DropdownMenu>
+  </div>
 );
