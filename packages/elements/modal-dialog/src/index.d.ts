3c8b4b1f930631f0d6345734c5e140d8748d27b4
James Kyle <me@thejameskyle.com>
2018-01-15 14:03:26 +1100

    Add missing types to modal-dialog definition

--- packages/elements/modal-dialog/src/index.d.ts
+++ packages/elements/modal-dialog/src/index.d.ts
@@ -8,10 +8,17 @@ interface Props {
   header?: ReactNode;
   children?: ReactNode;
   footer?: ReactNode;
+  height?: any;
   width?: string | number | 'small' | 'medium' | 'large' | 'x-large';
   onDialogDismissed?: (event: KeyboardOrMouseEvent) => void;
+  onClose?: any;
+  isChromeless?: any;
 }
 
 interface State {}
 
 export default class ModalDialog extends Component<Props, State> {}
+
+export class ModalHeader extends Component<any, any> {}
+export class ModalFooter extends Component<any, any> {}
+export class ModalTitle extends Component<any, any> {}
