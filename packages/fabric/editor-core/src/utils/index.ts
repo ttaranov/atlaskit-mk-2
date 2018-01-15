330b8d2d7b8fa2559cc40f907633499a73916cb2
Stanislav Sysoev <ssysoev@atlassian.com>
2018-01-16 09:13:40 +1100

    CHANGESET: Fix cursor position after mention and emoji
    Summary: Fix cursor position after mention and emoji
    
    Release notes: <none>
    
    Releases: @atlaskit/editor-core@patch
    
    Dependents: @atlaskit/comment@none, @atlaskit/conversation@none, @atlaskit/editor-bitbucket-transformer@none, @atlaskit/editor-confluence-transformer@none, @atlaskit/editor-jira-transformer@none, @atlaskit/editor-json-transformer@none, @atlaskit/editor-test-helpers@none, @atlaskit/renderer@none, @atlaskit/editor-core@none, @atlaskit/editor-common@none
    
    ---
    {"summary":"Fix cursor position after mention and emoji","releases":[{"name":"@atlaskit/editor-core","type":"patch"}],"dependents":[{"name":"@atlaskit/comment","dependencies":["@atlaskit/editor-core"],"type":"none"},{"name":"@atlaskit/conversation","dependencies":["@atlaskit/editor-core","@atlaskit/renderer","@atlaskit/comment"],"type":"none"},{"name":"@atlaskit/editor-bitbucket-transformer","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-common"],"type":"none"},{"name":"@atlaskit/editor-confluence-transformer","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-common"],"type":"none"},{"name":"@atlaskit/editor-jira-transformer","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-common"],"type":"none"},{"name":"@atlaskit/editor-json-transformer","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-common"],"type":"none"},{"name":"@atlaskit/editor-test-helpers","dependencies":["@atlaskit/editor-core","@atlaskit/editor-common"],"type":"none"},{"name":"@atlaskit/renderer","dependencies":["@atlaskit/editor-core","@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-json-transformer"],"type":"none"},{"name":"@atlaskit/editor-core","dependencies":["@atlaskit/renderer","@atlaskit/editor-test-helpers","@atlaskit/editor-common","@atlaskit/editor-json-transformer","@atlaskit/editor-bitbucket-transformer"],"type":"none"},{"name":"@atlaskit/editor-common","dependencies":["@atlaskit/editor-test-helpers"],"type":"none"}]}
    ---

--- packages/fabric/editor-core/src/utils/index.ts
+++ packages/fabric/editor-core/src/utils/index.ts
@@ -549,10 +549,11 @@ export const isTemporary = (id: string): boolean => {
   return id.indexOf('temporary:') === 0;
 };
 
-// See: https://github.com/ProseMirror/prosemirror/issues/710
-export const isChromeWithSelectionBug = !!navigator.userAgent.match(
-  /Chrome\/(5[89]|6[012])/,
-);
+// @see: https://github.com/ProseMirror/prosemirror/issues/710
+// @see: https://bugs.chromium.org/p/chromium/issues/detail?id=740085
+// Chrome > 58
+export const isChromeWithSelectionBug =
+  parseInt((navigator.userAgent.match(/Chrome\/(\d{2})/) || [])[1], 10) > 58;
 
 export const isEmptyNode = (schema: Schema) => {
   const {
