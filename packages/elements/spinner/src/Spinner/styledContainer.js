edc2d79efecf3e4943ee6b3f2dd5d994b1b0c95c
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-01-18 11:17:34 +1100

    Fix spinner performance by statically defining keyframes
    Works around a bug in styled-components v1 where keyframe definitions
    are not cached, resulting in identical keyframes being added to the DOM.

--- packages/elements/spinner/src/Spinner/styledContainer.js
+++ packages/elements/spinner/src/Spinner/styledContainer.js
@@ -1,6 +1,6 @@
 // @flow
 
-import styled, { keyframes } from 'styled-components';
+import styled, { injectGlobal } from 'styled-components';
 import type { SpinnerPhases } from '../types';
 
 type AnimationParams = {
@@ -8,32 +8,55 @@ type AnimationParams = {
   phase: SpinnerPhases,
 };
 
+export const keyframeNames = {
+  noop: 'atlaskitSpinnerNoop',
+  enter_rotate: 'atlaskitSpinnerEnterRotate',
+  leave_rotate: 'atlaskitSpinnerLeaveRotate',
+  leave_opacity: 'atlaskitSpinnerLeaveOpacity',
+};
+
+/* Define keyframes statically to prevent a perfomance issue in styled components v1 where the keyframes function
+ * does not cache previous values resulting in each spinner injecting the same keyframe definition
+ * in the DOM.
+ * This can be reverted to use the keyframes fn when we upgrade to styled components v2
+ */
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+  @keyframes ${keyframeNames.noop} {
+    from { opacity: 0; }
+    to { opacity: 0; }
+  }
+
+  @keyframes ${keyframeNames.enter_rotate} {
+    from { transform: rotate(50deg); }
+    to { transform: rotate(230deg); }
+  }
+
+  @keyframes ${keyframeNames.leave_rotate} {
+    from { transform: rotate(230deg); }
+    to { transform: rotate(510deg); }
+  }
+
+  @keyframes ${keyframeNames.leave_opacity} {
+    from { opacity: 1; }
+    to { opacity: 0; }
+  }
+`;
+
 export const getContainerAnimation = ({ delay, phase }: AnimationParams) => {
   if (phase === 'DELAY') {
     /* This hides the spinner and allows us to use animationend events to move to the next phase in
      * the same way we do with the other lifecycle stages */
-    return `animation: ${delay}s ${keyframes`
-        from { opacity: 0; }
-        to { opacity: 0; }
-      `};`;
+    return `animation: ${delay}s ${keyframeNames.noop};`;
   }
 
   if (phase === 'ENTER' || phase === 'IDLE') {
-    return `animation: 1s ease-in-out forwards ${keyframes`
-        from { transform: rotate(50deg); }
-        to { transform: rotate(230deg); }
-      `};`;
+    return `animation: 1s ease-in-out forwards ${keyframeNames.enter_rotate};`;
   }
 
   if (phase === 'LEAVE') {
-    return `animation: 0.53s ease-in-out forwards ${keyframes`
-        from { transform: rotate(230deg); }
-        to { transform: rotate(510deg); }
-      `},
-      0.2s ease-in-out 0.33s ${keyframes`
-        from { opacity: 1; }
-        to { opacity: 0; }
-      `};`;
+    return `animation: 0.53s ease-in-out forwards ${keyframeNames.leave_rotate},
+      0.2s ease-in-out 0.33s ${keyframeNames.leave_opacity};`;
   }
 
   return '';
--- packages/elements/spinner/src/Spinner/styledSvg.js
+++ packages/elements/spinner/src/Spinner/styledSvg.js
@@ -1,13 +1,90 @@
 // @flow
 
-import styled, { css, keyframes } from 'styled-components';
+import styled, { css, injectGlobal, keyframes } from 'styled-components';
 import { colors, themed } from '@atlaskit/theme';
 import type { SpinnerPhases } from '../types';
+import { SIZES_MAP } from './constants';
 
 type StyleParams = {
   invertColor: boolean,
   phase: SpinnerPhases,
   size: number,
+};
+
+const getStrokeWidth = (size: number) => Math.round(size / 10);
+
+const getStrokeCircumference = (size: number) => {
+  const strokeWidth = getStrokeWidth(size);
+  const strokeRadius = size / 2 - strokeWidth / 2;
+  return Math.PI * strokeRadius * 2;
+};
+
+export const keyframeNames = {
+  noop: 'atlaskitSpinnerNoop',
+  rotate: 'atlaskitSpinnerRotate',
+  enter_stroke_small: 'atlaskitSpinnerEnterStrokeSmall',
+  enter_stroke_medium: 'atlaskitSpinnerEnterStrokeMedium',
+  enter_stroke_large: 'atlaskitSpinnerEnterStrokeLarge',
+  enter_stroke_xlarge: 'atlaskitSpinnerEnterStrokeXLarge',
+};
+
+/* Define keyframes statically to prevent a perfomance issue in styled components v1 where the keyframes function
+ * does not cache previous values resulting in each spinner injecting the same keyframe definition
+ * in the DOM.
+ * This can be reverted to use the keyframes fn when we upgrade to styled components v2
+ */
+// eslint-disable-next-line no-unused-expressions
+injectGlobal`
+  @keyframes ${keyframeNames.noop} {
+    from { opacity: 0; }
+    to { opacity: 0; }
+  }
+
+  @keyframes ${keyframeNames.rotate} {
+    to { transform: rotate(360deg);
+  }
+
+  @keyframes ${keyframeNames.enter_stroke_small} {
+    from { stroke-dashoffset: ${getStrokeCircumference(SIZES_MAP.small)}px; }
+    to { stroke-dashoffset: ${getStrokeCircumference(SIZES_MAP.small) *
+      0.8}px; }
+  }
+
+  @keyframes ${keyframeNames.enter_stroke_medium} {
+    from { stroke-dashoffset: ${getStrokeCircumference(SIZES_MAP.medium)}px; }
+    to { stroke-dashoffset: ${getStrokeCircumference(SIZES_MAP.medium) *
+      0.8}px; }
+  }
+
+  @keyframes ${keyframeNames.enter_stroke_large} {
+    from { stroke-dashoffset: ${getStrokeCircumference(SIZES_MAP.large)}px; }
+    to { stroke-dashoffset: ${getStrokeCircumference(SIZES_MAP.large) *
+      0.8}px; }
+  }
+
+  @keyframes ${keyframeNames.enter_stroke_xlarge} {
+    from { stroke-dashoffset: ${getStrokeCircumference(SIZES_MAP.xlarge)}px; }
+    to { stroke-dashoffset: ${getStrokeCircumference(SIZES_MAP.xlarge) *
+      0.8}px; }
+  }
+`;
+
+/* If a standard size is used, we can use one of our statically defined keyframes, otherwise
+ * we're forced to dynamically create the keyframe and incur a performance cost.
+ */
+const getEnterStrokeKeyframe = (size: number) => {
+  const standardSizeName = Object.keys(SIZES_MAP).find(
+    sizeName => size === SIZES_MAP[sizeName],
+  );
+  if (standardSizeName) {
+    return keyframeNames[`enter_stroke_${standardSizeName}`];
+  }
+
+  const circumference = getStrokeCircumference(size);
+  return keyframes`
+    from { stroke-dashoffset: ${circumference}px; }
+    to { stroke-dashoffset: ${circumference * 0.8}px; }
+  `;
 };
 
 const spinnerColor = themed({ light: colors.N500, dark: colors.N0 });
@@ -23,23 +100,17 @@ export const getStrokeColor = ({
 
 export const svgStyles = css`
   ${(props: StyleParams) => {
-    const strokeWidth = Math.round(props.size / 10);
-    const strokeRadius = props.size / 2 - strokeWidth / 2;
-    const circumference = Math.PI * strokeRadius * 2;
+    const circumference = getStrokeCircumference(props.size);
 
-    const idleRotation = `0.86s cubic-bezier(0.4, 0.15, 0.6, 0.85) infinite ${keyframes`
-      to { transform: rotate(360deg); }
-      `}`;
+    const idleRotation = `0.86s cubic-bezier(0.4, 0.15, 0.6, 0.85) infinite ${
+      keyframeNames.rotate
+    }`;
 
-    const spinUpStroke = `0.8s ease-in-out ${keyframes`
-      from { stroke-dashoffset: ${circumference}px; }
-      to { stroke-dashoffset: ${circumference * 0.8}px; }
-      `}`;
+    const spinUpStroke = `0.8s ease-in-out ${getEnterStrokeKeyframe(
+      props.size,
+    )}`;
 
-    const spinUpOpacity = `0.2s ease-in-out ${keyframes`
-      from { opacity: 0; }
-      to { opacity: 1; }
-      `}`;
+    const spinUpOpacity = `0.2s ease-in-out akSpinnerContainerLeaveOpacity`;
 
     const activeAnimations = [idleRotation];
     if (props.phase === 'ENTER') {
@@ -53,7 +124,7 @@ export const svgStyles = css`
       stroke-dasharray: ${circumference}px;
       stroke-dashoffset: ${circumference * 0.8}px;
       stroke-linecap: round;
-      stroke-width: ${strokeWidth}px;
+      stroke-width: ${getStrokeWidth(props.size)}px;
       transform-origin: center;
     `;
   }};
