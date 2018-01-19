5bf2a564f61d3346406052c54780eddd9f2e2dc6
Ajay Narain Mathur <amathur@atlassian.com>
2018-01-19 15:27:49 +1100

    added tooltip prop

--- packages/elements/tooltip/docs/0-intro.js
+++ packages/elements/tooltip/docs/0-intro.js
@@ -41,5 +41,9 @@ export default md`
   but will log a deprecation warning. Support will be removed from version
   \`8.0.0\` onwards.
 
-
+  ${(
+    <Props
+      props={require('!!extract-react-types-loader!../src/components/Tooltip')}
+    />
+  )}
 `;
