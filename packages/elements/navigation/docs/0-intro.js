8557cde9d22f19ee0aec4de6629778c9e3ef4c21
Charles Lee <clee@atlassian.com>
2018-01-19 13:42:26 +1100

    remove examples

--- packages/elements/navigation/docs/0-intro.js
+++ packages/elements/navigation/docs/0-intro.js
@@ -1,21 +1,11 @@
 // @flow
 import React from 'react';
-import { md, Example, Props } from '@atlaskit/docs';
+import { md, Props } from '@atlaskit/docs';
 
 export default md`
   Use lozenges to highlight an item's status for quick recognition. Use
   subtle lozenges by default and in instances where they may dominate the
   screen, such as in long tables.
-
-  ## Examples
-
-  ${(
-    <Example
-      Component={require('../examples/0-basic').default}
-      title="Basic"
-      source={require('!!raw-loader!../examples/0-basic')}
-    />
-  )}
 
   ${<Props props={require('!!extract-react-types-loader!../src/')} />}
 `;
--- packages/elements/navigation/examples/0-basic.js
+++ packages/elements/navigation/examples/0-basic.js
@@ -1,18 +1,18 @@
 // @flow
 import React, { Component } from 'react';
-import AtlassianIcon from '@atlaskit/icon/glyph/atlassian';
-import SearchIcon from '@atlaskit/icon/glyph/search';
-import ArrowLeft from '@atlaskit/icon/glyph/arrow-left';
+// import AtlassianIcon from '@atlaskit/icon/glyph/atlassian';
+// import SearchIcon from '@atlaskit/icon/glyph/search';
+// import ArrowLeft from '@atlaskit/icon/glyph/arrow-left';
+//
+// import Navigation, {
+//   AkNavigationItemGroup,
+//   AkSearchDrawer,
+//   AkNavigationItem,
+//   AkContainerTitle,
+//   AkSearch,
+// } from '../src';
 
-import Navigation, {
-  AkNavigationItemGroup,
-  AkSearchDrawer,
-  AkNavigationItem,
-  AkContainerTitle,
-  AkSearch,
-} from '../src';
-
-const items = ['cat', 'dog', 'fish', 'lizard'];
+// const items = ['cat', 'dog', 'fish', 'lizard'];
 
 type State = {
   searchDrawerOpen: boolean,
@@ -27,50 +27,53 @@ class ExampleNav extends Component<void, State> {
 
   render() {
     return (
-      <Navigation
-        globalPrimaryIcon={<AtlassianIcon size="xlarge" label="Atlassian" />}
-        globalPrimaryItemHref="/components/navigation"
-        globalSearchIcon={<SearchIcon label="search" />}
-        onSearchDrawerOpen={() =>
-          this.setState({ searchDrawerOpen: !this.state.searchDrawerOpen })
-        }
-        drawers={[
-          <AkSearchDrawer
-            backIcon={<ArrowLeft label="Back" />}
-            primaryIcon={<SearchIcon label="Search" />}
-            header="Some Header"
-            isOpen={this.state.searchDrawerOpen}
-            onBackButton={() => this.setState({ searchDrawerOpen: false })}
-          >
-            <AkSearch
-              onSearchClear={() => this.setState({ value: '' })}
-              // $FlowFixMe
-              onInput={e => this.setState({ value: e.target.value })}
-              value={this.state.value}
-            >
-              {items
-                .filter(item => item.includes(this.state.value))
-                .map(item => <AkNavigationItem text={item} />)}
-            </AkSearch>
-          </AkSearchDrawer>,
-        ]}
-        containerHeaderComponent={() => (
-          <AkContainerTitle
-            icon={<AtlassianIcon label="Atlassian" />}
-            text="NavTitle"
-          />
-        )}
-      >
-        <AkNavigationItemGroup>
-          <AkNavigationItem text="Nav Item" href="/components/navigation" />
-          <AkNavigationItem
-            text="Selected Item"
-            isSelected
-            href="/components/navigation"
-          />
-        </AkNavigationItemGroup>
-      </Navigation>
+      <span> Example temporarily removed pending website optimisations </span>
     );
+    //   return (
+    //     <Navigation
+    //       globalPrimaryIcon={<AtlassianIcon size="xlarge" label="Atlassian" />}
+    //       globalPrimaryItemHref="/components/navigation"
+    //       globalSearchIcon={<SearchIcon label="search" />}
+    //       onSearchDrawerOpen={() =>
+    //         this.setState({ searchDrawerOpen: !this.state.searchDrawerOpen })
+    //       }
+    //       drawers={[
+    //         <AkSearchDrawer
+    //           backIcon={<ArrowLeft label="Back" />}
+    //           primaryIcon={<SearchIcon label="Search" />}
+    //           header="Some Header"
+    //           isOpen={this.state.searchDrawerOpen}
+    //           onBackButton={() => this.setState({ searchDrawerOpen: false })}
+    //         >
+    //           <AkSearch
+    //             onSearchClear={() => this.setState({ value: '' })}
+    //             // $FlowFixMe
+    //             onInput={e => this.setState({ value: e.target.value })}
+    //             value={this.state.value}
+    //           >
+    //             {items
+    //               .filter(item => item.includes(this.state.value))
+    //               .map(item => <AkNavigationItem text={item} />)}
+    //           </AkSearch>
+    //         </AkSearchDrawer>,
+    //       ]}
+    //       containerHeaderComponent={() => (
+    //         <AkContainerTitle
+    //           icon={<AtlassianIcon label="Atlassian" />}
+    //           text="NavTitle"
+    //         />
+    //       )}
+    //     >
+    //       <AkNavigationItemGroup>
+    //         <AkNavigationItem text="Nav Item" href="/components/navigation" />
+    //         <AkNavigationItem
+    //           text="Selected Item"
+    //           isSelected
+    //           href="/components/navigation"
+    //         />
+    //       </AkNavigationItemGroup>
+    //     </Navigation>
+    //   );
   }
 }
 
