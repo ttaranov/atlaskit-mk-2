620e47ea82cc5cf0fb68342a2f2c4bb608285b04
Ben Conolly <bconolly@atlassian.com>
2018-01-22 15:07:46 +1100

    better type

--- packages/elements/datetime-picker/src/components/internal/Input.js
+++ packages/elements/datetime-picker/src/components/internal/Input.js
@@ -19,7 +19,7 @@ type Props = {
 };
 
 export default class InputField extends Component<Props> {
-  input: null | ElementRef<typeof Input>;
+  input: ?ElementRef<typeof Input>;
 
   static defaultProps = {
     autoFocus: false,
