cfa4f363cad2b582f4aa03eeeb0d76f643cbb48f
Raja Bellebon <rbellebon@atlassian.com>
2018-01-22 14:38:07 +1100

    Add single select test

---
+++ packages/elements/single-select/tests/integration/single-select.js
@@ -0,0 +1,25 @@
+// @flow
+// eslint-disable-next-line
+import { BrowserTestCase } from '@atlaskit/webdriver-runner/runner';
+// eslint-disable-next-line
+import Page from '@atlaskit/webdriver-runner/wd-wrapper';
+
+const urlSingleSelect = `${
+  global.__baseUrl__
+}/mk-2/packages/elements/single-select/example/basic`;
+
+const singleSelectDefault =
+  '[role="dialog"] > div > div > div > div > div > div > div > div:nth-child(2)';
+const singleSelectMenu = '[data-role="droplistContent"]';
+
+BrowserTestCase(
+  'AK-4173 - Single Select should display its menu once clicked on it',
+  async client => {
+    const singleSelectTest = await new Page(client);
+    await singleSelectTest.goto(urlSingleSelect);
+    await singleSelectTest.click(singleSelectDefault);
+    const menuIsVisible = await singleSelectTest.isVisible(singleSelectMenu);
+    // eslint-disable-next-line
+    expect(menuIsVisible).toBe(true);
+  },
+);
