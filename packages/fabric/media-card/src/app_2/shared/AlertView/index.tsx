ce2da3ea450b2914d762eb04ea25054a98717a21
James Newell <jnewell@atlassian.com>
2018-01-29 17:56:09 +1100

    trying to get ellipsis working

--- packages/fabric/media-card/src/app_2/shared/AlertView/index.tsx
+++ packages/fabric/media-card/src/app_2/shared/AlertView/index.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
+import * as debounce from 'lodash.debounce';
+import { Ellipsify } from '../../../utils/ellipsify';
 import { Wrapper } from './styled';
+
+const contentWidthWhenCardIs400px = 384;
 
 export interface AlertViewProps {
   type: 'success' | 'failure';
@@ -9,7 +13,18 @@ export interface AlertViewProps {
   style?: {};
 }
 
-export default class AlertView extends React.Component<AlertViewProps> {
+export interface AlertViewState {
+  width?: number;
+}
+
+export default class AlertView extends React.Component<
+  AlertViewProps,
+  AlertViewState
+> {
+  state: AlertViewState = {};
+
+  el: HTMLDivElement;
+
   handleTryAgain = () => {
     const { onTryAgain } = this.props;
     if (onTryAgain) {
@@ -24,25 +39,61 @@ export default class AlertView extends React.Component<AlertViewProps> {
     }
   };
 
+  handleMount = (el: HTMLDivElement) => {
+    this.el = el;
+  };
+
+  handleResize = debounce(() => {
+    if (this.el) {
+      this.setState({ width: this.el.clientWidth });
+    }
+  }, 250);
+
+  componentDidMount() {
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnMount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+
   renderContent() {
     const { type, message } = this.props;
-    if (type === 'success') {
-      return <span>{message}</span>;
+    const { width } = this.state;
+
+    const text = type === 'success' ? message : 'Something went wrong.';
+
+    console.log('width', width);
+    if (width && width < contentWidthWhenCardIs400px) {
+      return <Ellipsify text={text} lines={2} inline />;
     } else {
-      return (
-        <span>
-          Something went wrong. <a onClick={this.handleTryAgain}>Try again</a>{' '}
-          or <a onClick={this.handleCancel}>cancel</a>.
-        </span>
-      );
+      return <Ellipsify text={text} lines={1} inline />;
     }
+  }
+
+  renderRetryAndCancel() {
+    const { type } = this.props;
+
+    if (type === 'success') {
+      return null;
+    }
+
+    return (
+      <span>
+        {' '}
+        <a onClick={this.handleTryAgain}>Try again</a> or{' '}
+        <a onClick={this.handleCancel}>cancel</a>.
+      </span>
+    );
   }
 
   render() {
     const { type, style } = this.props;
     return (
-      <Wrapper type={type} style={style}>
+      <Wrapper innerRef={this.handleMount} type={type} style={style}>
         {this.renderContent()}
+        {this.renderRetryAndCancel()}
       </Wrapper>
     );
   }
