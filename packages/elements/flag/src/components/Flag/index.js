3f0ca77351fa4920d562f7701c3ffa7e3b3c2df4
Raja Bellebon <rbellebon@atlassian.com>
2018-01-31 09:03:54 +1100

    As per design it should be large

--- packages/elements/flag/src/components/Flag/index.js
+++ packages/elements/flag/src/components/Flag/index.js
@@ -87,7 +87,7 @@ export default class Flag extends Component<FlagProps, State> {
         onClick={buttonAction}
         type="button"
       >
-        <ButtonIcon label={buttonLabel} />
+        <ButtonIcon label={buttonLabel} size="large" />
       </DismissButton>
     );
   };
