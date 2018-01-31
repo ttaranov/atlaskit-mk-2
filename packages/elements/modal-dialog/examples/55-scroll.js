af51ffbc2f8480bec7d3c346060f566d9970eb54
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-01-31 16:19:38 +1100

    Repurpose scroll example to demonstrate scroll behaviors as well

--- packages/elements/modal-dialog/examples/55-scroll.js
+++ packages/elements/modal-dialog/examples/55-scroll.js
@@ -3,28 +3,45 @@ import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 import Lorem from 'react-lorem-component';
 import Button from '@atlaskit/button';
+import FieldRadioGroup from '@atlaskit/field-radio-group';
 import Modal from '../src';
 
 const TallContainer = styled.div`
   height: 2000px;
 `;
 
-const Paragraph = styled.p`
-  padding-bottom: 200px;
-`;
+const scrollBehaviors = [
+  {
+    name: 'scrollBehavior',
+    value: 'inside',
+    label: 'inside',
+    defaultSelected: true,
+  },
+  { name: 'scrollBehavior', value: 'outside', label: 'outside' },
+];
 
 type State = {
   isOpen: boolean,
+  scrollBehavior: 'inside' | 'outside',
 };
 export default class ExampleScroll extends PureComponent<{}, State> {
   bottomRef: any;
-  state: State = { isOpen: false };
+  state: State = {
+    isOpen: false,
+    scrollBehavior: 'inside',
+  };
   open = () => this.setState({ isOpen: true });
   close = () => this.setState({ isOpen: false });
   scrollToBottom = () => this.bottomRef.scrollIntoView(true);
 
+  onScrollBehaviorChange = (e: any) => {
+    this.setState({
+      scrollBehavior: e.target.value,
+    });
+  };
+
   render() {
-    const { isOpen } = this.state;
+    const { isOpen, scrollBehavior } = this.state;
     const actions = [
       { text: 'Close', onClick: this.close },
       { text: 'Scroll to bottom', onClick: this.scrollToBottom },
@@ -32,16 +49,30 @@ export default class ExampleScroll extends PureComponent<{}, State> {
 
     return (
       <TallContainer>
-        <Paragraph>
-          Modals prevent the window from being scrolled both natively and
-          programatically. This means that browser quirks such as{' '}
-          <code>scrollIntoView</code> scrolling the window instead of only the
-          closest scroll parent will be prevented.
-        </Paragraph>
+        <p>
+          The scroll behavior of modals can be configured so that scrolling
+          happens inside the modal body or outside the modal, within the
+          viewport.
+        </p>
+        <p>
+          In either case, modals prevent the window from being scrolled both
+          natively and programatically. This means that certain browser issues
+          such as <code>scrollIntoView</code> scrolling the window instead of
+          only the closest scroll parent will be prevented.
+        </p>
+        <FieldRadioGroup
+          items={scrollBehaviors}
+          label="Scroll behavior:"
+          onRadioChange={this.onScrollBehaviorChange}
+        />
         <Button onClick={this.open}>Open Modal</Button>
-
         {isOpen && (
-          <Modal actions={actions} onClose={this.close} heading="Modal Title">
+          <Modal
+            actions={actions}
+            onClose={this.close}
+            heading="Modal Title"
+            scrollBehavior={scrollBehavior}
+          >
             <Lorem count={10} />
             <div
               ref={r => {
