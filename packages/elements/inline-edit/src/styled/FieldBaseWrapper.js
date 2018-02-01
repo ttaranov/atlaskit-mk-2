492b79883ebe8d9ff730ab3c507548b65ee57e1e
Mike Gardiner <mgardiner@atlassian.com>
2018-02-01 14:53:29 +1100

    Removed setting of display:flex in FieldBaseWrapper Changed stateless example to be able to include using FieldText & shouldFitContainer to test full width

--- packages/elements/inline-edit/src/styled/FieldBaseWrapper.js
+++ packages/elements/inline-edit/src/styled/FieldBaseWrapper.js
@@ -3,9 +3,16 @@ import styled from 'styled-components';
 
 // Display toggle needed to prevent edit click trigger exceeding hover width.
 // Max-width required to prevent text overflow when display is inline.
-const FieldBaseWrapper = styled.div`
+/*const FieldBaseWrapper = styled.div`
   display: ${({ displayFullWidth }) =>
     displayFullWidth ? 'flex' : 'inline-block'};
+  max-width: 100%;
+  flex-basis: auto;
+  flex-grow: 1;
+  flex-shrink: 1;
+`;*/
+// Removing setting the display here fixes an issue with InlineEdit
+const FieldBaseWrapper = styled.div`
   max-width: 100%;
   flex-basis: auto;
   flex-grow: 1;
