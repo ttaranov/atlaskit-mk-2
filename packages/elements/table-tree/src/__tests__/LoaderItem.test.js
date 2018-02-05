0a61bcc3ca57bfafdbcfdf3b16b95047fbb836e0
bswierczynski <bswierczynski@atlassian.com>
2018-02-05 12:33:11 +1100

    [WIP] add alternative unit tests to compare whiteboxy vs blackboxy solutions

--- packages/elements/table-tree/src/__tests__/LoaderItem.test.js
+++ packages/elements/table-tree/src/__tests__/LoaderItem.test.js
@@ -6,6 +6,8 @@ import LoaderItem from '../components/LoaderItem';
 import { Cell as StyledCell } from '../styled';
 
 describe('TableTree', () => {
+  const DELAY_BEFORE_ANIMATION_SHOWN = 500;
+
   beforeAll(() => {
     jest.useFakeTimers();
   });
@@ -14,7 +16,110 @@ describe('TableTree', () => {
     jest.useRealTimers();
   });
 
-  describe('LoaderItem', () => {
+  describe('LoaderItem (blackbox)', () => {
+    it("doesn't render anything initially", () => {
+      const wrapper = shallow(
+        <LoaderItem
+          isCompleting={false}
+          onComplete={() => {}}
+          delay={DELAY_BEFORE_ANIMATION_SHOWN}
+        />,
+      );
+      expect(wrapper.type()).toBe(null);
+
+      jest.runTimersToTime(DELAY_BEFORE_ANIMATION_SHOWN / 2);
+      wrapper.update();
+
+      expect(wrapper.type()).toBe(null);
+    });
+
+    it('renders the loader after a delay', () => {
+      const wrapper = mount(
+        <LoaderItem
+          delay={DELAY_BEFORE_ANIMATION_SHOWN}
+          isCompleting={false}
+          onComplete={() => {}}
+        />,
+      );
+
+      jest.runTimersToTime(DELAY_BEFORE_ANIMATION_SHOWN);
+      wrapper.update();
+
+      expect(wrapper.type()).not.toBe(null);
+      const cell = wrapper.find(StyledCell);
+      expect(cell).toHaveLength(1);
+      expect(cell.props()).toHaveProperty('width', '100%');
+      const spinner = cell.find(Spinner);
+      expect(spinner).toHaveLength(1);
+      expect(spinner.props()).toHaveProperty('isCompleting', false);
+    });
+
+    it('stops rendering the loader after its animation completes', () => {
+      const onCompleteSpy = jest.fn();
+      const wrapper = mount(
+        <LoaderItem
+          delay={DELAY_BEFORE_ANIMATION_SHOWN}
+          isCompleting={false}
+          onComplete={onCompleteSpy}
+        />,
+      );
+      jest.runTimersToTime(DELAY_BEFORE_ANIMATION_SHOWN);
+      wrapper.update();
+
+      expect(wrapper.children()).toHaveLength(1);
+
+      const spinner = wrapper.find(Spinner);
+      wrapper.setProps({ isCompleting: true });
+      wrapper.update();
+      spinner.prop('onComplete')();
+      wrapper.update();
+
+      expect(onCompleteSpy).toHaveBeenCalled();
+      expect(wrapper.children()).toHaveLength(0);
+    });
+
+    it("doesn't render the loader at all if completed immediately", () => {
+      const onCompleteSpy = jest.fn();
+      const wrapper = mount(
+        <LoaderItem
+          delay={DELAY_BEFORE_ANIMATION_SHOWN}
+          isCompleting
+          onComplete={onCompleteSpy}
+        />,
+      );
+
+      jest.runTimersToTime(DELAY_BEFORE_ANIMATION_SHOWN);
+      wrapper.update();
+
+      expect(wrapper.children()).toHaveLength(0);
+      expect(onCompleteSpy).toHaveBeenCalled();
+    });
+
+    it("doesn't render the loader at all if completed quickly", () => {
+      const onCompleteSpy = jest.fn();
+      const wrapper = mount(
+        <LoaderItem
+          delay={DELAY_BEFORE_ANIMATION_SHOWN}
+          isCompleting={false}
+          onComplete={onCompleteSpy}
+        />,
+      );
+
+      jest.runTimersToTime(DELAY_BEFORE_ANIMATION_SHOWN / 2);
+      wrapper.setProps({ isCompleting: true });
+      wrapper.update();
+
+      expect(onCompleteSpy).toHaveBeenCalled();
+      expect(wrapper.children()).toHaveLength(0);
+
+      jest.runTimersToTime(DELAY_BEFORE_ANIMATION_SHOWN);
+      wrapper.update();
+
+      expect(wrapper.children()).toHaveLength(0);
+    });
+  });
+
+  describe('LoaderItem (whitebox)', () => {
     it('starts in the delaying phase', () => {
       const wrapper = shallow(
         <LoaderItem isCompleting={false} onComplete={() => {}} />,
@@ -26,8 +131,7 @@ describe('TableTree', () => {
       const wrapper = mount(
         <LoaderItem isCompleting={false} onComplete={() => {}} />,
       );
-      const instance = wrapper.instance();
-      instance.setState({ phase: 'loading' });
+      wrapper.setState({ phase: 'loading' });
       wrapper.update();
 
       const cell = wrapper.find(StyledCell);
@@ -42,8 +146,7 @@ describe('TableTree', () => {
       const wrapper = mount(
         <LoaderItem isCompleting={false} onComplete={() => {}} />,
       );
-      const instance = wrapper.instance();
-      instance.setState({ phase: 'delaying' });
+      wrapper.setState({ phase: 'delaying' });
       wrapper.update();
 
       expect(wrapper.find(StyledCell)).toHaveLength(0);
@@ -54,8 +157,7 @@ describe('TableTree', () => {
       const wrapper = mount(
         <LoaderItem isCompleting={false} onComplete={() => {}} />,
       );
-      const instance = wrapper.instance();
-      instance.setState({ phase: 'complete' });
+      wrapper.setState({ phase: 'complete' });
       wrapper.update();
 
       expect(wrapper.find(StyledCell)).toHaveLength(0);
@@ -63,25 +165,26 @@ describe('TableTree', () => {
     });
 
     it('enters the loading phase after the delay has passed', () => {
-      const DELAY = 500;
       const wrapper = shallow(
-        <LoaderItem delay={DELAY} isCompleting={false} onComplete={() => {}} />,
+        <LoaderItem
+          delay={DELAY_BEFORE_ANIMATION_SHOWN}
+          isCompleting={false}
+          onComplete={() => {}}
+        />,
       );
-      const instance = wrapper.instance();
-      jest.runTimersToTime(DELAY / 2);
+      jest.runTimersToTime(DELAY_BEFORE_ANIMATION_SHOWN / 2);
 
-      expect(instance.state).toHaveProperty('phase', 'delaying');
+      expect(wrapper.state()).toHaveProperty('phase', 'delaying');
 
-      jest.runTimersToTime(DELAY);
-      expect(instance.state).toHaveProperty('phase', 'loading');
+      jest.runTimersToTime(DELAY_BEFORE_ANIMATION_SHOWN);
+      expect(wrapper.state()).toHaveProperty('phase', 'loading');
     });
 
     it('enters the complete phase when was delaying and isComplete was set to true', () => {
       const wrapper = shallow(
         <LoaderItem isCompleting={false} onComplete={() => {}} />,
       );
-      const instance = wrapper.instance();
-      instance.setState({ phase: 'delaying' });
+      wrapper.setState({ phase: 'delaying' });
       wrapper.update();
 
       wrapper.setProps({ isCompleting: true });
