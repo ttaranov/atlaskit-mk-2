25662947ed8b4957ba1b42a3270137d29a4ff251
Jimmy Kurniawan <jkurniawan@atlassian.com>
2018-02-06 14:54:04 +1100

    AK-4238 DPS-65 HOME-1349 Fixed test

--- packages/fabric/notification-indicator/__tests__/NotificationIndicatorSpec.tsx
+++ packages/fabric/notification-indicator/__tests__/NotificationIndicatorSpec.tsx
@@ -3,10 +3,33 @@ import { mount } from 'enzyme';
 import * as sinon from 'sinon';
 
 import Badge from '@atlaskit/badge';
-import { NotificationCountResponse } from '@atlaskit/notification-log-client';
+import {
+  NotificationLogClient,
+  NotificationCountResponse,
+} from '@atlaskit/notification-log-client';
 
 import { NotificationIndicator } from '../src';
-import MockNotificationLogClient from '../examples/MockNotificationLogClient';
+
+class MockNotificationLogClient extends NotificationLogClient {
+  private response?: Promise<NotificationCountResponse>;
+
+  constructor() {
+    super('', '');
+  }
+
+  public async countUnseenNotifications() {
+    return (
+      this.response ||
+      Promise.resolve({
+        count: 5,
+      })
+    );
+  }
+
+  public setResponse(response: Promise<NotificationCountResponse>) {
+    this.response = response;
+  }
+}
 
 describe('NotificationIndicator', () => {
   let notificationLogClient;
