00275cfdc3040588974a9c71cdbe2d4b1ec7fce5
ttjandra <ttjandra@atlassian.com>
2018-02-06 15:48:55 +1100

    FS-1091 update consumer docs to remove reference to url-search-params

--- packages/fabric/editor-core/docs/0-intro.tsx
+++ packages/fabric/editor-core/docs/0-intro.tsx
@@ -14,7 +14,6 @@ export default md`
 
   * Promise ([polyfill](https://www.npmjs.com/package/es6-promise), [browser support](http://caniuse.com/#feat=promises))
   * Fetch API ([polyfill](https://www.npmjs.com/package/whatwg-fetch), [browser support](http://caniuse.com/#feat=fetch))
-  * URLSearchParams API ([polyfill](https://www.npmjs.com/package/url-search-params), [browser support](http://caniuse.com/#feat=urlsearchparams))
   * Element.closest ([polyfill](https://www.npmjs.com/package/element-closest), [browser support](http://caniuse.com/#feat=element-closest))
 
   # Starting to use the editor
