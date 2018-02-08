00b037727062b8228274e0e9866dd015f9d8e1d5
Trey Shugart <treshugart@gmail.com>
2018-02-08 13:21:18 +1100

    Fix types.
    Update types to include support for needed event methods.

--- packages/elements/datetime-picker/src/types.js
+++ packages/elements/datetime-picker/src/types.js
@@ -1,4 +1,9 @@
 // @flow
 
-export type Event = { key: string, target: { value: string } };
+export type Event = {
+  key: string,
+  preventDefault: Function,
+  stopPropagation: Function,
+  target: { value: string },
+};
 export type Handler = (e: any) => void;
