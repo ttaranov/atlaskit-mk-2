c08c8ff31afdb40ce4849ea1a80318d0343fd652
Pete Gleeson <pgleeson@atlassian.com>
2018-02-08 13:57:17 +1100

    SHPXLI-241 adds copy flow types step to build

--- packages/elements/dropdown-menu/src/components/context/DropdownItemFocusManager.js
+++ packages/elements/dropdown-menu/src/components/context/DropdownItemFocusManager.js
@@ -3,8 +3,8 @@
 import React, { Component, type Node } from 'react';
 import PropTypes from 'prop-types';
 import { focusManagerContext } from '../../util/contextNamespace';
-import type { ItemId, FocusItem } from '../../../src/types';
-import { KEY_DOWN, KEY_UP, KEY_TAB } from '../../util/keys';
+import type { ItemId, FocusItem } from '../../types';
+import { KEY_DOWN, KEY_UP } from '../../util/keys';
 
 type Props = {
   /** Causes first registered item to receive focus */
