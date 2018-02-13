2189e1611a7d49d2546d3a4a4a1fc04face4f6a3
Jimmy Luong <jluong@jimmys-mbp.office.atlassian.com>
2018-02-13 18:28:07 +1100

    test(media-store): add create upload spec

--- packages/fabric/media-store/__tests__/media-store.spec.ts
+++ packages/fabric/media-store/__tests__/media-store.spec.ts
@@ -1,3 +1,35 @@
+import { MediaStore } from '../src/';
+
 describe('MediaStore', () => {
-  it('should do something', () => {});
+  const setup = (fetchResponse = Promise.resolve({ json: jest.fn() })) => {
+    const auth = { clientId: 'some-client-id', token: 'some-token' };
+    const mediaStore = new MediaStore({
+      apiUrl: 'some-api-url',
+      authProvider: () => Promise.resolve(auth),
+    });
+
+    mediaStore['fetch'] = jest.fn().mockReturnValueOnce(fetchResponse);
+
+    return {
+      mediaStore,
+      fetch: mediaStore['fetch'],
+    };
+  };
+
+  describe('createUpload', () => {
+    it('should fetch from /upload api', () => {
+      const createUpTo = randomInteger();
+      const { mediaStore, fetch } = setup();
+
+      return mediaStore.createUpload(createUpTo).then(() => {
+        expect(fetch).toBeCalledWith(`/upload?createUpTo=${createUpTo}`, {
+          method: 'POST',
+        });
+      });
+    });
+  });
 });
+
+function randomInteger(max: number = 100): number {
+  return Math.floor(Math.random() * max);
+}
