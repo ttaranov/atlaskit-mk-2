3ffaab10edb8a0fb4434227f3e57d98d8ebd7672
ttjandra <ttjandra@atlassian.com>
2018-02-13 16:21:24 +1100

    Message renderer takes in multiple providers

--- packages/fabric/editor-core/examples/14-message-renderer.tsx
+++ packages/fabric/editor-core/examples/14-message-renderer.tsx
@@ -3,20 +3,31 @@ import * as React from 'react';
 import Editor from './../src/editor';
 import RefApp from '../example-helpers/RefApp';
 
-export default function Example() {
-  return (
-    <RefApp
-      // tslint:disable-next-line:jsx-no-lambda
-      renderEditor={({ emojiProvider, onChange }) => {
-        return (
-          <Editor
-            appearance="message"
-            emojiProvider={emojiProvider}
-            maxHeight={305}
-            onChange={onChange}
-          />
-        );
-      }}
-    />
-  );
+export default class MessageRenderer extends React.PureComponent<any, any> {
+  render() {
+    return (
+      <RefApp
+        // tslint:disable-next-line:jsx-no-lambda
+        renderEditor={({
+          emojiProvider,
+          mentionProvider,
+          taskDecisionProvider,
+          contextIdentifierProvider,
+          onChange,
+        }) => {
+          return (
+            <Editor
+              appearance="message"
+              emojiProvider={emojiProvider}
+              mentionProvider={mentionProvider}
+              taskDecisionProvider={taskDecisionProvider}
+              contextIdentifierProvider={contextIdentifierProvider}
+              maxHeight={305}
+              onChange={onChange}
+            />
+          );
+        }}
+      />
+    );
+  }
 }
