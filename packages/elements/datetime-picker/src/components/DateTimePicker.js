92fd9f217a17db7e7d99feb941797d10244a1347
Trey Shugart <treshugart@gmail.com>
2018-02-13 15:08:12 +1100

    Share defaultTimes between the time picker and datetime picker.

--- packages/elements/datetime-picker/src/components/DateTimePicker.js
+++ packages/elements/datetime-picker/src/components/DateTimePicker.js
@@ -8,28 +8,7 @@ import styled from 'styled-components';
 
 import DatePicker from './DatePicker';
 import TimePicker from './TimePicker';
-
-const defaultTimes = [
-  '09:00',
-  '09:30',
-  '10:00',
-  '10:30',
-  '11:00',
-  '11:30',
-  '12:00',
-  '12:30',
-  '13:00',
-  '13:30',
-  '14:00',
-  '14:30',
-  '15:00',
-  '15:30',
-  '16:00',
-  '16:30',
-  '17:00',
-  '17:30',
-  '18:00',
-];
+import { defaultTimes } from '../internal';
 
 /* eslint-disable react/no-unused-prop-types */
 type Props = {
