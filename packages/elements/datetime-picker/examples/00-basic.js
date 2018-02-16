9271541ce56fc4419c3962d3ae378fa64ab1a0c3
Trey Shugart <treshugart@gmail.com>
2018-02-16 15:34:25 +1100

    Add htmlFor and ids to the label / selects.

--- packages/elements/datetime-picker/examples/00-basic.js
+++ packages/elements/datetime-picker/examples/00-basic.js
@@ -8,28 +8,49 @@ export default () => {
   return (
     <div>
       <h3>Date picker</h3>
-      <Label label="default" />
-      <DatePicker />
-      <Label label="controlled (value)" />
-      <DatePicker value="2018-01-02" />
-      <Label label="uncontrolled (defaultValue)" />
-      <DatePicker defaultValue="2018-01-02" />
+      <Label htmlFor="react-select-datepicker-1--input" label="default" />
+      <DatePicker id="datepicker-1" />
+      <Label
+        htmlFor="react-select-datepicker-2--input"
+        label="controlled (value)"
+      />
+      <DatePicker id="datepicker-2" value="2018-01-02" />
+      <Label
+        htmlFor="react-select-datepicker-3--input"
+        label="uncontrolled (defaultValue)"
+      />
+      <DatePicker id="datepicker-3" defaultValue="2018-01-02" />
 
       <h3>Time picker</h3>
-      <Label label="default" />
-      <TimePicker />
-      <Label label="controlled (value)" />
-      <TimePicker value="14:30" />
-      <Label label="uncontrolled (defaultValue)" />
-      <TimePicker defaultValue="14:30" />
+      <Label htmlFor="react-select-timepicker-1--input" label="default" />
+      <TimePicker id="timepicker-1" />
+      <Label
+        htmlFor="react-select-timepicker-2--input"
+        label="controlled (value)"
+      />
+      <TimePicker id="timepicker-2" value="14:30" />
+      <Label
+        htmlFor="react-select-timepicker-3--input"
+        label="uncontrolled (defaultValue)"
+      />
+      <TimePicker id="timepicker-3" defaultValue="14:30" />
 
       <h3>Date / time picker</h3>
-      <Label label="default" />
-      <DateTimePicker />
-      <Label label="controlled (UTC-08:00)" />
-      <DateTimePicker value="2018-01-02T14:30-08:00" />
-      <Label label="uncontrolled (UTC+10:00)" />
-      <DateTimePicker defaultValue="2018-01-02T14:30+10:00" />
+      <Label htmlFor="react-select-datetimepicker-1--input" label="default" />
+      <DateTimePicker id="datetimepicker-1" />
+      <Label
+        htmlFor="react-select-datetimepicker-2--input"
+        label="controlled (UTC-08:00)"
+      />
+      <DateTimePicker id="datetimepicker-2" value="2018-01-02T14:30-08:00" />
+      <Label
+        htmlFor="react-select-datetimepicker-3--input"
+        label="uncontrolled (UTC+10:00)"
+      />
+      <DateTimePicker
+        id="datetimepicker-3"
+        defaultValue="2018-01-02T14:30+10:00"
+      />
     </div>
   );
 };
