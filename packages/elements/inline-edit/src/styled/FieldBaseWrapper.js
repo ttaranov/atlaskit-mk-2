4cc1d92e538a96f47c270b21b153810b062be0e5
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-02-19 12:33:59 +1100

    Fix edit views not respecting shouldFitContainer

--- packages/elements/inline-edit/src/styled/FieldBaseWrapper.js
+++ packages/elements/inline-edit/src/styled/FieldBaseWrapper.js
@@ -3,16 +3,9 @@ import styled from 'styled-components';
 
 // Display toggle needed to prevent edit click trigger exceeding hover width.
 // Max-width required to prevent text overflow when display is inline.
-/*const FieldBaseWrapper = styled.div`
-  display: ${({ displayFullWidth }) =>
-    displayFullWidth ? 'flex' : 'inline-block'};
-  max-width: 100%;
-  flex-basis: auto;
-  flex-grow: 1;
-  flex-shrink: 1;
-`;*/
-// Removing setting the display here fixes an issue with InlineEdit
 const FieldBaseWrapper = styled.div`
+  display: ${({ displayFullWidth }) =>
+    displayFullWidth ? 'block' : 'inline-block'};
   max-width: 100%;
   flex-basis: auto;
   flex-grow: 1;
