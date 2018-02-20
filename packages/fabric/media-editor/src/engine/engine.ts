9a2ee0da49171d94d0d7ea4371ce595d94ddd0b2
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-02-20 17:47:32 +1100

    Use new and better MediaEditor bundle

--- packages/fabric/media-editor/src/engine/engine.ts
+++ packages/fabric/media-editor/src/engine/engine.ts
@@ -1,4 +1,4 @@
-import { Core } from './core/binaries/editorCore';
+import * as Core from './core/binaries/mediaEditor';
 import { ExportedImage, ShapeParameters, TextDirection, Tool } from '../common';
 import { ResourceManager } from './resourceManager';
 
