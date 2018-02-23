d99c3e124472a34eadb6aa5cda515266007ba9b9
Luke Batchelor <lbatchelor@atlassian.com>
2018-02-23 16:26:43 +1100

    WIP - testing CDN

--- build/webpack-config/config/index.js
+++ build/webpack-config/config/index.js
@@ -57,7 +57,14 @@ module.exports = function createWebpackConfig(
               path.join(process.cwd(), './src/examples-entry.js'),
             ]
           : path.join(cwd, './src/examples-entry.js'),
-      vendor: ['react', 'react-dom', 'styled-components', 'highlight.js'],
+      vendor: [
+        'react',
+        'react-dom',
+        'styled-components',
+        'highlight.js',
+        'react-router',
+        'react-router-dom',
+      ],
     },
     output: {
       filename: '[name].js',
@@ -184,13 +191,32 @@ function plugins(
   } /*: { cwd: string, env: string, noMinimize: boolean, report: boolean } */,
 ) {
   const plugins = [
-    new webpack.NamedChunksPlugin(),
+    new webpack.NamedChunksPlugin(
+      chunk =>
+        chunk.name && path.isAbsolute(chunk.name) ? chunk.id : chunk.name,
+    ),
     new webpack.NamedModulesPlugin(),
     //
     // Order of CommonsChunkPlugins is important,
     // each next one of them can drag some dependencies from the previous ones.
-    //
-    // Joins all vendor entry point packages into 1 chunk
+
+    new webpack.optimize.CommonsChunkPlugin({
+      async: 'async-deps',
+      minChunks: 1,
+    }),
+
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   async: 'large-async-deps',
+    //   minChunks: minChunks(module, count) {
+    //     const largeAsyncDeps = ['@atlaskit/emoji', 'rxjs/', 'text-encoding/', 'moment/', 'date-fns']
+    //     const context = module.context;
+
+    //     return (
+    //       context &&
+    //       largeAsyncDeps.some(depName => context)
+    //     );
+    //   },
+    // }),
 
     new webpack.optimize.CommonsChunkPlugin({
       async: 'editor-packages',
@@ -241,87 +267,7 @@ function plugins(
       chunks: ['main', 'examples'],
       minChunks(module, count) {
         const resource = module.resource;
-        return (
-          []
-            .concat(...module.getChunks().map(c => c.entrypoints))
-            .filter(e => e === 'main' || e === 'examples').length > 1
-        );
-      },
-    }),
-
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'common-shared-with-async-app',
-      chunks: ['main'],
-      minChunks(module, count) {
-        const context = module.context;
-        return (
-          context &&
-          (context.includes('fabric/editor') ||
-            context.includes('fabric/renderer') ||
-            context.includes('fabric/conversation') ||
-            context.includes('prosemirror') ||
-            context.includes('fabric/mention') ||
-            context.includes('fabric/emoji') ||
-            context.includes('fabric/task-decision') ||
-            context.includes('fabric/reactions') ||
-            context.includes('fabric/media') ||
-            context.includes('elements/'))
-        );
-      },
-    }),
-
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'common-shared-with-async-subdeps-app',
-      chunks: ['main'],
-      minChunks(module, count) {
-        const isTransitiveAsyncDependency = function(
-          reasons,
-          seen = new Set(),
-        ) {
-          return reasons
-            .filter(r => r.constructor.name === 'ModuleReason')
-            .some(reason => {
-              if (seen.has(reason)) {
-                return false;
-              }
-              seen.add(reason);
-
-              if (
-                reason.module.context &&
-                [
-                  'fabric/editor',
-                  'fabric/renderer',
-                  'fabric/conversation',
-                  'prosemirror',
-                  'fabric/mention',
-                  'fabric/emoji',
-                  'fabric/task-decision',
-                  'fabric/reactions',
-                  'fabric/media',
-                  'elements/',
-                ].some(x => reason.module.context.includes(x))
-              ) {
-                return true;
-              }
-              return isTransitiveAsyncDependency(reason.module.reasons, seen);
-            });
-        };
-        return module.resource && isTransitiveAsyncDependency(module.reasons);
-      },
-    }),
-
-    new webpack.optimize.CommonsChunkPlugin({
-      async: 'used-two-or-more-times',
-      minChunks(module, count) {
-        if (!hasWritten) {
-          console.log(module._chunks, module._chunksDebugIdent);
-          fs.writeFileSync(
-            'commonChunksList.json',
-            JSON.stringify(chunkslists),
-          );
-          hasWritten = true;
-        }
-        return count >= 2;
+        return !resource || !resource.includes('website/src/index.js');
       },
     }),
 
