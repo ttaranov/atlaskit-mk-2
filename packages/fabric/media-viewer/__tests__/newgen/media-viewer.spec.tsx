ad6b38d63261416ceaff04e4b23166dd30f874ac
Damien Klinnert <dklinnert@atlassian.com>
2018-02-26 10:28:28 +0100

    Use Atlaskit Spinner

--- packages/fabric/media-viewer/__tests__/newgen/media-viewer.spec.tsx
+++ packages/fabric/media-viewer/__tests__/newgen/media-viewer.spec.tsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
-import { reducer, effects, State, Action, Component, Spinner } from '../../src/newgen/media-viewer';
+import {
+  reducer,
+  effects,
+  State,
+  Action,
+  Component,
+} from '../../src/newgen/media-viewer';
 import { mount } from 'enzyme';
 import { Stubs } from '../_stubs';
 import { Subject } from 'rxjs/Subject';
 import { MediaItemType, MediaItem } from '@atlaskit/media-core';
+import Spinner from '@atlaskit/spinner';
 
-describe('media-viewer', () => {  
-
+describe('MediaViewer', () => {
   const token = 'some-token';
   const clientId = 'some-client-id';
   const serviceHost = 'some-service-host';
@@ -19,27 +25,23 @@ describe('media-viewer', () => {
   describe('reducer', () => {
     it('once loaded the name is saved as unkown', () => {
       const initialState: State = {
-        type: 'LOADING'
+        type: 'LOADING',
       };
       const action: Action = {
         type: 'LOADED',
-        item: { type: 'file', details: {} }
+        item: { type: 'file', details: {} },
       };
       expect(reducer(initialState, action)).toEqual({
         type: 'LOADED',
-        name: 'unkown'
+        name: 'unkown',
       });
-    });
-
-    it('must switch state between INIT and LOAD', () => {
-      // TODO
     });
   });
 
-  describe('component', () => {
+  describe('Component', () => {
     it('should render the spinner for the LOADING state', () => {
       const state: State = {
-        type: 'LOADING'
+        type: 'LOADING',
       };
 
       const c = mount(<Component {...state} dispatch={() => {}} />);
@@ -48,41 +50,41 @@ describe('media-viewer', () => {
   });
 
   describe('effects', () => {
-    it('should emit the LOADED action when initialised successfully', (done) => {
+    it('should emit the LOADED action when initialised successfully', done => {
       const subject = new Subject<MediaItem>();
       const context = Stubs.context(
         contextConfig,
         undefined,
-        Stubs.mediaItemProvider(subject)
+        Stubs.mediaItemProvider(subject),
       ) as any;
       const item = {
         id: '',
         occurrenceKey: '',
-        type: 'file' as MediaItemType      
+        type: 'file' as MediaItemType,
       };
       const action: Action = {
         type: 'INIT',
         props: {
           context,
           dataSource: { list: [item] },
-          initialItem: item
-        }
+          initialItem: item,
+        },
       };
 
-      effects(action)!.then((resultAction) => {
+      effects(action)!.then(resultAction => {
         expect(resultAction).toEqual({
           type: 'LOADED',
-          item: {          
-            type: 'file', 
-            details: { id: 'my-id' }
-          }
+          item: {
+            type: 'file',
+            details: { id: 'my-id' },
+          },
         });
         done();
       });
 
       subject.next({
         type: 'file',
-        details: { id: 'my-id' }
+        details: { id: 'my-id' },
       });
     });
   });
--- packages/fabric/media-viewer/src/newgen/media-viewer.tsx
+++ packages/fabric/media-viewer/src/newgen/media-viewer.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { Context, MediaItem } from '@atlaskit/media-core';
-import { MediaViewerDataSource, MediaViewerItem } from '../components/media-viewer';
+import {
+  MediaViewerDataSource,
+  MediaViewerItem,
+} from '../components/media-viewer';
+import Spinner from '@atlaskit/spinner';
 
 export type DispatchFn = (action: Action) => void;
 
@@ -11,99 +15,103 @@ export interface Props {
   collectionName?: string;
 }
 
-export type State = {
-  type: 'LOADING'
-} | {
-  type: 'LOADED',
-  name: string
-} | {
-  type: 'ERROR'
-}
+export type State =
+  | {
+      type: 'LOADING';
+    }
+  | {
+      type: 'LOADED';
+      name: string;
+    }
+  | {
+      type: 'ERROR';
+    };
 
-export type Action = { 
-  type: 'INIT',
-  props: Props
-} | {
-  type: 'LOADED',
-  item: MediaItem
-} | {
-  type: 'LOADING_ERROR',
-};
+export type Action =
+  | {
+      type: 'INIT';
+      props: Props;
+    }
+  | {
+      type: 'LOADED';
+      item: MediaItem;
+    }
+  | {
+      type: 'LOADING_ERROR';
+    };
 
 export const initialState: State = {
-  type: 'LOADING'
+  type: 'LOADING',
 };
 
 export const initialAction = (props: Props): Action => {
   return {
     type: 'INIT',
-    props
+    props,
   };
-}
+};
 
 export const reducer = (prevState: State, action: Action): State => {
-  switch(action.type) {
+  switch (action.type) {
     case 'INIT':
       return prevState;
     case 'LOADED':
       return {
         type: 'LOADED',
-        name: action.item.type === 'file' && action.item.details.name || 'unkown'
+        name:
+          (action.item.type === 'file' && action.item.details.name) || 'unkown',
       };
-    case  'LOADING_ERROR':
+    case 'LOADING_ERROR':
       return {
-        type: 'ERROR'
-      }
+        type: 'ERROR',
+      };
   }
 };
 
 export type ComponentProps = State & { dispatch: DispatchFn };
 
-export const Spinner = () => {
-  return <div>spinner</div>;
-}
-
 export const Component = (props: ComponentProps) => {
-  switch(props.type) {
+  switch (props.type) {
     case 'LOADING':
-      return (
-        <Spinner/>
-      );
+      return <Spinner />;
     case 'ERROR':
-      return (
-        <div>ERROR view</div>
-      );
+      return <div>ERROR view</div>;
     case 'LOADED':
-    return (
-      <div>{props.name}</div>
-    );    
+      // loaded means that we have a list of media items to start with.
+      // those items doesn't necessarily mean that have been completely loaded.
+
+      return <div>{props.name}</div>;
   }
 };
 
 export const effects = (action: Action): Promise<Action> | null => {
   switch (action.type) {
-    case 'INIT': 
+    case 'INIT':
       return new Promise((resolve, reject) => {
         const { props } = action;
         const { context } = props;
-        context.getMediaItemProvider(
-          props.initialItem.id, 
-          props.initialItem.type, 
-          props.collectionName).observable().subscribe({
-            next: (item) => {
+        context
+          .getMediaItemProvider(
+            props.initialItem.id,
+            props.initialItem.type,
+            props.collectionName,
+          )
+          .observable()
+          .subscribe({
+            next: item => {
               resolve({
                 type: 'LOADED',
-                item
-              })      
-            },
-            error: (err) => {
-              resolve({
-                type: 'LOADING_ERROR'
+                item,
               });
-            }
-        });  
+            },
+            error: err => {
+              resolve({
+                type: 'LOADING_ERROR',
+              });
+            },
+          });
       });
-    default: 
+    default:
       return null;
   }
 };
