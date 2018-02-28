18bb73162ca26e3f809d84563543ccefe1a6be99
Pete Gleeson <pgleeson@atlassian.com>
2018-02-28 16:53:57 +1100

    AK-4365 wraps edited and comment action in analytics

--- packages/elements/comment/__tests__/edited.js
+++ packages/elements/comment/__tests__/edited.js
@@ -2,27 +2,27 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { CommentAction } from '../src/';
+import { CommentEdited } from '../src/';
 import SubtleLink from '../src/components/SubtleLink';
 
 describe('@atlaskit comments', () => {
-  describe('CommentAction', () => {
+  describe('Edited', () => {
     it('should pass props down to SubtleLink', () => {
       const props = {
         onClick: jest.fn(),
         onFocus: jest.fn(),
         onMouseOver: jest.fn(),
       };
-      const wrapper = shallow(<CommentAction {...props} />);
+      const wrapper = shallow(<CommentEdited {...props} />);
       Object.keys(props).forEach(propName => {
         expect(wrapper.find(SubtleLink).prop(propName)).toBe(props[propName]);
       });
     });
 
-    it('should render a SubtleLink with comment action context', () => {
-      const wrapper = shallow(<CommentAction>Delete</CommentAction>);
+    it('should render a SubtleLink with comment edited context', () => {
+      const wrapper = shallow(<CommentEdited>Edited</CommentEdited>);
       const { analyticsContext } = wrapper.find(SubtleLink).props();
-      expect(analyticsContext).toEqual({ component: 'comment-action' });
+      expect(analyticsContext).toEqual({ component: 'comment-edited' });
     });
   });
 });
