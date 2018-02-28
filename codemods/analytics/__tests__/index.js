dd736091d41cad112f66b07f412d618c4352de5a
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-02-28 16:26:55 +1100

    Update codemod to only apply changes if they are not already applied

--- codemods/analytics/__tests__/index.js
+++ codemods/analytics/__tests__/index.js
@@ -5,5 +5,7 @@ const defineTest = require('jscodeshift/dist/testUtils').defineTest;
 
 describe('analytics', () => {
   defineTest(__dirname, 'index', null, 'AddsCorrectImports');
+  defineTest(__dirname, 'index', null, 'NoDuplicateImports');
   defineTest(__dirname, 'index', null, 'WrapsDefaultExport');
+  defineTest(__dirname, 'index', null, 'noDuplicateHocs');
 });
--- codemods/analytics/index.js
+++ codemods/analytics/index.js
@@ -1,23 +1,18 @@
 // @flow
 import UtilPlugin from '../plugins/util';
 
-const findLast = function(type) {
-  const found = this.find(type);
-
-  return found.at(found.length - 1);
-};
-
 const createImport = (j, specifierNames, source) => {
-  const specifiers = specifierNames.map(name =>
-    j.importSpecifier(j.identifier(name)),
+  const specifiers = specifierNames.map( name =>
+    j.importSpecifier(j.identifier(name))
   );
 
   return j.importDeclaration(specifiers, j.literal(source));
-};
+}
 
 const createEventMapPropFn = (j, action) => {
   const params = j.identifier('createAnalyticsEvent');
-  const body = j.blockStatement([]);
+  const body = j.blockStatement([
+  ]);
   const code = `
     createAnalyticsEvent => {
       const consumerEvent = createAnalyticsEvent({
@@ -29,66 +24,57 @@ const createEventMapPropFn = (j, action) => {
     }
   `;
 
-  return j(code)
-    .find(j.ArrowFunctionExpression)
-    .get().value;
-};
+  return j(code).find(j.ArrowFunctionExpression).get().value;
+}
 
-const createAnalyticsEventsHoc = (j, createEventMap, inner) => {
-  const eventMap = j.objectExpression(
-    Object.keys(createEventMap).map(propName => {
-      const action = createEventMap[propName];
-      return j.property(
-        'init',
-        j.identifier(propName),
-        createEventMapPropFn(j, action),
-      );
-    }),
-  );
-  const firstCall = j.callExpression(j.identifier('withAnalyticsEvents'), [
-    eventMap,
-  ]);
+const createAnalyticsEventsHoc = (j, filePath, inner) => {
+  console.log(filePath);
+  const createEventMap = { onClick: 'click' };
+  const eventMap = j.objectExpression(Object.keys(createEventMap).map( propName => {
+    const action = createEventMap[propName];
+    return j.property('init', j.identifier(propName), createEventMapPropFn(j, action));
+  }));
+  const firstCall = j.callExpression(j.identifier('withAnalyticsEvents'), [eventMap]);
 
   return j.callExpression(firstCall, [inner.value.declaration]);
-};
+}
 
 module.exports = (fileInfo: any, api: any) => {
   const j = api.jscodeshift;
   j.use(UtilPlugin);
-  // if (!j.Collection.prototype.hasOwnProperty('findLast')) {
-  //   j.registerMethods({
-  //     findLast,
-  //   });
-  // }
 
-  const sourceWithImports = j(fileInfo.source)
+  const existingImports = j(fileInfo.source)
+    .find(j.ImportDeclaration)
+    .filter( path => 
+      path.value.source.value === '@atlaskit/analytics-next'
+    );
+  
+  const sourceWithImports = existingImports.size() > 0 ?
+    fileInfo.source
+  : j(fileInfo.source)
     // Add relevant imports
     .findLast(j.ImportDeclaration)
-    .insertAfter(
-      createImport(
-        j,
-        ['withAnalyticsEvents', 'withAnalyticsContext'],
-        '@atlaskit/analytics-next',
-      ),
-    )
+    .insertAfter(createImport(j, ['withAnalyticsEvents', 'withAnalyticsContext'], '@atlaskit/analytics-next'))
     .closest(j.Program)
     .findLast(j.ImportDeclaration)
     .insertAfter(createImport(j, ['name', 'version'], '../../package.json'))
     .getAST();
 
-  const sourceWithHOC = j(sourceWithImports)
-    // Wrap default export with HOCs
+  const existingHOC = j(fileInfo.source)
     .find(j.ExportDefaultDeclaration)
-    .map(path => {
-      path.value.declaration = createAnalyticsEventsHoc(
-        j,
-        { onClick: 'click' },
-        path,
-      );
-      return path;
-    })
-    .getAST();
+    .find(j.CallExpression)
+    .find(j.Identifier, { name: 'withAnalyticsEvents'});
 
+  const sourceWithHOC = existingHOC.size() > 0
+    ? sourceWithImports
+    : j(sourceWithImports)
+      // Wrap default export with HOCs
+      .find(j.ExportDefaultDeclaration)
+      .map( path => {
+        path.value.declaration = createAnalyticsEventsHoc(j, fileInfo.path, path);
+        return path;
+      }).getAST();
+    
   // Print source
   return j(sourceWithHOC).toSource({ quote: 'single' });
 };
