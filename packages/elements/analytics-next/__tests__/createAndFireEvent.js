2a438c558175678363068f29f162e14594c6af5a
Pete Gleeson <pgleeson@atlassian.com>
2018-03-01 12:18:22 +1100

    adds createAndFireEvent utility to analytics-next package

---
+++ packages/elements/analytics-next/__tests__/createAndFireEvent.js
@@ -0,0 +1,34 @@
+// @flow
+
+import React, { type Node } from 'react';
+import { mount } from 'enzyme';
+import {
+  createAndFireEvent,
+  withAnalyticsEvents,
+  AnalyticsListener,
+} from '../src';
+
+type Props = {
+  onClick: () => void,
+  children: Node,
+};
+
+const Button = ({ onClick, children }: Props) => (
+  <button onClick={onClick}>{children}</button>
+);
+
+it('should create and fire analytics event', () => {
+  const onEvent = jest.fn();
+  const createAndFireOnAtlaskit = createAndFireEvent('atlaskit');
+  const ButtonWithAnalytics = withAnalyticsEvents({
+    onClick: createAndFireOnAtlaskit({ action: 'click' }),
+  })(Button);
+  mount(
+    <AnalyticsListener channel="atlaskit" onEvent={onEvent}>
+      <ButtonWithAnalytics>Clicky</ButtonWithAnalytics>
+    </AnalyticsListener>,
+  )
+    .find(Button)
+    .simulate('click');
+  expect(onEvent).toHaveBeenCalled();
+});
---
+++ packages/elements/analytics-next/src/createAndFireEvent.js
@@ -0,0 +1,11 @@
+//@flow
+import { type AnalyticsEventPayload } from './types';
+import { type CreateUIAnalyticsEventSignature } from './withAnalyticsEvents';
+
+export default (channel: string) => (payload: AnalyticsEventPayload) => (
+  createAnalyticsEvent: CreateUIAnalyticsEventSignature,
+) => {
+  const consumerEvent = createAnalyticsEvent(payload);
+  consumerEvent.clone().fire(channel);
+  return consumerEvent;
+};
