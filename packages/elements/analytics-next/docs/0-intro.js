04ae2d1b2c23dfb4a6eb3bb979deb0f8f975fc8a
Pete Gleeson <pgleeson@atlassian.com>
2018-03-04 18:46:36 +1100

    AK-4356 restructures docs to be more friendly for typical usecase

--- packages/elements/analytics-next/docs/0-intro.js
+++ packages/elements/analytics-next/docs/0-intro.js
@@ -1,168 +1,132 @@
 // @flow
 import React from 'react';
-import { code, md, Props } from '@atlaskit/docs';
+import { code, md } from '@atlaskit/docs';
 
-export default md`
-  ## Contents
-
-  * [withAnalyticsEvents](#withAnalyticsEvents)
-  * [UIAnalyticsEvent](#UIAnalyticsEvent)
-  * [AnalyticsListener](#AnalyticsListener)
-  * [AnalyticsContext](#AnalyticsContext)
-  * [withAnalyticsContext](#withAnalyticsContext)
-  * [AnalyticsEvent](#AnalyticsEvent)
-
-  <a name="withAnalyticsEvents"></a>
-  # withAnalyticsEvents
-
-  &nbsp;
-
-  ${code`import { withAnalyticsEvents } from '@atlaskit/analytics-next';`}
-
-  A HOC which provides the wrapped component with a method for creating \`UIAnalyticsEvent\`s, via \`props.createAnalyticsEvent\`. Please refer to the tutorial for a thorough explanation of events and how to use them.
-
-  Usage:
-
-  ${code`withAnalyticsEvents()(Button);`}
-
-  It accepts an optional map as its first argument, which provides a shortcut for passing a new analytics event as an additional parameter to the corresponding callback prop.
-
-  Here's how that might look:
-
-${code`
-  withAnalyticsEvents({
-    onChange: (createAnalyticsEvent, props) => {
-      return createAnalyticsEvent({ action: 'change', checked: !props.checked });
-    }
-  })(Checkbox);
-`}
-
-  Whenever the wrapped component would fire a prop callback (in this case \`onChange\`), the corresponding function will be run first. It is provided with a function for creating an analytics event, as well as the component's props, and it should return a new analytics event. This event will automatically be added as an additional argument to the prop callback.
-
-  Since creating and returning an event is such a common pattern an even more concise shorthand is supported:
-
-${code`
-  withAnalyticsEvents({
-    onClick: { action: 'click' }
-  })(Button)
-`}
-
-  You can simply provide a payload object and an event will be created automatically.
-
-  <a name="UIAnalyticsEvent"></a>
-  # UIAnalyticsEvent
-
-  The class of the event created by a component wrapped in \`withAnalyticsEvents\`. It has the following interface:
-
-  &nbsp;
-
-${code`
-/** An array of objects containing data provided by any AnalyticsContext
- * components in the tree above where this event was created. */
-context: Array<{}>;
-
-/** An array of functions provided by any AnalyticsListener components in the
- * tree above where this event was created. */
-handlers: Array<(event: UIAnalyticsEvent, channel?: string) => void>;
-
-/** An object containing an action field and other arbitrary data. Can be
- * modified via the .update() method. */
-payload: {
-  action: string,
-  [string]: any
+type InstrumentedItem = {
+  packageName: string,
+  component: string,
+  prop: string,
+  payload: Object,
 };
 
-/** Create a new event with the same context, handlers and payload as this
- * event. */
-clone(): UIAnalyticsEvent | null;
+const scrubRepeatedInfo = (
+  item: InstrumentedItem,
+  i: number,
+  items: InstrumentedItem[],
+) => {
+  const prev = i > 0 ? items[i - 1] : {};
+  return {
+    ...item,
+    packageName: item.packageName !== prev.packageName ? item.packageName : '',
+    component: item.component !== prev.component ? item.component : '',
+  };
+};
 
-/** Fire this event on the given channel. Listeners on this channel will be
- * called. */
-fire(channel?: string): void;
+const InstrumentedTable = ({ packages }: { packages: InstrumentedItem[] }) => (
+  <table>
+    <th>Package</th>
+    <th>Component</th>
+    <th>Prop</th>
+    <th>Payload</th>
+    {packages
+      .map(scrubRepeatedInfo)
+      .map(({ packageName, component, prop, payload }) => (
+        <tr key={packageName}>
+          <td>{packageName}</td>
+          <td>{component}</td>
+          <td>{prop}</td>
+          <td>{JSON.stringify(payload)}</td>
+        </tr>
+      ))}
+  </table>
+);
 
-/** Modify this event's payload. Accepts either a function, which will be passed
- * the current payload and must return a new payload, or an object, which will
- * be shallow merged into the current payload. */
-update(
-  updater: | { [string]: any }
-  | ((payload: { action: string, [string]: any }) => {
-      action: string,
-      [string]: any,
-    }),
-) => UIAnalyticsEvent;
-`}
+export default md`
+  ## Usage
 
-  <a name="AnalyticsListener"></a>
-  # AnalyticsListener
-
-  &nbsp;
-
-  ${code`import { AnalyticsListener } from '@atlaskit/analytics-next';`}
-
-  An \`AnalyticsListener\` wraps your app and listens to any events which are fired within it.
-
-  ${(
-    <Props
-      props={require('!!extract-react-types-loader!../src/AnalyticsListener')}
-    />
-  )}
-
-  <a name="AnalyticsContext"></a>
-  # AnalyticsContext
-
-  &nbsp;
-
-  ${code`import { AnalyticsContext } from '@atlaskit/analytics-next';`}
-
-  Wrap part of your tree in \`AnalyticsContext\` to provide data to any events created beneath it. When an event is created it snapshots all of the \`AnalyticsContext\`s above it in the tree and creates an array from the data. It's up to you to parse this information when you handle the event.
-
-  ${(
-    <Props
-      props={require('!!extract-react-types-loader!../src/AnalyticsContext')}
-    />
-  )}
-
-  <a name="withAnalyticsContext"></a>
-  # withAnalyticsContext
-
-  &nbsp;
-
-  ${code`import { withAnalyticsContext } from '@atlaskit/analytics-next';`}
-
-  This HOC wraps a component in an \`AnalyticsContext\` and allows you to provide a default \`data\` value for it.
-
-  Usage:
-
+  Many of our components support analytics out of the box. These components create
+  analytics events and hand them to you. This puts you in control of firing, listening 
+  and recording these events in which ever way you like. 
+  
+  Let's look at a simple component to understand how to use Button's click analytics. 
+  
+  ##### SaveButton.js
 ${code`
-const ButtonWithContext = withAnalyticsContext({
-  // Default context data
-  component: 'button',
-})(Button);
+import Button from '@atlaskit/button';
 
-/* ... */
-
-const Form = (props) => (
-  <div>
-    <Field />
-    <ButtonWith Context
-      // The consumer can overwrite the default context data
-      analyticsContext={{ component: 'submit-button' }}
-      {...props}
-    >
-      Submit
-    </ButtonWithContext>
-  </div>
+const SaveButton = ({ onClick }) => (
+  <Button onClick={onClick}>Save</Button>
 );
 `}
 
-  Note: if a consumer provides a value for \`analyticsContext\` it is shallow merged with the default data.
+  Button provides you a [UIAnalyticsEvent](#UIAnalyticsEvent) as the second arg
+  to the onClick hander. This is the pattern used for all callback props that
+  support analytics.
+  
+  Now you have the event, it is up to you to fire it.
 
-  <a name="AnalyticsEvent"></a>
-  # AnalyticsEvent
+  ##### SaveButton.js
+${code`
+import Button from '@atlaskit/button';
 
-  &nbsp;
+const SaveButton = ({ onClick }) => (
+  <Button
+    onClick={(e, analytic) => {
+      analytic.fire();
+      if (onClick) {
+        onClick(e);
+      }
+    }}
+  >
+    Save
+  </Button>
+);
+`}
 
-  ${code`import { AnalyticsEvent } from '@atlaskit/analytics-next';`}
+  This is a good opportunity to add more information to the analytics event before firing. 
+  
+  The next step is to set up a listener which receives the events.
 
-  A more generic type of event which only contains a payload and an update method. If you want to create an event outside of the UI you can create an instance of this class directly. Please see [UIAnalyticsEvent](#UIAnalyticsEvent) for more information.
+  #### App.js
+${code`
+import { AnalyticsListener } from '@atlaskit/analytics-next';
+import SaveButton from './SaveButton';
+
+const sendAnalytics = analytic => console.log(analytic);
+
+const App = () => (
+  <AnalyticsListener onEvent={sendAnalytics}>
+    <SaveButton />
+  </AnalyticsListener>,
+);
+`}
+
+  The \`onEvent\` handler will be called every time an analytics event is fired.
+  This where you can record the event by sending information to a backend service.
+  
+  That's it! Below are some links to handy resources.
+
+  * [More information on UIAnalyticsEvent](/mk-2/packages/elements/analytics-next/docs/reference#UIAnalyticsEvent)
+  * [The list of instrumented components](#InstrumentedComponents)
+  * [Adding extra information to an analytics event](/mk-2/packages/elements/analytics-next/docs/concepts#adding-more-information-to-an-event)
+  * [Analytics component reference](/mk-2/packages/elements/analytics-next/docs/reference)
+
+  <a name="InstrumentedComponents"></a>
+  ## Instrumented Components
+
+  This table shows all the component interactions that are instrumented. 
+
+  ${(
+    <InstrumentedTable
+      packages={[
+        {
+          packageName: '@atlaskit/button',
+          component: 'Button',
+          prop: 'onClick',
+          payload: { action: 'click' },
+        },
+      ]}
+    />
+  )}
+
 `;
