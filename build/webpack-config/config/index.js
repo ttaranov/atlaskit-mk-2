dd37ea0f40f546b6c5194ffe5572198cc917eb26
Charles Lee <clee@atlassian.com>
2018-03-05 16:51:32 +1100

    added info commment to webpack config

--- build/webpack-config/config/index.js
+++ build/webpack-config/config/index.js
@@ -207,6 +207,8 @@ function plugins(
         return (
           count === 1 &&
           (context &&
+            // We're intentionally excluding p-queue and rxjs-async-map from this chunk as it currently breaks our build
+            // These two packages are being used by the media-store package. Unfortunately neither of them are transpiled, and are thus breaking in ie11.
             !context.includes('node_modules/p-queue') &&
             (context && !context.includes('node_modules/rxjs-async-map')))
         );
