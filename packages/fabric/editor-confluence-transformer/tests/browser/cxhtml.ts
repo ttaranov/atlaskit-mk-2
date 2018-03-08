fce379ebc6762ab69471ad690a666614e93f8dad
Luke Batchelor <lbatchelor@atlassian.com>
2018-03-08 14:59:11 +1100

    Removed String.raw fix from TS package as it was not required

--- packages/fabric/editor-confluence-transformer/tests/browser/cxhtml.ts
+++ packages/fabric/editor-confluence-transformer/tests/browser/cxhtml.ts
@@ -55,7 +55,6 @@ import {
   decisionList,
   decisionItem,
 } from '@atlaskit/editor-test-helpers';
-import stringRaw from 'string-raw';
 
 import {
   ConfluenceTransformer,
@@ -968,20 +967,14 @@ describe('ConfluenceTransformer: encode - parse:', () => {
   describe('fab:adf', () => {
     check(
       'p encoded in fab:adf tag between two p',
-      // we use stringRaw here instead of String.raw (with a polyfill) because of issues with the babel plugin (See AK-4444)
-      stringRaw(
-        `<p>hello</p><fab:adf><![CDATA[{\"type\":\"paragraph\",\"content\":[{\"type\":\"text\",\"text\":\"storage\"}]}]]></fab:adf><p>world</p>`,
-      ),
+      String.raw`<p>hello</p><fab:adf><![CDATA[{\"type\":\"paragraph\",\"content\":[{\"type\":\"text\",\"text\":\"storage\"}]}]]></fab:adf><p>world</p>`,
       doc(p('hello'), p('storage'), p('world')),
     );
 
     describe('decisionList', () => {
       check(
         'decisionList with single decided item between p',
-        // we use stringRaw here instead of String.raw (with a polyfill) because of issues with the babel plugin (See AK-4444)
-        stringRaw(
-          `<p>hello</p><fab:adf><![CDATA[{\"type\":\"decisionList\",\"attrs\":{\"localId\":\"test-list-id\"},\"content\":[{\"type\":\"decisionItem\",\"attrs\":{\"localId\":\"test-id\",\"state\":\"DECIDED\"},\"content\":[{\"type\":\"text\",\"text\":\"Heading\"}]}]}]]></fab:adf><p>world</p>`,
-        ),
+        String.raw`<p>hello</p><fab:adf><![CDATA[{\"type\":\"decisionList\",\"attrs\":{\"localId\":\"test-list-id\"},\"content\":[{\"type\":\"decisionItem\",\"attrs\":{\"localId\":\"test-id\",\"state\":\"DECIDED\"},\"content\":[{\"type\":\"text\",\"text\":\"Heading\"}]}]}]]></fab:adf><p>world</p>`,
         doc(
           p('hello'),
           decisionList({ localId: 'test-list-id' })(
