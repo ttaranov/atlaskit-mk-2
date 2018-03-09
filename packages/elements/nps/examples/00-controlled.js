8b231b4a0f6117067c41b949196f16dbcd0eeb56
Weston Selleck <wselleck@atlassian.com>
2018-03-08 19:48:39 -0600

    UV-616 Better names, remove isOpen prop

--- packages/elements/nps/examples/00-controlled.js
+++ packages/elements/nps/examples/00-controlled.js
@@ -3,44 +3,21 @@ import React from 'react';
 import AkNPS from '../src/NPS';
 
 export default class extends React.Component<any, any> {
-  constructor(...args: Array<any>) {
-    super(...args);
-    this.state = {
-      page1SubmitCount: 0,
-      submitCount: 0,
-    };
-  }
-
   render() {
     return (
       <div>
-        <div>Page 1 Submit Count: {this.state.page1SubmitCount}</div>
-        <div>Submit Count: {this.state.submitCount}</div>
         <AkNPS
           isDismissable
           canOptOut
-          onSubmitPage1={() => {
-            this.setState({
-              page1SubmitCount: this.state.page1SubmitCount + 1,
-            });
-          }}
-          onSubmit={() => {
-            console.log('here');
-            this.setState({
-              submitCount: this.state.submitCount + 1,
-            });
-          }}
+          onFeedbackSubmit={() => {}}
+          onFollowupSubmit={() => {}}
+          onFinish={() => {}}
           onDismiss={() => {}}
           onOptOut={() => {}}
-          roles={[
-            {
-              value: 'engineering',
-              text: 'Engineering',
-            },
-          ]}
+          roles={['Engineering']}
+          product="Stride"
           strings={{
-            product: 'Stride',
-            page1Subheader: 'I want to override the Page 1 Subheader',
+            feedbackDescription: 'I want to override the Feedback Description',
           }}
         />
       </div>
