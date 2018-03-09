17b592b34dce8b3e0ea54eec84139917e4c168c3
Ben Conolly <bconolly@atlassian.com>
2018-03-09 15:26:26 +1100

    remove onloadcomplete

--- website/src/pages/Package/CodeSandbox.js
+++ website/src/pages/Package/CodeSandbox.js
@@ -63,7 +63,6 @@ export default class CodeSandbox extends Component<{}, {}> {
           branch: 'master',
           host: 'bitbucket',
         }}
-        onLoadComplete={console.log}
         importReplacements={[
           [`packages/${groupId}/${packageId}/src`, pkgJSON.name],
           ['packages/elements/icon/glyph/*', '@atlaskit/icon/glyph/'],
