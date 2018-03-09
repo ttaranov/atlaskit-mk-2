feca454e9e3e5db736029b151a05593299c7898b
Pete Gleeson <pgleeson@atlassian.com>
2018-03-09 15:09:10 +1100

    AK-4365 updates prop types comment and analytics docs

--- packages/core/comment/src/components/Edited.js
+++ packages/core/comment/src/components/Edited.js
@@ -1,17 +1,21 @@
 // @flow
 
 import React, { type Node } from 'react';
+import { type UIAnalyticsEvent } from '@atlaskit/analytics-next';
 import SubtleLink from './SubtleLink';
 
 type Props = {
   /** Content to render indicating that the comment has been edited. */
   children?: Node,
-  /** Handler called when the element is clicked. */
-  onClick?: Function,
-  /** Handler called when the element is focused. */
-  onFocus?: Function,
-  /** Handler called when the element is moused over. */
-  onMouseOver?: Function,
+  /** Handler called when the element is clicked. The second argument can be used
+   * to track analytics events. See documentation in analytics-next package for details. */
+  onClick?: (e: SyntheticEvent<>, analyticsEvent: UIAnalyticsEvent) => void,
+  /** Handler called when the element is focused. The second argument can be used
+   * to track analytics events. See documentation in analytics-next package for details. */
+  onFocus?: (e: SyntheticEvent<>, analyticsEvent: UIAnalyticsEvent) => void,
+  /** Handler called when the element is moused over. The second argument can be used
+   * to track analytics events. See documentation in analytics-next package for details. */
+  onMouseOver?: (e: SyntheticEvent<>, analyticsEvent: UIAnalyticsEvent) => void,
 };
 
 const Edited = ({ children, onClick, onFocus, onMouseOver }: Props) => (
