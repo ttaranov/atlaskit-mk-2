f5a850661cf86541beb8477b758ee078fb5dc9fd
Ben Conolly <bconolly@atlassian.com>
2018-03-09 15:23:36 +1100

    update to 0.4.2

--- website/src/pages/Package/CodeSandbox.js
+++ website/src/pages/Package/CodeSandbox.js
@@ -57,7 +57,6 @@ export default class CodeSandbox extends Component<{}, {}> {
       <CodeSandboxer
         examplePath={getExamplePath(groupId, packageId, example.id)}
         pkgJSON={pkgJSON}
-        preload
         gitInfo={{
           account: 'atlassian',
           repository: 'atlaskit-mk-2',
