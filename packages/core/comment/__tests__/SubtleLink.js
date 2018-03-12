bab0fc0599b2b07b5edbd7cf88c5ebd8293ad13c
Pete Gleeson <pgleeson@atlassian.com>
2018-03-12 17:58:10 +1100

    AK-4365 converts snapshot test to enzyme test

--- packages/core/comment/__tests__/SubtleLink.js
+++ packages/core/comment/__tests__/SubtleLink.js
@@ -1,17 +1,24 @@
 // @flow
 import React from 'react';
 import { shallow } from 'enzyme';
+import Button from '@atlaskit/button';
+import { name, version } from '../package.json';
 import SubtleLink from '../src/components/SubtleLink';
 
 describe('@atlaskit comments', () => {
   describe('SubtleLink', () => {
-    it('should render a span and a button', () => {
+    it('should override analytics context of button', () => {
       const wrapper = shallow(
         <SubtleLink analyticsContext={{ component: 'comment-action' }}>
           Like
         </SubtleLink>,
       ).dive();
-      expect(wrapper).toMatchSnapshot();
+      const { analyticsContext } = wrapper.find(Button).props();
+      expect(analyticsContext).toEqual({
+        component: 'comment-action',
+        package: name,
+        version,
+      });
     });
   });
 });
