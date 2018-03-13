f1fc49b8825fbc5b5ff3ecd51375dea7c1b1a291
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-03-13 18:27:50 +1100

    Ensure we dynamically load the EditorView

--- packages/media/media-picker/src/popup/components/views/editor/editorViewLoader.ts
+++ packages/media/media-picker/src/popup/components/views/editor/editorViewLoader.ts
@@ -1,7 +1,6 @@
 import { ComponentClass } from 'react';
 
-export default (): Promise<ComponentClass<any>> => {
-  return import(/* webpackChunkName:"@atlaskit-internal_media-editor-view" */
+export default (): Promise<ComponentClass<any>> =>
+  import(/* webpackChunkName:"@atlaskit-internal_media-editor-view" */
 
   './editorView/editorView').then(module => module.EditorView);
-};
