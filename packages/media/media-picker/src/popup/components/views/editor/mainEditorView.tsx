9133f96dd3b3b0ed98901e0f501fb9182d30080e
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-03-13 16:10:29 +1100

    Remove comment

--- packages/media/media-picker/src/popup/components/views/editor/mainEditorView.tsx
+++ packages/media/media-picker/src/popup/components/views/editor/mainEditorView.tsx
@@ -11,8 +11,6 @@ import { MainContainer } from './styles';
 import { editorClose } from '../../../actions/editorClose';
 import { editorShowError } from '../../../actions/editorShowError';
 import { editorShowImage } from '../../../actions/editorShowImage';
-// TODO [MSW-421]: re-enable code splitting once Stride support is merged
-// import { EditorView } from './editorView/editorView';
 import editorViewLoader from './editorViewLoader';
 export interface MainEditorViewStateProps {
   readonly editorData?: EditorData;
