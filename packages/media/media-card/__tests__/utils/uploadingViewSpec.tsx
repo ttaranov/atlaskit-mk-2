0ef0a54c4d9fc96d32df05109c75769503e19b1a
Jimmy Luong <jluong@atlassian.com>
2018-03-13 12:37:52 +1100

    test(media-card): remove invalid specs for uploading view

--- packages/media/media-card/__tests__/utils/uploadingViewSpec.tsx
+++ packages/media/media-card/__tests__/utils/uploadingViewSpec.tsx
@@ -3,32 +3,18 @@ import { shallow } from 'enzyme';
 
 import { UploadingView } from '../../src/utils/uploadingView';
 import { MediaImage, CardActionsView } from '../../src/utils';
-import { CardActionType } from '../../src/actions';
 
 describe('UploadingView', () => {
-  it('should not render the cancel action when deleteAction is not provided', () => {
-    const card = shallow(<UploadingView progress={0} />);
-    expect(card.find(CardActionsView)).toHaveLength(0);
-  });
+  it('should render card actions when some action is provided', () => {
+    const actions = [
+      {
+        label: 'Delete',
+        handler: () => {},
+      },
+    ];
+    const card = shallow(<UploadingView progress={0} actions={actions} />);
 
-  it('should not render the cancel action when an action that is not type delete is provided', () => {
-    const randomAction = { label: 'Close', type: undefined, handler: () => {} };
-    const card = shallow(
-      <UploadingView progress={0} actions={[randomAction]} />,
-    );
-    expect(card.find(CardActionsView)).toHaveLength(0);
-  });
-
-  it('should render the cancel action when a deleteAction is provided', () => {
-    const deleteAction = {
-      label: 'Delete',
-      type: CardActionType.delete,
-      handler: () => {},
-    };
-    const card = shallow(
-      <UploadingView progress={0} actions={[deleteAction]} />,
-    );
-    expect(card.find(CardActionsView)).toHaveLength(1);
+    expect(card.find(CardActionsView).prop('actions')).toEqual(actions);
   });
 
   it('should not render the image when dataURI is not provided', () => {
