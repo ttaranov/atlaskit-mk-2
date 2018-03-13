a4a4204326b5d19a05118e609fef6b89e8e444a3
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-03-13 19:22:33 +1100

    Update jest mock comment

--- packages/core/breadcrumbs/__tests__/analytics.js
+++ packages/core/breadcrumbs/__tests__/analytics.js
@@ -10,6 +10,9 @@ import {
 } from '../package.json';
 import '../src/components/BreadcrumbsStateless';
 
+// This is a global mock for this file that will mock all components wrapped with analytics
+// and replace them with an empty SFC that returns null. This includes components imported
+// directly in this file and others imported as dependencies of those imports.
 jest.mock('@atlaskit/analytics-next', () => ({
   withAnalyticsEvents: jest.fn(() => jest.fn(() => () => null)),
   withAnalyticsContext: jest.fn(() => jest.fn(() => () => null)),
