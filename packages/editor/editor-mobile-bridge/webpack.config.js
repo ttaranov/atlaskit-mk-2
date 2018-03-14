83af1a4c6db4ee56cfc5fd9bd7d2dc4347f21d37
Stanislav Sysoev <ssysoev@atlassian.com>
2018-03-14 16:03:07 +1100

    CHANGESET: Small changes in build process for editor-mobile-bridge
    Summary: Small changes in build process for editor-mobile-bridge
    
    Release notes: <none>
    
    Releases: @atlaskit/editor-mobile-bridge@patch
    
    Dependents: []
    
    ---
    {"summary":"Small changes in build process for editor-mobile-bridge","releases":[{"name":"@atlaskit/editor-mobile-bridge","type":"patch"}],"dependents":[]}
    ---

--- packages/editor/editor-mobile-bridge/webpack.config.js
+++ packages/editor/editor-mobile-bridge/webpack.config.js
@@ -1,14 +1,18 @@
 // @flow
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
+  entry: {
+    bundle: './src/index.tsx',
+  },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist', 'bundle'),
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js', '.jsx', '.json'],
+    mainFields: ['atlaskit:src', 'browser', 'main'],
+    extensions: ['.js', '.ts', '.tsx'],
   },
   module: {
     rules: [
@@ -17,11 +21,26 @@ module.exports = {
         loader: 'json-loader',
       },
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          cacheDirectory: true,
+        },
+      },
+      {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        loader: 'ts-loader',
-        options: { transpileOnly: true },
+        loader: require.resolve('ts-loader'),
+        options: {
+          transpileOnly: true,
+        },
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html.ejs'),
+    }),
+  ],
 };
