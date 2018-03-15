67abe7352a1e81646934f2acb8046505ebcc9772
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-03-15 11:37:28 +1100

    Fix controlled examples

--- packages/core/datetime-picker/examples/00-basic.js
+++ packages/core/datetime-picker/examples/00-basic.js
@@ -1,10 +1,45 @@
 // @flow
 
-import React from 'react';
+import React, { Component, type Node } from 'react';
 import { Label } from '@atlaskit/field-base';
 import { DatePicker, DateTimePicker, TimePicker } from '../src';
 
+type Props = {
+  initialValue: string,
+  children: ({ value: string, onChange: (value: string) => void }) => Node,
+};
+
+type State = {
+  value: string,
+};
+
+class Controlled extends Component<Props, State> {
+  state: State;
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      value: props.initialValue || '',
+    };
+  }
+
+  onChange = (value: string) => {
+    console.log(value);
+    this.setState({ value });
+  };
+
+  render() {
+    return this.props.children({
+      value: this.state.value,
+      onChange: this.onChange,
+    });
+  }
+}
+
 export default () => {
+  let datePickerValue = '2018-01-02';
+  let timePickerValue;
+  let datetimePickerValue;
   return (
     <div>
       <h3>Date picker</h3>
@@ -14,7 +49,11 @@ export default () => {
         htmlFor="react-select-datepicker-2--input"
         label="controlled (value)"
       />
-      <DatePicker id="datepicker-2" value="2018-01-02" />
+      <Controlled initialValue="2018-01-02">
+        {({ value, onChange }) => (
+          <DatePicker id="datepicker-2" value={value} onChange={onChange} />
+        )}
+      </Controlled>
       <Label
         htmlFor="react-select-datepicker-3--input"
         label="uncontrolled (defaultValue)"
@@ -28,7 +67,11 @@ export default () => {
         htmlFor="react-select-timepicker-2--input"
         label="controlled (value)"
       />
-      <TimePicker id="timepicker-2" value="14:30" />
+      <Controlled initialValue="14:30">
+        {({ value, onChange }) => (
+          <TimePicker id="timepicker-2" value={value} onChange={onChange} />
+        )}
+      </Controlled>
       <Label
         htmlFor="react-select-timepicker-3--input"
         label="uncontrolled (defaultValue)"
@@ -42,7 +85,15 @@ export default () => {
         htmlFor="react-select-datetimepicker-2--input"
         label="controlled (UTC-08:00)"
       />
-      <DateTimePicker id="datetimepicker-2" value="2018-01-02T14:30-08:00" />
+      <Controlled initialValue="2018-01-02T14:30-08:00">
+        {({ value, onChange }) => (
+          <DateTimePicker
+            id="datetimepicker-2"
+            value={value}
+            onChange={onChange}
+          />
+        )}
+      </Controlled>
       <Label
         htmlFor="react-select-datetimepicker-3--input"
         label="uncontrolled (UTC+10:00)"
