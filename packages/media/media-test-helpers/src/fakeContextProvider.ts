ca6751a0130c87ac6bf600cd08ea161ea3d81c57
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-03-20 14:23:21 -0700

    Remove Sinon from fakeContextProvider

--- packages/media/media-test-helpers/src/fakeContextProvider.ts
+++ packages/media/media-test-helpers/src/fakeContextProvider.ts
@@ -1,40 +1,39 @@
-import * as sinon from 'sinon';
 import { Observable } from 'rxjs';
 import { Context } from '@atlaskit/media-core';
 
 export const fakeContext = (stubbedContext = {}): Context => {
-  const getMediaItemProvider = sinon
-    .stub()
-    .returns({ observable: sinon.stub().returns(Observable.of('nothing')) });
-
-  const getMediaCollectionProvider = sinon
-    .stub()
-    .returns({ observable: sinon.stub().returns(Observable.of('nothing')) });
-
-  const getDataUriService = sinon.stub().returns({
-    fetchOriginalDataUri: sinon
-      .stub()
-      .returns(Promise.resolve('fake-original-data-uri')),
-    fetchImageDataUri: sinon
-      .stub()
-      .returns(Promise.resolve('fake-image-data-uri')),
+  const returns = value => jest.fn().mockReturnValue(value);
+  const getMediaItemProvider = returns({
+    observable: returns(Observable.of('nothing')),
   });
-
-  const addLinkItem = sinon
-    .stub()
-    .returns({ observable: sinon.stub().returns(Observable.of('nothing')) });
-
-  const getUrlPreviewProvider = sinon
-    .stub()
-    .returns({ observable: sinon.stub().returns(Observable.of('nothing')) });
+  const getMediaCollectionProvider = returns({
+    observable: returns(Observable.of('nothing')),
+  });
+  const getDataUriService = returns({
+    fetchOriginalDataUri: returns(Promise.resolve('fake-original-data-uri')),
+    fetchImageDataUri: returns(Promise.resolve('fake-image-data-uri')),
+  });
+  const addLinkItem = returns({
+    observable: returns(Observable.of('nothing')),
+  });
+  const getUrlPreviewProvider = returns({
+    observable: returns(Observable.of('nothing')),
+  });
+  const getLocalPreview = jest.fn();
+  const setLocalPreview = jest.fn();
+  const removeLocalPreview = jest.fn();
+  const refreshCollection = jest.fn();
 
   const defaultContext: Context = {
+    getLocalPreview,
+    setLocalPreview,
+    removeLocalPreview,
     getMediaItemProvider,
     getMediaCollectionProvider,
     getDataUriService,
     addLinkItem,
     getUrlPreviewProvider,
-    refreshCollection: sinon.spy(),
+    refreshCollection,
     config: {
       serviceHost: 'some-service-host',
       authProvider: () =>
@@ -47,9 +46,7 @@ export const fakeContext = (stubbedContext = {}): Context => {
 
   const wrappedStubbedContext = {};
   Object.keys(stubbedContext).forEach(methodName => {
-    wrappedStubbedContext[methodName] = sinon
-      .stub()
-      .returns(stubbedContext[methodName]);
+    wrappedStubbedContext[methodName] = returns(stubbedContext[methodName]);
   });
 
   return {
