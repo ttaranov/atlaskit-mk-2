372bf15dd4136e380c7d9c4de3b64de710d0cb23
Martin Doms <mdoms@atlassian.com>
2018-03-22 09:33:56 +1100

    Test cleanup, added test case

--- packages/editor/editor-jira-transformer/__tests__/emoji.ts
+++ packages/editor/editor-jira-transformer/__tests__/emoji.ts
@@ -5,43 +5,50 @@ import { createJIRASchema } from '@atlaskit/editor-common';
 
 describe('emojiHelper', () => {
   it(`maps correctly formed image tags`, () => {
-    let imageElement = new Image();
+    const imageElement = new Image();
     imageElement.src = '/images/icons/emoticons/smile.png';
-    let result = mapImageToEmoji(imageElement);
+    const result = mapImageToEmoji(imageElement);
     expect(result).toEqual('🙂');
   });
 
   it(`maps image tags with unqualified image url`, () => {
-    let imageElement = new Image();
+    const imageElement = new Image();
     imageElement.src = 'smile.png';
-    let result = mapImageToEmoji(imageElement);
+    const result = mapImageToEmoji(imageElement);
     expect(result).toEqual('🙂');
   });
 
   it(`maps image tags with preceding slash`, () => {
-    let imageElement = new Image();
+    const imageElement = new Image();
     imageElement.src = '/smile.png';
-    let result = mapImageToEmoji(imageElement);
+    const result = mapImageToEmoji(imageElement);
     expect(result).toEqual('🙂');
   });
 
   it(`does not map image tags without src`, () => {
-    let imageElement = new Image();
-    let result = mapImageToEmoji(imageElement);
+    const imageElement = new Image();
+    const result = mapImageToEmoji(imageElement);
     expect(result).toBeNull();
   });
 
   it(`does not map image tags with no filename`, () => {
-    let imageElement = new Image();
+    const imageElement = new Image();
     imageElement.src = '/images/icons/emoticons/';
-    let result = mapImageToEmoji(imageElement);
+    const result = mapImageToEmoji(imageElement);
     expect(result).toBeNull();
   });
 
   it(`does not map image tags with file name that does not correspond to an emoji`, () => {
-    let imageElement = new Image();
+    const imageElement = new Image();
     imageElement.src = '/images/icons/emoticons/invalidemoji.png';
-    let result = mapImageToEmoji(imageElement);
+    const result = mapImageToEmoji(imageElement);
+    expect(result).toBeNull();
+  });
+
+  it(`does not map image tags with empty src`, () => {
+    const imageElement = new Image();
+    imageElement.src = '';
+    const result = mapImageToEmoji(imageElement);
     expect(result).toBeNull();
   });
 });
