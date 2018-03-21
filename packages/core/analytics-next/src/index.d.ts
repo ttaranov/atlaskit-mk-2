306db63499cc29314f343e5c5eb797e7f21f615b
Aleksandr Sasha Motsjonov <amotsjonov@atlassian.com>
2018-03-21 16:41:23 +1100

    There needs to be explicitly defined member that TS code could use as part of it’s own public API

--- packages/core/analytics-next/src/index.d.ts
+++ packages/core/analytics-next/src/index.d.ts
@@ -1,3 +1,5 @@
 // This deliberately empty typing file.
 // There are typings defined in `/typings` folder for internal (AK) usage only.
 declare module '@atlaskit/analytics-next';
+// These are the only members needs to be exported since they might be used as external types by TS code
+export type UIAnalyticsEventInterface = any;
