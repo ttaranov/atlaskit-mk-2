9372d156127ef55046a981e7bef57005cf98ed2b
Pete Gleeson <pgleeson@atlassian.com>
2018-03-21 11:18:56 +1100

    AK-4478 fixes problem in custom label example

--- packages/core/pagination/examples/03-custom-labels.js
+++ packages/core/pagination/examples/03-custom-labels.js
@@ -5,8 +5,8 @@ import Pagination from '../src';
 
 export default () => (
   <Pagination
-    value={10}
     defaultValue={4}
+    total={10}
     onChange={e => console.log('page changed', e)}
     i18n={{ prev: '← Пред', next: 'След →' }}
   />
