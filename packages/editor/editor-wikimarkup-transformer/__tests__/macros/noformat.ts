2815fbe52ba21690ec2f84e15837373637ba5ae9
Jarred Mack <jmack2@.atlassian.com>
2018-03-22 10:58:56 +1100

    Add color, panel, quote, text tests

---
+++ packages/editor/editor-wikimarkup-transformer/__tests__/macros/noformat.ts
@@ -0,0 +1,18 @@
+import { code, doc, p } from '@atlaskit/editor-test-helpers';
+import { checkParseEncodeRoundTrips } from '../_test-helpers';
+import { defaultSchema } from '@atlaskit/editor-common';
+
+// Nodes
+
+describe.skip('WikiMarkup Transformer', () => {
+  describe('', () => {
+    const WIKI_NOTATION = ``;
+
+    checkParseEncodeRoundTrips(
+      WIKI_NOTATION,
+      defaultSchema,
+      WIKI_NOTATION,
+      doc(),
+    );
+  });
+});
