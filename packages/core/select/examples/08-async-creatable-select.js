8f62f9f72857c8661bff830941056987c551c2af
Charles Lee <clee@atlassian.com>
2018-03-23 10:57:31 +1100

    update async creatable select example to be more robust

--- packages/core/select/examples/08-async-creatable-select.js
+++ packages/core/select/examples/08-async-creatable-select.js
@@ -6,20 +6,15 @@ import { AsyncCreatableSelect as AsyncCreatable } from '../src';
 
 import { cities } from './common/data';
 
-// you control how the options are filtered
-const filter = (inputValue: string) =>
-  cities.filter(i => i.label.toLowerCase().includes(inputValue.toLowerCase()));
-
-// async load function using callback (promises also supported)
-const loadOptions = (inputValue, callback) => {
-  setTimeout(() => {
-    callback(filter(inputValue));
-  }, 1000);
-};
-
 type State = {
   allowCreateWhileLoading: boolean,
+  options: Array<*>,
 };
+
+const createOption = (inputValue: string) => ({
+  label: inputValue,
+  value: inputValue.toLowerCase().replace(/\W/g, ''),
+});
 
 export default class AsyncCreatableExample extends Component<*, State> {
   state = {
@@ -27,10 +22,23 @@ export default class AsyncCreatableExample extends Component<*, State> {
     options: cities,
   };
   handleCreateOption = inputValue => {
-    this.setState(state => ({
-      ...state,
-      options: [inputValue, ...state.options],
-    }));
+    console.log('handleCreateOption here');
+    this.setState({
+      options: [createOption(inputValue), ...this.state.options],
+    });
+  };
+  // you control how the options are filtered
+  filterOptions = inputValue => {
+    return this.state.options.filter(option =>
+      option.label.toLowerCase().includes(inputValue.toLowerCase()),
+    );
+  };
+
+  // async load function using callback (promises also supported)
+  loadOptions = (inputValue, callback) => {
+    setTimeout(() => {
+      callback(this.filterOptions(inputValue));
+    }, 1000);
   };
   toggleValue = event => {
     this.setState(state => ({ ...state, [event.value]: !state[event.value] }));
@@ -40,10 +48,8 @@ export default class AsyncCreatableExample extends Component<*, State> {
     return (
       <Fragment>
         <AsyncCreatable
-          defaultOptions
-          loadOptions={loadOptions}
+          loadOptions={this.loadOptions}
           allowCreateWhileLoading={allowCreateWhileLoading}
-          options={cities}
           onCreateOption={this.handleCreateOption}
           placeholder="Choose a City"
         />
