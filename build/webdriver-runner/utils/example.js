d42a0c52d79a5febd3a3832485968a63d50643d1
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-03-23 15:12:46 -0700

    Expose baseUrl and getExampleUrl from webdriver-runner package

---
+++ build/webdriver-runner/utils/example.js
@@ -0,0 +1,11 @@
+// @flow
+
+const baseUrl = 'http://localhost:9000';
+
+const getExampleUrl = (group, packageName, exampleName) =>
+  `${baseUrl}/examples.html?groupId=${group}&packageId=${packageName}&exampleId=${exampleName}`;
+
+module.exports = {
+  baseUrl,
+  getExampleUrl,
+};
