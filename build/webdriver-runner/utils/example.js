398c6b58b5a3a35ceedc82964d694d03b569b087
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-03-26 11:26:29 +1100

    Add missing flow annotations for getExampleUrl

--- build/webdriver-runner/utils/example.js
+++ build/webdriver-runner/utils/example.js
@@ -2,7 +2,11 @@
 
 const baseUrl = 'http://localhost:9000';
 
-const getExampleUrl = (group, packageName, exampleName = '') =>
+const getExampleUrl = (
+  group: string,
+  packageName: string,
+  exampleName: string = '',
+) =>
   `${baseUrl}/examples.html?groupId=${group}&packageId=${packageName}&exampleId=${exampleName}`;
 
 module.exports = {
