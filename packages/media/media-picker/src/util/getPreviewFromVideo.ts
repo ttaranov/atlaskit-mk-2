d27c68acc22bf84b4607bc55d6e689f1bcf10b64
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-04-04 15:07:51 +1000

    terminate snapshoter after taking video snapshot

--- packages/media/media-picker/src/util/getPreviewFromVideo.ts
+++ packages/media/media-picker/src/util/getPreviewFromVideo.ts
@@ -11,7 +11,7 @@ export const getPreviewFromVideo = (file: File): Promise<ImagePreview> =>
 
     img.onload = () => {
       const dimensions = { width: img.width, height: img.height };
-
+      snapshoter.end();
       resolve({ src, dimensions });
     };
 
