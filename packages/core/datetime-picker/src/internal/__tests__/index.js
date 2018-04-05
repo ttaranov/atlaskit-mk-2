88df7cf27f8505ef6f88255d33bf183f7cfe23f4
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-04-05 12:11:17 +1000

    Refactor tests to not rely on timezone

--- packages/core/datetime-picker/src/internal/__tests__/index.js
+++ packages/core/datetime-picker/src/internal/__tests__/index.js
@@ -2,6 +2,7 @@
 
 import { mount } from 'enzyme';
 import React from 'react';
+import { parse, format } from 'date-fns';
 
 import {
   ClearIndicator,
@@ -23,40 +24,43 @@ test('DropdownIndicator', () => {
   expect(mount(<DropdownIndicator icon="asdf" />)).toMatchSnapshot();
 });
 
+// Convert our test values to the local timezone and use those as expected values so that
+// test results don't rely on a specific timezone
+const testISODate = parse('2018-04-12T09:30+1000');
+const testValue = format(testISODate);
+const testDateValue = format(testISODate, 'YYYY-MM-DD');
+const testTimeValue = format(testISODate, 'HH:mm');
+const testZoneValue = format(testISODate, 'ZZ');
+
 test('parseDateIntoStateValues', () => {
-  expect(parseDateIntoStateValues('', '2018-04-12', '', '')).toEqual({
-    dateValue: '2018-04-12',
+  expect(parseDateIntoStateValues('', testDateValue, '', '')).toEqual({
+    dateValue: testDateValue,
     timeValue: '',
     zoneValue: '',
   });
-  expect(parseDateIntoStateValues('', '', '09:30', '')).toEqual({
+  expect(parseDateIntoStateValues('', '', testTimeValue, '')).toEqual({
     dateValue: '',
-    timeValue: '09:30',
+    timeValue: testTimeValue,
     zoneValue: '',
   });
-  expect(parseDateIntoStateValues('', '2018-04-12', '09:30', '')).toEqual({
-    dateValue: '2018-04-12',
-    timeValue: '09:30',
-    zoneValue: '',
-  });
-  expect(parseDateIntoStateValues('2018-04-12T09:30+1000', '', '', '')).toEqual(
-    {
-      dateValue: '2018-04-12',
-      timeValue: '09:30',
-      zoneValue: '+1000',
-    },
-  );
   expect(
-    parseDateIntoStateValues(
-      '2018-04-12T09:30+1000',
-      '2017-05-03',
-      '14:00',
-      '-0500',
-    ),
+    parseDateIntoStateValues('', testDateValue, testTimeValue, ''),
   ).toEqual({
-    dateValue: '2018-04-12',
-    timeValue: '09:30',
-    zoneValue: '+1000',
+    dateValue: testDateValue,
+    timeValue: testTimeValue,
+    zoneValue: '',
+  });
+  expect(parseDateIntoStateValues(testValue, '', '', '')).toEqual({
+    dateValue: testDateValue,
+    timeValue: testTimeValue,
+    zoneValue: testZoneValue,
+  });
+  expect(
+    parseDateIntoStateValues(testISODate, '2017-05-03', '14:00', '-0500'),
+  ).toEqual({
+    dateValue: testDateValue,
+    timeValue: testTimeValue,
+    zoneValue: testZoneValue,
   });
   expect(parseDateIntoStateValues('', '', '', '')).toEqual({
     dateValue: '',
