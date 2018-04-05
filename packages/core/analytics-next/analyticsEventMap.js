09a05b08612486edd08e5192fb3ba8ec46875a45
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-04-06 09:22:55 +1000

    Cleanup analyticsEventMap and add missing mount tests

--- packages/core/analytics-next/analyticsEventMap.js
+++ packages/core/analytics-next/analyticsEventMap.js
@@ -11,18 +11,13 @@
 type AnalyticsEventConfig = {
   /** Path to component being wrapped with analytics */
   path: string,
-  /** Path to analytics test file that will be created and/or already exists */
-  testPath: string,
   /** The 'component' context value that will be exposed via analytics context */
   context: string,
   /** Any components that derive from the base component that will therefore have analytics
    * as well. E.g. any stateful version of a stateless component.
    */
   derivatives?: string[],
-  /** The name of the component used in the component test file. This is also used
-   * as the name of the base (unwrapped) component export in the component file path.
-   * This name should be consistent, some names were manually updated so that they aligned.
-   */
+  /** The name of the component being wrapped. */
   component: string,
   /** A map of prop callbacks that will be instrumented with analytics.
    *  The key represents the prop callback name and the value represents the 'action'
@@ -31,51 +26,35 @@ type AnalyticsEventConfig = {
   props: {
     [propName: string]: string | string[],
   },
-  /** Path to the components existing test file so that we can add mount tests to it */
-  componentTestPath?: string,
-  /** Signals to the codemod to not override the analytics tests in the component test
-   *  file as some manual work has been done that cannot be automated.
-   */
-  manualComponentTestOverride?: boolean,
-  /** Signals that this map entry is for test purposes and should not be part of other exports */
-  test?: true,
-  ignore?: boolean,
 };
 const analyticsEventMap: AnalyticsEventConfig[] = [
   {
     path: 'avatar/src/components/Avatar.js',
-    testPath: 'avatar/src/components/__tests__/analytics.js',
     context: 'avatar',
     component: 'Avatar',
     props: {
       onClick: 'click',
     },
-    componentTestPath: 'avatar/src/components/__tests__/Avatar.js',
   },
   {
     path: 'blanket/src/Blanket.js',
-    testPath: 'blanket/src/__tests__/analytics.js',
     context: 'blanket',
     component: 'Blanket',
     props: {
       onBlanketClicked: 'click',
     },
-    componentTestPath: 'blanket/src/__tests__/blanket.js',
   },
   {
     path: 'breadcrumbs/src/components/BreadcrumbsStateless.js',
-    testPath: 'breadcrumbs/__tests__/analytics.js',
     context: 'breadcrumbs',
     component: 'BreadcrumbsStateless',
     derivatives: ['Breadcrumbs'],
     props: {
       onExpand: 'expand',
     },
-    componentTestPath: 'breadcrumbs/__tests__/Breadcrumbs.test.js',
   },
   {
     path: 'breadcrumbs/src/components/BreadcrumbsItem.js',
-    testPath: 'breadcrumbs/__tests__/analytics-item.js',
     context: 'breadcrumbs-item',
     component: 'BreadcrumbsItem',
     overwrite: 'Button',
@@ -83,82 +62,59 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
     props: {
       onClick: 'click',
     },
-    componentTestPath: 'breadcrumbs/__tests__/Item.test.js',
-    manualComponentTestOverride: true,
   },
   {
     path: 'button/src/components/Button.js',
-    testPath: 'button/__tests__/analytics.js',
     context: 'button',
     component: 'Button',
     props: {
       onClick: 'click',
     },
-    componentTestPath: 'button/src/__tests__/testDefaultBehaviour.js',
   },
   {
     path: 'calendar/src/components/Calendar.js',
-    testPath: 'calendar/src/components/__tests__/analytics.js',
     context: 'calendar',
     component: 'Calendar',
     props: {
       onChange: 'change',
       onSelect: 'select',
     },
-    componentTestPath: 'calendar/src/components/__tests__/Calendar.js',
-    refIssue: true,
-    rerun: true,
   },
   {
     path: 'checkbox/src/CheckboxStateless.js',
-    testPath: 'checkbox/src/__tests__/analytics.js',
     context: 'checkbox',
     component: 'CheckboxStateless',
     derivatives: ['Checkbox'],
-    wrapTarget: 'CheckboxWithTheme',
     props: {
       onChange: 'change',
     },
-    componentTestPath: 'checkbox/src/__tests__/index.js',
-    manualComponentTestOverride: true,
   },
   {
     path: 'datetime-picker/src/components/DatePicker.js',
-    testPath:
-      'datetime-picker/src/components/__tests__/analytics-datepicker.js',
     context: 'date-picker',
     component: 'DatePicker',
     props: {
       onChange: 'change',
     },
-    componentTestPath: 'datetime-picker/src/components/__tests__/DatePicker.js',
   },
   {
     path: 'datetime-picker/src/components/TimePicker.js',
-    testPath:
-      'datetime-picker/src/components/__tests__/analytics-timepicker.js',
     context: 'time-picker',
     component: 'TimePicker',
     props: {
       onChange: 'change',
     },
-    componentTestPath: 'datetime-picker/src/components/__tests__/TimePicker.js',
   },
   {
     path: 'datetime-picker/src/components/DateTimePicker.js',
-    testPath:
-      'datetime-picker/src/components/__tests__/analytics-datetimepicker.js',
     context: 'date-picker',
     component: 'DateTimePicker',
     props: {
       onChange: 'change',
     },
-    componentTestPath:
-      'datetime-picker/src/components/__tests__/DateTimePicker.js',
   },
   {
     path: 'dropdown-menu/src/components/DropdownMenuStateless.js',
-    testPath: 'dropdown-menu/__tests__/analytics.js',
     context: 'dropdown-menu',
     component: 'DropdownMenuStateless',
     derivatives: ['DropdownMenu'],
@@ -167,32 +123,25 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
     props: {
       onOpenChange: 'toggle',
     },
-    componentTestPath: 'dropdown-menu/__tests__/DropdownMenuStateless.js',
   },
   {
     path: 'droplist/src/components/Droplist.js',
-    testPath: 'droplist/src/__tests__/analytics.js',
     context: 'droplist',
     component: 'Droplist',
     props: {
       onOpenChange: 'toggle',
     },
-    componentTestPath: 'droplist/src/__tests__/index.js',
   },
   {
     path: 'droplist/src/components/Item.js',
-    testPath: 'droplist/src/__tests__/analytics-item.js',
     context: 'droplist-item',
     component: 'DroplistItem',
     props: {
       onActivate: 'activate',
     },
-    componentTestPath: 'droplist/src/__tests__/index.js',
-    manualComponentTestOverride: true,
   },
   {
     path: 'dynamic-table/src/components/Stateless.js',
-    testPath: 'dynamic-table/__tests__/analytics.js',
     context: 'dynamic-table',
     component: 'DynamicTable',
     props: {
@@ -201,11 +150,9 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onRankStart: 'rankStart',
       onRankEnd: 'rankEnd',
     },
-    componentTestPath: 'dynamic-table/__tests__/Stateless.js',
   },
   {
     path: 'field-base/src/components/FieldBaseStateless.js',
-    testPath: 'field-base/src/__tests__/analytics.js',
     context: 'field-base',
     component: 'FieldBaseStateless',
     derivatives: ['FieldBase'],
@@ -216,45 +163,34 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onDialogFocus: 'focus',
       onFocus: 'focus',
     },
-    componentTestPath: 'field-base/src/__tests__/index.js',
   },
   {
     path: 'field-radio-group/src/Radio.js',
-    testPath: 'field-radio-group/src/__tests__/analytics-radio.js',
     context: 'field-radio-group',
     component: 'AkRadio',
-    wrapTarget: 'RadioWithTheme',
     props: {
       onChange: 'change',
     },
-    componentTestPath: 'field-radio-group/src/__tests__/Radio.js',
-    manualComponentTestOverride: true,
   },
   {
     path: 'field-radio-group/src/RadioGroupStateless.js',
-    testPath: 'field-radio-group/src/__tests__/analytics-radio-group.js',
     context: 'field-radio-group',
     component: 'AkFieldRadioGroup',
     derivatives: ['RadioGroup'],
     props: {
       onRadioChange: 'change',
     },
-    componentTestPath: 'field-radio-group/src/__tests__/RadioGroup.js',
   },
   {
     path: 'field-range/src/FieldRange.js',
-    testPath: 'field-range/src/__tests__/analytics.js',
     context: 'field-range',
     component: 'FieldRange',
     props: {
       onChange: 'change',
     },
-    componentTestPath: 'field-range/src/__tests__/fieldRangeSpec.js',
-    needsMountTest: true,
   },
   {
     path: 'field-text/src/FieldTextStateless.js',
-    testPath: 'field-text/src/__tests__/analytics.js',
     context: 'field-text',
     component: 'FieldTextStateless',
     derivatives: ['FieldText'],
@@ -266,22 +202,18 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onKeyPress: 'keypress',
       onKeyUp: 'keyup',
     },
-    componentTestPath: 'field-text/src/__tests__/index.js',
   },
   {
     path: 'field-text-area/src/FieldTextAreaStateless.js',
-    testPath: 'field-text-area/src/__tests__/analytics.js',
     context: 'field-text-area',
     component: 'FieldTextAreaStateless',
     derivatives: ['FieldTextArea'],
     props: {
       onChange: 'change',
     },
-    componentTestPath: 'field-text-area/src/__tests__/index.js',
   },
   {
     path: 'flag/src/components/Flag/index.js',
-    testPath: 'flag/__tests__/analytics.js',
     context: 'flag',
     component: 'Flag',
     props: {
@@ -291,23 +223,17 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onMouseOut: 'mouseout',
       onMouseOver: 'mouseover',
     },
-    componentTestPath: 'flag/__tests__/Flag.js',
-    manualComponentTestOverride: true,
   },
   {
     path: 'icon/src/components/Icon.js',
-    testPath: 'icon/__tests__/analytics.js',
     context: 'icon',
     component: 'Icon',
     props: {
       onClick: 'click',
     },
-    componentTestPath: 'icon/__tests__/IconSpec.js',
-    manualComponentTestOverride: true,
   },
   {
     path: 'inline-dialog/src/InlineDialog/index.js',
-    testPath: 'inline-dialog/__tests__/analytics.js',
     context: 'inline-dialog',
     component: 'InlineDialog',
     props: {
@@ -316,11 +242,9 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onContentFocus: 'focus',
       onClose: 'close',
     },
-    componentTestPath: 'inline-dialog/__tests__/index.js',
   },
   {
     path: 'inline-edit/src/InlineEditStateless.js',
-    testPath: 'inline-edit/src/__tests__/analytics.js',
     context: 'inline-edit',
     component: 'InlineEditStateless',
     derivatives: ['InlineEdit'],
@@ -329,23 +253,18 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onConfirm: 'confirm',
       onEditRequested: 'edit',
     },
-    componentTestPath: 'inline-edit/src/__tests__/InlineEdit.js',
   },
   {
     path: 'input/src/SingleLineTextInput.js',
-    testPath: 'input/src/__tests__/analytics.js',
     context: 'input',
     component: 'SingleLineTextInput',
     props: {
       onConfirm: 'confirm',
       onKeyDown: 'keydown',
     },
-    componentTestPath: 'input/src/__tests__/index.js',
-    refIssue: true,
   },
   {
     path: 'item/src/components/Item.js',
-    testPath: 'item/__tests__/analytics.js',
     context: 'item',
     component: 'Item',
     props: {
@@ -354,21 +273,17 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onMouseEnter: 'mouseenter',
       onMouseLeave: 'mouseleave',
     },
-    componentTestPath: 'item/__tests__/Item.js',
   },
   {
     path: 'modal-dialog/src/components/Modal.js',
-    testPath: 'modal-dialog/__tests__/analytics.js',
     context: 'modal-dialog',
     component: 'ModalDialog',
     props: {
       onClose: 'close',
     },
-    componentTestPath: 'modal-dialog/__tests__/modalDialog.js',
   },
   {
     path: 'multi-select/src/components/Stateless.js',
-    testPath: 'multi-select/__tests__/analytics.js',
     context: 'multi-select',
     component: 'MultiSelectStateless',
     props: {
@@ -378,12 +293,10 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onRemoved: 'removed',
       onOpenChange: 'toggle',
     },
-    componentTestPath: 'multi-select/__tests__/statelessBehaviour.js',
   },
 
   {
     path: 'multi-select/src/components/Stateful.js',
-    testPath: 'multi-select/__tests__/analytics.js',
     context: 'multi-select',
     component: 'MultiSelect',
     props: {
@@ -392,12 +305,9 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onSelectedChange: ['selected', 'removed'],
       onOpenChange: 'toggle',
     },
-    componentTestPath: 'multi-select/__tests__/stateful.js',
-    ignore: true,
   },
   {
     path: 'navigation/src/components/js/Navigation.js',
-    testPath: 'navigation/__tests__/analytics.js',
     context: 'navigation',
     component: 'Navigation',
     props: {
@@ -406,55 +316,42 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onToggleStart: 'toggle',
       onToggleEnd: 'toggle',
     },
-    componentTestPath: 'navigation/__tests__/Navigation.js',
   },
   {
     path: 'onboarding/src/components/Spotlight.js',
-    testPath: 'onboarding/__tests__/analytics.js',
     context: 'spotlight',
     component: 'Spotlight',
     props: {
       targetOnClick: 'click',
     },
-    componentTestPath: 'onboarding/__tests__/index.js',
-    manualComponentTestOverride: true,
   },
   {
     path: 'pagination/src/components/Pagination.js',
-    testPath: 'pagination/__tests__/analytics.js',
     context: 'pagination',
     component: 'Pagination',
     props: {
       onChange: 'change',
     },
-    componentTestPath: 'pagination/__tests__/index.js',
   },
   {
     path: 'progress-indicator/src/components/Dots.js',
-    testPath: 'progress-indicator/__tests__/analytics.js',
     context: 'progress-indicator',
     component: 'ProgressDots',
     props: {
       onSelect: 'select',
     },
-    componentTestPath: 'progress-indicator/__tests__/index.js',
   },
   {
     path: 'select/src/Select.js',
-    testPath: 'select/src/__tests__/analytics.js',
     context: 'select',
     component: 'Select',
     props: {
       onChange: 'change',
       onKeyDown: 'keydown',
     },
-    componentTestPath: 'select/src/__tests__/Select.js',
-    needsMountTest: true,
-    refIssue: true,
   },
   {
     path: 'single-select/src/components/StatelessSelect.js',
-    testPath: 'single-select/__tests__/analytics.js',
     context: 'single-select',
     component: 'StatelessSelect',
     derivatives: ['SingleSelect'],
@@ -463,55 +360,42 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onSelected: 'change',
       onOpenChange: 'toggle',
     },
-    componentTestPath: 'single-select/__tests__/stateless.js',
   },
   {
     path: 'spinner/src/Spinner/index.js',
-    testPath: 'spinner/src/Spinner/__tests__/analytics.js',
     context: 'spinner',
     component: 'Spinner',
     props: {
       onComplete: 'complete',
     },
-    componentTestPath: 'spinner/src/Spinner/__tests__/index.js',
   },
   {
     path: 'table-tree/src/components/Row.js',
-    testPath: 'table-tree/src/__tests__/analytics.js',
     context: 'table-tree',
     component: 'Row',
     props: {
       onExpand: 'toggle',
       onCollapse: 'toggle',
     },
-    componentTestPath: 'table-tree/src/__tests__/functional.js',
-    manualComponentTestOverride: true,
-    hasExtractReactTypesIssue: true,
   },
   {
     path: 'tabs/src/components/Tabs.js',
-    testPath: 'tabs/__tests__/analytics.js',
     context: 'tabs',
     component: 'Tabs',
     props: {
       onSelect: 'change',
     },
-    componentTestPath: 'tabs/__tests__/index.js',
   },
   {
     path: 'tag/src/Tag/index.js',
-    testPath: 'tag/src/Tag/__tests__/analytics.js',
     context: 'tag',
     component: 'Tag',
     props: {
       onAfterRemoveAction: 'remove',
     },
-    componentTestPath: 'tag/src/Tag/__tests__/index.js',
-    manualComponentTestOverride: true,
   },
   {
     path: 'toggle/src/ToggleStateless.js',
-    testPath: 'toggle/src/__tests__/analytics.js',
     context: 'toggle',
     component: 'ToggleStateless',
     derivatives: ['Toggle'],
@@ -520,55 +404,28 @@ const analyticsEventMap: AnalyticsEventConfig[] = [
       onChange: 'change',
       onFocus: 'focus',
     },
-    componentTestPath: 'toggle/src/__tests__/index.js',
   },
   {
     path: 'tooltip/src/components/Tooltip.js',
-    testPath: 'tooltip/src/components/__tests__/analytics.js',
     context: 'tooltip',
     component: 'Tooltip',
     props: {
       onMouseOver: 'mouseover',
       onMouseOut: 'mouseout',
     },
-    componentTestPath: 'tooltip/src/components/__tests__/Tooltip.js',
-  },
-  {
-    path: '__testfixtures__/addsTestsMultipleProps',
-    testPath: '__testfixtures__/addsTestsMultipleProps',
-    context: 'button',
-    component: 'Button',
-    props: {
-      onClick: 'click',
-      onChange: 'change',
-    },
-    test: true,
-  },
-  {
-    path: '__testfixtures__',
-    testPath: '__testfixtures__',
-    context: 'button',
-    component: 'Button',
-    props: {
-      onClick: 'click',
-    },
-    test: true,
   },
 ];
 
-module.exports.analyticsPackages = analyticsEventMap
-  .filter(pkg => pkg.test !== true)
-  .map(config => {
-    const path = config.path;
+module.exports.analyticsPackages = analyticsEventMap.map(config => {
+  const path = config.path;
 
-    return path.substring(0, path.indexOf('/'));
-  });
+  return path.substring(0, path.indexOf('/'));
+});
 
 module.exports.analyticsEventMap = analyticsEventMap;
 
-module.exports.instrumentedComponents = analyticsEventMap
-  .filter(pkg => pkg.test !== true)
-  .reduce((acc, config) => {
+module.exports.instrumentedComponents = analyticsEventMap.reduce(
+  (acc, config) => {
     const path = config.path;
     const packageSuffix = path.substring(0, path.indexOf('/'));
     const items = [];
@@ -585,4 +442,6 @@ module.exports.instrumentedComponents = analyticsEventMap
       });
     });
     return acc.concat(items);
-  }, []);
+  },
+  [],
+);
---
+++ packages/core/select/src/__tests__/Select.js
@@ -0,0 +1,25 @@
+// @flow
+
+import React from 'react';
+import { mount } from 'enzyme';
+
+import SelectWithAnalytics from '../Select';
+
+describe('SelectWithAnalytics', () => {
+  beforeEach(() => {
+    jest.spyOn(global.console, 'warn');
+    jest.spyOn(global.console, 'error');
+  });
+  afterEach(() => {
+    global.console.warn.mockRestore();
+    global.console.error.mockRestore();
+  });
+
+  it('should mount without errors', () => {
+    mount(<SelectWithAnalytics />);
+    /* eslint-disable no-console */
+    expect(console.warn).not.toHaveBeenCalled();
+    expect(console.error).not.toHaveBeenCalled();
+    /* eslint-enable no-console */
+  });
+});
