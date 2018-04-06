df7d8452ceeb8a8b2cdc6ad22de99315c8711dd7
Charles Lee <clee@atlassian.com>
2018-04-06 10:57:03 +1000

    CHANGESET: @atlaskit/select now exports the createFilter
    Summary: @atlaskit/select now exports the createFilter
    
    Release notes: <none>
    
    Releases: @atlaskit/select@minor
    
    Dependents: @atlaskit/datetime-picker@none
    
    ---
    {"summary":"@atlaskit/select now exports the createFilter","releases":[{"name":"@atlaskit/select","type":"minor"}],"dependents":[{"name":"@atlaskit/datetime-picker","dependencies":["@atlaskit/select"],"type":"none"}]}
    ---

--- packages/core/select/src/index.js
+++ packages/core/select/src/index.js
@@ -1,9 +1,7 @@
 // @flow
-export { components } from 'react-select';
-export { mergeStyles } from 'react-select';
+export { components, createFilter, mergeStyles } from 'react-select';
 
 export { default } from './Select';
-export { createFilter, mergeStyles, components } from 'react-select';
 export { default as AsyncSelect } from './AsyncSelect';
 export { default as CheckboxSelect } from './CheckboxSelect';
 export { default as CountrySelect } from './CountrySelect';
