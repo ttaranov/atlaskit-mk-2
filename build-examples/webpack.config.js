3c9c63dc8a2a0ede4fd60b9c2b2b0aa744501f32
Ajay Narain Mathur <amathur@atlassian.com>
2018-04-06 15:57:13 +1000

    webpack config to create a docs bundle

--- build-examples/webpack.config.js
+++ build-examples/webpack.config.js
@@ -1,7 +1,38 @@
 // @flow
 
 const path = require('path');
-// const bolt = require('bolt');
+const bolt = require('bolt');
+const fs = require('fs');
+
+function ensureDir(filePath) {
+  try {
+    fs.statSync(filePath);
+    return true;
+  } catch (err) {
+    if (err.code !== 'ENOENT') {
+      throw err;
+    }
+  }
+
+  return false;
+}
+
+function getWorkspaces() {
+  return bolt.getWorkspaces({ cwd: path.join(__dirname, '..') });
+}
+
+async function getWorkspacesWithDocs() {
+  const wsMap = {};
+  const workspaces = await getWorkspaces();
+  //console.log(fs.statSync(path.join(__dirname, '../build/atlaskid/docs/0-intro.js')));
+  const workspacesWithDocs = workspaces.filter(workspace =>
+    ensureDir(path.join(workspace.dir, 'docs/0-intro.js')),
+  );
+  workspacesWithDocs.forEach(w => {
+    wsMap[w.name] = path.join(w.dir, 'docs/0-intro.js');
+  });
+  return wsMap;
+}
 
 // async function printWorkspaces() {
 //   const wsMap = {};
@@ -15,9 +46,10 @@ const path = require('path');
 // }
 
 async function webpackConfig() {
+  const wsMap = await getWorkspacesWithDocs();
   return {
     entry: {
-      foo: path.join(__dirname, '../packages/core/button/docs/0-intro.js'),
+      ...wsMap,
     },
     output: {
       filename: '[name].js',
@@ -32,6 +64,14 @@ async function webpackConfig() {
           options: {
             cacheDirectory: true,
           },
+        },
+        {
+          test: /\.(gif|jpe?g|png|ico)$/,
+          loader: 'url-loader?limit=10000',
+        },
+        {
+          test: /\.less$/,
+          use: ['style-loader', 'css-loader', 'less-loader'],
         },
       ],
     },
