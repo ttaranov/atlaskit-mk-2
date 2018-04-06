deeb5e64ed6b02035760a329648136286a7783f3
Charles Lee <clee@atlassian.com>
2018-04-06 10:54:29 +1000

    export createFilter, mergeStyles and component from index

--- packages/core/select/src/index.js
+++ packages/core/select/src/index.js
@@ -3,6 +3,7 @@ export { components } from 'react-select';
 export { mergeStyles } from 'react-select';
 
 export { default } from './Select';
+export { createFilter, mergeStyles, components } from 'react-select';
 export { default as AsyncSelect } from './AsyncSelect';
 export { default as CheckboxSelect } from './CheckboxSelect';
 export { default as CountrySelect } from './CountrySelect';
