89551732a7c01533a5a9a0094bd53e5fa44ef58e
Oscar Wallhult <owallhult@atlassian.com>
2018-04-09 10:22:22 +1000

    Get rid of outdent dependency

--- packages/core/code/src/CodeBlock.js
+++ packages/core/code/src/CodeBlock.js
@@ -1,7 +1,6 @@
 // @flow
 import React, { PureComponent } from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import outDent from 'outdent';
 import { normalizeLanguage } from './supportedLanguages';
 import { type Theme, applyTheme } from './themes/themeBuilder';
 
@@ -59,7 +58,7 @@ export default class CodeBlock extends PureComponent<CodeBlockProps, {}> {
 
     return (
       <SyntaxHighlighter {...props} onCopy={this.handleCopy}>
-        {outDent.string(codeText)}
+        {codeText}
       </SyntaxHighlighter>
     );
   }
