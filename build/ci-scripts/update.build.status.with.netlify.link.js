5d86099262ae56fe64c15e635f33eb8027a0423a
Luke Batchelor <lbatchelor@atlassian.com>
2018-04-10 16:32:36 +1000

    Fingers crossed

--- build/ci-scripts/update.build.status.with.netlify.link.js
+++ build/ci-scripts/update.build.status.with.netlify.link.js
@@ -2,10 +2,17 @@ const fs = require('fs');
 const path = require('path');
 const updateBuildStatus = require('../utils/updateBuildStatus');
 
-const pathToNetlifyOutputFile = path.join(__dirname, '..', '..', 'output.txt');
+const netlifyLogFilePath = './netlify-build.txt';
 
 try {
-  const logFile = fs.readFileSync(pathToNetlifyOutputFile, 'utf-8');
+  const buildStatusOpts = {
+    buildName: 'Netlify build',
+    description: 'A static preview build hosted on netlify',
+    url: URL,
+    state: 'SUCCESSFUL',
+  };
+
+  const logFile = fs.readFileSync(netlifyLogFilePath, 'utf-8');
   const lines = logFile.split('\n');
   const indexOfLineBeforeUrl = lines.findIndex(
     line => line.indexOf('Deploy is live (permalink):') > -1,
@@ -14,16 +21,10 @@ try {
     /http:\/\/.+?.netlify.com/,
   );
   if (!permalinkUrlMatch) {
-    console.error('Unable to find permalinkUrl in output.txt');
-    process.exit(1);
+    throw new Error(`Unable to find permalinkUrl in ${netlifyLogFilePath}`);
   }
   const permalinkUrl = permalinkUrlMatch[0];
-  const buildStatusOpts = {
-    buildName: 'Netlify build',
-    description: 'A static preview build hosted on netlify',
-    url: permalinkUrl,
-    state: 'SUCCESSFUL',
-  };
+
   console.log('Updating build status...');
   updateBuildStatus(buildStatusOpts);
 } catch (e) {
