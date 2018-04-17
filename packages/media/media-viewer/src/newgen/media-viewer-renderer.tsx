268a00590f77f9f9f14cf00023d12aeb48e201ae
Damien Klinnert <dklinnert@atlassian.com>
2018-04-17 21:05:03 +1000

    inline FileViewer component

--- packages/media/media-viewer/src/newgen/media-viewer-renderer.tsx
+++ packages/media/media-viewer/src/newgen/media-viewer-renderer.tsx
@@ -38,33 +38,21 @@ export const Viewer: StatelessComponent<Props> = ({ model, item, context }) => {
     case 'PENDING':
       return <Spinner />;
     case 'SUCCESSFUL':
-      return <FileViewer context={context} item={item} />;
+      if (!(context && item)) {
+        return <Spinner />;
+      }
+      switch (item.details.mediaType) {
+        case 'image':
+          return <ImageViewer context={context} item={item} />;
+        case 'audio':
+        case 'video':
+          return <VideoViewer context={context} item={item} />;
+        case 'doc':
+          return <PDFViewer context={context} item={item} />;
+        default:
+          return <ErrorMessage>This file is unsupported</ErrorMessage>;
+      }
     case 'FAILED':
       return <ErrorMessage>Error</ErrorMessage>;
-  }
-};
-
-export type FileViewerProps = {
-  context?: Context;
-  item?: FileItem;
-};
-
-export const FileViewer: StatelessComponent<FileViewerProps> = ({
-  context,
-  item,
-}) => {
-  if (!(context && item)) {
-    return <Spinner />;
-  }
-  switch (item.details.mediaType) {
-    case 'image':
-      return <ImageViewer context={context} item={item} />;
-    case 'audio':
-    case 'video':
-      return <VideoViewer context={context} item={item} />;
-    case 'doc':
-      return <PDFViewer context={context} item={item} />;
-    case 'unknown':
-      return <ErrorMessage>This file is unsupported</ErrorMessage>;
   }
 };
