391bc50db5999930cbaff647e95ad782c14315ad
Eduard Shvedai <eshvedai@atlassian.com>
2018-04-30 14:54:55 +1000

    ED-4451: added click handler for gap cursor

--- packages/editor/editor-core/src/types/editor-props.ts
+++ packages/editor/editor-core/src/types/editor-props.ts
@@ -75,9 +75,7 @@ export interface EditorProps {
   allowPlaceholderCursor?: boolean;
   allowTemplatePlaceholders?: boolean | PlaceholderTextOptions;
   allowDate?: boolean;
-
-  // Temporary flag to enable gap cursor while it's under development
-  UNSAFE_allowGapCursor?: boolean;
+  allowGapCursor?: boolean;
 
   // Temporary flag to enable layouts while it's under development
   UNSAFE_allowLayouts?: boolean;
