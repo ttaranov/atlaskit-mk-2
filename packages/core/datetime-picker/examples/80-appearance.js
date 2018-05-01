ce0f258d55579df3542fc574a800db196daaa4b2
Mike Gardiner <mgardiner@atlassian.com>
2018-05-01 10:54:36 +1000

    Timepicker example clarified how icon & subtle appears

--- packages/core/datetime-picker/examples/80-appearance.js
+++ packages/core/datetime-picker/examples/80-appearance.js
@@ -2,15 +2,22 @@
 
 import React from 'react';
 import { Label } from '@atlaskit/field-base';
+import CalendarIcon from '@atlaskit/icon/glyph/calendar';
 import { DatePicker, DateTimePicker, TimePicker } from '../src';
 
 export default () => {
   return (
     <div>
-      <Label label="TimePicker - default appearance" />
+      <Label label="TimePicker - no icon by default" />
       <TimePicker onChange={console.log} />
-      <Label label="TimePicker - subtle appearance" />
-      <TimePicker onChange={console.log} appearance="subtle" />
+      <Label label="TimePicker - icon provided" />
+      <TimePicker onChange={console.log} icon={CalendarIcon} />
+      <Label label="TimePicker - icon provided & subtle appearance" />
+      <TimePicker
+        onChange={console.log}
+        icon={CalendarIcon}
+        appearance="subtle"
+      />
 
       <Label label="DatePicker - default appearance" />
       <DatePicker onChange={console.log} />
