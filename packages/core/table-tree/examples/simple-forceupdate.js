a377fbb366a0923f176aead3e3312a4a377563af
Ajay Narain Mathur <amathur@atlassian.com>
2018-05-10 09:45:43 +1000

    updated table tree api's to cover scenarios where consumers can update data in table tree on the fly

--- packages/core/table-tree/examples/simple-forceupdate.js
+++ packages/core/table-tree/examples/simple-forceupdate.js
@@ -1,114 +1,93 @@
 // @flow
 import React, { Component } from 'react';
-import TableTree, {
-  Headers,
-  Header,
-  Rows,
-  Row,
-  Cell,
-  toTableTreeData,
-} from '../src';
-
-let uuid = 0;
+import TableTree, { Headers, Header, Rows, Row, Cell } from '../src';
 
 const ROOTS = [
   {
     title: 'Chapter 1: Clean Code',
-    id: ++uuid,
     page: 1,
     numbering: '1',
     hasChildren: true,
   },
   {
     title: 'Chapter 2: Meaningful names',
-    id: ++uuid,
     page: 17,
     numbering: '2',
   },
   {
     title: 'Chapter 3: Functions',
-    id: ++uuid,
     page: 17,
     numbering: '3',
     hasChildren: true,
   },
   {
     title: 'Chapter 4: Comments',
-    id: ++uuid,
     page: 53,
     numbering: '4',
     children: [],
   },
   {
     title: 'Chapter 5: Formatting',
-    id: ++uuid,
     page: 75,
     numbering: '5',
     children: [],
   },
 ];
 
-function getChildren() {
-  return [
-    {
-      title: 'There Will Be Code',
-      id: ++uuid,
-      page: 2,
-      numbering: '1.1',
-      hasChildren: true,
-    },
-    {
-      title: 'Bad code',
-      id: ++uuid,
-      page: 3,
-      numbering: '1.2',
-    },
-    {
-      title: 'The Total Cost of Owning a Mess',
-      id: ++uuid,
-      page: 4,
-      numbering: '1.3',
-      hasChildren: true,
-    },
-  ];
-}
+const CHILDREN = [
+  {
+    title: 'There Will Be Code',
+    page: 2,
+    numbering: '1.1',
+    hasChildren: true,
+  },
+  {
+    title: 'Bad code',
+    page: 3,
+    numbering: '1.2',
+  },
+  {
+    title: 'The Total Cost of Owning a Mess',
+    page: 4,
+    numbering: '1.3',
+    hasChildren: true,
+  },
+];
 
 function fetchRoots() {
   return Promise.resolve(ROOTS);
 }
 
 function fetchChildrenOf() {
-  return Promise.resolve(getChildren());
-}
-
-function getData(parentItem: ?Object) {
-  return !parentItem ? fetchRoots() : fetchChildrenOf();
+  return Promise.resolve(CHILDREN);
 }
 
 export default class extends Component<*, *> {
   state = {
-    rootIds: null,
-    itemsById: {},
+    roots: null,
   };
 
   componentDidMount() {
-    this.loadTableData();
+    fetchRoots().then(roots => {
+      this.setState({
+        roots,
+      });
+    });
   }
 
-  loadTableData = (parentItem: ?Object) => {
-    if (parentItem && parentItem.childIds) {
+  loadChildren = (parentItem: Object) => {
+    if (parentItem.children) {
       return;
     }
 
-    getData(parentItem).then(items => {
-      this.setState({
-        ...toTableTreeData(items, parentItem, this.state.itemsById),
-      });
+    fetchChildrenOf().then(childItems => {
+      // eslint-disable-next-line
+      parentItem.children = childItems;
+      this.forceUpdate();
     });
   };
 
   render() {
-    const { rootIds, itemsById } = this.state;
     return (
       <TableTree>
         <Headers>
@@ -117,14 +96,14 @@ export default class extends Component<*, *> {
           <Header width={100}>Page</Header>
         </Headers>
         <Rows
-          items={rootIds && rootIds.map(rootId => itemsById[rootId])}
-          render={({ title, numbering, page, hasChildren, childIds }) => (
+          items={this.state.roots}
+          render={({ title, numbering, page, hasChildren, children }) => (
             <Row
               expandLabel={'Expand'}
               collapseLabel={'Collapse'}
               itemId={numbering}
-              onExpand={this.loadTableData}
-              items={childIds && childIds.map(id => itemsById[id])}
+              onExpand={this.loadChildren}
+              items={children}
               hasChildren={hasChildren}
             >
               <Cell singleLine>{title}</Cell>
---
+++ packages/core/table-tree/examples/upgrade-guide-examples/async-data-loading.js
@@ -0,0 +1,92 @@
+// @flow
+
+import React, { Component } from 'react';
+import TableTree, {
+  Headers,
+  Header,
+  Rows,
+  Row,
+  Cell,
+  toTableTreeData,
+} from '../../src';
+
+type State = {
+  rootIds: Array<number | string>,
+  itemsById: Object,
+};
+
+class WithStaticData extends Component<{}, State> {
+  state = {
+    rootIds: [],
+    itemsById: {},
+  };
+
+  componentDidMount() {
+    this.loadChildFor();
+  }
+
+  loadChildFor = (parentItem: ?Object) => {
+    getChildren(parentItem).then(item => {
+      this.setState({
+        ...toTableTreeData(item, parentItem, this.state.itemsById, {
+          keyId: 'title',
+        }),
+      });
+    });
+  };
+
+  render() {
+    const { itemsById, rootIds } = this.state;
+    return (
+      <TableTree>
+        <Headers>
+          <Header width={300}>Chapter title</Header>
+          <Header width={100}>Numbering</Header>
+          <Header width={100}>Page</Header>
+        </Headers>
+        <Rows
+          items={rootIds && rootIds.map(rootId => itemsById[rootId])}
+          render={({ title, numbering, page, hasChildren, childIds }) => (
+            <Row
+              onExpand={this.loadChildFor}
+              expandLabel={'Expand'}
+              collapseLabel={'Collapse'}
+              itemId={numbering}
+              items={childIds && childIds.map(id => itemsById[id])}
+              hasChildren={hasChildren}
+            >
+              <Cell singleLine>{title}</Cell>
+              <Cell singleLine>{numbering}</Cell>
+              <Cell singleLine>{page}</Cell>
+            </Row>
+          )}
+        />
+      </TableTree>
+    );
+  }
+}
+
+const getChildren = (parentItem: ?Object) => {
+  if (!parentItem) {
+    return Promise.resolve([
+      {
+        id: 'title One',
+        title: 'title One',
+        numbering: '1',
+        page: '1',
+        hasChildren: true,
+      },
+    ]);
+  }
+  return Promise.resolve([
+    {
+      id: `${parentItem.id}: child`,
+      title: `${parentItem.title}: child`,
+      numbering: `${parentItem.numbering}.1`,
+      page: '1',
+      hasChildren: true,
+    },
+  ]);
+};
+
+export default () => <WithStaticData />;
