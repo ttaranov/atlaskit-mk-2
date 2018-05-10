e4f7b1c34d502831b43ce40171ed093f08345865
Pete Gleeson <pgleeson@atlassian.com>
2018-05-10 16:41:58 +1000

    adds enzyme-mount-wrapper to enzyme setup

--- build/jest-config/setup-enzyme.js
+++ build/jest-config/setup-enzyme.js
@@ -1,5 +1,6 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import mountWrapper from '@pgleeson/enzyme-mount-wrapper';
 
 // https://github.com/airbnb/enzyme#installation
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter(), mountWrapper });
