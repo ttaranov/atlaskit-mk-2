13ebf356b4e53c2274e0a08184e35cb03fac4b17
Scott Sidwell <ssidwell@atlassian.com>
2018-05-13 11:01:17 +1000

    ED-4638: Select-All inside code-block selects only the code-block contents

---
+++ packages/editor/editor-core/src/plugins/code-block/pm-plugins/ide-ux.ts
@@ -0,0 +1,33 @@
+import { EditorState, TextSelection, Plugin } from 'prosemirror-state';
+import { keydownHandler } from 'prosemirror-keymap';
+
+const isSelectionEntirelyInsideCodeBlock = (state: EditorState): boolean =>
+  state.selection.$from.sameParent(state.selection.$to) &&
+  state.selection.$from.parent.type === state.schema.nodes.codeBlock;
+
+export default new Plugin({
+  props: {
+    handleKeyDown: keydownHandler({
+      'Mod-A': (state: EditorState, dispatch) => {
+        if (isSelectionEntirelyInsideCodeBlock(state)) {
+          const { $from, $to } = state.selection;
+          const isFullCodeBlockSelection =
+            $from.parentOffset === 0 &&
+            $to.parentOffset === $to.parent.nodeSize - 2;
+          if (!isFullCodeBlockSelection) {
+            dispatch(
+              state.tr.setSelection(
+                new TextSelection(
+                  state.doc.resolve($from.start()),
+                  state.doc.resolve($to.end()),
+                ),
+              ),
+            );
+            return true;
+          }
+        }
+        return false;
+      },
+    }),
+  },
+});
