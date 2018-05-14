1ed1b467d6c44f0f70781f50dab1ee68d7c2c101
Ajay Narain Mathur <amathur@atlassian.com>
2018-05-15 08:27:37 +1000

    [code review] updated the argumnent in toTableTreeData helper function to represent the context of value better

--- packages/core/table-tree/src/utils/toTableTreeData.js
+++ packages/core/table-tree/src/utils/toTableTreeData.js
@@ -1,7 +1,7 @@
 // @flow
 
 export default function toTableTreeData(
-  rowsData: Array<Object>,
+  addedRowsData: Array<Object>,
   parentItem: ?Object = null,
   oldItemsById: ?Object,
   {
@@ -13,20 +13,20 @@ export default function toTableTreeData(
   rootIds?: Array<number | string>,
 } {
   const newItemsById = {};
-  for (const rowData of rowsData) {
+  for (const rowData of addedRowsData) {
     newItemsById[rowData[keyId]] = rowData;
   }
 
   if (parentItem === null || parentItem === undefined) {
     return {
       itemsById: newItemsById,
-      rootIds: rowsData.map(rowData => rowData[keyId]),
+      rootIds: addedRowsData.map(rowData => rowData[keyId]),
     };
   }
 
   const updatedParentItem = {
     ...parentItem,
-    [childId]: rowsData.map(child => child[keyId]),
+    [childId]: addedRowsData.map(child => child[keyId]),
   };
 
   return {
