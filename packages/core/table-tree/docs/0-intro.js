172973a6f7f016b6d1b9fecf4684df4050594ee4
Ajay Narain Mathur <amathur@atlassian.com>
2018-05-14 12:34:34 +1000

    doc update

--- packages/core/table-tree/docs/0-intro.js
+++ packages/core/table-tree/docs/0-intro.js
@@ -1,50 +1,50 @@
 // @flow
 import React from 'react';
-import { md, Example, Props } from '@atlaskit/docs';
+import { code, md, Example, Props } from '@atlaskit/docs';
 
 export default md`
-  The component displays a table of expandable, nested rows that form a tree-like hierarchy.
-  Child rows can be loaded asynchronously, on expansion.
+The component displays a table of expandable, nested rows that form a tree-like hierarchy.
+Child rows can be loaded asynchronously, on expansion.
 
-  ## Basic Usage
+## Basic Usage
 
-  Import the default exported Component and provide the data in \`rootItems\` prop.
+Import the default exported Component and provide the data in \`items\` prop.
 
-  ${(
-    <Example
-      Component={require('../examples/single-component').default}
-      source={require('!!raw-loader!../examples/single-component')}
-      title="Basic Usage: With Static Data"
-      language="jsx"
-    />
-  )}
+${(
+  <Example
+    Component={require('../examples/single-component').default}
+    source={require('!!raw-loader!../examples/single-component')}
+    title="Basic Usage: With Static Data"
+    language="jsx"
+  />
+)}
 
-  ### Expected data structure for \`rootItems\` props
+### Expected data structure for \`items\` props
 
-  ~~~js
-  [
-    {
-      id: //Item 1 id,
-      content: {
+${code`
+[
+  {
+    id: //Item 1 id,
+    content: {
 
-      },
-      hasChildren:
-      children: [
-        // Item 1 children
-        {
-          // Child 1
-        }
-      ]
     },
-    {
-      // Item 2
-    }
-  ]
-  ~~~
+    hasChildren:
+    children: [
+      // Item 1 children
+      {
+        // Child 1
+      }
+    ]
+  },
+  {
+    // Item 2
+  }
+]
+`}
 
-  ${(
-    <Props
-      props={require('!!extract-react-types-loader!../src/components/TableTree')}
-    />
-  )}
+${(
+  <Props
+    props={require('!!extract-react-types-loader!../src/components/TableTree')}
+  />
+)}
 `;
