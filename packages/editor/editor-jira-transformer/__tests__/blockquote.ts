955e6caad8306c9eaf37cc97c2b293ab2c795658
Chethana Krishnakumar <ckrishnakumar@cli-26005.office.atlassian.com>
2018-05-16 11:03:22 +1000

    revert changes made for testing

--- packages/editor/editor-jira-transformer/__tests__/blockquote.ts
+++ packages/editor/editor-jira-transformer/__tests__/blockquote.ts
@@ -5,7 +5,6 @@ import { createJIRASchema } from '@atlaskit/editor-common';
 const schema = createJIRASchema({ allowBlockQuote: true, allowLists: true });
 
 // Nodes
-// testing
 describe('JIRATransformer', () => {
   describe('blockquote', () => {
     checkParseEncodeRoundTrips(
