c2199d9049760fa5a9dc867eb234b3f40a273486
Damien Klinnert <dklinnert@atlassian.com>
2018-05-17 14:08:44 +0200

    add initial zooming feature for document viewer

--- packages/media/media-viewer/src/newgen/viewers/pdf/pdfComponent.tsx
+++ packages/media/media-viewer/src/newgen/viewers/pdf/pdfComponent.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { PDFWrapper } from '../../styled';
 import * as PDFJSViewer from 'pdfjs-dist/web/pdf_viewer';
 import * as pdfjsLib from 'pdfjs-dist/build/pdf';
+import { ZoomControls } from '../../zoomControls';
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = '/'; // TODO: use web workers instead of fake worker.
 
@@ -13,9 +14,14 @@ export const fetch = async (url: string): Promise<Blob> => {
   return pdfjsLib.getDocument(url).promise;
 };
 
-export class PDFViewer extends React.PureComponent<Props, {}> {
+export class PDFViewer extends React.PureComponent<Props, { scale: number }> {
   private el: HTMLDivElement;
   private pdfViewer: any;
+
+  constructor() {
+    super();
+    this.state = { scale: 1 };
+  }
 
   componentDidMount() {
     this.pdfViewer = new PDFJSViewer.PDFViewer({ container: this.el });
@@ -26,10 +32,16 @@ export class PDFViewer extends React.PureComponent<Props, {}> {
     this.el = el;
   };
 
+  private handleZoom = scale => {
+    this.pdfViewer.currentScale = scale;
+    this.setState({ scale: scale });
+  };
+
   render() {
     return (
       <PDFWrapper innerRef={this.savePdfElement}>
         <div className="pdfViewer" />
+        <ZoomControls zoomLevel={this.state.scale} onChange={this.handleZoom} />
       </PDFWrapper>
     );
   }
