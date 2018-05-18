f6f70b6e5125a01c0faa87381d9ad2477264c56a
David Richard <drichard@atlassian.com>
2018-05-18 13:27:06 -0700

    Change ObjectResult subText rendering.

--- packages/search/quick-search/src/components/Results/__tests__/ObjectResult.test.js
+++ packages/search/quick-search/src/components/Results/__tests__/ObjectResult.test.js
@@ -52,15 +52,20 @@ describe('Object Result', () => {
     ).toBe(null);
   });
 
-  it('should render the `containerName` prop', () => {
-    resultWrapper.setProps({ containerName: 'takeaway' });
+  it('should render the `containerName` prop if no objectKey provided', () => {
+    resultWrapper.setProps({ containerName: 'Burger Sling' });
     expect(resultWrapper.text()).toEqual(
-      expect.stringContaining('in takeaway'),
+      expect.stringContaining('Burger Sling'),
     );
   });
 
-  it('should render the `objectKey` prop', () => {
-    resultWrapper.setProps({ objectKey: 'KFC-11' });
-    expect(resultWrapper.text()).toEqual(expect.stringContaining('KFC-11'));
+  it('should render the `objectKey` and `containerName` prop together', () => {
+    resultWrapper.setProps({
+      objectKey: 'KFC-11',
+      containerName: 'Burger Sling',
+    });
+    expect(resultWrapper.text()).toEqual(
+      expect.stringContaining('KFC-11 · Burger Sling'),
+    );
   });
 });
