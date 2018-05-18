a71b10a1a76c5ef4dc272354b88ed7e3ae51fd74
Eduard Shvedai <eshvedai@atlassian.com>
2018-05-18 14:32:47 +1000

    upskip backwards compat test

--- packages/editor/editor-common/__tests__/json-schema/backwards-compat.ts
+++ packages/editor/editor-common/__tests__/json-schema/backwards-compat.ts
@@ -41,7 +41,7 @@ expect.extend({
   },
 });
 
-describe.skip('JSON schema', () => {
+describe('JSON schema', () => {
   it('should be backwards compatible', async () => {
     let existingSchema: any;
 
