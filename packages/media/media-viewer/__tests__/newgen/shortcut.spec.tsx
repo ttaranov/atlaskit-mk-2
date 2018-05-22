72f757a28202b2370440658126f6f892813ca44d
Ivan Loire <iloire@atlassian.com>
2018-05-22 12:36:44 +0200

    Remove unnecesary test

--- packages/media/media-viewer/__tests__/newgen/shortcut.spec.tsx
+++ packages/media/media-viewer/__tests__/newgen/shortcut.spec.tsx
@@ -9,23 +9,6 @@ describe('Shortcut', () => {
     document.addEventListener = originalEventListener;
   });
 
-  it('should register key event listener on mount', done => {
-    document.addEventListener = name => {
-      expect(name).toEqual('keydown');
-      done();
-    };
-
-    const el = mount(
-      <div>
-        <Shortcut keyCode="Escape" handler={() => {}} />
-      </div>,
-    );
-
-    el.simulate('keyDown', {
-      key: 'Escape',
-    });
-  });
-
   it('should de-register the key event listener on unmount', done => {
     document.removeEventListener = name => {
       expect(name).toEqual('keydown');
