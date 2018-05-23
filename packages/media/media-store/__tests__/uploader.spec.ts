97bf02fbfc9f37fc9050cfdfd94de895df88affb
Michael James <mjames@atlassian.com>
2018-05-24 08:26:55 +1000

    fix(media-store): No longer creates an empty file when uploading a file
    * Stops ghost files from being created in the passed in collection

--- packages/media/media-store/__tests__/uploader.spec.ts
+++ packages/media/media-store/__tests__/uploader.spec.ts
@@ -1,7 +1,9 @@
+jest.mock('uuid');
 jest.mock('chunkinator');
 jest.mock('../src/media-store');
 
-import chunkinator, { Options } from 'chunkinator';
+import * as uuid from 'uuid';
+import chunkinator from 'chunkinator';
 import { uploadFile } from '../src';
 import { MediaStore } from '../src/media-store';
 
@@ -14,37 +16,23 @@ describe('Uploader', () => {
     };
     const createFileFromUpload = jest
       .fn()
-      .mockReturnValue(Promise.resolve({ data: { id: '123' } }));
-    const createUpload = () => {
-      return Promise.resolve({ data: [{ id: 'upload-id-123' }] });
-    };
-    const appendChunksToUpload = jest.fn().mockReturnValue(Promise.resolve(1));
-    const createFile = jest
+      .mockReturnValue(Promise.resolve({ data: { id: 'file-id-123' } }));
+    const createUpload = jest
       .fn()
-      .mockReturnValue(Promise.resolve({ data: { id: 'id-upfront-123' } }));
+      .mockReturnValue(Promise.resolve({ data: [{ id: 'upload-id-123' }] }));
+    const appendChunksToUpload = jest.fn().mockReturnValue(Promise.resolve(1));
     const MediaStoreMock = jest.fn().mockImplementation(() => ({
-      createFile,
       createUpload,
       createFileFromUpload,
       appendChunksToUpload,
     }));
 
-    const blob: Blob = null as any;
-    ChunkinatorMock.mockImplementation((_file, config: Options, callbacks) => {
+    ChunkinatorMock.mockImplementation((_file, _, callbacks) => {
       return {
         response: (async () => {
           callbacks.onProgress(0.1);
-          await config.processingFunction!([
-            { hash: '1', blob },
-            { hash: '2', blob },
-            { hash: '3', blob },
-          ]);
           callbacks.onProgress(0.2);
-          await config.processingFunction!([
-            { hash: '4', blob },
-            { hash: '5', blob },
-            { hash: '6', blob },
-          ]);
+          return [{ hash: '1' }, { hash: '2' }, { hash: '3' }];
         })(),
         cancel: jest.fn(),
       };
@@ -54,13 +42,14 @@ describe('Uploader', () => {
       MediaStoreMock,
       ChunkinatorMock,
       config,
-      createFile,
       createFileFromUpload,
       createUpload,
       appendChunksToUpload,
     };
   };
+
   const authProvider = () => Promise.resolve({ clientId: '', token: '' });
+  (uuid.v4 as jest.Mock<any>).mockReturnValue('some-uuid');
 
   it('should pass down the file content to Chunkinator', async () => {
     const { MediaStoreMock, ChunkinatorMock, config } = setup();
@@ -83,40 +72,38 @@ describe('Uploader', () => {
     expect(MediaStoreMock).toBeCalledWith(config);
   });
 
-  it('should use the file and collection names when creating the file', async () => {
+  it('should use the file name, collection name and mimeType when creating the file', async () => {
     const {
       MediaStoreMock,
       ChunkinatorMock,
       config,
-      createFile,
       createFileFromUpload,
     } = setup();
 
     (MediaStore as any) = MediaStoreMock;
     (chunkinator as any) = ChunkinatorMock;
 
-    ChunkinatorMock.mockImplementation(() => {
-      return { response: Promise.resolve(), cancel: jest.fn() };
-    });
-
     await uploadFile(
-      { content: '', name: 'file-name', collection: 'some-collection' },
+      {
+        content: '',
+        name: 'file-name',
+        collection: 'some-collection',
+        mimeType: 'image/jpeg',
+      },
       config,
     ).deferredFileId;
-    const occurrenceKey = createFile.mock.calls[0][0].occurrenceKey;
 
     expect(createFileFromUpload).toHaveBeenCalledTimes(1);
     expect(createFileFromUpload).toBeCalledWith(
-      { uploadId: 'upload-id-123', name: 'file-name' },
+      { uploadId: 'upload-id-123', name: 'file-name', mimeType: 'image/jpeg' },
       {
-        occurrenceKey,
+        occurrenceKey: 'some-uuid',
         collection: 'some-collection',
-        replaceFileId: 'id-upfront-123',
       },
     );
   });
 
-  it('should append the chunks to the upload in processing function', async () => {
+  it('should append the chunks to the upload after chunks have been uploaded and the upload session has been created', async () => {
     const {
       MediaStoreMock,
       ChunkinatorMock,
@@ -130,37 +117,23 @@ describe('Uploader', () => {
 
     await uploadFile({ content: '' }, config, { onProgress }).deferredFileId;
 
-    expect(appendChunksToUpload).toHaveBeenCalledTimes(2);
+    expect(appendChunksToUpload).toHaveBeenCalledTimes(1);
     expect(appendChunksToUpload.mock.calls[0][0]).toEqual('upload-id-123');
     expect(appendChunksToUpload.mock.calls[0][1].chunks).toEqual([
       '1',
       '2',
       '3',
     ]);
-    expect(appendChunksToUpload.mock.calls[0][1].offset).toEqual(0);
-    expect(appendChunksToUpload.mock.calls[1][0]).toEqual('upload-id-123');
-    expect(appendChunksToUpload.mock.calls[1][1].chunks).toEqual([
-      '4',
-      '5',
-      '6',
-    ]);
-    expect(appendChunksToUpload.mock.calls[1][1].offset).toEqual(3);
   });
 
   it('should call onProgress with the upload percentage', async () => {
-    const {
-      MediaStoreMock,
-      ChunkinatorMock,
-      config,
-      createFileFromUpload,
-    } = setup();
+    const { MediaStoreMock, ChunkinatorMock, config } = setup();
     const onProgress = jest.fn();
 
     (MediaStore as any) = MediaStoreMock;
     (chunkinator as any) = ChunkinatorMock;
 
     await uploadFile({ content: '' }, config, { onProgress }).deferredFileId;
-    await createFileFromUpload();
 
     expect(onProgress).toHaveBeenCalledTimes(2);
     expect(onProgress.mock.calls[0][0]).toEqual(0.1);
@@ -173,15 +146,11 @@ describe('Uploader', () => {
     (MediaStore as any) = MediaStoreMock;
     (chunkinator as any) = ChunkinatorMock;
 
-    ChunkinatorMock.mockImplementation(() => {
-      return { response: Promise.resolve(), cancel: jest.fn() };
-    });
-
     const fileId = await uploadFile({ content: '' }, config).deferredFileId;
-    expect(fileId).toBe('id-upfront-123');
+    expect(fileId).toBe('file-id-123');
   });
 
-  it('should reject if there was an error with the upload', () => {
+  it('should reject if chunkinator rejects', () => {
     const { MediaStoreMock, ChunkinatorMock, config } = setup();
 
     (MediaStore as any) = MediaStoreMock;
@@ -199,50 +168,94 @@ describe('Uploader', () => {
     ).rejects.toEqual('some upload error');
   });
 
-  it('should use id upfront for the new file', async () => {
-    const { MediaStoreMock, ChunkinatorMock, config, createFile } = setup();
+  it('should NOT attempt to create an upload session if chunkinator fails to upload chunks', async () => {
+    expect.assertions(3);
+    const {
+      MediaStoreMock,
+      ChunkinatorMock,
+      config,
+      createUpload,
+      appendChunksToUpload,
+      createFileFromUpload,
+    } = setup();
 
     (MediaStore as any) = MediaStoreMock;
     (chunkinator as any) = ChunkinatorMock;
 
     ChunkinatorMock.mockImplementation(() => {
-      return { response: Promise.resolve(), cancel: jest.fn() };
+      return {
+        response: Promise.reject('some upload error'),
+        cancel: jest.fn(),
+      };
     });
 
-    const promiseFileId = uploadFile({ content: '' }, config).deferredFileId;
-    // notice that we are not awaiting uploadFile here because we want to check that createFile gets called in parallel
-    expect(createFile).toHaveBeenCalledTimes(1);
-
-    const fileId = await promiseFileId;
-    expect(fileId).toBe('id-upfront-123');
+    try {
+      await uploadFile(
+        { content: '', name: 'file-name', collection: 'some-collection' },
+        config,
+      ).deferredFileId;
+    } catch {
+      expect(createUpload).toHaveBeenCalledTimes(0);
+      expect(appendChunksToUpload).toHaveBeenCalledTimes(0);
+      expect(createFileFromUpload).toHaveBeenCalledTimes(0);
+    }
   });
 
-  it('should create the file after all chunks have been appended', async () => {
+  it('should NOT attempt to append chunks to upload if an upload session can NOT be created', async () => {
     expect.assertions(3);
     const {
-      createUpload,
+      MediaStoreMock,
       ChunkinatorMock,
       config,
-      createFile,
+      createUpload,
       appendChunksToUpload,
       createFileFromUpload,
     } = setup();
 
+    (MediaStore as any) = MediaStoreMock;
     (chunkinator as any) = ChunkinatorMock;
 
-    (MediaStore as any) = jest.fn().mockImplementation(() => ({
-      createUpload,
-      createFile,
-      createFileFromUpload() {
-        expect(appendChunksToUpload).toHaveBeenCalledTimes(2);
-        return createFileFromUpload();
-      },
-      appendChunksToUpload() {
-        expect(createFileFromUpload).toHaveBeenCalledTimes(0);
-        return appendChunksToUpload();
-      },
-    }));
+    createUpload.mockReturnValue(
+      Promise.reject('failed-to-create-upload-session'),
+    );
+    try {
+      await uploadFile(
+        { content: '', name: 'file-name', collection: 'some-collection' },
+        config,
+      ).deferredFileId;
+    } catch {
+      expect(createUpload).toHaveBeenCalledTimes(1);
+      expect(appendChunksToUpload).toHaveBeenCalledTimes(0);
+      expect(createFileFromUpload).toHaveBeenCalledTimes(0);
+    }
+  });
 
-    await uploadFile({ content: '' }, config).deferredFileId;
+  it('should NOT attempt to create a file from an upload if chunks can NOT be appended to said upload', async () => {
+    expect.assertions(3);
+    const {
+      MediaStoreMock,
+      ChunkinatorMock,
+      config,
+      createUpload,
+      appendChunksToUpload,
+      createFileFromUpload,
+    } = setup();
+
+    (MediaStore as any) = MediaStoreMock;
+    (chunkinator as any) = ChunkinatorMock;
+
+    appendChunksToUpload.mockReturnValue(
+      Promise.reject('failed-to-create-upload-session'),
+    );
+    try {
+      await uploadFile(
+        { content: '', name: 'file-name', collection: 'some-collection' },
+        config,
+      ).deferredFileId;
+    } catch {
+      expect(createUpload).toHaveBeenCalledTimes(1);
+      expect(appendChunksToUpload).toHaveBeenCalledTimes(1);
+      expect(createFileFromUpload).toHaveBeenCalledTimes(0);
+    }
   });
 });
