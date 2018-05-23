28ca1740e25a69621755f414644290b13bb3dfe5
Charles Lee <clee@atlassian.com>
2018-05-23 14:32:10 +1000

    add compact-single-select example

--- packages/core/select/examples/17-compact-single-select.js
+++ packages/core/select/examples/17-compact-single-select.js
@@ -1,22 +1,31 @@
 // @flow
-import React, { Fragment } from 'react';
-import Select from '../src';
+import React from 'react';
+import { AsyncSelect as Select } from '../src';
+
+const promiseOptions = inputValue =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve([
+        { label: 'Adelaide', value: 'adelaide' },
+        { label: 'Brisbane', value: 'brisbane' },
+        { label: 'Canberra', value: 'canberra' },
+        { label: 'Darwin', value: 'darwin' },
+        { label: 'Hobart', value: 'hobart' },
+        { label: 'Melbourne', value: 'melbourne' },
+        { label: 'Perth', value: 'perth' },
+        { label: 'Sydney', value: 'sydney' },
+      ]);
+    }, 1000);
+  });
 
 const SingleExample = () => (
   <Select
+    isClearable
     className="compact-select"
     classNamePrefix="react-select"
+    isSearchable
     spacing="compact"
-    options={[
-      { label: 'Adelaide', value: 'adelaide' },
-      { label: 'Brisbane', value: 'brisbane' },
-      { label: 'Canberra', value: 'canberra' },
-      { label: 'Darwin', value: 'darwin' },
-      { label: 'Hobart', value: 'hobart' },
-      { label: 'Melbourne', value: 'melbourne' },
-      { label: 'Perth', value: 'perth' },
-      { label: 'Sydney', value: 'sydney' },
-    ]}
+    loadOptions={promiseOptions}
     placeholder="Choose a City"
   />
 );
