9a1cd95f3b020425ff117a6824932d187a6498da
Jared Crowe <jcrowe@atlassian.com>
2018-05-28 16:21:48 +1000

    fix example

--- packages/core/navigation-next/examples/shared/shortcuts-plugin.js
+++ packages/core/navigation-next/examples/shared/shortcuts-plugin.js
@@ -4,35 +4,37 @@ import React, { Component } from 'react';
 import ShortcutIcon from '@atlaskit/icon/glyph/shortcut';
 import { NavAPISubscriber, navAPIUtils } from '../../src';
 
-const appendShortcuts = navAPIUtils.appendToGroup('root/index:menu', [
-  {
-    id: 'shortcut-separator',
-    key: 'shortcut-separator',
-    type: 'Separator',
-  },
-  {
-    icon: ShortcutIcon,
-    id: 'shortcut-1',
-    key: 'shortcut-1',
-    type: 'Item',
-    text: 'Shortcut 1',
-  },
-  {
-    icon: ShortcutIcon,
-    id: 'shortcut-2',
-    key: 'shortcut-2',
-    type: 'Item',
-    text: 'Shortcut 2',
-  },
-]);
+const { appendChildren, findId } = navAPIUtils;
+
+const appendShortcuts = findId('root/index:menu')(
+  appendChildren([
+    {
+      id: 'shortcut-separator',
+      key: 'shortcut-separator',
+      type: 'Separator',
+    },
+    {
+      icon: ShortcutIcon,
+      id: 'shortcut-1',
+      key: 'shortcut-1',
+      type: 'Item',
+      text: 'Shortcut 1',
+    },
+    {
+      icon: ShortcutIcon,
+      id: 'shortcut-2',
+      key: 'shortcut-2',
+      type: 'Item',
+      text: 'Shortcut 2',
+    },
+  ]),
+);
 
 class Shortcuts extends Component<any> {
   componentDidMount() {
     const { navAPI } = this.props;
-    navAPI.addReducer('root/index', this.shortcutsReducer, 'shortcuts');
+    navAPI.addReducer('root/index', appendShortcuts, 'shortcuts');
   }
-
-  shortcutsReducer = view => appendShortcuts(view);
 
   render() {
     return null;
