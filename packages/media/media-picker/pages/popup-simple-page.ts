b01af7a320ccc44566b73dd57c0a9a297a9931a7
Jimmy Luong <jluong@atlassian.com>
2018-05-31 09:36:05 +0100

    feat(media-picker): add events to popup simple page object

--- packages/media/media-picker/pages/popup-simple-page.ts
+++ packages/media/media-picker/pages/popup-simple-page.ts
@@ -1,5 +1,11 @@
 import { Client } from 'webdriverio';
 import { getExampleUrl } from '@atlaskit/webdriver-runner/utils/example';
+import { PopupUploadEventPayloadMap } from '../src/components/popup';
+
+export type Event = {
+  readonly name: string;
+  readonly payload: any;
+};
 
 /**
  * Popup Simple Example Page Object
@@ -25,8 +31,32 @@ export class PopupSimplePage {
     return this.client.$(`//div[contains(., "${filename}")]`);
   }
 
+  getInsertButton() {
+    return this.client.$('.insert-button');
+  }
+
+  async getEvents(): Promise<Event[]> {
+    return JSON.parse(await this.client.getText('#events'));
+  }
+
+  async getEvent(name: keyof PopupUploadEventPayloadMap): Promise<Event> {
+    await this.client.waitUntil(
+      async () => (await this.getEvents()).some(isEvent(name)),
+      3000,
+    );
+
+    const events = await this.getEvents();
+
+    const event = events.find(isEvent(name));
+    if (event) {
+      return event;
+    } else {
+      throw new Error(`Event ${name} not found`);
+    }
+  }
+
   async uploadFile(localPath: string) {
-    await this.getUploadButton().then();
+    await this.getUploadButton().click();
     await this.client.chooseFile('input', localPath);
   }
 }
@@ -37,4 +67,8 @@ export async function gotoPopupSimplePage(
   const page = new PopupSimplePage(client);
   await page.go();
   return page;
+}
+
+function isEvent(name: string) {
+  return (event: Event) => event.name === name;
 }
