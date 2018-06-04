c41a2ea88d71bf80d9f3dbc27cc1289d79e33a7b
Jamie Kyle <me@thejameskyle.com>
2018-06-04 12:03:55 -0700

    wip

--- website2/workspaces.webpack.config.js
+++ website2/workspaces.webpack.config.js
@@ -1,5 +1,8 @@
-var path = require('path');
-var webpack = require('webpack');
+'use strict';
+const path = require('path');
+const webpack = require('webpack');
+
+const cwd = process.cwd();
 
 module.exports = {
   mode: 'none',
@@ -7,14 +10,35 @@ module.exports = {
     index: ['./src/index.js'],
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(cwd, 'dist', 'dll'),
     filename: '[name].js',
     library: '[name]_[hash]',
   },
+  externals: [
+    (context, request, callback) => {
+      if (request.startsWith('.')) {
+        callback();
+      } else {
+        callback(null, 'commonjs ' + request);
+      }
+    },
+  ],
   plugins: [
     new webpack.DllPlugin({
-      path: path.join(__dirname, 'dist', 'webpack-manifest.json'),
+      path: path.join(cwd, 'dist', 'dll', 'webpack-manifest.json'),
       name: '[name]_[hash]',
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: require.resolve('babel-loader'),
+        options: {
+          cacheDirectory: true,
+        },
+      },
+    ],
+  },
 };
