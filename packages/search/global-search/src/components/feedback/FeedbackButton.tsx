9eba466aaa0a984393574367cde2b2cd86c80c09
David Richard <drichard@atlassian.com>
2018-06-05 14:57:45 -0700

    XPSRCH-792: Adds a HOC to launch a feedback dialog.

---
+++ packages/search/global-search/src/components/feedback/FeedbackButton.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import Button from '@atlaskit/button';
+import FeedbackDialog from './FeedbackDialog';
+import { AutoDismissFlag, FlagGroup } from '@atlaskit/flag';
+import SuccessIcon from '@atlaskit/icon/glyph/check-circle';
+import { colors } from '@atlaskit/theme';
+
+// Positions the button right of the drawer
+const ButtonPositionHack = styled.div`
+  position: fixed;
+  left: 620px;
+  margin-top: 8px;
+`;
+
+export interface Props {
+  collectorId: string;
+}
+
+export default class FeedbackButton extends React.Component<Props> {
+  state = {
+    showDialog: false,
+    showFlag: false,
+  };
+
+  showDialog = () => this.setState({ showDialog: true });
+  hideDialog = () => this.setState({ showDialog: false });
+  showFlag = () => this.setState({ showFlag: true });
+  hideFlag = () => this.setState({ showFlag: false });
+
+  handleSubmit = () => {
+    this.hideDialog();
+    setTimeout(this.showFlag, 800);
+  };
+
+  render() {
+    const { showDialog, showFlag } = this.state;
+
+    return (
+      <ButtonPositionHack>
+        <Button appearance="primary" onClick={this.showDialog}>
+          Give Feedback
+        </Button>
+        {showDialog && (
+          <FeedbackDialog
+            collectorId={this.props.collectorId}
+            onClose={this.hideDialog}
+            onSubmit={this.handleSubmit}
+          />
+        )}
+        <FlagGroup onDismissed={this.hideFlag}>
+          {showFlag ? (
+            <AutoDismissFlag
+              id="feedback.success"
+              key="feedback.success"
+              icon={<SuccessIcon primaryColor={colors.G300} label="Info" />}
+              title="Thank you"
+              description="We'll use your feedback to improve this experience."
+            />
+          ) : (
+            []
+          )}
+        </FlagGroup>
+      </ButtonPositionHack>
+    );
+  }
+}
