55363484305d50601a7e09bb3373e7d223f76ee2
Trey Shugart <treshugart@gmail.com>
2018-06-06 09:53:10 +1000

    Refactor appearance to be more clear and add ability to pass a function.

--- packages/core/theme/src/components/Appearance.js
+++ packages/core/theme/src/components/Appearance.js
@@ -1,18 +1,38 @@
 // @flow
 
+type ValidProps = {} | Function | string;
 type Props = {
   children: Function,
-  props: {} | string,
+  props: ValidProps,
   theme: {},
 };
 
-export default ({ children, props, theme }: Props) => {
-  const appearance = typeof props === 'object' ? 'default' : props;
-  const merged = typeof props === 'object' ? { ...props } : {};
+function resolveAppearance(props: ValidProps) {
+  return typeof props === 'string' ? props : 'default';
+}
+
+function resolveProps(props: ValidProps): {} {
+  if (typeof props === 'function') {
+    return props();
+  } else if (typeof props === 'object') {
+    return { ...props };
+  }
+  return {};
+}
+
+function mergePropsWithTheme(appearance: string, props: {}, theme: {}) {
+  let merged = { ...props };
   Object.keys(theme).forEach(key => {
-    if (!(key in merged)) {
+    if (!(key in props)) {
       merged[key] = theme[key]({ appearance });
     }
   });
+  return merged;
+}
+
+export default ({ children, props, theme }: Props) => {
+  const resolvedAppearance = resolveAppearance(props);
+  const resolvedProps = resolveProps(props);
+  const merged = mergePropsWithTheme(resolvedAppearance, resolvedProps, theme);
   return children(merged);
 };
---
+++ packages/core/theme/src/components/__tests__/Appearance.js
@@ -0,0 +1,52 @@
+// @flow
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import Appearance from '../Appearance';
+
+const theme = {
+  key1: ({ appearance }) => `${appearance} -> value1`,
+  key2: ({ appearance }) => `${appearance} -> value2`,
+};
+
+test('props -> string', done => {
+  shallow(
+    <Appearance props="custom" theme={theme}>
+      {merged => {
+        expect(merged).toEqual({
+          key1: 'custom -> value1',
+          key2: 'custom -> value2',
+        });
+        done();
+      }}
+    </Appearance>,
+  );
+});
+
+test('props -> object', done => {
+  shallow(
+    <Appearance props={{ key2: 'custom2' }} theme={theme}>
+      {merged => {
+        expect(merged).toEqual({
+          key1: 'default -> value1',
+          key2: 'custom2',
+        });
+        done();
+      }}
+    </Appearance>,
+  );
+});
+
+test('props -> function', done => {
+  shallow(
+    <Appearance props={() => ({ key2: 'custom2' })} theme={theme}>
+      {merged => {
+        expect(merged).toEqual({
+          key1: 'default -> value1',
+          key2: 'custom2',
+        });
+        done();
+      }}
+    </Appearance>,
+  );
+});
