f67118bd9f79c1beed3dbce57bd90b3f0988b797
Raja Bellebon <rbellebon@atlassian.com>
2018-06-08 10:46:14 +1000

    Add relevant examples

--- packages/core/tag/examples/1-colors.js
+++ packages/core/tag/examples/1-colors.js
@@ -4,20 +4,20 @@ import Tag from '../src';
 
 export default () => (
   <div>
-    <Tag text="standard Tag" color="standard" />
-    <Tag text="blue Tag" color="blue" />
-    <Tag text="green Tag" color="green" />
-    <Tag text="teal Tag" color="teal" />
-    <Tag text="purple Tag" color="purple" />
-    <Tag text="red Tag" color="red" />
-    <Tag text="yellow Tag" color="yellow" />
-    <Tag text="grey Tag" color="grey" />
-    <Tag text="greenLight Tag" color="greenLight" />
-    <Tag text="tealLight Tag" color="tealLight" />
-    <Tag text="blueLight Tag" color="blueLight" />
-    <Tag text="purpleLight Tag" color="purpleLight" />
-    <Tag text="redLight Tag" color="redLight" />
-    <Tag text="yellowLight Tag" color="yellowLight" />
-    <Tag text="greyLight Tag" color="greyLight" />
+    <Tag text="standard Tag" appearance="standard" />
+    <Tag text="blue Tag" appearance="blue" />
+    <Tag text="green Tag" appearance="green" />
+    <Tag text="teal Tag" appearance="teal" />
+    <Tag text="purple Tag" appearance="purple" />
+    <Tag text="red Tag" appearance="red" />
+    <Tag text="yellow Tag" appearance="yellow" />
+    <Tag text="grey Tag" appearance="grey" />
+    <Tag text="greenLight Tag" appearance="greenLight" />
+    <Tag text="tealLight Tag" appearance="tealLight" />
+    <Tag text="blueLight Tag" appearance="blueLight" />
+    <Tag text="purpleLight Tag" appearance="purpleLight" />
+    <Tag text="redLight Tag" appearance="redLight" />
+    <Tag text="yellowLight Tag" appearance="yellowLight" />
+    <Tag text="greyLight Tag" appearance="greyLight" />
   </div>
 );
--- packages/core/tag/examples/10-withTheme.js
+++ packages/core/tag/examples/10-withTheme.js
@@ -1,12 +1,31 @@
 // @flow
+import { AtlaskitThemeProvider } from '@atlaskit/theme';
 import React from 'react';
 import Tag from '../src';
 
 export default () => (
   <div>
-    <Tag
-      text="Custom tag"
-      color={{ backgroundColor: 'green', textColor: 'blue' }}
-    />
+    <AtlaskitThemeProvider mode="light">
+      <p>
+        standard: <Tag text="standard">Standard</Tag>
+      </p>
+      <p>
+        appearance: blueLight <Tag text="Blue Light" appearance="blueLight" />
+      </p>
+      <p>
+        appearance: {`{ backgroundColor: 'green' }`}{' '}
+        <Tag
+          text="background colored"
+          appearance={{ backgroundColor: 'green' }}
+        />
+      </p>
+      <p>
+        appearance: {`{ backgroundColor: 'yellow', textColor: 'blue' }`}{' '}
+        <Tag
+          text="background and text colored"
+          appearance={{ backgroundColor: 'yellow', textColor: 'blue' }}
+        />
+      </p>
+    </AtlaskitThemeProvider>
   </div>
 );
