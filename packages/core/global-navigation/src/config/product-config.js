fc4ffff8cddbd6b64452a7f1481dc5151f4d1c4c
Harsha Ivaturi <isriharsha@atlassian.com>
2018-06-12 12:05:47 +1000

    Refractor product-config

--- packages/core/global-navigation/src/config/product-config.js
+++ packages/core/global-navigation/src/config/product-config.js
@@ -7,10 +7,12 @@ import SignInIcon from '@atlaskit/icon/glyph/sign-in';
 import Dropdown from '@atlaskit/dropdown-menu';
 import type { GlobalNavigationProps } from '../components/GlobalNavigation/types';
 
-// Remove items with no props passed from the product.
-const removeEmptyItems = items =>
+const isEmpty = obj => obj && !!Object.keys(obj).length;
+
+// Remove empty and falsey (including 0) keys from an object
+const compact = items =>
   Object.keys(items)
-    .filter(item => !!items[item])
+    .filter(item => isEmpty(item))
     .reduce((acc, curr) => {
       acc[curr] = items[curr];
       return acc;
@@ -43,102 +45,121 @@ const generateAvatar = profileIconUrl => () => (
   />
 );
 
+function configFactory(onClick, tooltip, otherConfig = {}) {
+  if (!onClick && (tooltip || !isEmpty(otherConfig))) {
+    console.warn(
+      `One of the items in the Global Navigation is missing an onClick handler. This item will not be rendered in Global Navigation.`,
+    );
+  }
+
+  if (!onClick) return null;
+
+  return {
+    onClick,
+    ...(tooltip ? { tooltip, label: tooltip } : null),
+    ...compact(otherConfig),
+  };
+}
+
+function helpConfigFactory(items, tooltip, otherConfig = {}) {
+  if (!items && (tooltip || !isEmpty(otherConfig))) {
+    console.warn(
+      'You have provided some prop(s) for help, but not helpItems. Help will not be rendered in Global Navigation',
+    );
+  }
+
+  if (!items) return null;
+
+  return {
+    component: generateDropDown(QuestionIcon, items),
+    ...(tooltip ? { tooltip, label: tooltip } : null),
+    ...compact(otherConfig),
+  };
+}
+
+function profileConfigFactory(
+  items,
+  tooltip,
+  href,
+  profileIconUrl,
+  otherConfig = {},
+) {
+  if (!items && !href && (tooltip || !isEmpty(otherConfig))) {
+    console.warn(
+      'You provided some prop(s) for profile, but not profileItems or loginHref. Profile will not be rendered in Global Navigation',
+    );
+  }
+
+  if (!items && !href) return null;
+
+  if (items && href) {
+    console.warn(
+      'You have provided both loginHref and profileItems. loginUrl prop will be ignored by Global Navigation',
+    );
+  }
+
+  const profileComponent = items
+    ? { component: generateDropDown(generateAvatar(profileIconUrl), items) }
+    : { icon: SignInIcon, href };
+
+  return {
+    ...profileComponent,
+    ...(tooltip ? { tooltip, label: tooltip } : null),
+    ...compact(otherConfig),
+  };
+}
+
 export default function generateProductConfig(props: GlobalNavigationProps) {
-  // Add key only if prop is passed.
-  // TODO: Make this function less verbose
-  // TODO: Add warnings when improper pairs of props are passed. eg: profileItems with loginHref
-  const product = props.onProductClick && {
-    ...(props.productIcon ? { icon: props.productIcon } : null),
-    ...(props.onProductClick ? { onClick: props.onProductClick } : null),
-    ...(props.productTooltip
-      ? { tooltip: props.productTooltip, label: props.productTooltip }
-      : null),
-  };
-  const create = props.onCreateClick && {
-    ...(props.onCreateClick ? { onClick: props.onCreateClick } : null),
-    ...(props.createTooltip
-      ? { tooltip: props.createTooltip, label: props.createTooltip }
-      : null),
-  };
-  const search = props.onSearchClick && {
-    ...(props.onSearchClick ? { onClick: props.onSearchClick } : null),
-    ...(props.searchTooltip
-      ? { tooltip: props.searchTooltip, label: props.searchTooltip }
-      : null),
-  };
-  const yourWork = props.onYourWorkClick && {
-    ...(props.onYourWorkClick ? { onClick: props.onYourWorkClick } : null),
-    ...(props.yourWorkTooltip
-      ? { tooltip: props.yourWorkTooltip, label: props.yourWorkTooltip }
-      : null),
-  };
-  const notification = props.onNotificationClick && {
-    ...(props.onNotificationClick
-      ? { onClick: props.onNotificationClick }
-      : null),
-    ...(props.notificationTooltip
-      ? { tooltip: props.notificationTooltip, label: props.notificationTooltip }
-      : null),
-    ...(props.notificationCount
-      ? {
-          badge: ((notificationCount: number) => () => (
-            <Badge appearance="important" value={notificationCount} />
-          ))(props.notificationCount),
-        }
-      : null),
-  };
-  const people = props.onPeopleClick && {
-    ...(props.onPeopleClick ? { onClick: props.onPeopleClick } : null),
-    ...(props.peopleTooltip
-      ? { tooltip: props.peopleTooltip, label: props.peopleTooltip }
-      : null),
-  };
-  const appSwitcher = props.appSwitcherComponent && {
-    ...(props.appSwitcherComponent
-      ? {
-          component: props.appSwitcherComponent,
-        }
-      : null),
-  };
-  const help = props.helpItems && {
-    ...(props.helpTooltip
-      ? { tooltip: props.helpTooltip, label: props.helpTooltip }
-      : null),
-    ...(props.helpItems
-      ? {
-          component: (helpItems => generateDropDown(QuestionIcon, helpItems))(
-            props.helpItems,
-          ),
-        }
-      : null),
-  };
-  const profile = (props.profileItems || props.loginHref) && {
-    ...(props.profileTooltip
-      ? { tooltip: props.profileTooltip, label: props.profileTooltip }
-      : null),
-    ...(props.profileItems
-      ? {
-          component: (profileItems =>
-            generateDropDown(
-              generateAvatar(props.profileIconUrl),
-              profileItems,
-            ))(props.profileItems),
-        }
-      : { icon: SignInIcon }),
-    ...(props.loginHref && !props.profileItems
-      ? { href: props.loginHref }
-      : null),
+  const {
+    onProductClick,
+    productTooltip,
+    productIcon,
+    onCreateClick,
+    createTooltip,
+    onSearchClick,
+    searchTooltip,
+    onYourWorkClick,
+    yourWorkTooltip,
+    onNotificationClick,
+    notificationTooltip,
+    notificationCount,
+    onPeopleClick,
+    peopleTooltip,
+    helpItems,
+    helpTooltip,
+    profileItems,
+    profileTooltip,
+    loginHref,
+    profileIconUrl,
+    appSwitcherComponent: appSwitcher,
+  } = props;
+
+  const notificationBadge = {
+    badge: notificationCount
+      ? () => <Badge appearance="important" value={notificationCount} />
+      : null,
   };
 
-  return removeEmptyItems({
-    product,
-    create,
-    search,
-    yourWork,
-    notification,
-    people,
-    appSwitcher,
-    help,
-    profile,
+  return compact({
+    product: configFactory(onProductClick, productTooltip, {
+      icon: productIcon,
+    }),
+    create: configFactory(onCreateClick, createTooltip),
+    search: configFactory(onSearchClick, searchTooltip),
+    yourWork: configFactory(onYourWorkClick, yourWorkTooltip),
+    notification: configFactory(
+      onNotificationClick,
+      notificationTooltip,
+      notificationBadge,
+    ),
+    people: configFactory(onPeopleClick, peopleTooltip),
+    help: helpConfigFactory(helpItems, helpTooltip),
+    profile: profileConfigFactory(
+      profileItems,
+      profileTooltip,
+      loginHref,
+      profileIconUrl,
+    ),
+    appSwitcherComponent: appSwitcher ? { component: appSwitcher } : null,
   });
 }
