48958704a5a0e0ecc38bef974d623ce9493ef762
Pete Gleeson <pgleeson@atlassian.com>
2018-06-14 14:52:46 +1000

    removes manager and passes tests

--- packages/core/layer-manager/src/components/FocusLock/FocusLock.js
+++ packages/core/layer-manager/src/components/FocusLock/FocusLock.js
@@ -4,50 +4,7 @@ import NodeResolver from 'react-node-resolver';
 import createFocusTrap from 'focus-trap';
 import tabbable from 'tabbable';
 
-type Trap = Object;
-
-const createManager = () => {
-  let traps: Trap[] = [];
-  return {
-    add(trap: Trap, enabled: boolean) {
-      const [head, ...others] = traps;
-      if (enabled) {
-        if (head) {
-          head.pause();
-        }
-        traps = [trap.activate(), ...traps];
-      } else {
-        traps = head ? [head, trap, ...others] : [trap];
-      }
-    },
-    remove(trap: Trap) {
-      const [head, next] = traps;
-      trap.deactivate();
-      if (trap === head && next) {
-        next.unpause();
-      }
-      traps = traps.filter(t => t !== trap);
-    },
-    unpause(trap: Trap) {
-      const [head] = traps;
-      if (head && trap !== head) {
-        head.pause();
-      }
-      traps = [trap, ...traps.filter(t => t !== trap)];
-      trap.activate();
-    },
-    pause(trap: Trap) {
-      const [head, next, ...others] = traps;
-      trap.pause();
-      if (trap === head && next) {
-        next.activate();
-        traps = [next, head, ...others];
-      }
-    },
-  };
-};
-
-const { Consumer } = createContext(createManager());
+const { Consumer, Provider } = createContext(() => {});
 
 type FocusTrapProps = {
   children: Element<any>,
@@ -55,21 +12,26 @@ type FocusTrapProps = {
   enabled?: boolean,
 };
 
-class FocusTrap extends React.Component<FocusTrapProps & { manager: Object }> {
+class FocusTrap extends React.Component<
+  FocusTrapProps & { trapCreated: boolean => void },
+> {
   static defaultProps = {
     enabled: false,
   };
   trap: Object = {};
+  hasChildTrap = false;
   trapFocus = (wasEnabled: boolean, nowEnabled: boolean) => {
-    const { manager } = this.props;
     if (wasEnabled && !nowEnabled) {
-      manager.pause(this.trap);
+      this.trap.pause();
     } else if (!wasEnabled && nowEnabled) {
-      manager.unpause(this.trap);
+      this.trap.unpause();
     }
   };
+  onTrapCreated = enabled => {
+    this.hasChildTrap = enabled;
+  };
   resolveNode = (elem: HTMLElement) => {
-    const { initialFocus, enabled, manager } = this.props;
+    const { initialFocus, enabled, trapCreated } = this.props;
     if (elem) {
       const initialFocusElement = initialFocus || tabbable(elem)[0] || elem;
       this.trap = createFocusTrap(elem, {
@@ -77,23 +39,32 @@ class FocusTrap extends React.Component<FocusTrapProps & { manager: Object }> {
         clickOutsideDeactivates: true,
         escapeDeactivates: false,
       });
-      manager.add(this.trap, enabled);
+      trapCreated(enabled);
+      if (!this.hasChildTrap && enabled) {
+        this.trap.activate();
+      }
     }
   };
   componentWillUnmount() {
-    this.props.manager.remove(this.trap);
+    this.trap.deactivate();
   }
   componentDidUpdate(prevProps: FocusTrapProps) {
     this.trapFocus(!!prevProps.enabled, !!this.props.enabled);
   }
   render() {
     const { children } = this.props;
-    return <NodeResolver innerRef={this.resolveNode}>{children}</NodeResolver>;
+    return (
+      <Provider value={this.onTrapCreated}>
+        <NodeResolver innerRef={this.resolveNode}>{children}</NodeResolver>
+      </Provider>
+    );
   }
 }
 
 const FocusTrapConsumer = (props: FocusTrapProps) => (
-  <Consumer>{manager => <FocusTrap {...props} manager={manager} />}</Consumer>
+  <Consumer>
+    {trapCreated => <FocusTrap {...props} trapCreated={trapCreated} />}
+  </Consumer>
 );
 
 export default FocusTrapConsumer;
