1372e516aae8e6d1f15e693fefa052d46b07696a
James Newell <jnewell@atlassian.com>
2018-06-15 11:13:01 +1000

    add missing tests

--- packages/media/media-card/__tests__/links/GenericCardView/index.test.tsx
+++ packages/media/media-card/__tests__/links/GenericCardView/index.test.tsx
@@ -8,10 +8,9 @@ import {
 import { LinkCardGenericView } from '../../../src/links/cardGenericView';
 
 describe('LinkCardGenericView', () => {
-  // const site = 'Hello world';
-  // const linkUrl = 'http://localhost:9001/';
-  // const thumbnailUrl = 'http://localhost:9001/some/thumbnail';
-  // const iconUrl = 'http://localhost:9001/some/icon';
+  const site = 'Hello world';
+  const linkUrl = 'http://localhost:9001/';
+  const thumbnailUrl = 'http://localhost:9001/some/thumbnail';
   const errorMessage = 'Some random error occured';
 
   it('should render the error view when there is an error message', () => {
@@ -46,16 +45,73 @@ describe('LinkCardGenericView', () => {
     expect(element.find(BlockResolvedView)).toHaveLength(1);
   });
 
-  it('should render the context text when');
-  it('should render the context text when');
-  it('should render the context text when');
+  it('should render the context text when I have a site and url', () => {
+    const element = shallow(
+      <LinkCardGenericView site={site} linkUrl={linkUrl} />,
+    );
+    expect(element.find(BlockResolvedView).prop('context')).toEqual(
+      expect.objectContaining({ text: site }),
+    );
+  });
 
-  it('should render a title when');
-  it('should not render a title when');
+  it('should render the context text when I have a url', () => {
+    const element = shallow(<LinkCardGenericView linkUrl={linkUrl} />);
+    expect(element.find(BlockResolvedView).prop('context')).toEqual(
+      expect.objectContaining({ text: linkUrl }),
+    );
+  });
 
-  it('should render a description when');
-  it('should not render a description when');
+  it('should render empty context text', () => {
+    const element = shallow(<LinkCardGenericView />);
+    expect(element.find(BlockResolvedView).prop('context')).toEqual(
+      expect.objectContaining({ text: '' }),
+    );
+  });
 
-  it('should render a preview when');
-  it('should render a thumbnail when');
+  it('should render a title when there is a title', () => {
+    const element = shallow(<LinkCardGenericView title="foobar" />);
+    expect(element.find(BlockResolvedView).prop('title')).toEqual({
+      text: 'foobar',
+    });
+  });
+
+  it('should not render a title when there is no title', () => {
+    const element = shallow(<LinkCardGenericView />);
+    expect(element.find(BlockResolvedView).prop('title')).toBeUndefined();
+  });
+
+  it('should render a description when there is a description', () => {
+    const element = shallow(<LinkCardGenericView description="foobar" />);
+    expect(element.find(BlockResolvedView).prop('description')).toEqual({
+      text: 'foobar',
+    });
+  });
+
+  it('should not render a description when there is no description', () => {
+    const element = shallow(<LinkCardGenericView />);
+    expect(element.find(BlockResolvedView).prop('description')).toBeUndefined();
+  });
+
+  it('should render a preview when the card is square and we have a thumbnailUrl', () => {
+    const element = shallow(
+      <LinkCardGenericView appearance="square" thumbnailUrl={thumbnailUrl} />,
+    );
+    expect(element.find(BlockResolvedView).prop('preview')).toEqual(
+      thumbnailUrl,
+    );
+    expect(element.find(BlockResolvedView).prop('thumbnail')).toBeUndefined();
+  });
+
+  it('should render a thumbnail when the card is horizontal and we have a thumbnailUrl', () => {
+    const element = shallow(
+      <LinkCardGenericView
+        appearance="horizontal"
+        thumbnailUrl={thumbnailUrl}
+      />,
+    );
+    expect(element.find(BlockResolvedView).prop('preview')).toBeUndefined();
+    expect(element.find(BlockResolvedView).prop('thumbnail')).toEqual(
+      thumbnailUrl,
+    );
+  });
 });
