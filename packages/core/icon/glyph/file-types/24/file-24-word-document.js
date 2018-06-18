3eae148c429d95b8b744e2d696eb7d3c805eefb3
Raja Bellebon <rbellebon@atlassian.com>
2018-06-18 17:05:57 +1000

    Add files and objects new icons

---
+++ packages/core/icon/glyph/file-types/24/file-24-word-document.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _index = require('../../../es5/index');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var FileTypes24File24WordDocumentIcon = function FileTypes24File24WordDocumentIcon(props) {
+  return _react2.default.createElement(_index2.default, _extends({ dangerouslySetGlyph: '<svg width="24" height="24" viewBox="0 0 24 24" focusable="false" role="presentation"><path stroke="#004E8C" stroke-width="2" d="M3 1a2 2 0 0 0-2 2v18a2 2 0 0 0 2 2h18a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H3zm1.236 4.023L15.36 3.444a1.438 1.438 0 0 1 1.625 1.222c.012.1.012.1.014.202v14.266c0 .794-.644 1.438-1.438 1.438-.098-.003-.098-.003-.196-.014L4.24 19.02A1.437 1.437 0 0 1 3 17.596V6.446c0-.716.527-1.322 1.236-1.423zm5.763 5.314h.1l-.05.191-.05-.191zm2.098 4.406l.05.177h-.097l.047-.177zm.073.257l.113.396-.17.13-.187-.141.103-.385h.141zm1.198-5.046l.06-.228.658-.529.023.03-.16.727h-.581zm-3.47 0l-.08-.305.227-.176.231.179-.078.302h-.3zM8.043 14.92h-.09L8 14.754l.043.166zM6.16 9.954l-.163-.737.018-.024.65.522.064.24h-.57zM8.065 15l.103.392-.183.139-.168-.13L7.93 15h.135zm.108-5.204l.865 3.908-.976.216v-1h.078v1l-.969-.25 1.002-3.874zm3.758.023l1.014 3.846-.967.255v-1h.078v1l-.977-.214.852-3.887zm-1.882 5.037l-.927-3.244.961-.275v1h-.078v-1l.962.272-.918 3.247zM16 6h3.5A1.5 1.5 0 0 1 21 7.5v9a1.5 1.5 0 0 1-1.5 1.5H16V6z" fill-rule="evenodd"/></svg>' }, props));
+};
+exports.default = FileTypes24File24WordDocumentIcon;
