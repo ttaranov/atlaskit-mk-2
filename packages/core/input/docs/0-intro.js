d7fa810a5de84cb9d400dd48232bab932b3294fb
Raja Bellebon <rbellebon@atlassian.com>
2018-06-18 18:54:31 +1000

    Add docs to make the input documentation a bit clearer

---
+++ packages/core/input/docs/0-intro.js
@@ -0,0 +1,8 @@
+// @flow
+import React from 'react';
+import { md } from '@atlaskit/docs';
+
+export default md`
+  Input component is an internal component that renders a simple HTML input element.
+  It is used in inline-edit and in field-base to create field-text.
+`;
