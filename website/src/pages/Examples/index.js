1300ad309b31b7341077944dda64f84ae7c4feff
Ben Conolly <ben@thinkmill.com.au>
2018-06-18 11:21:32 +1000

    remove console log

--- website/src/pages/Examples/index.js
+++ website/src/pages/Examples/index.js
@@ -129,8 +129,6 @@ class ExampleNavigation extends Component {
     const { error } = this.state;
     const example = examples && examples.children.find(e => e.id === exampleId);
 
-    console.log(error);
-
     return (
       <Nav>
         <NavSection style={{ marginLeft: 8 }}>
