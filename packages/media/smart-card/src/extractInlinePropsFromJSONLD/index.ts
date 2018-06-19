c235f4555af256284186c70d027a32f62ec42bb9
James Newell <jnewell@atlassian.com>
2018-06-19 10:43:37 +1000

    move things in order to change case

--- packages/media/smart-card/src/extractInlinePropsFromJSONLD/index.ts
+++ packages/media/smart-card/src/extractInlinePropsFromJSONLD/index.ts
@@ -1,24 +1,18 @@
-import { BlockCard } from '@atlaskit/media-ui';
+import { InlineCard } from '@atlaskit/media-ui';
 import { genericExtractPropsFromJSONLD } from '../genericExtractPropsFromJSONLD';
 import { extractPropsFromObject } from './extractPropsFromObject';
-import { extractPropsFromDocument } from './extractPropsFromDocument';
-import { extractPropsFromSpreadsheet } from './extractPropsFromSpreadsheet';
 
 const extractorPrioritiesByType = {
   Object: 0,
-  Document: 5,
-  Spreadsheet: 10,
 };
 
 const extractorFunctionsByType = {
   Object: extractPropsFromObject,
-  Document: extractPropsFromDocument,
-  Spreadsheet: extractPropsFromSpreadsheet,
 };
 
-export function extractBlockPropsFromJSONLD(
+export function extractInlinePropsFromJSONLD(
   json: any,
-): BlockCard.ResolvedViewProps {
+): InlineCard.ResolvedViewProps {
   return genericExtractPropsFromJSONLD({
     extractorPrioritiesByType: extractorPrioritiesByType,
     extractorFunctionsByType: extractorFunctionsByType,
