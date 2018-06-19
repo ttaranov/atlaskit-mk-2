b620979116c196930f03378f471cce6111c89820
eduardo_soares <esoares@atlassian.com>
2018-06-19 16:17:26 +1000

    CHANGESET: merge analytics-next context into GAS payload attributes
    Summary: merge analytics-next context into GAS payload attributes
    
    Release notes: <none>
    
    Releases: @atlaskit/analytics-listeners@patch
    
    Dependents: []
    
    ---
    {"summary":"merge analytics-next context into GAS payload attributes","releases":[{"name":"@atlaskit/analytics-listeners","type":"patch"}],"dependents":[]}
    ---

--- packages/elements/analytics-listeners/__tests__/FabricElementsListenerSpec.tsx
+++ packages/elements/analytics-listeners/__tests__/FabricElementsListenerSpec.tsx
@@ -7,9 +7,10 @@ import FabricElementsListener, {
 import {
   DummyComponentWithAnalytics,
   TaggedDummyComponentWithAnalytics,
+  DummyComponentWithAttributesWithAnalytics,
   Props,
 } from '../examples/helpers';
-import { AnalyticsListener } from '@atlaskit/analytics-next';
+import { AnalyticsListener, AnalyticsContext } from '@atlaskit/analytics-next';
 import { AnalyticsWebClient } from '../src/types';
 
 describe('<FabricElementsListener />', () => {
@@ -58,21 +59,61 @@ describe('<FabricElementsListener />', () => {
     });
   };
 
-  it('should listen and fire an UI event with analyticsWebClient', () => {
-    fireAndVerifySentEvent(DummyComponentWithAnalytics, {
-      action: 'someAction',
-      actionSubject: 'someComponent',
-      source: 'unknown',
-      tags: [ELEMENTS_TAG],
+  describe('Listen and fire an UI event with analyticsWebClient', () => {
+    it('should fire event with elements tag', () => {
+      fireAndVerifySentEvent(DummyComponentWithAnalytics, {
+        action: 'someAction',
+        actionSubject: 'someComponent',
+        source: 'unknown',
+        tags: [ELEMENTS_TAG],
+      });
     });
-  });
 
-  it('should listen and fire an UI event with analyticsWebClient without duplicating the tag', () => {
-    fireAndVerifySentEvent(TaggedDummyComponentWithAnalytics, {
-      action: 'someAction',
-      actionSubject: 'someComponent',
-      source: 'unknown',
-      tags: [ELEMENTS_TAG, 'foo'],
+    it('should fire event without duplicating the tag', () => {
+      fireAndVerifySentEvent(TaggedDummyComponentWithAnalytics, {
+        action: 'someAction',
+        actionSubject: 'someComponent',
+        source: 'unknown',
+        tags: [ELEMENTS_TAG, 'foo'],
+      });
+    });
+
+    it('should fire event with context merged into the attributes', () => {
+      const component = mount(
+        <FabricElementsListener client={clientPromise} logger={loggerMock}>
+          <AnalyticsContext data={{ issueId: 100, greeting: 'hello' }}>
+            <AnalyticsContext data={{ issueId: 200, msg: 'boo' }}>
+              <AnalyticsContext data={{ issueId: 300 }}>
+                <DummyComponentWithAttributesWithAnalytics
+                  onClick={jest.fn()}
+                />
+              </AnalyticsContext>
+            </AnalyticsContext>
+          </AnalyticsContext>
+        </FabricElementsListener>,
+      );
+
+      const analyticsListener = component.find(AnalyticsListener);
+      const dummy = analyticsListener.find('#dummy');
+      dummy.simulate('click');
+
+      return clientPromise.then(client => {
+        expect(client.sendUIEvent).toBeCalledWith({
+          action: 'someAction',
+          actionSubject: 'someComponent',
+          source: 'unknown',
+          attributes: {
+            packageName: '@atlaskit/foo',
+            packageVersion: '1.0.0',
+            componentName: 'foo',
+            fooBar: 'yay',
+            greeting: 'hello',
+            msg: 'boo',
+            issueId: 300, // right most object attribute wins the conflict
+          },
+          tags: [ELEMENTS_TAG],
+        });
+      });
     });
   });
 });
--- packages/elements/analytics-listeners/src/FabricElementsListener.tsx
+++ packages/elements/analytics-listeners/src/FabricElementsListener.tsx
@@ -4,6 +4,8 @@ import { AnalyticsListener } from '@atlaskit/analytics-next';
 import { GasPayload } from '@atlaskit/analytics-gas-types';
 import { sendEvent } from './analytics-web-client-wrapper';
 import { ListenerProps } from './types';
+import * as reduce from 'lodash.reduce';
+import * as merge from 'lodash.merge';
 
 export const ELEMENTS_CHANNEL = 'fabric-elements';
 export const ELEMENTS_TAG = 'fabricElements';
@@ -12,17 +14,39 @@ export type ListenerFunction = (
   event: { payload: GasPayload; context: Array<{}> },
 ) => void;
 
+// merge all context objects from left to right. In case of attribute conflict the right one takes precedence
+const processContext = (contexts: Array<{}>) =>
+  reduce(contexts, (result, item) => merge(result || {}, item));
+
+const updatePayloadWithContext = (event: {
+  payload: GasPayload;
+  context: Array<{}>;
+}) => {
+  if (event.context.length == 0) {
+    return event.payload;
+  }
+  const mergedContext: any = processContext(event.context);
+  event.payload.attributes = merge(
+    mergedContext,
+    event.payload.attributes || {},
+  );
+  return event.payload;
+};
+
 export default class FabricElementsListener extends React.Component<
   ListenerProps
 > {
   listenerHandler: ListenerFunction = event => {
     const { client, logger } = this.props;
     if (event.payload) {
-      const tags: Set<string> = new Set(event.payload.tags || []);
+      const payload = updatePayloadWithContext(event);
+
+      const tags: Set<string> = new Set(payload.tags || []);
       tags.add(ELEMENTS_TAG);
-      event.payload.tags = Array.from(tags);
+      payload.tags = Array.from(tags);
+
+      sendEvent(client, logger)(payload);
     }
-    sendEvent(client, logger)(event.payload);
   };
 
   render() {
