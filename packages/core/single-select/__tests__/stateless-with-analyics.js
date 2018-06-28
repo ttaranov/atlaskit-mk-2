f7ab44f43731d8cb3280a5305a0b06a95b3bfd59
Pete Gleeson <pgleeson@atlassian.com>
2018-06-28 18:01:59 +1000

    fixes tests in datetime-picker and single-select

---
+++ packages/core/single-select/__tests__/stateless-with-analyics.js
@@ -0,0 +1,25 @@
+// @flow
+import React from 'react';
+import { mount } from 'enzyme';
+import { StatelessSelect as StatelessSelectWithAnalytics } from '../src';
+
+describe('StatelessSelectWithAnalytics', () => {
+  beforeEach(() => {
+    jest.spyOn(global.console, 'warn');
+    jest.spyOn(global.console, 'error');
+  });
+  afterEach(() => {
+    global.console.warn.mockRestore();
+    global.console.error.mockRestore();
+  });
+
+  it('should mount without errors', () => {
+    mount(
+      <StatelessSelectWithAnalytics isLoading isOpen={false} label="test" />,
+    );
+    /* eslint-disable no-console */
+    expect(console.warn).not.toHaveBeenCalled();
+    expect(console.error).not.toHaveBeenCalled();
+    /* eslint-enable no-console */
+  });
+});
