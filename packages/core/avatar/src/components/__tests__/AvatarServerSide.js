942baaa2ca5b76535be1f96941beeed3e2cf34d3
Raja Bellebon <rbellebon@atlassian.com>
2018-07-02 11:53:32 +1000

    WIP

---
+++ packages/core/avatar/src/components/__tests__/AvatarServerSide.js
@@ -0,0 +1,10 @@
+// @flow
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import Avatar from '../Avatar';
+
+describe('Avatar', () => {
+  it('should be possible to create a server side friendly component', () => {
+    expect(() => ReactDOMServer.renderToString(<Avatar />)).not.toThrowError();
+  });
+});
