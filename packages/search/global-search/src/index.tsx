c0eba252dfc840d96241172d466e886716ac5545
David Richard <drichard@atlassian.com>
2018-07-06 11:16:00 +1000

    XPSRCH-795: Remove unused export.

--- packages/search/global-search/src/index.tsx
+++ packages/search/global-search/src/index.tsx
@@ -7,6 +7,3 @@ export { Config } from './api/configureSearchClients';
 export {
   default as withFeedbackButton,
 } from './components/feedback/withFeedbackButton';
-
-export { default as esMessages } from './i18n/es';
-export { default as frMessages } from './i18n/fr';
