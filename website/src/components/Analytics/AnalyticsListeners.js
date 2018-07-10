383fa9b48d6384670f4ede6440f4027b8729f8f8
Pete Gleeson <pgleeson@atlassian.com>
2018-07-10 13:36:39 +1000

    plugs analytics-web-client into fabric listeners on the website

--- website/src/components/Analytics/AnalyticsListeners.js
+++ website/src/components/Analytics/AnalyticsListeners.js
@@ -1,17 +1,37 @@
 // @flow
 import React from 'react';
+import AnalyticsWebClient, {
+  envType,
+  originType,
+  tenantType,
+  userType,
+  eventType,
+  platformType,
+} from '@atlassiansox/analytics-web-client';
 import FabricAnalyticsListeners from '@atlaskit/analytics-listeners';
 import FeatureFlag from '../FeatureFlag';
 import GoogleAnalyticsListener from './GoogleAnalyticsListener';
 import { GOOGLE_ANALYTICS_ID } from '../../constants';
 
-// AK-4967 - replace with real implementation from analytics-web-client
-const mockAnalyticsClient = Promise.resolve({
-  sendUIEvent: e => console.log('sendUIEvent', e),
-  sendOperationalEvent: e => console.log('sendOperationalEvent', e),
-  sendTrackEvent: e => console.log('sendTrackEvent', e),
-  sendScreenEvent: e => console.log('sendScreenEvent', e),
+const env = websiteEnv => {
+  switch (websiteEnv) {
+    case 'production':
+      return envType.PROD;
+    case 'staging':
+      return envType.STAGING;
+    default:
+      return envType.LOCAL;
+  }
+};
+
+const analyticsClient = new AnalyticsWebClient({
+  env: env(WEBSITE_ENV),
+  product: 'atlaskitWebsite',
+  version: '1.0.0',
+  locale: 'en-US',
 });
+
+const clientPromise = Promise.resolve(analyticsClient);
 
 const AnalyticsListeners = ({ children }) => {
   return (
@@ -19,7 +39,7 @@ const AnalyticsListeners = ({ children }) => {
       <FeatureFlag name="send-analytics-to-pipeline">
         {yes =>
           yes ? (
-            <FabricAnalyticsListeners client={mockAnalyticsClient}>
+            <FabricAnalyticsListeners client={clientPromise}>
               {children}
             </FabricAnalyticsListeners>
           ) : (
