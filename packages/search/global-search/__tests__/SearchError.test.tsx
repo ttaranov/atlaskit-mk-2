aedcc4dfbce5af70e13ad3ce5623236c7a909eec
David Richard <drichard@atlassian.com>
2018-07-10 15:35:07 +1000

    XPSRCH-795: Fix them tests.

--- packages/search/global-search/__tests__/SearchError.test.tsx
+++ packages/search/global-search/__tests__/SearchError.test.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
-import { shallow, ShallowWrapper } from 'enzyme';
 import Button from '@atlaskit/button';
 import SearchError from '../src/components/SearchError';
+import { mountWithIntl } from './helpers/_intl-enzyme-test-helper';
 
 describe('SearchError', () => {
   it('should retry when clicking the try again button', () => {
     const retryMock = jest.fn();
-    const wrapper = shallow(<SearchError onRetryClick={retryMock} />);
+    const wrapper = mountWithIntl(<SearchError onRetryClick={retryMock} />);
 
-    wrapper.find(Button).prop('onClick')();
+    const onClick: Function = wrapper.find(Button).prop('onClick');
+    onClick();
     expect(retryMock).toHaveBeenCalled();
   });
 });
