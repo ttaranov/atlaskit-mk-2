7b78714b7215f0a7c2a7068c9a0cf6e6367012a6
Trey Shugart <treshugart@gmail.com>
2018-07-10 11:36:06 +1000

    Auto-merge component functions.

--- packages/core/theme/src/components/Theme.js
+++ packages/core/theme/src/components/Theme.js
@@ -7,11 +7,20 @@ type Props = {
   children: (*) => React.Node | React.Node,
 };
 
+const noop = () => null;
+
 export default function Theme({ children, ...props }: Props) {
   return (
     <Consumer>
       {theme => {
-        const merged = { ...theme, ...props };
+        const merged = { ...theme };
+
+        Object.keys(props).forEach(k => {
+          const v = props[k];
+          merged[k] =
+            typeof v === 'function' ? state => v(state, theme[k] || noop) : v;
+        });
+
         return typeof children === 'function' ? (
           children(merged)
         ) : (
