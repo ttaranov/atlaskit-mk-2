fe46cd2bc740f08e4d6f105e9263f46fc2134a92
Ahmed Fouad <afouad@atlassian.com>
2018-07-11 13:54:21 +1000

    XPSRCH-608 add unit tests

--- packages/search/global-search/__tests__/GlobalQuickSearch.test.tsx
+++ packages/search/global-search/__tests__/GlobalQuickSearch.test.tsx
@@ -4,7 +4,8 @@ import GlobalQuickSearchWithAnalytics, {
   GlobalQuickSearch,
   Props,
 } from '../src/components/GlobalQuickSearch';
-import { QuickSearch } from '@atlaskit/quick-search';
+import * as AnalyticsHelper from '../src/util/analytics-event-helper';
+import { ADVANCED_SEARCH_RESULT_ID } from '../src/components/SearchResultsUtil';
 
 const noop = () => {};
 const DEFAULT_PROPS = {
@@ -54,5 +55,160 @@ describe('GlobalQuickSearch', () => {
     onSearchInput({ target: { value: 'foo' } });
 
     expect(searchMock).toHaveBeenCalledWith('foo');
+  });
+
+  describe('Search result events', () => {
+    const searchSessionId = 'random-session-id';
+    let fireHighlightEventSpy;
+    let fireSearchResultSelectedEventSpy;
+    let fireAdvancedSearchSelectedEventSpy;
+    beforeEach(() => {
+      fireHighlightEventSpy = jest.spyOn(
+        AnalyticsHelper,
+        'fireHighlightedSearchResult',
+      );
+      fireSearchResultSelectedEventSpy = jest.spyOn(
+        AnalyticsHelper,
+        'fireSelectedSearchResult',
+      );
+      fireAdvancedSearchSelectedEventSpy = jest.spyOn(
+        AnalyticsHelper,
+        'fireSelectedAdvancedSearch',
+      );
+    });
+
+    afterEach(() => {
+      jest.resetAllMocks();
+    });
+
+    const deepRender = () =>
+      render({ searchSessionId })
+        .dive()
+        .dive()
+        .prop('firePrivateAnalyticsEvent');
+
+    ['ArrowUp', 'ArrowDown'].forEach(key => {
+      it('should trigger highlight event', () => {
+        const firePrivateAnalyticsEvent = deepRender();
+        const eventData = {
+          resultId: 'result-id',
+          type: 'recent-result',
+          contentType: 'confluence-page',
+          sectionIndex: 2,
+          index: 11,
+          indexWithinSection: 2,
+          key,
+        };
+        // call
+        firePrivateAnalyticsEvent(
+          'atlaskit.navigation.quick-search.keyboard-controls-used',
+          eventData,
+        );
+
+        // asserts
+        expect(fireHighlightEventSpy.mock.calls.length).toBe(1);
+        expect(fireHighlightEventSpy.mock.calls[0][0]).toMatchObject(eventData);
+        expect(fireHighlightEventSpy.mock.calls[0][1]).toBe(searchSessionId);
+
+        // verify other spies are not called
+        [
+          fireAdvancedSearchSelectedEventSpy,
+          fireSearchResultSelectedEventSpy,
+        ].forEach(spy => expect(spy.mock.calls.length).toBe(0));
+      });
+    });
+
+    it('should not fire highlight event on enter', () => {
+      const firePrivateAnalyticsEvent = deepRender();
+      const eventData = {
+        resultId: 'result-id',
+        type: 'recent-result',
+        contentType: 'confluence-page',
+        sectionIndex: 2,
+        index: 11,
+        indexWithinSection: 2,
+        key: 'Enter',
+      };
+      // call
+      firePrivateAnalyticsEvent(
+        'atlaskit.navigation.quick-search.keyboard-controls-used',
+        eventData,
+      );
+
+      // verify
+      [
+        fireAdvancedSearchSelectedEventSpy,
+        fireSearchResultSelectedEventSpy,
+        fireHighlightEventSpy,
+      ].forEach(spy => expect(spy.mock.calls.length).toBe(0));
+    });
+
+    it('should fire selected search event', () => {
+      const firePrivateAnalyticsEvent = deepRender();
+      const eventData = {
+        resultId: 'result-id',
+        type: 'recent-result',
+        contentType: 'confluence-page',
+        sectionIndex: 2,
+        index: 11,
+        indexWithinSection: 2,
+        method: 'click',
+        newTab: false,
+      };
+
+      // call
+      firePrivateAnalyticsEvent(
+        'atlaskit.navigation.quick-search.submit',
+        eventData,
+      );
+
+      // assert
+      expect(fireSearchResultSelectedEventSpy.mock.calls.length).toBe(1);
+      expect(fireSearchResultSelectedEventSpy.mock.calls[0][0]).toMatchObject(
+        eventData,
+      );
+      expect(fireSearchResultSelectedEventSpy.mock.calls[0][1]).toBe(
+        searchSessionId,
+      );
+
+      // verify
+      [fireAdvancedSearchSelectedEventSpy, fireHighlightEventSpy].forEach(spy =>
+        expect(spy.mock.calls.length).toBe(0),
+      );
+    });
+
+    it('should fire advanced search event', () => {
+      const firePrivateAnalyticsEvent = deepRender();
+      const eventData = {
+        resultId: ADVANCED_SEARCH_RESULT_ID,
+        type: 'recent-result',
+        contentType: 'confluence-page',
+        sectionIndex: 2,
+        index: 11,
+        indexWithinSection: 2,
+        method: 'click',
+        newTab: false,
+      };
+
+      // call
+      firePrivateAnalyticsEvent(
+        'atlaskit.navigation.quick-search.submit',
+        eventData,
+      );
+
+      // assert
+      expect(fireAdvancedSearchSelectedEventSpy.mock.calls.length).toBe(1);
+      expect(fireAdvancedSearchSelectedEventSpy.mock.calls[0][0]).toMatchObject(
+        eventData,
+      );
+      expect(fireAdvancedSearchSelectedEventSpy.mock.calls[0][1]).toBe(
+        searchSessionId,
+      );
+
+      // verify
+      [fireSearchResultSelectedEventSpy, fireHighlightEventSpy].forEach(spy =>
+        expect(spy.mock.calls.length).toBe(0),
+      );
+    });
   });
 });
