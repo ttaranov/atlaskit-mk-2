d2f853776b06fbb31a26e0c20b45b62798b1f579
Aleksandr Sasha Motsjonov <amotsjonov@atlassian.com>
2018-07-12 16:09:19 +1000

    Reverting interface changes

--- packages/elements/analytics-next-types/index.d.ts
+++ packages/elements/analytics-next-types/index.d.ts
@@ -124,7 +124,7 @@ interface AnalyticsEventCreator<TOwnProps> {
   (
     create: CreateUIAnalyticsEventSignature,
     props: TOwnProps,
-  ): UIAnalyticsEventInterface;
+  ): UIAnalyticsEventInterface | void;
 }
 
 export interface EventMap<TOwnProps> {
