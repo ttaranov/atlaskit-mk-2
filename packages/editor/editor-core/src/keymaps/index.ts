d23bc26fbc8d4f310d048c913f0e14189c73241d
David Richard <drichard@atlassian.com>
2018-07-13 16:32:20 +1000

    Run prettier over all editor files.

--- packages/editor/editor-core/src/keymaps/index.ts
+++ packages/editor/editor-core/src/keymaps/index.ts
@@ -10,7 +10,10 @@ export const toggleStrikethrough = makeKeyMapWithCommon(
   'Mod-Shift-s',
 );
 export const toggleCode = makeKeyMapWithCommon('Code', 'Mod-Shift-m');
-export const pastePlainText = makeKeyMapWithCommon('Paste Plain Text', 'Mod-Shift-v');
+export const pastePlainText = makeKeyMapWithCommon(
+  'Paste Plain Text',
+  'Mod-Shift-v',
+);
 export const clearFormatting = makeKeyMapWithCommon(
   'Clear formatting',
   'Mod-\\',
