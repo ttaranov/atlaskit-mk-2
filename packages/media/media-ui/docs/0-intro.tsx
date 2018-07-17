68e2f7ec36df2bc77b0e275800fd2cfff5defb2d
Ivan Demchenko <idemchenko@atlassian.com>
2018-07-17 15:53:42 +1000

    fixed build, yarn lock and markings

--- packages/media/media-ui/docs/0-intro.tsx
+++ packages/media/media-ui/docs/0-intro.tsx
@@ -24,81 +24,81 @@ export default md`
 
   * Link:
 
-    * ${'`text: string`'} - text to display
-    * ${'`onClick?: () => void`'} - the optional click handler
+    * \`text: string\` - text to display
+    * \`onClick?: () => void\` - the optional click handler
 
   * Resolving:
 
-    * ${'`url: string`'} - the url to display
-    * ${'`onClick?: () => void`'} - the optional click handler
+    * \`url: string\` - the url to display
+    * \`onClick?: () => void\` - the optional click handler
 
   * Forbidden
 
-    * ${'`url: string`'} - the url to display
-    * ${'`onClick?: () => void`'} - the optional click handler
-    * ${'`onAuthorise?: () => void`'} - what to do when a user hit "Try another account" button
+    * \`url: string\` - the url to display
+    * \`onClick?: () => void\` - the optional click handler
+    * \`onAuthorise?: () => void\` - what to do when a user hit "Try another account" button
 
   * Unauthorized
 
-    * ${'`url: string`'} - the url to display
-    * ${'`icon?: string`'} - the icon of the service (e.g. Dropbox/Asana/Google/etc) to display
-    * ${'`onClick?: () => void`'} - the optional click handler
-    * ${'`onAuthorise?: () => void`'} - handler for "Connect" button
+    * \`url: string\` - the url to display
+    * \`icon?: string\` - the icon of the service (e.g. Dropbox/Asana/Google/etc) to display
+    * \`onClick?: () => void\` - the optional click handler
+    * \`onAuthorise?: () => void\` - handler for "Connect" button
 
   * Errored
 
-    * ${'`url: string`'} - the url to display
-    * ${'`message: string`'} - the error message to display
-    * ${'`onClick?: () => void`'} - the optional click handler
-    * ${'`onRetry?: () => void`'} - what to do when a user clicks "Try again" button
+    * \`url: string\` - the url to display
+    * \`message: string\` - the error message to display
+    * \`onClick?: () => void\` - the optional click handler
+    * \`onRetry?: () => void\` - what to do when a user clicks "Try again" button
 
   * Resolved
 
-    * ${'`icon?: string`'} - the icon of the service (e.g. Dropbox/Asana/Google/etc) to display
-    * ${'`title: string`'} - the name of the resource
-    * ${'`lozenge?: LozengeViewModel`'} - the optional lozenge that might represent the statu of the resource, for example
-    * ${'`isSelected?: boolean`'} - a flag that determines whether the card is selected in edit mode
-    * ${'`onClick?: () => void`'} - the optional click handler
+    * \`icon?: string\` - the icon of the service (e.g. Dropbox/Asana/Google/etc) to display
+    * \`title: string\` - the name of the resource
+    * \`lozenge?: LozengeViewModel\` - the optional lozenge that might represent the statu of the resource, for example
+    * \`isSelected?: boolean\` - a flag that determines whether the card is selected in edit mode
+    * \`onClick?: () => void\` - the optional click handler
 
   ### BlockCard
 
   * Resolving:
 
-    * ${'`onClick?`'} - the optional click handler
+    * \`onClick?\` - the optional click handler
 
   * Forbidden
 
-    * ${'`url: string`'} - the url of the resorce to display
-    * ${'`onClick?: () => void`'} - the click hander that determines what to do when used clicked on the card
-    * ${'`onAuthorise?: () => void`'} - what to do when a user hit "Try another account" button
+    * \`url: string\` - the url of the resorce to display
+    * \`onClick?: () => void\` - the click hander that determines what to do when used clicked on the card
+    * \`onAuthorise?: () => void\` - what to do when a user hit "Try another account" button
 
   * Unauthorized
 
-    * ${'`icon?: string;`'} - the icon of the service (e.g. Dropbox/Asana/Google/etc) to display
-    * ${'`url: string`'} - the url of the resorce to display
-    * ${'`onClick?: () => void`'} - the click hander that determines what to do when used clicked on the card
-    * ${'`onAuthorise?: () => void`'} - handler for "Connect" button
+    * \`icon?: string;\` - the icon of the service (e.g. Dropbox/Asana/Google/etc) to display
+    * \`url: string\` - the url of the resorce to display
+    * \`onClick?: () => void\` - the click hander that determines what to do when used clicked on the card
+    * \`onAuthorise?: () => void\` - handler for "Connect" button
 
   * Errored
 
-    * ${'`url: string`'} - the url of the resorce to display
-    * ${'`message: string`'} - the error message
-    * ${'`onClick?: () => void`'} - the optional click hander
-    * ${'`onRetry?: () => void`'} - what to do when a user clicks "Try again" button
+    * \`url: string\` - the url of the resorce to display
+    * \`message: string\` - the error message
+    * \`onClick?: () => void\` - the optional click hander
+    * \`onRetry?: () => void\` - what to do when a user clicks "Try again" button
 
   * Resolved
 
-      * ${'`context?: ContextViewModel`'}
-      * ${'`link?: string`'}
-      * ${'`icon?: IconWithTooltip`'}
-      * ${'`user?: UserViewModel`'}
-      * ${'`thumbnail?: string`'}
-      * ${'`preview?: string`'}
-      * ${'`title?: TextWithTooltip`'}
-      * ${'`byline?: TextWithTooltip`'}
-      * ${'`description?: TextWithTooltip`'}
-      * ${'`details?: DetailViewModel[]`'}
-      * ${'`users?: UserViewModel[]`'}
-      * ${'`actions?: Action[]`'}
-      * ${'`onClick?: () => void`'}
+    * \`context?: ContextViewModel\`
+    * \`link?: string\`
+    * \`icon?: IconWithTooltip\`
+    * \`user?: UserViewModel\`
+    * \`thumbnail?: string\`
+    * \`preview?: string\`
+    * \`title?: TextWithTooltip\`
+    * \`byline?: TextWithTooltip\`
+    * \`description?: TextWithTooltip\`
+    * \`details?: DetailViewModel[]\`
+    * \`users?: UserViewModel[]\`
+    * \`actions?: Action[]\`
+    * \`onClick?: () => void\`
 `;
