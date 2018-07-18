145644ecf191af905b395d6c84a281b673e79ff7
Damien Klinnert <dklinnert@atlassian.com>
2018-07-18 21:53:26 +0200

    move all zooming logic into InteractiveImg

--- packages/media/media-viewer/src/newgen/viewers/image/interactive-img.tsx
+++ packages/media/media-viewer/src/newgen/viewers/image/interactive-img.tsx
@@ -1,15 +1,28 @@
 import * as React from 'react';
-import { Img } from '../../styled';
+import { ImageWrapper, Img } from '../../styled';
 import { ZoomLevel } from '../../domain/zoomLevel';
+import { closeOnDirectClick } from '../../utils/closeOnDirectClick';
+import { ZoomControls } from '../../zoomControls';
 
 export type Props = {
   src: string;
+  onClose?: () => void;
+};
+
+export type State = {
   zoomLevel: ZoomLevel;
 };
 
-export class InteractiveImg extends React.Component<Props, {}> {
+const initialState: State = {
+  zoomLevel: new ZoomLevel(),
+};
+
+export class InteractiveImg extends React.Component<Props, State> {
+  state: State = initialState;
+
   render() {
-    const { src, zoomLevel } = this.props;
+    const { src, onClose } = this.props;
+    const { zoomLevel } = this.state;
     // We need to set new border value every time the zoom changes
     // to force a re layout in Chrome.
     // https://stackoverflow.com/questions/16687023/bug-with-transform-scale-and-overflow-hidden-in-chrome
@@ -19,6 +32,15 @@ export class InteractiveImg extends React.Component<Props, {}> {
       transform: `scale(${zoomLevel.value})`,
       border,
     };
-    return <Img src={src} style={imgStyle} />;
+    return (
+      <ImageWrapper onClick={closeOnDirectClick(onClose)}>
+        <Img src={src} style={imgStyle} />;
+        <ZoomControls zoomLevel={zoomLevel} onChange={this.onZoomChange} />
+      </ImageWrapper>
+    );
   }
+
+  private onZoomChange = (zoomLevel: ZoomLevel) => {
+    this.setState({ zoomLevel });
+  };
 }
