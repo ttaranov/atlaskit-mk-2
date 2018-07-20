f94b67781f5672152db709ab94e02e82cae593eb
Craig Petchell <cpetchell@atlassian.com>
2018-07-20 13:50:52 +1000

    FS-2816 - test focus on click behaviour for Message editor

--- packages/editor/editor-core/__tests__/ui/Message.tsx
+++ packages/editor/editor-core/__tests__/ui/Message.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import Message from '../../src/ui/Appearance/Message';
+import EditorActions from '../../src/actions';
 import { createEditor, doc, p } from '@atlaskit/editor-test-helpers';
 import { mount } from 'enzyme';
 
@@ -55,12 +56,52 @@ describe('message editor', () => {
   });
 
   describe('focus', () => {
-    it('should focus when clicking anywhere on editor', () => {
-      expect(true).toBe(false);
+    let editorActionsFocusSpy;
+    let messageFocusEditor;
+
+    beforeEach(() => {
+      editorActionsFocusSpy = jest.fn();
+      const editorActions = {
+        focus: editorActionsFocusSpy,
+      } as EditorActions;
+      const { editorView } = editor(doc(p('Hello world')));
+      const message = mount(
+        <Message
+          editorView={editorView}
+          providerFactory={{} as any}
+          editorDOMElement={<div />}
+          editorActions={editorActions}
+        />,
+      );
+
+      // Test private method
+      messageFocusEditor = (message.instance() as any).focusEditor;
+    });
+
+    it('should focus when clicking anywhere but a popup', () => {
+      const closest = jest.fn();
+      // no popup
+      closest.mockReturnValue(undefined);
+      messageFocusEditor({
+        target: {
+          closest,
+        },
+      });
+      expect(closest).toHaveBeenCalledWith('[data-editor-popup]');
+      expect(editorActionsFocusSpy).toHaveBeenCalled();
     });
 
     it('should not focus when clicking within a popup', () => {
-      expect(true).toBe(false);
+      const closest = jest.fn();
+      // found popup
+      closest.mockReturnValue({});
+      messageFocusEditor({
+        target: {
+          closest,
+        },
+      });
+      expect(closest).toHaveBeenCalledWith('[data-editor-popup]');
+      expect(editorActionsFocusSpy).not.toHaveBeenCalled();
     });
   });
 });
