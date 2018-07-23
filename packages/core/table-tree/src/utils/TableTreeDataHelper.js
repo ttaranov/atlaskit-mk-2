378c74b32628e9ed2973aa894e9dccccf206ada1
Ajay Narain Mathur <amathur@atlassian.com>
2018-07-23 10:36:40 +1000

    added append item method to table tree helper class

--- packages/core/table-tree/src/utils/TableTreeDataHelper.js
+++ packages/core/table-tree/src/utils/TableTreeDataHelper.js
@@ -62,4 +62,54 @@ export default class TableTreeDataHelper {
 
     return addChildItems(items, allItems, parentItem);
   }
+
+  appendItems(
+    items: Array<Object>,
+    allItems: Array<Object> = [],
+    parentItem: ?Object,
+  ) {
+    const appendRootItems = (rootItems: Array<Object>) => {
+      rootItems.forEach((rootItem, index) => {
+        const key = rootItem[this.key];
+        if (key === undefined) {
+          throw new Error(
+            `No property '${this.key}' found in rootItem[${index}]`,
+          );
+        }
+        this.keysCache[key] = index + allItems.length;
+      });
+      return allItems.concat(rootItems);
+    };
+
+    const appendChildItems = (
+      newitems: Array<Object>,
+      allTableItems: Array<Object>,
+      itemParent: Object,
+    ) => {
+      const parentCacheKey = itemParent[this.key];
+      if (parentCacheKey === undefined) {
+        throw new Error(`No property '${this.key}' found in parent item`);
+      }
+      const parentLocation = this.keysCache[parentCacheKey];
+
+      const allItemsCopy = [...allTableItems];
+      const objectToChange = get(allItemsCopy, parentLocation);
+      const existingChildren = objectToChange.children || [];
+      objectToChange.children = existingChildren.concat(newitems);
+
+      // Update cache
+      newitems.forEach((item, index) => {
+        this.keysCache[item[this.key]] = `${parentLocation}.children[${index +
+          existingChildren.length}]`;
+      });
+
+      return set(allItemsCopy, parentLocation, objectToChange);
+    };
+
+    if (!parentItem) {
+      return appendRootItems(items, allItems);
+    }
+
+    return appendChildItems(items, allItems, parentItem);
+  }
 }
