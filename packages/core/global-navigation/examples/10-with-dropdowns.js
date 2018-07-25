c65b9ae6bb6ce509fe336aad0183a49b248a02fa
Harsha Ivaturi <isriharsha@atlassian.com>
2018-07-25 13:04:44 +1000

    Fix dropdown example

--- packages/core/global-navigation/examples/10-with-dropdowns.js
+++ packages/core/global-navigation/examples/10-with-dropdowns.js
@@ -2,27 +2,20 @@
 
 import React from 'react';
 import { LayoutManager, NavigationProvider } from '@atlaskit/navigation-next';
-import Dropdown, {
-  DropdownItemGroup,
-  DropdownItem,
-} from '@atlaskit/dropdown-menu';
+import { DropdownItemGroup, DropdownItem } from '@atlaskit/dropdown-menu';
 
 import GlobalNavigation from '../src';
 
 const ExampleDropdown = () => (
-  <Dropdown>
-    <DropdownItemGroup title="Heading">
-      <DropdownItem>
-        Hello it with some really quite long text here.
-      </DropdownItem>
-      <DropdownItem>Some text 2</DropdownItem>
-      <DropdownItem isDisabled>Some disabled text</DropdownItem>
-      <DropdownItem>Some more text</DropdownItem>
-      <DropdownItem href="//atlassian.com" target="_new">
-        A link item
-      </DropdownItem>
-    </DropdownItemGroup>
-  </Dropdown>
+  <DropdownItemGroup title="Heading">
+    <DropdownItem>Hello it with some really quite long text here.</DropdownItem>
+    <DropdownItem>Some text 2</DropdownItem>
+    <DropdownItem isDisabled>Some disabled text</DropdownItem>
+    <DropdownItem>Some more text</DropdownItem>
+    <DropdownItem href="//atlassian.com" target="_new">
+      A link item
+    </DropdownItem>
+  </DropdownItemGroup>
 );
 
 const GlobalNav = () => (
