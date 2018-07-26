4218c50dbb463657a2d5b688410be00696b7343f
Damien Klinnert <dklinnert@atlassian.com>
2018-07-26 11:05:03 +0200

    MSW-701: zoom into viewport center

--- packages/media/media-viewer/__tests__/newgen/viewers/image/interactive-img.spec.tsx
+++ packages/media/media-viewer/__tests__/newgen/viewers/image/interactive-img.spec.tsx
@@ -3,15 +3,26 @@ import { mount } from 'enzyme';
 import Button from '@atlaskit/button';
 import { InteractiveImg } from '../../../../src/newgen/viewers/image/interactive-img';
 import { ZoomControls } from '../../../../src/newgen/zoomControls';
-import { Rectangle } from '../../../../src/newgen/domain/rectangle';
 import { Img } from '../../../../src/newgen/styled';
 import { ZoomLevel } from '../../../../src/newgen/domain/zoomLevel';
-import { computeProjection } from '../../../../src/newgen/domain/projection';
+import { Camera, Rectangle } from '../../../../src/newgen/domain/camera';
 
 function createFixture() {
   const onClose = jest.fn();
   const el = mount(<InteractiveImg src={''} onClose={onClose} />);
-  return { el, onClose };
+  const viewport = new Rectangle(400, 300);
+  const originalImg = new Rectangle(800, 600);
+  const camera = new Camera(viewport, originalImg);
+  const zoomLevel = new ZoomLevel(1);
+
+  el.setState({
+    camera: {
+      status: 'SUCCESSFUL',
+      data: camera,
+    },
+    zoomLevel,
+  });
+  return { el, onClose, camera, zoomLevel };
 }
 
 describe('InteractiveImg', () => {
@@ -35,23 +46,9 @@ describe('InteractiveImg', () => {
   });
 
   it('sets the correct width and height on the Img element', () => {
-    const { el } = createFixture();
-    const viewport = new Rectangle(400, 300);
-    const img = new Rectangle(800, 600);
-    const zoomLevel = new ZoomLevel(1);
+    const { el, camera, zoomLevel } = createFixture();
 
-    el.setState({
-      dimensions: {
-        status: 'SUCCESSFUL',
-        data: {
-          viewport,
-          img,
-        },
-      },
-      zoomLevel,
-    });
-    const { width, height } = computeProjection(img, viewport, zoomLevel.value);
     const styleProp = el.find(Img).prop('style');
-    expect(styleProp).toEqual({ width, height });
+    expect(styleProp).toEqual(camera.scaledImg(zoomLevel.value));
   });
 });
