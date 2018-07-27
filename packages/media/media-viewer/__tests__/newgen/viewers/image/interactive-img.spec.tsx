7e97d7a4592b3832c9ce48c320d7203ed8eb8a36
Damien Klinnert <dklinnert@atlassian.com>
2018-07-27 07:16:51 +0200

    add tests for InteractiveImg

--- packages/media/media-viewer/__tests__/newgen/viewers/image/interactive-img.spec.tsx
+++ packages/media/media-viewer/__tests__/newgen/viewers/image/interactive-img.spec.tsx
@@ -1,11 +1,15 @@
 import * as React from 'react';
-import { mount } from 'enzyme';
+import { mount, ReactWrapper } from 'enzyme';
 import Button from '@atlaskit/button';
 import { InteractiveImg } from '../../../../src/newgen/viewers/image/interactive-img';
 import { ZoomControls } from '../../../../src/newgen/zoomControls';
-import { Img } from '../../../../src/newgen/styled';
+import { ImageWrapper, Img } from '../../../../src/newgen/styled';
 import { ZoomLevel } from '../../../../src/newgen/domain/zoomLevel';
-import { Camera, Rectangle } from '../../../../src/newgen/domain/camera';
+import {
+  Camera,
+  Rectangle,
+  Vector2,
+} from '../../../../src/newgen/domain/camera';
 
 function createFixture() {
   const onClose = jest.fn();
@@ -25,30 +29,58 @@ function createFixture() {
   return { el, onClose, camera, zoomLevel };
 }
 
+function clickZoomIn(el: ReactWrapper<any, any>) {
+  el
+    .find(ZoomControls)
+    .find(Button)
+    .last()
+    .simulate('click');
+}
+
+function clickZoomOut(el: ReactWrapper<any, any>) {
+  el
+    .find(ZoomControls)
+    .find(Button)
+    .first()
+    .simulate('click');
+}
+
 describe('InteractiveImg', () => {
   it('it allows zooming', async () => {
     const { el } = createFixture();
+    expect(el.find(ZoomControls)).toHaveLength(1);
 
     expect(el.state('zoomLevel').value).toEqual(1);
-    expect(el.find(ZoomControls)).toHaveLength(1);
-    el
-      .find(ZoomControls)
-      .find(Button)
-      .first()
-      .simulate('click');
+
+    clickZoomOut(el);
     expect(el.state('zoomLevel').value).toBeLessThan(1);
-    el
-      .find(ZoomControls)
-      .find(Button)
-      .last()
-      .simulate('click');
+
+    clickZoomIn(el);
     expect(el.state('zoomLevel').value).toEqual(1);
   });
 
   it('sets the correct width and height on the Img element', () => {
     const { el, camera, zoomLevel } = createFixture();
-
     const styleProp = el.find(Img).prop('style');
     expect(styleProp).toEqual(camera.scaledImg(zoomLevel.value));
+  });
+
+  it('sets the correct scrollLeft and scrollTop values on the ImageWrapper', () => {
+    const { el, camera, zoomLevel } = createFixture();
+    const imgWrapper = el.find(ImageWrapper).getDOMNode();
+
+    const prevOffset = new Vector2(imgWrapper.scrollLeft, imgWrapper.scrollTop);
+    const prevScale = zoomLevel.value;
+    const nextScale = zoomLevel.zoomIn().value;
+
+    clickZoomIn(el);
+
+    const expectedOffset = camera.scaledOffset(
+      prevOffset,
+      prevScale,
+      nextScale,
+    );
+    expect(imgWrapper.scrollLeft).toEqual(expectedOffset.x);
+    expect(imgWrapper.scrollTop).toEqual(expectedOffset.y);
   });
 });
