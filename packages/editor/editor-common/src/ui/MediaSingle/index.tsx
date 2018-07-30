01146cca9eabccdfaff4945d771b3f8fecfecc4a
Jake Coppinger <jake@jakecoppinger.com>
2018-07-30 15:16:28 +1000

    Cleaning up

--- packages/editor/editor-common/src/ui/MediaSingle/index.tsx
+++ packages/editor/editor-common/src/ui/MediaSingle/index.tsx
@@ -24,9 +24,6 @@ export interface Props {
 }
 
 const fixedWidthPercentages: number[] = [0.5, 0.75, 1];
-// const fixedWidthPixels: number[] = fixedWidthPercentages.map(
-//   percentage => percentage * maxImageWidth,
-// );
 const maxImageWidth: number = 680;
 
 class MediaSingle extends React.Component<Props> {
@@ -50,45 +47,39 @@ class MediaSingle extends React.Component<Props> {
       }))
       .reduce((prev, curr) => (curr.diff < prev.diff ? curr : prev));
 
-    console.log({ delta, imagePercentage, closestPercentage });
-
     if (this.props.updatePercentage) {
-      console.log('setting percentage to ', closestPercentage.percentage);
       this.props.updatePercentage(
         closestPercentage.percentage as MediaSingleImagePercentage,
       );
     }
   };
 
-  mapSize = (size: { width: number; height: number }) => {
-    console.log({ maxImageWidth });
-    const imagePercentage: number = size.width / maxImageWidth;
-    const maxImageHeight: number = size.height / imagePercentage;
-
-    // maxImageWidth * ratio / aspectRatio
-
+  mapPercentage = (percentage: number) => {
     const closestPercentage: {
-      percentage: number;
+      p: number;
       diff: number;
     } = fixedWidthPercentages
-      .map(percentage => ({
-        percentage,
-        diff: Math.abs(imagePercentage - percentage),
+      .map(p => ({
+        p,
+        diff: Math.abs(percentage - p),
       }))
       .reduce((prev, curr) => (curr.diff < prev.diff ? curr : prev));
 
     console.log(closestPercentage);
 
-    const newPercentage: number =
-      closestPercentage.diff < 0.05
-        ? closestPercentage.percentage
-        : imagePercentage;
+    return closestPercentage.diff < 0.05 ? closestPercentage.p : percentage;
+  };
 
-    const newSize = {
+  mapSize = (size: { width: number; height: number }) => {
+    console.log({ maxImageWidth });
+    const imagePercentage: number = size.width / maxImageWidth;
+    const newPercentage = this.mapPercentage(imagePercentage);
+    const maxImageHeight: number = size.height / imagePercentage;
+
+    return {
       width: maxImageWidth * newPercentage,
       height: maxImageHeight * newPercentage,
     };
-    return newSize;
   };
 
   render() {
@@ -101,11 +92,9 @@ class MediaSingle extends React.Component<Props> {
           height: maxImageWidth * percentage / aspectRatio,
         }}
         lockAspectRatio={aspectRatio}
-        // grid={[gridSize, aspectRatio ? gridSize / aspectRatio : gridSize]}
         maxWidth="100%"
         onResizeStop={this.handleResizeStop}
         mapSize={this.mapSize}
-        // onResizeStop={this.handleResizeStop}
       >
         {React.Children.only(children)}
       </Resizable>
