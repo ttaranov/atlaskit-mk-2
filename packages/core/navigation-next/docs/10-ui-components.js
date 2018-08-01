54fcc70ea86627bcb3dd89549303939d8dfc5179
Jared Crowe <jcrowe@atlassian.com>
2018-08-01 18:18:39 +1000

    start writing views controller docs

--- packages/core/navigation-next/docs/10-ui-components.js
+++ packages/core/navigation-next/docs/10-ui-components.js
@@ -3,35 +3,27 @@
 import React from 'react';
 import { Example, md, Props } from '@atlaskit/docs';
 
-export default md`## Contents
+import { Contents, ContentsProvider, H } from './shared';
 
-* [ContainerHeader](#ContainerHeader)
-* [GlobalItem](#GlobalItem)
-* [GlobalNav](#GlobalNav)
-* [Group](#Group)
-* [GroupHeading](#GroupHeading)
-* [Item](#Item)
-* [ItemAvatar](#ItemAvatar)
-* [LayoutManager](#LayoutManager)
-* [Section](#Section)
-* [Separator](#Separator)
+export default (
+  <ContentsProvider>{md`
+${<Contents />}
 
 &nbsp;
-
+  
 ***
 
-<a name="ContainerHeader"></a>
-# ContainerHeader
+${<H>ContainerHeader</H>}
 
 The ContainerHeader is intended to be used in the container navigation layer for a project, space, etc. It is essentially an Item with some custom styles applied to it and has an almost identical props interface. The only difference is that the ContainerHeader does not accept a spacing prop.
 
 ${(
-  <Example
-    Component={require('./examples/ui-components/ContainerHeader').default}
-    title="ContainerHeader"
-    source={require('!!raw-loader!./examples/ui-components/ContainerHeader')}
-  />
-)}
+    <Example
+      Component={require('./examples/ui-components/ContainerHeader').default}
+      title="ContainerHeader"
+      source={require('!!raw-loader!./examples/ui-components/ContainerHeader')}
+    />
+  )}
 
 ## ContainerHeader props
 
@@ -41,156 +33,148 @@ See [Item](#Item).
 
 ***
 
-<a name="GlobalItem"></a>
-# GlobalItem
+${<H>GlobalItem</H>}
 
 GlobalItems are rendered inside the global navigation layer.
 
 ${(
-  <Example
-    Component={require('./examples/ui-components/GlobalItem').default}
-    title="GlobalItem"
-    source={require('!!raw-loader!./examples/ui-components/GlobalItem')}
-  />
-)}
+    <Example
+      Component={require('./examples/ui-components/GlobalItem').default}
+      title="GlobalItem"
+      source={require('!!raw-loader!./examples/ui-components/GlobalItem')}
+    />
+  )}
 
 ${(
-  <Props
-    heading="GlobalItem props"
-    props={require('!!extract-react-types-loader!../src/components/GlobalItem')}
-  />
-)}
+    <Props
+      heading="GlobalItem props"
+      props={require('!!extract-react-types-loader!../src/components/GlobalItem')}
+    />
+  )}
 
 ***
 
-<a name="GlobalNav"></a>
-# GlobalNav
+${<H>GlobalNav</H>}
 
 If you're building an Atlassian product you probably don't want to use this component directly. Please take a look at the @atlaskit/global-navigation component instead.
 
 The GlobalNav component is a primitive layout component which renders GlobalItems into one of two slots.
 
 ${(
-  <Props
-    heading="GlobalNav props"
-    props={require('!!extract-react-types-loader!../src/components/GlobalNav')}
-  />
-)}
+    <Props
+      heading="GlobalNav props"
+      props={require('!!extract-react-types-loader!../src/components/GlobalNav')}
+    />
+  )}
 
 ***
 
-<a name="Group"></a>
-# Group
+${<H>Group</H>}
 
 A useful component for rendering a group of Items with a heading and a separator. The heading and separator will only be rendered if the Group has children.
 
 A section of the product or container navigation may contain multiple groups.
 
 ${(
-  <Example
-    Component={require('./examples/ui-components/Group').default}
-    title="Group"
-    source={require('!!raw-loader!./examples/ui-components/Group')}
-  />
-)}
+    <Example
+      Component={require('./examples/ui-components/Group').default}
+      title="Group"
+      source={require('!!raw-loader!./examples/ui-components/Group')}
+    />
+  )}
 
 ${(
-  <Props
-    heading="Group props"
-    props={require('!!extract-react-types-loader!../src/components/Group')}
-  />
-)}
+    <Props
+      heading="Group props"
+      props={require('!!extract-react-types-loader!../src/components/Group')}
+    />
+  )}
 
 ***
 
-<a name="GroupHeading"></a>
-# GroupHeading
+${<H>GroupHeading</H>}
 
 The heading for a group of items.
 
 ${(
-  <Props
-    heading="GroupHeading props"
-    props={require('!!extract-react-types-loader!../src/components/GroupHeading')}
-  />
-)}
+    <Props
+      heading="GroupHeading props"
+      props={require('!!extract-react-types-loader!../src/components/GroupHeading')}
+    />
+  )}
 
 ***
 
-<a name="Item"></a>
-# Item
+${<H>Item</H>}
 
 ${(
-  <Example
-    Component={require('./examples/ui-components/Item').default}
-    title="Item"
-    source={require('!!raw-loader!./examples/ui-components/Item')}
-  />
-)}
+    <Example
+      Component={require('./examples/ui-components/Item').default}
+      title="Item"
+      source={require('!!raw-loader!./examples/ui-components/Item')}
+    />
+  )}
 
 ${(
-  <Props
-    heading="Item props"
-    props={require('!!extract-react-types-loader!../src/components/Item')}
-  />
-)}
+    <Props
+      heading="Item props"
+      props={require('!!extract-react-types-loader!../src/components/Item')}
+    />
+  )}
 
 ***
 
-<a name="ItemAvatar"></a>
-# ItemAvatar
+${<H>ItemAvatar</H>}
 
 The ItemAvatar is a useful wrapper around Atlaskit's Avatar component, which will update its background color based on the state of the Item that it's in. It's intended to be used inside the before or after props of an Item, and you need to pass through the provided item state.
 
 ${(
-  <Props
-    heading="ItemAvatar props"
-    props={require('!!extract-react-types-loader!../src/components/ItemAvatar')}
-  />
-)}
+    <Props
+      heading="ItemAvatar props"
+      props={require('!!extract-react-types-loader!../src/components/ItemAvatar')}
+    />
+  )}
 
 ***
 
-<a name="LayoutManager"></a>
-# LayoutManager
+${<H>LayoutManager</H>}
 
 The LayoutManager is the entrypoint to navigation. It is in charge of laying out the global, product, and container layers of navigation, along with your page content. Read the [Getting Started guide](/packages/core/navigation-next/docs/composing-your-navigation) for for information.
 
 ${(
-  <Props
-    heading="LayoutManager props"
-    props={require('!!extract-react-types-loader!../src/components/LayoutManager')}
-  />
-)}
+    <Props
+      heading="LayoutManager props"
+      props={require('!!extract-react-types-loader!../src/components/LayoutManager')}
+    />
+  )}
 
 ***
 
-<a name="Section"></a>
-# Section
+${<H>Section</H>}
 
 The product or container navigation layers can be separated into Sections - a navigation is essentially a flat array of Sections. Within a Section, use Groups for further levels of division.
 
 Use Sections to perform nesting transitions. If a Section's props update and its parentId matches its previous id, or vice versa, it will automatically perform a nested navigation animation as it re-renders.
 
 ${(
-  <Example
-    Component={require('../examples/30-section').default}
-    title="Section"
-    source={require('!!raw-loader!../examples/30-section')}
-  />
-)}
+    <Example
+      Component={require('../examples/30-section').default}
+      title="Section"
+      source={require('!!raw-loader!../examples/30-section')}
+    />
+  )}
 
 ${(
-  <Props
-    heading="Section props"
-    props={require('!!extract-react-types-loader!../src/components/Section')}
-  />
-)}
+    <Props
+      heading="Section props"
+      props={require('!!extract-react-types-loader!../src/components/Section')}
+    />
+  )}
 
 ***
 
-<a name="Separator"></a>
-# Separator
+${<H>Separator</H>}
 
 Separates a group of items. This component takes no props.
-`;
+`}</ContentsProvider>
+);
---
+++ packages/core/navigation-next/docs/30-controlling-navigation-views.js
@@ -0,0 +1,92 @@
+// @flow
+
+import React from 'react';
+import { code, md } from '@atlaskit/docs';
+
+import { Contents, ContentsProvider, IframeExample, H } from './shared';
+
+export default (
+  <ContentsProvider>{md`
+This guide will introduce you to some of the more advanced concepts in navigation-next.
+
+${<Contents listType="ol" />}
+
+${<H>Navigation views</H>}
+
+If you've followed the previous guide you'll know how to use the UI components to compose a navigation. This is all well and good if your navigation is simple and will never change as the user navigates around your app. But what if we do want to change the state of our navigation?
+
+We refer to each state the navigation can be in as a 'view'. As an example, here are some of Jira's navigation 'views':
+
+${(
+    <IframeExample
+      source={require('!!raw-loader!../examples/9999-views-controller-views-example')}
+      title="Navigation views"
+      url="/examples.html?groupId=core&packageId=navigation-next&exampleId=views-controller-views-example"
+    />
+  )}
+
+${<H>Representing a view as data</H>}
+
+Representing a view as a Javascript array makes it really easy for us to work with this data. Let's start by taking the Product home view above and turning it into JSON.
+
+${code`// Component representation
+const componentView = (
+  <Fragment>
+    <Section key="header">
+      {({ className }) => (
+        <div className={className}>
+          <div css={{ padding: '16px 0' }}>
+            <JiraWordmark />
+          </div>
+        </div>
+      )}
+    </Section>
+    <Section key="menu">
+      {({ className }) => (
+        <div className={className}>
+          <Item before={DashboardIcon} text="Dashboards" />
+          <Item before={FolderIcon} text="Projects" />
+          <Item before={IssueIcon} text="Issues and filters" />
+          <Item before={PortfolioIcon} text="Portfolio" />
+        </div>
+      )}
+    </Section>
+  </Fragment>
+);
+
+// JSON representation
+const jsonView = [
+  {
+    type: 'Section',
+    key: 'header',
+    items: [
+      {
+        type: () => (
+          <div css={{ padding: '16px 0' }}>
+            <JiraWordmark />
+          </div>
+        ),
+        key: 'jira-wordmark',
+      },
+    ],
+  },
+  {
+    type: 'Section',
+    key: 'header',
+    items: [
+      { type: 'Item' before: DashboardIcon, text: 'Dashboards' },
+      { type: 'Item' before: FolderIcon, text: 'Projects' },
+      { type: 'Item' before: IssueIcon, text: 'Issues and filters' },
+      { type: 'Item' before: PortfolioIcon, text: 'Portfolio' },
+    ],
+  },
+]`}
+
+In this model a view is represented as an array of items. Each item in this array has a \`type\` property, a \`key\` property, and maybe an \`items\` property (if this item contains other items).
+
+A few things to note:
+
+* 
+
+`}</ContentsProvider>
+);
