9848af3eaab8b10d746b4d41984687bb3dfebc56
Charles Lee <clee@atlassian.com>
2018-08-03 14:55:49 +1000

    updated FixedLayer to use react-popper instead of Layer

--- packages/core/datetime-picker/src/internal/FixedLayer.js
+++ packages/core/datetime-picker/src/internal/FixedLayer.js
@@ -3,6 +3,8 @@
 import React, { type Node } from 'react';
 import Layer from '@atlaskit/layer';
 import { layers } from '@atlaskit/theme';
+import ScrollLock from 'react-scrolllock';
+import { Popper, Manager, Reference } from '@atlaskit/popper';
 
 type Props = {
   /** A ref to the container that the content should be layered around for height calculation
@@ -33,6 +35,47 @@ const FixedLayer = ({ containerRef, content }: Props) => {
     /* Need to wrap layer in a fixed position div so that it will render its content as fixed
      * We need to set the intial top value to where the container is and zIndex so that it still
      * applies since we're creating a new stacking context. */
+    <Manager>
+      <ScrollLock />
+      <Reference>
+        {({ ref }) => (
+          <div
+            ref={ref}
+            data-layer-child
+            style={{
+              position: 'absolute',
+              top: 0,
+              height: containerRect.height,
+              width: containerRect.width,
+              background: 'red',
+            }}
+          />
+        )}
+      </Reference>
+      <Popper>
+        {({ ref, style, placement }) => {
+          return (
+            <div
+              ref={ref}
+              style={{
+                ...style,
+                zIndex: layers.dialog(),
+              }}
+              placement={placement}
+            >
+              {content}
+            </div>
+          );
+        }}
+      </Popper>
+    </Manager>
+  );
+};
+
+export const OldFixedLayer = ({ containerRef, content }: Props) => {
+  if (!containerRef) return <div />;
+  const containerRect = containerRef.getBoundingClientRect();
+  return (
     <div
       style={{
         position: 'fixed',
