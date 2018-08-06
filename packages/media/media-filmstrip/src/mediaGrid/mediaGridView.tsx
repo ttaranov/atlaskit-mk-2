de927800193793b953041ce602a89297d77d729c
Jake Coppinger <jake@jakecoppinger.com>
2018-08-06 13:22:16 +1000

    wip

--- packages/media/media-filmstrip/src/mediaGrid/mediaGridView.tsx
+++ packages/media/media-filmstrip/src/mediaGrid/mediaGridView.tsx
@@ -1,13 +1,30 @@
-MediaItem {
-  dataURI: string;
-  dimensions: {
-    width: number,
-    height: number,
-  }  
+import * as React from 'react';
+import { Component } from 'react';
+
+// MediaItem {
+//   dataURI: string,
+//   dimensions: {
+//     width: number,
+//     height: number,
+//   }
+// }
+
+// <MediaGridView
+
+// />
+
+export interface MediaGridViewProps {
+  // items: MediaItem[];
+  width?: number;
+  itemsPerRow?: number;
 }
 
-<MediaGripView
-  items={MediaItem[]}
-  width?={number}
-  itemsPerRow?={3}
-/>
+class MediaGridView extends Component<MediaGridViewProps> {
+  static defaultProps: Partial<MediaGridViewProps> = {
+    itemsPerRow: 3,
+  };
+
+  render() {
+    return <h1>Hello world</h1>;
+  }
+}
