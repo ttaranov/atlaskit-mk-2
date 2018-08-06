055691837456d9aeeba3df84177643962c113cb6
Will Mendes <willmendesneto@gmail.com>
2018-08-06 18:19:49 +1000

    fix(drawer): adding missed styled to simulate the issue

--- packages/core/drawer/examples-util/helpers.js
+++ packages/core/drawer/examples-util/helpers.js
@@ -6,20 +6,41 @@ import { colors, math, gridSize } from '@atlaskit/theme';
 
 const Wrapper = styled.div`
   bottom: 0;
+  color: rgb(23, 43, 77);
   height: auto;
-  border: 1px solid #333;
   left: 0;
+  letter-spacing: -0.16px;
   position: relative;
   right: 0;
+  text-decoration: none solid rgb(23, 43, 77);
   top: 0;
   width: 100%;
-  z-index: 1;
-  padding: 20px;
+  zindex: 1;
+  column-rule-color: rgb(23, 43, 77);
+  perspective-origin: 495.5px 22px;
+  transform-origin: 495.5px 22px;
+  caret-color: rgb(23, 43, 77);
+  background: rgb(255, 255, 255) none repeat scroll 0% 0% / auto padding-box
+    border-box;
+  border: 0 solid rgba(0, 0, 0, 0);
+  outline: rgb(23, 43, 77) none 0px;
+  padding: 20px 24px 24px 16px;
 `;
 
 const ChildrenWrapper = styled.div`
   box-sizing: border-box;
-  width: 100%;
+  color: rgb(23, 43, 77);
+  display: table;
+  height: 0;
+  letter-spacing: -0.16px;
+  text-decoration: none solid rgb(23, 43, 77);
+  width: 951px;
+  column-rule-color: rgb(23, 43, 77);
+  perspective-origin: 475.5px 0px;
+  transform-origin: 475.5px 0px;
+  caret-color: rgb(23, 43, 77);
+  border: 0 none rgb(23, 43, 77);
+  outline: rgb(23, 43, 77) none 0px;
 `;
 
 export const BlockWithChildren = ({ children }: { children: ?Node }) => (
