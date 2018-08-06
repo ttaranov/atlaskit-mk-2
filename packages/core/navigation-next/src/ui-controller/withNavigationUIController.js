2e148df4e0396efdbc3d0f9d0b40e2089a17a3a0
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-08-06 12:31:21 +1000

    CHANGESET: Add variable name displayNames for anonymous function SFC components to improve debugging
    Summary: Add variable name displayNames for anonymous function SFC components to improve debugging experience
    
    Release notes: <none>
    
    Releases: @atlaskit/navigation-next@patch
    
    Dependents: @atlaskit/global-navigation@none, @atlaskit/navigation-next@none
    
    ---
    {"summary":"Add variable name displayNames for anonymous function SFC components to improve debugging experience","releases":[{"name":"@atlaskit/navigation-next","type":"patch"}],"dependents":[{"name":"@atlaskit/global-navigation","dependencies":["@atlaskit/navigation-next"],"type":"none"},{"name":"@atlaskit/navigation-next","dependencies":["@atlaskit/global-navigation"],"type":"none"}]}
    ---

--- packages/core/navigation-next/src/ui-controller/withNavigationUIController.js
+++ packages/core/navigation-next/src/ui-controller/withNavigationUIController.js
@@ -3,13 +3,20 @@
 import React, { type ComponentType } from 'react';
 import UIControllerSubscriber from './UIControllerSubscriber';
 
-export default (WrappedComponent: ComponentType<*>) => (props: *) => (
-  <UIControllerSubscriber>
-    {navigationUIController => (
-      <WrappedComponent
-        navigationUIController={navigationUIController}
-        {...props}
-      />
-    )}
-  </UIControllerSubscriber>
-);
+export default (WrappedComponent: ComponentType<*>) => {
+  const WithNavigationUI = (props: *) => (
+    <UIControllerSubscriber>
+      {navigationUIController => (
+        <WrappedComponent
+          navigationUIController={navigationUIController}
+          {...props}
+        />
+      )}
+    </UIControllerSubscriber>
+  );
+
+  WithNavigationUI.displayName = `WithNavigationUI(${WrappedComponent.displayName ||
+    WrappedComponent.name})`;
+
+  return WithNavigationUI;
+};
