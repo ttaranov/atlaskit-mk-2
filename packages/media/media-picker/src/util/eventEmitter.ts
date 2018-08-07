6ef7c34fd6dc025eb8525f0d15d4c95437fa9a4f
Stanislav Sysoev <ssysoev@atlassian.com>
2018-08-07 15:47:28 +1000

    Fix media event emitter types

--- packages/media/media-picker/src/util/eventEmitter.ts
+++ packages/media/media-picker/src/util/eventEmitter.ts
@@ -23,29 +23,28 @@ export interface EventEmitter<M extends EventPayloadMap<P>, P = any> {
     event: E,
     handler: EventPayloadListener<M, E>,
   ): void;
-  removeAllListeners<E extends keyof M>(event: E): void;
+  removeAllListeners<E extends keyof M>(event?: E): void;
   emit<E extends keyof M>(event: E, payload: M[E]): boolean;
 }
 
 export class GenericEventEmitter<M extends EventPayloadMap<P>, P = any>
   implements EventEmitter<M> {
-  private readonly emitter = new EventEmitter2({ wildcard: true });
+  private readonly emitter = new EventEmitter2({
+    wildcard: true,
+  }) as EventEmitter<M, P>;
 
   once<E extends keyof M>(
     event: E,
     listener: EventPayloadListener<M, E>,
   ): void {
-    // @ts-ignore
     this.emitter.once(event, listener);
   }
 
   on<E extends keyof M>(event: E, listener: EventPayloadListener<M, E>): void {
-    // @ts-ignore
     this.emitter.on(event, listener);
   }
 
   onAny<E extends keyof M>(listener: (event: E, payload: M[E]) => void): void {
-    // @ts-ignore
     this.emitter.onAny(listener);
   }
 
@@ -53,12 +52,10 @@ export class GenericEventEmitter<M extends EventPayloadMap<P>, P = any>
     event: E,
     listener: EventPayloadListener<M, E>,
   ): void {
-    // @ts-ignore
     this.emitter.addListener(event, listener);
   }
 
   off<E extends keyof M>(event: E, listener: EventPayloadListener<M, E>): void {
-    // @ts-ignore
     this.emitter.off(event, listener);
   }
 
@@ -66,22 +63,19 @@ export class GenericEventEmitter<M extends EventPayloadMap<P>, P = any>
     event: E,
     handler: EventPayloadListener<M, E>,
   ): void {
-    // @ts-ignore
     this.emitter.removeListener(event, handler);
   }
 
-  removeAllListeners<E extends keyof M>(event?: E | E[]): void {
+  removeAllListeners<E extends keyof M>(event?: E): void {
     // We want to explicitly call removeAllListeners without any argument if event is undefined, otherwise will EventEmitter fail
     if (event === undefined) {
       this.emitter.removeAllListeners();
     } else {
-      // @ts-ignore
       this.emitter.removeAllListeners(event);
     }
   }
 
   emit<E extends keyof M>(event: E, payload: M[E]): boolean {
-    // @ts-ignore
     return this.emitter.emit(event, payload);
   }
 }
