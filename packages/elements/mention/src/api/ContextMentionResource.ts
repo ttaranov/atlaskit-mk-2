f24b4c5612cafc9bba75f2d7a6d0a1316ac0a04f
Stanislav Sysoev <ssysoev@atlassian.com>
2018-08-07 14:59:58 +1000

    Fix ContextMentionResource types

--- packages/elements/mention/src/api/ContextMentionResource.ts
+++ packages/elements/mention/src/api/ContextMentionResource.ts
@@ -12,6 +12,12 @@ import { padArray } from '../util';
 import { MentionDescription } from '../types';
 export { MentionDescription };
 
+export type MentionProviderFunctions = {
+  [Key in keyof MentionProvider]: MentionProvider[Key] extends Function
+    ? MentionProvider[Key]
+    : never
+};
+
 /**
  * This component is stateful and should be instantianted per contextIdentifiers.
  */
@@ -31,7 +37,7 @@ export default class ContextMentionResource implements MentionProvider {
     return this.contextIdentifier;
   }
 
-  callWithContextIds = <K extends keyof MentionProvider>(
+  callWithContextIds = <K extends keyof MentionProviderFunctions>(
     f: K,
     declaredArgs: number,
   ): MentionProvider[K] => (...args: any[]) => {
@@ -42,15 +48,16 @@ export default class ContextMentionResource implements MentionProvider {
       argsLength !== declaredArgs
         ? padArray(args, declaredArgs - argsLength, undefined)
         : args;
-    return (this.mentionProvider[f] as any)(
+    return (this.mentionProvider[f] as Function)(
       ...mentionArgs,
       this.contextIdentifier,
     );
   };
 
-  callDefault = <K extends keyof MentionProvider>(f: K): MentionProvider[K] => (
-    ...args: any[]
-  ) => (this.mentionProvider[f] as any)(...args);
+  callDefault = <K extends keyof MentionProviderFunctions>(
+    f: K,
+  ): MentionProvider[K] => (...args: any[]) =>
+    (this.mentionProvider[f] as Function)(...args);
 
   subscribe = this.callDefault('subscribe');
 
