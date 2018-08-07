cbac87d92d2a19b518c60035e34e475ce96c99bf
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-08-07 16:56:32 +1000

    wip

--- packages/media/media-filmstrip/examples/6-media-grid.tsx
+++ packages/media/media-filmstrip/examples/6-media-grid.tsx
@@ -12,32 +12,41 @@ import {
 const image = {
   id: imageFileId.id,
   dimensions: {
-    width: 300,
-    height: 100,
+    width: 1138,
+    height: 759,
   },
 };
 const largeImage = {
   id: largeImageFileId.id,
   dimensions: {
-    width: 300,
-    height: 100,
+    width: 273,
+    height: 775,
   },
 };
 const smallImage = {
   id: smallImageFileId.id,
   dimensions: {
-    width: 300,
-    height: 100,
+    width: 20,
+    height: 20,
   },
 };
 const wideImage = {
   id: wideImageFileId.id,
   dimensions: {
-    width: 300,
-    height: 100,
+    width: 582,
+    height: 54,
   },
 };
-const items: PublicGridItem[] = [image, largeImage, smallImage, wideImage];
+const items: PublicGridItem[] = [
+  image,
+  largeImage,
+  image,
+  smallImage,
+  wideImage,
+  largeImage,
+  wideImage,
+  largeImage,
+];
 const context = createStorybookContext();
 
 class Example extends Component<{}, {}> {
