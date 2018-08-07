6b238f3b22139a28765a089ed860fc71f1d4b7b9
Aleksandr Sasha Motsjonov <amotsjonov@atlassian.com>
2018-08-07 17:08:08 +1000

    Introduce tests for media grid view

---
+++ packages/media/media-filmstrip/__tests__/mediaGridViewSpec.tsx
@@ -0,0 +1,186 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { gridItems } from '../example-helpers/media-grid-items';
+import { GridItem, MediaGridView } from '../src/mediaGrid/mediaGridView';
+import { RowWrapper } from '../src/mediaGrid/styled';
+
+describe('MediaGridView', () => {
+  let onItemsChange: jest.Mock<any>;
+  beforeEach(() => {
+    onItemsChange = jest.fn();
+  });
+
+  it('should render all the items', () => {
+    const numberOfItems = gridItems.length;
+    const component = shallow(
+      <MediaGridView items={gridItems} onItemsChange={onItemsChange} />,
+    );
+    expect(component.find('img')).toHaveLength(numberOfItems);
+    expect(
+      component
+        .find('img')
+        .at(0)
+        .props().src,
+    ).toEqual(gridItems[0].dataURI);
+    expect(
+      component
+        .find('img')
+        .at(gridItems.length - 1)
+        .props().src,
+    ).toEqual(gridItems[gridItems.length - 1].dataURI);
+  });
+
+  it('should have 3 rows with 3 items in a row', () => {
+    const component = shallow(
+      <MediaGridView
+        items={gridItems.slice(0, 9)}
+        onItemsChange={onItemsChange}
+        itemsPerRow={3}
+      />,
+    );
+    expect(component.find(RowWrapper)).toHaveLength(3);
+    expect(
+      component
+        .find(RowWrapper)
+        .at(0)
+        .find('img'),
+    ).toHaveLength(3);
+  });
+
+  it('should have 3 items per row by default', () => {
+    const component = shallow(
+      <MediaGridView
+        items={gridItems.slice(0, 9)}
+        onItemsChange={onItemsChange}
+      />,
+    );
+    expect(
+      component
+        .find(RowWrapper)
+        .at(0)
+        .find('img'),
+    ).toHaveLength(3);
+  });
+
+  it('should choose appropriate height 1', () => {
+    /*
+    Original images sizes: {800x1600}, {1200x1600}.
+    Total container width: 1010
+    One margin width: 10
+    Result: Images are resized in half
+              1010
+      _______________________
+     |        | |            |
+     |        | |            |
+     |        | |            | 800
+     |        | |            |
+     |        | |            |
+     |        | |            |
+     |        | |            |
+     |________|_|____________|
+        400    10     600
+     */
+    const items: GridItem[] = [
+      {
+        dataURI: 'some-url-1',
+        dimensions: {
+          width: 800,
+          height: 1600,
+        },
+      },
+      {
+        dataURI: 'some-url-2',
+        dimensions: {
+          width: 1200,
+          height: 1600,
+        },
+      },
+    ];
+    const component = shallow(
+      <MediaGridView
+        items={items}
+        width={1010}
+        onItemsChange={onItemsChange}
+      />,
+    );
+    expect(
+      component
+        .find('img')
+        .at(0)
+        .props().style,
+    ).toEqual({
+      width: 400,
+      height: 800,
+    });
+    expect(
+      component
+        .find('img')
+        .at(1)
+        .props().style,
+    ).toEqual({
+      width: 600,
+      height: 800,
+    });
+  });
+
+  it('should choose appropriate height 2', () => {
+    /*
+    Original images sizes: {400x800}, {3000x1200}.
+    Total container width: 1210
+    One margin width: 10
+    Result: Images are resized in half
+                   1210
+      _________________________________
+     |        | |                      |
+     |        | |                      |
+     |        | |                      |
+     |        | |                      | 400
+     |        | |                      |
+     |        | |                      |
+     |        | |                      |
+     |________|_|______________________|
+        200    10         1000
+     */
+    const items: GridItem[] = [
+      {
+        dataURI: 'some-url-1',
+        dimensions: {
+          width: 400,
+          height: 800,
+        },
+      },
+      {
+        dataURI: 'some-url-2',
+        dimensions: {
+          width: 3000,
+          height: 1200,
+        },
+      },
+    ];
+    const component = shallow(
+      <MediaGridView
+        items={items}
+        width={1210}
+        onItemsChange={onItemsChange}
+      />,
+    );
+    expect(
+      component
+        .find('img')
+        .at(0)
+        .props().style,
+    ).toEqual({
+      width: 200,
+      height: 400,
+    });
+    expect(
+      component
+        .find('img')
+        .at(1)
+        .props().style,
+    ).toEqual({
+      width: 1000,
+      height: 400,
+    });
+  });
+});
---
+++ packages/media/media-filmstrip/example-helpers/media-grid-items.ts
@@ -0,0 +1,78 @@
+import { GridItem } from '../src/mediaGrid/mediaGridView';
+
+const image1 = {
+  dataURI:
+    'https://images.unsplash.com/photo-1533468432434-200de3b5d528?ixlib=rb-0.3.5&s=83b6a800c6d49aed6732730b11c8ae72&auto=format&fit=crop&w=975&q=80',
+  dimensions: {
+    width: 975,
+    height: 1301,
+  },
+};
+const image2 = {
+  dataURI:
+    'https://images.unsplash.com/photo-1533458504656-81a904b29a69?ixlib=rb-0.3.5&s=be9e4317bee4599fc101a2840da3c56a&auto=format&fit=crop&w=2250&q=80',
+  dimensions: {
+    width: 2250,
+    height: 1500,
+  },
+};
+const image3 = {
+  dataURI:
+    'https://images.unsplash.com/photo-1533396257172-78e7d58da599?ixlib=rb-0.3.5&s=140bed06f491d46adee0d16eebff8fa4&auto=format&fit=crop&w=934&q=80',
+  dimensions: {
+    width: 934,
+    height: 1399,
+  },
+};
+const image4 = {
+  dataURI:
+    'https://images.unsplash.com/photo-1533475184589-ad2b25374b56?ixlib=rb-0.3.5&s=4fb3ed72b064d35d3ae90f1d5267a723&auto=format&fit=crop&w=2250&q=80',
+  dimensions: {
+    width: 2250,
+    height: 1500,
+  },
+};
+
+const image5 = {
+  dataURI:
+    'https://images.unsplash.com/photo-1533523705247-88786d3e9242?ixlib=rb-0.3.5&s=7c0bc9e4f95892807c111c518d73ddc9&dpr=1&auto=format&fit=crop&w=1000&q=80&cs=tinysrgb',
+  dimensions: {
+    width: 1000,
+    height: 1250,
+  },
+};
+
+const image6 = {
+  dataURI:
+    'https://images.unsplash.com/photo-1533456307239-052e029c1362?ixlib=rb-0.3.5&s=d5ee830a902673977e754694f361c77b&auto=format&fit=crop&w=2964&q=80',
+  dimensions: {
+    width: 2964,
+    height: 5269,
+  },
+};
+
+const image7 = {
+  dataURI:
+    'https://images.unsplash.com/photo-1533447448066-75ac9b65b459?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=f3ef24611e57a5ed32f031efa98d5ba4&auto=format&fit=crop&w=2850&q=80',
+  dimensions: {
+    width: 2850,
+    height: 1902,
+  },
+};
+
+export const gridItems: GridItem[] = [
+  image6,
+  image1,
+  image2,
+  image6,
+  image3,
+  image7,
+  image4,
+  image1,
+  image5,
+  image1,
+  image6,
+  image1,
+  image5,
+  image2,
+];
