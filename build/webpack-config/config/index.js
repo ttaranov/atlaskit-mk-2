3da4cea6707c56e3718eb3aceedeef44a26ff096
Marco De Jongh <mdejongh@atlassian.com>
2018-08-09 11:40:39 +1000

    WIP

--- build/webpack-config/config/index.js
+++ build/webpack-config/config/index.js
@@ -10,6 +10,106 @@ const HappyPack = require('happypack');
 
 const { createDefaultGlob } = require('./utils');
 
+const statsOptions = {
+  // fallback value for stats options when an option is not defined (has precedence over local webpack defaults)
+  all: undefined,
+
+  // Add asset Information
+  assets: true,
+
+  // Sort assets by a field
+  // You can reverse the sort with `!field`.
+  assetsSort: 'field',
+
+  // Add build date and time information
+  builtAt: false,
+
+  // Add information about cached (not built) modules
+  cached: false,
+
+  // Show cached assets (setting this to `false` only shows emitted files)
+  cachedAssets: false,
+
+  // Add children information
+  children: false,
+
+  // Add chunk information (setting this to `false` allows for a less verbose output)
+  chunks: false,
+
+  // Add namedChunkGroups information
+  chunkGroups: false,
+
+  // Add built modules information to chunk information
+  chunkModules: false,
+
+  // Add the origins of chunks and chunk merging info
+  chunkOrigins: false,
+
+  // Sort the chunks by a field
+  // You can reverse the sort with `!field`. Default is `id`.
+  chunksSort: 'field',
+
+  // `webpack --colors` equivalent
+  colors: false,
+
+  // Display the distance from the entry point for each module
+  depth: false,
+
+  // Display the entry points with the corresponding bundles
+  entrypoints: false,
+
+  // Add --env information
+  env: false,
+
+  // Add errors
+  errors: false,
+
+  // Add details to errors (like resolving log)
+  errorDetails: false,
+
+  // Add the hash of the compilation
+  hash: false,
+
+  // Add built modules information
+  modules: true,
+  // excludeModules: new RegExp('*.md'),
+
+  // Sort the modules by a field
+  // You can reverse the sort with `!field`. Default is `id`.
+  modulesSort: 'field',
+
+  // Show dependencies and origin of warnings/errors (since webpack 2.5.0)
+  moduleTrace: false,
+
+  // Show performance hint when file size exceeds `performance.maxAssetSize`
+  performance: false,
+
+  // Show the exports of the modules
+  providedExports: false,
+
+  // Add public path information
+  publicPath: false,
+
+  // Add information about the reasons why modules are included
+  reasons: false,
+
+  // Add the source code of modules
+  source: false,
+
+  // Add timing information
+  timings: true,
+
+  // Show which exports of a module are used
+  usedExports: false,
+
+  // Add webpack version information
+  version: false,
+
+  // Add warnings
+  warnings: false,
+  warningsFilter: () => true,
+};
+
 module.exports = function createWebpackConfig(
   {
     globs = createDefaultGlob(),
@@ -30,6 +130,7 @@ module.exports = function createWebpackConfig(
   const isProduction = mode === 'production';
 
   return {
+    stats: statsOptions,
     mode,
     performance: {
       // performance hints are used to warn you about large bundles but come at their own perf cost
@@ -216,17 +317,16 @@ function getPlugins(
     }),
   ];
 
-  if (report) {
-    plugins.push(
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'static',
-        statsOptions: { source: false },
-        generateStatsFile: true,
-        openAnalyzer: true,
-        logLevel: 'error',
-      }),
-    );
-  }
+  plugins.push(
+    new BundleAnalyzerPlugin({
+      analyzerMode: report ? 'static' : 'disabled',
+      generateStatsFile: true,
+      openAnalyzer: report,
+      logLevel: 'error',
+      statsOptions: statsOptions,
+      defaultSizes: 'gzip',
+    }),
+  );
 
   return plugins;
 }
