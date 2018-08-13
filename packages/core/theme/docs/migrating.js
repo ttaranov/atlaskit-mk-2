572ce63af4072937b8a0b3702da55ea59d458279
Trey Shugart <treshugart@gmail.com>
2018-08-14 08:40:10 +1000

    Make migration docs better and more consistent with other migration docs.

--- packages/core/theme/docs/migrating.js
+++ packages/core/theme/docs/migrating.js
@@ -1,46 +1,50 @@
 // @flow
 
-import { md } from '@atlaskit/docs';
+import { code, md } from '@atlaskit/docs';
 
 export default md`
-  ## 5.1 -> 6.0
+  ## 5.x - 6.x
 
   The only breaking changes between these two versions are for experimental APIs.
 
   The only experimental API that changed here is the \`Theme\` component. Themes are no longer components, but functions that return objects.
 
+  ### Theme shape
+  
   Before, you'd pass an object to the \`values\` prop. Now, you pass a function that returns an object.
 
-  ~~~
-  import { Theme } from '@atlaskit/theme';
+  ${code`
+    import { Theme } from '@atlaskit/theme';
 
-  - const theme = {
-  -   mode: 'light'
-  - }
-  + const theme = parentTheme => ({
-  +   ...parentTheme,
-  +   mode: 'light'
-  + });
+    - const theme = {
+    -   mode: 'light'
+    - }
+    + const theme = parentTheme => ({
+    +   ...parentTheme,
+    +   mode: 'light'
+    + });
 
-  <Theme values={theme} />
-  ~~~
+    <Theme values={theme} />
+  `}
+
+  ### Component-specific theme functions
 
   Component themes are no longer bound, passing in the parent theme. They're now just functions, however you define them, and they can get the parent theme from the execution context of the parent function.
 
-  ~~~
-  import { Theme } from '@atlaskit/theme';
+  ${code`
+    import { Theme } from '@atlaskit/theme';
 
-  - const theme = {
-  -   badge({ appearance }, parentTheme) {
-  -     return { ... };
-  -   }
-  - }
-  + const theme = parentTheme => ({
-  +   badge({ appearance }) {
-  +     return { ... }
-  +   }
-  + });
+    - const theme = {
+    -   badge({ appearance }, parentTheme) {
+    -     return { ... };
+    -   }
+    - }
+    + const theme = parentTheme => ({
+    +   badge({ appearance }) {
+    +     return { ... }
+    +   }
+    + });
 
-  <Theme values={theme} />
-  ~~~
+    <Theme values={theme} />
+  `}
 `;
