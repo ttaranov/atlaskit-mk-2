db387e979a04ca3468527d2292cb6585597a2128
Charles Lee <clee@atlassian.com>
2018-08-14 11:05:14 +1000

    Added RadioIcon export

--- packages/core/radio/src/index.js
+++ packages/core/radio/src/index.js
@@ -1,4 +1,5 @@
 // @flow
 export { default } from './RadioGroup';
+export { default as RadioIcon } from './RadioIcon';
 export { default as AkFieldRadioGroup } from './RadioGroupStateless';
 export { default as AkRadio } from './Radio';
