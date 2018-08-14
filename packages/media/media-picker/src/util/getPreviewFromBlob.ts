b86e7a25d87c9ebf0be42f32e9f4c0ba89bb71d0
Ali Chamas <achamas@atlassian.com>
2018-08-14 15:58:20 +1000

    initial commit

--- packages/media/media-picker/src/util/getPreviewFromBlob.ts
+++ packages/media/media-picker/src/util/getPreviewFromBlob.ts
@@ -2,26 +2,38 @@ import { MediaType } from '@atlaskit/media-core';
 import { Preview } from '../domain/preview';
 import { fileToBase64 } from '../popup/tools/fileToBase64';
 import { ImagePreview } from '../index';
+import { readSupportedImageMetaData } from '../../../media-ui/src/imageMetaData';
 
 export const getPreviewFromBlob = (
-  file: Blob,
+  file: File,
   mediaType: MediaType,
 ): Promise<Preview> =>
   new Promise((resolve, reject) => {
     fileToBase64(file)
       .then(src => {
         if (mediaType === 'image') {
-          const img = new Image();
-          img.src = src;
-
-          img.onload = () => {
-            const { src } = img;
-            const dimensions = { width: img.width, height: img.height };
-
-            resolve({ src, dimensions } as ImagePreview);
-          };
-
-          img.onerror = reject;
+          readSupportedImageMetaData(src).then(metadata => {
+            let scaleFactor = 1;
+            if (file.name) {
+              // filenames with scale ratio in name take precedence - eg. filename@2x.png
+              const match = file.name.trim().match(/@([0-9]+)x\.[a-z]{3}$/);
+              if (match) {
+                scaleFactor = parseInt(match[1], 10);
+              }
+            } else if (metadata.tags) {
+              // then we use metadata
+              const dpi = metadata.tags.XResolution as number;
+              scaleFactor = dpi / 72;
+            }
+            resolve({
+              src,
+              dimensions: {
+                width: metadata.width,
+                height: metadata.height,
+              },
+              scaleFactor,
+            } as ImagePreview);
+          });
         } else {
           resolve({ src });
         }
---
+++ packages/media/media-ui/typings/exif.d.ts
@@ -0,0 +1,2 @@
+// https://github.com/exif-js/exif-js/issues/134
+declare module 'exif-js';
