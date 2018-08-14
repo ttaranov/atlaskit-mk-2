2c508123cc3b9757018d6edb2d87221df3f6d1fa
Will Mendes <willmendesneto@gmail.com>
2018-08-14 17:37:43 +1000

    fix(navigation-next): applying given feedback for tests

--- packages/core/navigation-next/src/view-controller/__tests__/unit/reducer-utils.js
+++ packages/core/navigation-next/src/view-controller/__tests__/unit/reducer-utils.js
@@ -1,70 +1,80 @@
 // @flow
 import reducerUtils from '../../reducer-utils';
 
-const view = { id: 'item-id', items: [], legacyId: 'legacy-item-id' };
+const item = { id: 'new-item', legacyId: 'legacy-item-id' };
 
 describe('NavigationNext View Controller: Reducer Utils', () => {
   describe('#prependChildren', () => {
     it('should prepend the view item in `items` array', () => {
-      const itemToPrepend = { ...view, id: 'my-new-id' };
-      expect(reducerUtils.prependChildren([view])(itemToPrepend)).toEqual({
-        ...itemToPrepend,
-        items: [view],
-      });
-    });
-
-    it('should combine the view items in `items` array if second view item has a list of items', () => {
-      const itemToPrepend = {
-        ...view,
-        id: 'my-new-id',
-        items: [{ ...view, id: 'another-id' }],
+      const firstViewItem = {
+        id: 'menu',
+        items: [{ id: 'item1' }, { id: 'item2' }, { id: 'item3' }],
       };
-      expect(reducerUtils.prependChildren([view])(itemToPrepend)).toEqual({
-        ...itemToPrepend,
-        items: [view, ...itemToPrepend.items],
-      });
+      const viewItems = [firstViewItem];
+
+      const reducedViewItems = reducerUtils.findId('menu')(
+        reducerUtils.prependChildren([item]),
+      )(viewItems);
+
+      expect(reducedViewItems).toEqual([
+        {
+          ...firstViewItem,
+          items: [item, ...firstViewItem.items],
+        },
+      ]);
     });
   });
 
   describe('#appendChildren', () => {
     it('should append the view item in `items` array', () => {
-      const itemToPrepend = { ...view, id: 'my-new-id' };
-      expect(reducerUtils.appendChildren([view])(itemToPrepend)).toEqual({
-        ...itemToPrepend,
-        items: [view],
-      });
-    });
-
-    it('should append the first view items with the second in `items` array if second view item has a list of items', () => {
-      const itemToPrepend = {
-        ...view,
-        id: 'my-new-id',
-        items: [{ ...view, id: 'another-id' }],
+      const firstViewItem = {
+        id: 'menu',
+        items: [{ id: 'item1' }, { id: 'item2' }, { id: 'item3' }],
       };
-      expect(reducerUtils.appendChildren([view])(itemToPrepend)).toEqual({
-        ...itemToPrepend,
-        items: [...itemToPrepend.items, view],
-      });
+      const viewItems = [firstViewItem];
+
+      const reducedViewItems = reducerUtils.findId('menu')(
+        reducerUtils.appendChildren([item]),
+      )(viewItems);
+
+      expect(reducedViewItems).toEqual([
+        {
+          ...firstViewItem,
+          items: [...firstViewItem.items, item],
+        },
+      ]);
     });
   });
 
   describe('#insertBefore', () => {
     it('should append the view item in `items` array', () => {
-      const itemToPrepend = { ...view, id: 'my-new-id' };
-      expect(reducerUtils.insertBefore([view])(itemToPrepend)).toEqual([
-        view,
-        itemToPrepend,
-      ]);
+      const firstViewItem = {
+        id: 'menu',
+        items: [{ id: 'item1' }, { id: 'item2' }, { id: 'item3' }],
+      };
+      const viewItems = [firstViewItem];
+
+      const reducedViewItems = reducerUtils.findId('menu')(
+        reducerUtils.insertBefore([item]),
+      )(viewItems);
+
+      expect(reducedViewItems).toEqual([item, firstViewItem]);
     });
   });
 
   describe('#insertAfter', () => {
     it('should append the view item in `items` array', () => {
-      const itemToPrepend = { ...view, id: 'my-new-id' };
-      expect(reducerUtils.insertAfter([view])(itemToPrepend)).toEqual([
-        itemToPrepend,
-        view,
-      ]);
+      const firstViewItem = {
+        id: 'menu',
+        items: [{ id: 'item1' }, { id: 'item2' }, { id: 'item3' }],
+      };
+      const viewItems = [firstViewItem];
+
+      const reducedViewItems = reducerUtils.findId('menu')(
+        reducerUtils.insertAfter([item]),
+      )(viewItems);
+
+      expect(reducedViewItems).toEqual([firstViewItem, item]);
     });
   });
 });
