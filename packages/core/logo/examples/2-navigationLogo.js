14e8000db3169da5b9803b155fe1882f5b028386
raja07 <rbellebon@atlassian.com>
2018-08-14 14:40:23 +1000

    Remove globalItem in example

--- packages/core/logo/examples/2-navigationLogo.js
+++ packages/core/logo/examples/2-navigationLogo.js
@@ -1,6 +1,8 @@
 // @flow
-import React from 'react';
-import { GlobalItem } from '@atlaskit/navigation-next';
+import React, { Fragment } from 'react';
+import { colors } from '@atlaskit/theme';
+import styled from 'styled-components';
+
 import {
   BitbucketIcon,
   ConfluenceIcon,
@@ -11,107 +13,45 @@ import {
   StrideIcon,
 } from '../src';
 
-type ItemType = *;
-
-type VariationCategory = {
-  title: string,
-  items: Array<ItemType>,
-  itemComponent?: ComponentType<ItemType>,
-};
-
-const icons: Array<VariationCategory> = [
-  {
-    title: 'Icons',
-    items: [
-      {
-        description: 'Bitbucket',
-        icon: BitbucketIcon,
-        size: 'large',
-      },
-      {
-        description: 'Confluence',
-        icon: ConfluenceIcon,
-        size: 'large',
-      },
-      {
-        description: 'JiraCore',
-        icon: JiraCoreIcon,
-        size: 'large',
-      },
-      {
-        description: 'Jira',
-        icon: JiraIcon,
-        size: 'large',
-      },
-      {
-        description: 'Jira Software',
-        icon: JiraSoftwareIcon,
-        size: 'large',
-      },
-      {
-        description: 'Jira Service Desk',
-        icon: JiraServiceDeskIcon,
-        size: 'large',
-      },
-      {
-        description: 'Stride',
-        icon: StrideIcon,
-        size: 'large',
-      },
-    ],
-  },
+const logoOptions = [
+  BitbucketIcon,
+  ConfluenceIcon,
+  JiraCoreIcon,
+  JiraIcon,
+  JiraServiceDeskIcon,
+  JiraSoftwareIcon,
+  StrideIcon,
 ];
 
-const Container = props => (
-  <div
-    css={{
-      display: 'flex',
-      flexDirection: 'row',
-      flexWrap: 'wrap',
-    }}
-    {...props}
-  />
+const WrapperDiv = styled.div`
+  text-align: center;
+  border-radius: 50%;
+  height: 40px;
+  width: 40px;
+  margin: 5px;
+  color: white;
+  background: ${colors.B500};
+`;
+/* eslint-disable */
+const Wrapper = props => (
+  <Fragment>
+    <WrapperDiv>{props.children}</WrapperDiv>
+    <br />
+  </Fragment>
 );
-const VariationWrapper = props => (
-  <div css={{ margin: '0 24px 24px 0' }} {...props} />
-);
-const ItemWrapper = props => (
-  <div
-    css={{
-      alignItems: 'center',
-      display: 'flex',
-      flexDirection: 'row',
-      margin: '4px 0',
-      width: '270px',
-    }}
-    {...props}
-  />
-);
-const Description = ({ size, children }: ItemType) => (
-  <div
-    css={{
-      marginLeft: '16px',
-      marginTop: size === 'small' ? '8px' : '0',
-    }}
-  >
-    {children}
-  </div>
-);
+/* eslint-enable */
+logoOptions.map(Child => (
+  <Wrapper>
+    <Child />
+  </Wrapper>
+));
 
 export default () => (
-  <Container>
-    {icons.map(
-      ({ title, items, itemComponent: ItemComponent = GlobalItem }) => (
-        <VariationWrapper key={title}>
-          <h3>{title}</h3>
-          {items.map(({ description, ...item }) => (
-            <ItemWrapper key={description}>
-              <ItemComponent {...item} />
-              <Description {...item}>{description}</Description>
-            </ItemWrapper>
-          ))}
-        </VariationWrapper>
-      ),
-    )}
-  </Container>
+  <Fragment>
+    {logoOptions.map(Child => (
+      <Wrapper key={Child.name}>
+        <Child />
+      </Wrapper>
+    ))}
+  </Fragment>
 );
