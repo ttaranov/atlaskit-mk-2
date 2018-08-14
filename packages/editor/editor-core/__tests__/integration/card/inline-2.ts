481b9289ac6d4a5b65199c39ddf11f8a2c5f2e16
Alex Hixon <ahixon@atlassian.com>
2018-08-14 11:55:25 +1000

    add integration tests for card

---
+++ packages/editor/editor-core/__tests__/integration/card/inline-2.ts
@@ -0,0 +1,51 @@
+import { BrowserTestCase } from '@atlaskit/webdriver-runner/runner';
+import Page from '@atlaskit/webdriver-runner/wd-wrapper';
+import {
+  getDocFromElement,
+  fullpage,
+  editable,
+  clipboardHelper,
+  copyAsPlaintextButton,
+  clipboardInput,
+} from '../_helpers';
+
+[fullpage].forEach(editor => {
+  BrowserTestCase(
+    `pasting an link then typing still converts to inline card`,
+    {
+      skip: ['chrome'],
+    },
+    async client => {
+      let browser = await new Page(client);
+
+      // copy stuff to clipboard
+      await browser.goto(clipboardHelper);
+      await browser.isVisible(clipboardInput);
+      await browser.type(clipboardInput, 'https://www.atlassian.com');
+      await browser.click(copyAsPlaintextButton);
+
+      // open up editor
+      await browser.goto(editor.path);
+      await browser.waitForSelector(editor.placeholder);
+      await browser.click(editor.placeholder);
+      await browser.waitForSelector(editable);
+
+      // type some text into the paragraph first
+      await browser.type(editable, 'hello have a link ');
+
+      // paste the link
+      await browser.paste(editable);
+
+      // type some text around it
+      await browser.type(editable, ' more typing');
+      await browser.startOfLine(editable);
+      await browser.type(editable, 'more typing ');
+
+      // selectors on the nodeview do not work correctly in chrome
+      await browser.waitForSelector('.inlineCardView-content-wrap');
+
+      const doc = await browser.$eval(editable, getDocFromElement);
+      expect(doc).toMatchDocSnapshot();
+    },
+  );
+});
