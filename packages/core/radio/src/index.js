3316054f9572a70901d291dbfb5edcb534268e70
Charles Lee <clee@atlassian.com>
2018-08-15 11:20:25 +1000

    radio-input export added

--- packages/core/radio/src/index.js
+++ packages/core/radio/src/index.js
@@ -1,5 +1,6 @@
 // @flow
 export { default } from './RadioGroup';
 export { default as RadioIcon } from './RadioIcon';
+export { default as RadioInput } from './RadioInput';
 export { default as AkFieldRadioGroup } from './RadioGroupStateless';
 export { default as AkRadio } from './Radio';
