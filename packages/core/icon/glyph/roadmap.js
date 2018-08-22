de31b07bfcfdcd7ce867172d11a579f5aa8bb646
Waiyee Loo <wloo@atlassian.com>
2018-08-22 13:50:37 +1000

    AK-5256 Add new roadmap icon

---
+++ packages/core/icon/glyph/roadmap.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _index = require('../es5/index');
+
+var _index2 = _interopRequireDefault(_index);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var RoadmapIcon = function RoadmapIcon(props) {
+  return _react2.default.createElement(_index2.default, _extends({ dangerouslySetGlyph: '<svg width="18" height="20" viewBox="0 0 18 20" focusable="false" role="presentation"><path d="M3 0h10a3 3 0 0 1 0 6H3a3 3 0 1 1 0-6zm0 2a1 1 0 1 0 0 2h10a1 1 0 0 0 0-2H3zm4 5h8a3 3 0 0 1 0 6H7a3 3 0 0 1 0-6zm0 2a1 1 0 0 0 0 2h8a1 1 0 0 0 0-2H7zm-4 5h6a3 3 0 0 1 0 6H3a3 3 0 0 1 0-6zm0 2a1 1 0 0 0 0 2h6a1 1 0 0 0 0-2H3z" fill="currentColor"/></svg>' }, props));
+};
+exports.default = RoadmapIcon;
