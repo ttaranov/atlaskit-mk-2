9a97ffe36c5372b2091c6e687d19272b4dfd8758
Aleksandr Sasha Motsjonov <amotsjonov@atlassian.com>
2018-08-23 11:00:53 +1000

    Smart Media Editor shell

--- packages/media/media-editor/src/index.tsx
+++ packages/media/media-editor/src/index.tsx
@@ -1,3 +1,4 @@
 export * from './react/mediaEditor';
 export * from './react/toolbar';
+export { SmartMediaEditor } from './react/smartMediaEditor';
 export * from './common';
---
+++ packages/media/media-editor/src/react/smartMediaEditor.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export interface SmartMediaEditorProps {
+  id: string;
+  onFinish: (id: string) => void;
+}
+
+export class SmartMediaEditor extends React.Component<SmartMediaEditorProps> {
+  render() {
+    return null;
+  }
+}
