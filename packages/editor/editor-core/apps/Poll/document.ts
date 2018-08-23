df1faee66e272270653629716c96d275c98b1795
Eduard Shvedai <eshvedai@atlassian.com>
2018-08-24 00:12:46 +1000

    fix creating multiple polls on the page

--- packages/editor/editor-core/apps/Poll/document.ts
+++ packages/editor/editor-core/apps/Poll/document.ts
@@ -1,4 +1,30 @@
 import { EDITOR_APPS_EXTENSION_TYPE, POLL_EXTENSION_KEY } from './';
+import { generateUuid } from '@atlaskit/editor-common';
+
+export const getPollDefaultNode = () => ({
+  type: 'extension',
+  attrs: {
+    extensionType: EDITOR_APPS_EXTENSION_TYPE,
+    extensionKey: POLL_EXTENSION_KEY,
+    parameters: {
+      id: `${generateUuid()}`,
+      title: 'Title',
+      choices: [
+        {
+          id: `${generateUuid()}`,
+          value: 'Option 1',
+        },
+        {
+          id: `${generateUuid()}`,
+          value: 'Option 2',
+        },
+      ],
+      createDate: 1534859278962,
+      finishDate: 1535551200000,
+    },
+    layout: 'default',
+  },
+});
 
 export const exampleDocument = {
   version: 1,
@@ -23,11 +49,11 @@ export const exampleDocument = {
           title: 'Tabs or spaces?',
           choices: [
             {
-              id: '1',
+              id: `${generateUuid()}`,
               value: 'Tabs',
             },
             {
-              id: '2',
+              id: `${generateUuid()}`,
               value: 'Spaces',
             },
           ],
--- packages/editor/editor-core/examples/5-full-page-with-poll.tsx
+++ packages/editor/editor-core/examples/5-full-page-with-poll.tsx
@@ -1,38 +1,14 @@
 import * as React from 'react';
 import { default as FullPageExample } from './5-full-page';
 import GraphBarIcon from '@atlaskit/icon/glyph/graph-bar';
-import { exampleDocument } from '../apps/Poll/document';
-
-const pollExtensionDefault = {
-  type: 'extension',
-  attrs: {
-    extensionType: 'com.atlassian.editor.apps.core',
-    extensionKey: 'poll',
-    parameters: {
-      id: '123',
-      title: 'Tabs or spaces?',
-      choices: [
-        {
-          id: '1',
-          value: 'Tabs',
-        },
-        {
-          id: '2',
-          value: 'Spaces',
-        },
-      ],
-      createDate: 1534859278962,
-      finishDate: 1535551200000,
-    },
-  },
-};
+import { exampleDocument, getPollDefaultNode } from '../apps/Poll/document';
 
 const quickInsertItems = [
   {
     title: 'Poll',
     icon: () => <GraphBarIcon label="Poll" />,
     action(insert) {
-      return insert(pollExtensionDefault);
+      return insert(getPollDefaultNode());
     },
   },
 ];
@@ -55,7 +31,7 @@ const insertMenuItems = [
     tooltipPosition: 'right',
     elemBefore: <GraphBarIcon label="Poll" />,
     onClick: function(editorActions) {
-      editorActions.replaceSelection(pollExtensionDefault);
+      editorActions.replaceSelection(getPollDefaultNode());
     },
   },
 ];
