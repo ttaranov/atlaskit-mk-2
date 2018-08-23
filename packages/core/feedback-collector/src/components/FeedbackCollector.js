c7a935d42fcceb845e5476606dc0a3e4b40edc91
Carla <cbaba@atlassian.com>
2018-08-23 15:45:00 +1000

    apply design changes

--- packages/core/feedback-collector/src/components/FeedbackCollector.js
+++ packages/core/feedback-collector/src/components/FeedbackCollector.js
@@ -4,8 +4,9 @@ import styled from 'styled-components';
 import { FieldTextAreaStateless } from '@atlaskit/field-text-area';
 import Button from '@atlaskit/button';
 import Select from '@atlaskit/select';
-import Form, { Field, FormHeader, FormSection } from '@atlaskit/form';
+import Form, { Field } from '@atlaskit/form';
 import Checkbox, { CheckboxGroup } from '@atlaskit/checkbox';
+import Modal, { ModalFooter } from '@atlaskit/modal-dialog';
 // import type { Element } from 'react';
 
 type Props = {
@@ -15,7 +16,7 @@ type Props = {
 };
 
 type State = {
-  type: 'bug' | 'compliment' | 'suggestion' | 'question' | 'empty',
+  type: 'bug' | 'comment' | 'suggestion' | 'question' | 'empty',
 };
 
 const Footer = styled.span`
@@ -25,10 +26,10 @@ const Footer = styled.span`
 `;
 
 const options = {
-  bug: 'Yikes! Describe your bug or issue',
-  compliment: 'Aw thanks! What would you like to share?',
-  suggestion: "That's great! What would you like to share with us?",
-  question: 'What would you like to ask us?',
+  bug: 'Describe the bug or issue',
+  comment: "Let us know what's on your mind?",
+  suggestion: "Let us know what you'd like to improve",
+  question: 'What would you like to know?',
   empty: 'Select an option',
 };
 
@@ -41,65 +42,78 @@ export default class FeedbackCollector extends React.Component<Props, State> {
     type: 'empty',
   };
 
+  isTypeSelected = () => this.state.type !== 'empty';
+
+  renderActions = () => (
+    <ModalFooter>
+      <Footer>
+        <Button
+          appearance="primary"
+          type="submit"
+          isDisabled={!this.isTypeSelected()}
+        >
+          Submit
+        </Button>
+        <Button appearance="subtle" type="button">
+          Cancel
+        </Button>
+      </Footer>
+    </ModalFooter>
+  );
+
   render() {
     return (
-      <Form name="feedback-collector">
-        <FormHeader title="Tell us what you think" />
-        <FormSection>
+      <Modal heading="Share us your thoughts" footer={this.renderActions}>
+        <Form name="feedback-collector">
           <Field>
             <Select
               isSearchable={false}
               onChange={option => this.setState({ type: option.value })}
+              menuPortalTarget={document.body}
+              styles={{ menuPortal: base => ({ ...base, zIndex: 9999 }) }}
               defaultValue={{
-                label: "Choose what you'd like to give feedback on",
+                label: 'I want to...',
               }}
               options={[
-                { label: 'I have a bug to report', value: 'bug' },
-                { label: 'I have a compliment', value: 'compliment' },
-                { label: 'I have a suggestion', value: 'suggestion' },
-                { label: 'I have a question', value: 'question' },
+                { label: 'Ask a question', value: 'question' },
+                { label: 'Leave a comment', value: 'comment' },
+                { label: 'Report a bug', value: 'bug' },
+                { label: 'Suggest an improvement', value: 'suggestion' },
               ]}
             />
           </Field>
-        </FormSection>
-        {this.state.type !== 'empty' ? (
-          <Fragment>
-            <Field label={options[this.state.type]} isRequired>
-              <FieldTextAreaStateless
-                name="repo_name"
-                isRequired
-                shouldFitContainer
-              />
-            </Field>
 
-            <Field>
-              <CheckboxGroup>
-                <Checkbox
-                  value="Basic checkbox"
-                  name="checkbox-basic"
-                  label="Atlassian can contact me about this feedback"
+          {this.isTypeSelected() ? (
+            <Fragment>
+              <Field label={options[this.state.type]} isRequired>
+                <FieldTextAreaStateless
+                  name="repo_name"
+                  isRequired
+                  shouldFitContainer
                 />
+              </Field>
 
-                <Checkbox
-                  value="Basic checkbox"
-                  name="checkbox-basic"
-                  label="I'd like to participate in product research"
-                />
-              </CheckboxGroup>
-            </Field>
-            <Footer>
-              <Button appearance="primary" type="submit">
-                Submit
-              </Button>
-              <Button appearance="subtle" type="button">
-                Cancel
-              </Button>
-            </Footer>
-          </Fragment>
-        ) : (
-          <Fragment />
-        )}
-      </Form>
+              <Field>
+                <CheckboxGroup>
+                  <Checkbox
+                    value="Basic checkbox"
+                    name="checkbox-basic"
+                    label="Atlassian can contact me about this feedback"
+                  />
+
+                  <Checkbox
+                    value="Basic checkbox"
+                    name="checkbox-basic"
+                    label="I'd like to participate in product research"
+                  />
+                </CheckboxGroup>
+              </Field>
+            </Fragment>
+          ) : (
+            <Fragment />
+          )}
+        </Form>
+      </Modal>
     );
   }
 }
