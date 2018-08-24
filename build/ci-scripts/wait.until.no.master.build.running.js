51fa93987142b02a5344be76fdb887ca3f580f32
Luke Batchelor <lbatchelor@atlassian.com>
2018-08-24 11:11:10 +1000

    WIP

---
+++ build/ci-scripts/wait.until.no.master.build.running.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+const axios = require('axios');
+const pWaitFor = require('p-wait-for');
+/*
+   This script is a stop gap solution until this code is moved into landkid.
+   It simply waits until there is no build running in master.
+   We run this at the end of a landkid build so that we never merge a PR whilst
+   master is still running.
+*/
+
+const BUILDS_PER_PAGE = 30;
+const REPO_OWNER = process.env.BITBUCKET_REPO_OWNER || 'atlassian';
+const REPO_SLUG = process.env.BITBUCKET_REPO_SLUG || 'atlaskit-mk-2';
+const PIPELINES_ENDPOINT = `https://api.bitbucket.org/2.0/repositories/${REPO_OWNER}/${REPO_SLUG}/pipelines/`;
+const INTERVAL = 15000;
+
+const axiosRequestConfig = {
+  params: {
+    pagelen: BUILDS_PER_PAGE,
+    // get the most recent builds first
+    sort: '-created_on',
+    'target.ref_name': 'lbatch/fixing-more-bugs-in-changelog',
+    'target.ref_type': 'BRANCH',
+  },
+};
+
+function noMasterRunning() {
+  console.log(+new Date(), 'Checking if master is running...');
+  return axios.get(PIPELINES_ENDPOINT, axiosRequestConfig).then(response => {
+    const allPipelines = response.data.values;
+    console.log(allPipelines.map(p => p.state.name));
+    const runningPipelines = allPipelines
+      .filter(
+        pipeline =>
+          pipeline.state.name === 'IN_PROGRESS' ||
+          pipeline.state.name === 'PENDING',
+      )
+      // remove the scheduled builds (website, etc)
+      .filter(job => job.trigger.name !== 'SCHEDULE');
+    console.log(runningPipelines.length, 'master build running');
+    return runningPipelines.length === 0;
+  });
+}
+
+console.log(
+  'Waiting until there is no master build running so that we can merge...',
+);
+
+pWaitFor(noMasterRunning, 15000);
