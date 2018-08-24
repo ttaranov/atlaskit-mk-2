81d32c82668d48633423374593f9a47d877776c2
Nathan Flew <nflew@atlassian.com>
2018-08-24 10:40:44 +1000

    Fix default dates for RSVP

--- packages/editor/editor-core/apps/RSVP/ui/index.tsx
+++ packages/editor/editor-core/apps/RSVP/ui/index.tsx
@@ -100,6 +100,10 @@ export class RSVPApp extends Component<Props, State> {
     attendees: [] as Array<Attendee>,
   };
 
+  static defaultProps = {
+    duration: 7200000,
+  };
+
   constructor(props) {
     super(props);
     this.userId = getUserId();
@@ -151,10 +155,20 @@ export class RSVPApp extends Component<Props, State> {
     }
   }
 
+  private ensureDateTime(dateTime) {
+    if (dateTime) {
+      return dateTime;
+    }
+
+    const date = new Date();
+    return date.setDate(date.getDate() + 7);
+  }
+
   render() {
     const { title, dateTime, duration, location, showMap } = this.props;
     const { appState, showForm, hasJoined, attendees } = this.state;
-    const endTime = addMilliseconds(dateTime, duration);
+    const eventDateTime = this.ensureDateTime(dateTime);
+    const endTime = addMilliseconds(eventDateTime, duration);
     return (
       <RSVP>
         <div className="info">
@@ -163,9 +177,10 @@ export class RSVPApp extends Component<Props, State> {
               <CalendarIcon label="Date and Time" /> Date and Time
             </h5>
             <p>
-              {format(dateTime, DATE_FORMAT)}
+              {format(eventDateTime, DATE_FORMAT)}
               <br />
-              {format(dateTime, TIME_FORMAT)} - {format(endTime, TIME_FORMAT)}
+              {format(eventDateTime, TIME_FORMAT)} -{' '}
+              {format(endTime, TIME_FORMAT)}
               <br />
               <a
                 target="_blank"
