3224ae380309cf0374e264f2c64366f75c5cd963
Charles Lee <clee@atlassian.com>
2018-08-28 16:28:43 +1000

    remove theme functionality

--- packages/core/radio/src/Radio.js
+++ packages/core/radio/src/Radio.js
@@ -1,16 +1,88 @@
 // @flow
-import React from 'react';
-import { ThemeProvider, withTheme } from 'styled-components';
-import Radio from './RadioBase';
-import { type RadioBasePropTypes } from './types';
+import React, { Component } from 'react';
+import RadioInput from './RadioInput';
+import { Label } from './styled/Radio';
+import type { RadioBasePropTypes } from './types';
 
-const RadioWithTheme = withTheme(Radio);
-const emptyTheme = {};
-
-export default (props: RadioBasePropTypes) => {
-  return (
-    <ThemeProvider theme={emptyTheme}>
-      <RadioWithTheme {...props} />
-    </ThemeProvider>
-  );
+type State = {
+  isHovered: boolean,
+  isFocused: boolean,
+  isActive: boolean,
+  isMouseDown: boolean,
 };
+
+export default class Radio extends Component<RadioBasePropTypes, State> {
+  static defaultProps = {
+    isDisabled: false,
+    isInvalid: false,
+    isChecked: false,
+  };
+
+  state: State = {
+    isHovered: false,
+    isFocused: false,
+    isActive: false,
+    isMouseDown: false,
+  };
+
+  onBlur = () => {
+    this.setState({
+      // onBlur is called after onMouseDown if the checkbox was focused, however
+      // in this case on blur is called immediately after, and we need to check
+      // whether the mouse is down.
+      isActive: this.state.isMouseDown && this.state.isActive,
+      isFocused: false,
+    });
+  };
+  onFocus = () => {
+    this.setState({ isFocused: true });
+  };
+  onMouseLeave = () => this.setState({ isActive: false, isHovered: false });
+  onMouseEnter = () => this.setState({ isHovered: true });
+  onMouseUp = () => this.setState({ isActive: false, isMouseDown: false });
+  onMouseDown = () => this.setState({ isActive: true, isMouseDown: true });
+
+  render() {
+    const {
+      children,
+      isDisabled,
+      isRequired,
+      isInvalid,
+      isChecked,
+      name,
+      onChange,
+      onInvalid,
+      value,
+      ...props
+    } = this.props;
+    const { isFocused, isHovered, isActive } = this.state;
+
+    return (
+      <Label
+        {...props}
+        isDisabled={isDisabled}
+        onMouseDown={this.onMouseDown}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+        onMouseUp={this.onMouseUp}
+      >
+        <RadioInput
+          isChecked={isChecked}
+          isDisabled={isDisabled}
+          isFocused={isFocused}
+          isHovered={isHovered}
+          isInvalid={isInvalid}
+          isRequired={isRequired}
+          isActive={isActive}
+          onChange={onChange}
+          onInvalid={onInvalid}
+          onBlur={this.onBlur}
+          onFocus={this.onFocus}
+          name={name}
+          value={value}
+        />
+        <span>{children}</span>
+      </Label>
+    );
+  }
+}
