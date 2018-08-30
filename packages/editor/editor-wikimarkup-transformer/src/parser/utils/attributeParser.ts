f2549143af3874fc250f2cfc17a6ad56b11cfbc5
Jerome Wang <jwang2@atlassian.com>
2018-08-31 09:37:41 +1000

    fix the build

--- packages/editor/editor-wikimarkup-transformer/src/parser/utils/attributeParser.ts
+++ packages/editor/editor-wikimarkup-transformer/src/parser/utils/attributeParser.ts
@@ -8,10 +8,6 @@ export function parseAttrs(
 
   // take only first value of the same keys
   Object.keys(output).forEach(key => {
-    console.log(output);
-
-    console.log(key);
-
     if (Array.isArray(output[key])) {
       output[key] = output[key][0];
     }
