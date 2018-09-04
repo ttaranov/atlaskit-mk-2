0db739d499abf56422c7cd9f80ed180a4f479663
Tong Li <tli@atlassian.com>
2018-09-04 15:29:26 +1000

    keep width and height when transform back to wiki attachment

--- packages/editor/editor-wikimarkup-transformer/src/encoder/nodes/media-group.ts
+++ packages/editor/editor-wikimarkup-transformer/src/encoder/nodes/media-group.ts
@@ -1,10 +1,11 @@
 import { Node as PMNode } from 'prosemirror-model';
 import { NodeEncoder } from '..';
+import { media } from './media';
 
 export const mediaGroup: NodeEncoder = (node: PMNode): string => {
   const result: string[] = [];
   node.forEach(n => {
-    result.push(`!${n.attrs.id}|thumbnail!`);
+    result.push(media(n));
   });
 
   return result.join('\n');
