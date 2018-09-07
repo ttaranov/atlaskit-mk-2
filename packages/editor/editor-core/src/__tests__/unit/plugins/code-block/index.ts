756184e093b052e4a72bc5d91b97738a7e44e4d6
Jake Coppinger <jcoppinger@atlassian.com>
2018-09-07 12:08:06 +1000

    CHANGESET: ED-5060 Code blocks now use new floating toolbar
    Summary: ED-5060 Code blocks now use new floating toolbar
    
    Release notes: <none>
    
    Releases: @atlaskit/editor-core@minor
    
    Dependents: []
    
    ---
    {"summary":"ED-5060 Code blocks now use new floating toolbar","releases":[{"name":"@atlaskit/editor-core","type":"minor"}],"dependents":[]}
    ---

--- packages/editor/editor-core/src/__tests__/unit/plugins/code-block/index.ts
+++ packages/editor/editor-core/src/__tests__/unit/plugins/code-block/index.ts
@@ -1,85 +1,146 @@
-import { stateKey as codeBlockPluginKey } from '../../../../plugins/code-block/pm-plugins/main';
 import {
   code_block,
   doc,
   createEditor,
   p,
+  table,
+  tr,
+  td,
+  sendKeyToPm,
+  createEvent,
 } from '@atlaskit/editor-test-helpers';
+
+import { pluginKey as codeBlockPluginKey } from '../../../../plugins/code-block/pm-plugins/main';
+import tablesPlugin from '../../../../plugins/table';
+import listPlugin from '../../../../plugins/lists';
+import {
+  removeCodeBlock,
+  changeLanguage,
+} from '../../../../plugins/code-block/actions';
 import { setTextSelection } from '../../../../utils';
+import codeBlockPlugin from '../../../../plugins/code-block';
+import { SelectOption } from '../../../../plugins/floating-toolbar/ui/Select';
 
 describe('code-block', () => {
+  const event = createEvent('event');
   const editor = (doc: any) => {
     return createEditor({
       doc,
+      editorPlugins: [codeBlockPlugin(), listPlugin, tablesPlugin()],
       editorProps: { allowCodeBlocks: true },
       pluginKey: codeBlockPluginKey,
     });
   };
 
   describe('plugin', () => {
-    describe('#state.init', () => {
-      it('should set activeCodeBlock if initial selection is inside a code-block', () => {
-        const { editorView, plugin } = editor(
-          doc(code_block()('para{<>}graph')),
+    describe('API', () => {
+      it('should be able to identify code block node', () => {
+        const { pluginState } = editor(doc(code_block()('te{<>}xt')));
+        expect(pluginState.element).not.toBe(undefined);
+      });
+
+      it('should not identify code block if initial selection is outside a code-block', () => {
+        const { pluginState } = editor(doc(p('paragraph{<>}')));
+        expect(pluginState.element).toBe(undefined);
+      });
+
+      it('should be able to remove code block type using function removeCodeBlock', () => {
+        const { editorView } = editor(doc(code_block()('te{<>}xt')));
+        removeCodeBlock(editorView.state, editorView.dispatch);
+        expect(editorView.state.doc).toEqualDocument(doc(p()));
+      });
+
+      it('should be possible to remove code block with no text inside table', () => {
+        const { pluginState, editorView } = editor(
+          doc(table()(tr(td({})(code_block()('{<>}'))))),
         );
-        const activeCodeBlock = plugin.spec.state.init({}, editorView.state);
-        expect(activeCodeBlock.pos).toBe(0);
-        expect(activeCodeBlock.node).toEqualDocument(
-          editorView.state.doc.nodeAt(0),
+        expect(pluginState.element).not.toBe(undefined);
+        removeCodeBlock(editorView.state, editorView.dispatch);
+        expect(editorView.state.doc).toEqualDocument(
+          doc(table()(tr(td({})(p())))),
         );
       });
 
-      it('should not set activeCodeBlock if initial selection is outside a code-block', () => {
-        const { editorView, plugin } = editor(doc(p('paragraph{<>}')));
-        const activeCodeBlock = plugin.spec.state.init({}, editorView.state);
-        expect(activeCodeBlock).toBeUndefined();
+      it('should be able to remove codeBlock using function removeCodeBlock even if it has no text content', () => {
+        const { pluginState, editorView } = editor(doc(code_block()('{<>}')));
+        expect(pluginState.element).not.toBe(undefined);
+        removeCodeBlock(editorView.state, editorView.dispatch);
+        expect(editorView.state.doc).toEqualDocument(doc(p()));
+      });
+
+      it('should not remove surrounding blocks while removing codeBlock', () => {
+        const { editorView } = editor(
+          doc(p('testing'), code_block()('te{<>}xt'), p('testing')),
+        );
+        removeCodeBlock(editorView.state, editorView.dispatch);
+        expect(editorView.state.doc).toEqualDocument(
+          doc(p('testing'), p('testing')),
+        );
+      });
+    });
+
+    describe('toolbarVisible', () => {
+      describe('when focus is inside the code block', () => {
+        it('it is true', () => {
+          const { pluginState } = editor(
+            doc(p('text'), code_block()('text te{<>}xt')),
+          );
+          expect(pluginState.toolbarVisible).toBe(true);
+        });
+      });
+
+      describe('when editor is blur', () => {
+        it('is false', () => {
+          const { editorView, plugin, pluginState } = editor(
+            doc(p('te{<>}xt'), code_block()('text')),
+          );
+          plugin.props.handleDOMEvents!.blur(editorView, event);
+          expect(pluginState.toolbarVisible).toBe(false);
+        });
+      });
+    });
+
+    describe('language picker', () => {
+      describe('when selecting new language', () => {
+        it('language in state should update', () => {
+          const { pluginState, editorView } = editor(
+            doc(code_block()('text{<>}')),
+          );
+          const language: SelectOption = {
+            value: 'someValue',
+            label: 'someLabel',
+          };
+          changeLanguage(language)(editorView.state, editorView.dispatch);
+          setTimeout(() => {
+            expect(pluginState.language).toEqual(language);
+          }, 0);
+        });
+      });
+    });
+    describe('keyMaps', () => {
+      describe('when Enter key is pressed', () => {
+        it('a new paragraph should be created in code block', () => {
+          const { editorView } = editor(doc(code_block()('text{<>}')));
+          sendKeyToPm(editorView, 'Enter');
+          expect(editorView.state.doc).toEqualDocument(
+            doc(code_block()('text\n')),
+          );
+        });
+      });
+
+      describe('when Enter key is pressed twice', () => {
+        it('a new paragraph should be created outside code block', () => {
+          const { editorView } = editor(doc(code_block()('text{<>}')));
+          sendKeyToPm(editorView, 'Enter');
+          sendKeyToPm(editorView, 'Enter');
+          expect(editorView.state.doc).toEqualDocument(
+            doc(code_block()('text'), p()),
+          );
+        });
       });
     });
 
     describe('#state.update', () => {
-      describe('when entering code block', () => {
-        it('sets the activeCodeBlock', () => {
-          const {
-            refs: { cbPos },
-            editorView,
-          } = editor(doc(p('paragraph{<>}'), code_block()('codeBlock{cbPos}')));
-
-          setTextSelection(editorView, cbPos);
-          const activeCodeBlock = codeBlockPluginKey.getState(editorView.state);
-          expect(activeCodeBlock.pos).toBe(11);
-          expect(activeCodeBlock.node).toEqualDocument(
-            editorView.state.doc.nodeAt(11),
-          );
-          editorView.destroy();
-        });
-      });
-
-      describe('when moving to a different code block', () => {
-        it('should update the activeCodeBlock', () => {
-          const {
-            refs: { cbPos },
-            editorView,
-          } = editor(
-            doc(
-              code_block()('codeBlock{<>}'),
-              code_block()('codeBlock{cbPos}'),
-            ),
-          );
-
-          let activeCodeBlock = codeBlockPluginKey.getState(editorView.state);
-          expect(activeCodeBlock).toBeDefined();
-
-          setTextSelection(editorView, cbPos);
-
-          activeCodeBlock = codeBlockPluginKey.getState(editorView.state);
-          expect(activeCodeBlock.pos).toBe(11);
-          expect(activeCodeBlock.node).toEqualDocument(
-            editorView.state.doc.nodeAt(11),
-          );
-          editorView.destroy();
-        });
-      });
-
       describe('when moving within the same code block', () => {
         it('should not update state', () => {
           const {
@@ -106,32 +167,12 @@ describe('code-block', () => {
           expect(codeBlockPluginKey.getState(editorView.state)).toBeDefined();
           setTextSelection(editorView, pPos);
 
-          expect(codeBlockPluginKey.getState(editorView.state)).toBeUndefined();
+          expect(codeBlockPluginKey.getState(editorView.state)).toEqual({
+            element: undefined,
+            language: undefined,
+            toolbarVisible: false,
+          });
           editorView.destroy();
-        });
-      });
-
-      describe('when cursor at the end of the code block', () => {
-        it('should set active element pos immediately before the code block', () => {
-          const { pluginState } = editor(doc(code_block()('codeBlock{<>}')));
-
-          expect(pluginState.pos).toBe(0);
-        });
-      });
-
-      describe('when cursor at the beginning of the code block', () => {
-        it('should set active element pos immediately before the code block', () => {
-          const { pluginState } = editor(doc(code_block()('{<>}codeBlock')));
-
-          expect(pluginState.pos).toBe(0);
-        });
-      });
-
-      describe('when cursor at the middle of the code block', () => {
-        it('should set active element pos immediately before the code block', () => {
-          const { pluginState } = editor(doc(code_block()('code{<>}Block')));
-
-          expect(pluginState.pos).toBe(0);
         });
       });
     });
