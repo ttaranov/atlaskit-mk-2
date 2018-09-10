d3c6758c9cb8cfcb35f24756d499757ab3f4c4bd
Julien Hoarau <jhoarau@atlassian.com>
2018-09-11 09:59:33 +1000

    FS-2963 Merge missing files

--- packages/editor/editor-core/src/plugins/status/index.tsx
+++ packages/editor/editor-core/src/plugins/status/index.tsx
@@ -1,7 +1,17 @@
+import * as React from 'react';
 import { status } from '@atlaskit/editor-common';
-
+import { findDomRefAtPos } from 'prosemirror-utils';
 import { EditorPlugin } from '../../types';
-import createStatusPlugin from './plugin';
+import createStatusPlugin, { StatusState, pluginKey } from './plugin';
+import WithPluginState from '../../ui/WithPluginState';
+import StatusPicker from './ui/statusPicker';
+import { setColorPickerAt, insertStatus } from './actions';
+
+export type StatusType = {
+  color: string;
+  text: string;
+  localId?: string;
+};
 
 const statusPlugin: EditorPlugin = {
   nodes() {
@@ -15,6 +25,39 @@ const statusPlugin: EditorPlugin = {
         plugin: createStatusPlugin,
       },
     ];
+  },
+
+  contentComponent({ editorView }) {
+    const { dispatch } = editorView;
+    return (
+      <WithPluginState
+        plugins={{
+          statusState: pluginKey,
+        }}
+        render={({ statusState = {} as StatusState }) => {
+          if (statusState.showStatusPickerAt === null) {
+            return null;
+          }
+
+          const element = findDomRefAtPos(
+            statusState.showStatusPickerAt,
+            editorView.domAtPos.bind(editorView),
+          ) as HTMLElement;
+
+          return (
+            <StatusPicker
+              element={element}
+              onSelect={status =>
+                insertStatus(status)(editorView.state, dispatch)
+              }
+              closeStatusPicker={() =>
+                setColorPickerAt(null)(editorView.state, dispatch)
+              }
+            />
+          );
+        }}
+      />
+    );
   },
 };
 
--- packages/elements/status/src/index.ts
+++ packages/elements/status/src/index.ts
@@ -1,1 +1,5 @@
 export { Status, Props, Color } from './components/Status';
+export {
+  StatusPicker,
+  Props as StatusPickerProps,
+} from './components/StatusPicker';
