a6046936294983aa919666f1884287a6961ae58b
Oscar Wallhult <oscar@fupi.me>
2018-09-12 22:47:44 +1000

    Definition fix

--- typings/node.d.ts
+++ typings/node.d.ts
@@ -1,3 +1,13 @@
 declare namespace NodeJS {
   interface ReadableStream {}
+  interface EventEmitter {}
+  interface Process {
+    stdin: any;
+    stdout: any;
+    cwd(): string;
+    exit(code?: number): never;
+    nextTick(callback: Function, ...args: any[]): void;
+  }
 }
+
+declare var process: NodeJS.Process;
