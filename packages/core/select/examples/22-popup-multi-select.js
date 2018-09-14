06897518761c59eb41f39762d303be56d94a0374
Sean Curtis <scurtis@atlassian.com>
2018-09-14 13:35:22 +1000

    Updated checkbox import.

--- packages/core/select/examples/22-popup-multi-select.js
+++ packages/core/select/examples/22-popup-multi-select.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import Button from '@atlaskit/button';
-import Checkbox from '@atlaskit/checkbox';
+import { Checkbox } from '@atlaskit/checkbox';
 import DownIcon from '@atlaskit/icon/glyph/hipchat/chevron-down';
 import { PopupSelect } from '../src';
 
