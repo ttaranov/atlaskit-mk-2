35f562a29bccaf16943752edf7bc8022c9266dbc
Jared Crowe <jcrowe@atlassian.com>
2018-09-17 10:01:23 +1000

    tidy up line lengths of docs comments

--- packages/core/global-navigation/src/components/GlobalNavigation/types.js
+++ packages/core/global-navigation/src/components/GlobalNavigation/types.js
@@ -17,60 +17,66 @@ export type NavigationStateShape = InitialNavigationStateShape & {
 };
 
 export type GlobalNavDrawerProps = {
-  /** A prop to take control over the opening and closing of drawer. NOTE: GlobalNavigation controls the drawer behaviour by default. */
+  /** A prop to take control over the opening and closing of drawer. NOTE:
+   * GlobalNavigation controls the drawer behaviour by default. */
   isCreateDrawerOpen?: boolean,
-  /** The contents of the create drawer. This is ignored if onCreateClick is passed */
+  /** The contents of the create drawer. This is ignored if onCreateClick is
+   * passed. */
   createDrawerContents?: ComponentType<*>,
-  /** A callback function which will be fired when the create drawer is opened
+  /** A callback function which will be fired when the create drawer is opened.
    * */
   onCreateDrawerOpen?: () => void,
-  /** A callback function which will be fired when the create drawer is closed
+  /** A callback function which will be fired when the create drawer is closed.
    * */
   onCreateDrawerClose?: () => void,
-  /** A prop to decide if the contents of the drawer should unmount on drawer close. It is true by default.
-   * */
+  /** A prop to decide if the contents of the drawer should unmount on drawer
+   * close. It is true by default. */
   shouldCreateDrawerUnmountOnExit?: boolean,
 
-  /** A prop to take control over the opening and closing of drawer. NOTE: GlobalNavigation controls the drawer behaviour by default. */
+  /** A prop to take control over the opening and closing of drawer. NOTE:
+   * GlobalNavigation controls the drawer behaviour by default. */
   isSearchDrawerOpen?: boolean,
-  /** The contents of the search drawer. This is ignored if onSearchClick is passed */
+  /** The contents of the search drawer. This is ignored if onSearchClick is
+   * passed. */
   searchDrawerContents?: ComponentType<*>,
-  /** A callback function which will be called when the search drawer is opened
+  /** A callback function which will be called when the search drawer is opened.
    * */
   onSearchDrawerOpen?: () => void,
-  /** A callback function which will be called when the search drawer is closed
+  /** A callback function which will be called when the search drawer is closed.
    * */
   onSearchDrawerClose?: () => void,
-  /** A prop to decide if the contents of the drawer should unmount on drawer close. It is true by default.
-   * */
+  /** A prop to decide if the contents of the drawer should unmount on drawer
+   * close. It is true by default. */
   shouldSearchDrawerUnmountOnExit?: boolean,
 
-  /** A prop to take control over the opening and closing of drawer. NOTE: GlobalNavigation controls the drawer behaviour by default. */
+  /** A prop to take control over the opening and closing of drawer. NOTE:
+   * GlobalNavigation controls the drawer behaviour by default. */
   isNotificationDrawerOpen?: boolean,
-  /** The contents of the notifications drawer */
+  /** The contents of the notifications drawer. */
   notificationDrawerContents?: ComponentType<*>,
   /** A callback function which will be called when the notifications drawer is
-   * opened */
+   * opened. */
   onNotificationDrawerOpen?: () => void,
   /** A callback function which will be called when the notifications drawer is
-   * closed */
+   * closed. */
   onNotificationDrawerClose?: () => void,
-  /** A prop to decide if the contents of the drawer should unmount on drawer close. It is true by default.
-   * */
+  /** A prop to decide if the contents of the drawer should unmount on drawer
+   * close. It is true by default. */
   shouldNotificationDrawerUnmountOnExit?: boolean,
 
-  /** A prop to take control over the opening and closing of drawer. NOTE: GlobalNavigation controls the drawer behaviour by default. */
+  /** A prop to take control over the opening and closing of drawer. NOTE:
+   * GlobalNavigation controls the drawer behaviour by default. */
   isStarredDrawerOpen?: boolean,
-  /** The contents of the starred drawer */
+  /** The contents of the starred drawer. */
   starredDrawerContents?: ComponentType<*>,
-  /** A callback function which will be called when the starred drawer is opened
-   * */
+  /** A callback function which will be called when the starred drawer is
+   * opened. */
   onStarredDrawerOpen?: () => void,
-  /** A callback function which will be called when the starred drawer is closed
-   * */
+  /** A callback function which will be called when the starred drawer is
+   * closed. */
   onStarredDrawerClose?: () => void,
-  /** A prop to decide if the contents of the drawer should unmount on drawer close. It is true by default.
-   * */
+  /** A prop to decide if the contents of the drawer should unmount on drawer
+   * close. It is true by default. */
   shouldStarredDrawerUnmountOnExit?: boolean,
 };
 
@@ -78,7 +84,7 @@ export type GlobalNavigationProps = {
   /** The product logo. Expected to be an Atlaskit Logo component. */
   productIcon?: ComponentType<{}>,
   /** A callback function which will be called when the product logo item is
-   * clicked. If this is passed, the drawer does not show up*/
+   * clicked. If this is passed, the drawer does not show up. */
   onProductClick?: () => void,
   /** The text to display in the tooltip for the product logo item. */
   productTooltip?: string,
@@ -86,7 +92,7 @@ export type GlobalNavigationProps = {
   productHref?: string,
 
   /** A callback function which will be called when the product logo item is
-   * clicked. If this is passed, the drawer does not show up*/
+   * clicked. If this is passed, the drawer does not show up. */
   onCreateClick?: ?() => void,
   /** The text to display in the tooltip for the create item. */
   createTooltip?: string,
@@ -98,7 +104,7 @@ export type GlobalNavigationProps = {
   starredTooltip?: string,
 
   /** A callback function which will be called when the product logo item is
-   * clicked. If this is passed, the drawer does not show up*/
+   * clicked. If this is passed, the drawer does not show up. */
   onSearchClick?: ?() => void,
   /** The text to display in the tooltip for the search item. */
   searchTooltip?: string,
@@ -123,7 +129,7 @@ export type GlobalNavigationProps = {
   loginHref?: string,
 
   /** A callback function which will be called when the product logo item is
-   * clicked. If this is passed, the drawer does not show up*/
+   * clicked. If this is passed, the drawer does not show up. */
   onNotificationClick?: ?() => void,
   /** The number of unread notifications. Will render as a badge above the
    * notifications item. */
