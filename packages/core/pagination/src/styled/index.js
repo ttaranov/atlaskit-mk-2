f99f6bec7988d6ab524423a717b6a5b44cfd8d55
Ajay Narain Mathur <amathur@atlassian.com>
2018-09-18 15:09:09 +1000

    updated the pagination styles

--- packages/core/pagination/src/styled/index.js
+++ packages/core/pagination/src/styled/index.js
@@ -4,12 +4,6 @@ import Button from '@atlaskit/button';
 
 export const Container = styled.div`
   display: flex;
-  width: 100%;
-`;
-
-export const ButtonActive = styled(Button)`
-  color: #000 !important; /* @todo: until Button supports not faded disabled state */
-  font-weight: bold;
 `;
 
 export const Ellipsis = styled.span`
