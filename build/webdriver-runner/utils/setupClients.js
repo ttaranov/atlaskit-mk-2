466d1055b6a1b59e9737f185fb06385a00360d0a
ckrishnakumar <ckrishnakumar@atlassian.com>
2018-09-18 13:23:45 +1000

    update chrome

--- build/webdriver-runner/utils/setupClients.js
+++ build/webdriver-runner/utils/setupClients.js
@@ -19,7 +19,7 @@ function setBrowserStackClients() /*: Array<?Object>*/ {
       os: 'Windows',
       os_version: '10',
       browserName: 'chrome',
-      browser_version: '68.0',
+      browser_version: '69.0',
       resolution: '1440x900',
     },
     firefox: {
