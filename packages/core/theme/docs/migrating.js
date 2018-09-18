c62685518e46166a91c80aab2d1ca734cbf595ab
Trey Shugart <treshugart@gmail.com>
2018-09-18 13:01:23 +1000

    Update migration guide.

--- packages/core/theme/docs/migrating.js
+++ packages/core/theme/docs/migrating.js
@@ -3,6 +3,30 @@
 import { code, md } from '@atlaskit/docs';
 
 export default md`
+  ## 6.x - 7.x
+
+  The only breaking changes between these two versions are for experimental APIs.
+
+  The only experimental API that changed here is the \`Theme\` component. Themes are no longer components, but functions that return objects.
+
+  ### values -> theme
+
+  The \`values\` prop has been renamed to \`theme\` to be more aligned with the API shape on components, and it better describes the prop itself, especially with the addition of \`props\`.
+
+  We opted *not* to make this changed backward compatible by keeping \`value\` for a short time as the API is still experimental because we have no known consumers of the new API yet and we've converted all of our components as part of this update.
+
+  ${code`
+    import { Theme } from '@atlaskit/theme';
+
+    const theme = parentTheme => ({
+      ...parentTheme,
+      mode: 'light'
+    });
+
+    - <Theme values={theme} />
+    + <Theme theme={theme} />
+  `}
+
   ## 5.x - 6.x
 
   The only breaking changes between these two versions are for experimental APIs.
