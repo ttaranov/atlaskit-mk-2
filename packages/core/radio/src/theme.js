322241a4efe12670f54fd0a11e40ab54d323144f
Charles Lee <clee@atlassian.com>
2018-09-20 16:43:27 +1000

    fix flow types

--- packages/core/radio/src/theme.js
+++ packages/core/radio/src/theme.js
@@ -2,8 +2,8 @@
 import { colors } from '@atlaskit/theme';
 import type { RadioIconProps } from './types';
 
-const getBorderColor = (props: IconWrapperProps) => {
-  if (props.isDisabled) return '';
+const getBorderColor = (props: RadioIconProps) => {
+  if (props.isDisabled) return { light: '', dark: '' };
   if (props.isFocused) return { light: colors.B100, dark: colors.B75 };
   if (props.isActive) return { light: 'currentColor', dark: 'currentColor' };
   if (props.isInvalid) return { light: colors.R300, dark: colors.R300 };
@@ -11,7 +11,7 @@ const getBorderColor = (props: IconWrapperProps) => {
   return { light: colors.N40, dark: colors.DN80 };
 };
 
-const getDotColor = props => {
+const getDotColor = (props: RadioIconProps) => {
   const { isChecked, isDisabled, isActive } = props;
 
   let color = { light: colors.N10, dark: colors.DN10 };
@@ -26,7 +26,7 @@ const getDotColor = props => {
   return color;
 };
 
-const getCircleColor = props => {
+const getCircleColor = (props: RadioIconProps) => {
   const { isChecked, isDisabled, isActive, isHovered, isInvalid } = props;
 
   // set the default
@@ -49,8 +49,14 @@ const getCircleColor = props => {
   return color;
 };
 
-type ThemeProps = {
-  radio: (
+const getTextColor = ({ isDisabled }) => {
+  return isDisabled
+    ? { light: colors.N80, dark: colors.N80 }
+    : { light: colors.N900, dark: colors.DN600 };
+};
+
+export type ThemeProps = {
+  radio?: (
     state: RadioIconProps,
   ) => {
     dotColor: string,
@@ -58,19 +64,17 @@ type ThemeProps = {
     borderColor: string,
     textColor: string,
   },
-  mode: 'light' | 'dark',
+  mode?: 'light' | 'dark',
 };
 
 export default function(props: ThemeProps) {
   const mode = props.mode || 'light';
   return {
-    radio: state => ({
+    radio: (state: RadioIconProps) => ({
       dotColor: getDotColor(state)[mode],
       circleColor: getCircleColor(state)[mode],
       borderColor: getBorderColor(state)[mode],
-      textColor: state.isDisabled
-        ? { light: colors.N80, dark: colors.N80 }
-        : { light: colors.N900, dark: colors.DN600 },
+      textColor: getTextColor(state)[mode],
       ...(props.radio ? props.radio(state) : null),
     }),
     mode,
