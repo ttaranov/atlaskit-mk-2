47f1cf0a8c791aecb96972a771edccd0f05c7a25
Nathan Flew <nflew@atlassian.com>
2018-09-21 13:44:09 +1000

    Extension integration tests

---
+++ packages/editor/editor-core/src/__tests__/integration/extensions/quick-insert.ts
@@ -0,0 +1,25 @@
+import { BrowserTestCase } from '@atlaskit/webdriver-runner/runner';
+import Page from '@atlaskit/webdriver-runner/wd-wrapper';
+import {
+  getDocFromElement,
+  fullpage,
+  editable,
+  quickInsert,
+} from '../_helpers';
+
+BrowserTestCase(
+  `Extension: Quick Insert`,
+  { skip: ['edge', 'ie', 'safari'] },
+  async client => {
+    const page = await new Page(client);
+    await page.goto(fullpage.path);
+    await page.waitForSelector(fullpage.placeholder);
+    await page.click(fullpage.placeholder);
+
+    await quickInsert(page, 'Bodied extension');
+    await quickInsert(page, 'Action');
+
+    const doc = await page.$eval(editable, getDocFromElement);
+    expect(doc).toMatchDocSnapshot();
+  },
+);
