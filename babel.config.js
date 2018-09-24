c66ece0807f1d4621a03ebc1643dba2753fad969
Luke Batchelor <lbatchelor@atlassian.com>
2018-09-24 13:24:44 +1000

    Fidding with babel-config

--- babel.config.js
+++ babel.config.js
@@ -5,16 +5,29 @@ module.exports = {
     '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/syntax-dynamic-import',
+    ['styled-components', { minify: false }],
   ],
   presets: ['@babel/react', '@babel/flow'],
   env: {
     'production:cjs': {
+      plugins: ['@babel/transform-runtime'],
       presets: [['@babel/env', { modules: 'commonjs' }]],
-      ignore: ['__mocks__', '__tests__', '__fixtures__', 'node_modules'],
+      ignore: [
+        '**/__mocks__',
+        '**/__tests__',
+        '**/__fixtures__',
+        'node_modules',
+      ],
     },
     'production:esm': {
+      plugins: ['@babel/transform-runtime'],
       presets: [['@babel/env', { modules: false }]],
-      ignore: ['__mocks__', '__tests__', '__fixtures__', 'node_modules'],
+      ignore: [
+        '**/__mocks__',
+        '**/__tests__',
+        '**/__fixtures__',
+        'node_modules',
+      ],
     },
     test: {
       presets: ['@babel/env'],
