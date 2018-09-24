f8fef50ccd93c12e731805045b29af5ce5a1b8b7
Trey Shugart <treshugart@gmail.com>
2018-09-24 16:30:30 +1000

    Hurr.

--- packages/core/theme/src/hoc.js
+++ packages/core/theme/src/hoc.js
@@ -6,7 +6,7 @@ import Theme from './components/Theme';
 // Pre-executes the theme and passes it as a prop to the supplied component.
 // This is useful for ensuring that the current theme is accessible as props
 // in styled-components.
-export function withTheme<Props = {}>(El: ComponentType<Props>) {
+export function withTheme<Props: {}>(El: ComponentType<Props>) {
   return function createComponent(props: Props) {
     return <Theme>{t => <El {...props} theme={t} />}</Theme>;
   };
