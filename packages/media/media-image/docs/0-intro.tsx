ca3c488d34bea6bedf3bf73c747528f539e854b8
raja07 <rbellebon@atlassian.com>
2018-09-25 11:02:00 +1000

    Add some text to docs for Media image

---
+++ packages/media/media-image/docs/0-intro.tsx
@@ -0,0 +1,26 @@
+import { md, code } from '@atlaskit/docs';
+
+export default md`
+  # MediaImage
+
+  This package is required by other Media Components, and should not be used
+  directly.
+
+  To be filled
+
+  ## Installation
+
+  ${code`
+yarn add @atlaskit/media-image
+  `}
+
+  ## Using the component
+
+  To be filled
+
+  ${code`
+import { MediaImage } from '@atlaskit/media-image';
+
+   To be filled
+  `}
+`;
