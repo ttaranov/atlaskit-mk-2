9bba7a21d7d0c9542cc215590e78be38b466526a
raja07 <rbellebon@atlassian.com>
2018-09-25 16:33:31 +1000

    Add inline dialog basic

--- packages/core/inline-dialog/docs/0-intro.js
+++ packages/core/inline-dialog/docs/0-intro.js
@@ -1,8 +1,10 @@
 // @flow
 import React from 'react';
-import { md, Props } from '@atlaskit/docs';
+import { code, md, Example, Props } from '@atlaskit/docs';
 
 export default md`
+  ### Usage
+
   Inline Dialog components launch a modal that is displayed outside a block of content, and over the top of surrounding elements. They can be made to change position to fit in the page.
 
   The content takes two different react elements:
@@ -10,6 +12,17 @@ export default md`
     - The children, which are the elements the modal will be positioned relative to, using the position prop.
     - The content, which is the content to display inside the modal.
 
+  ${code`
+  import InlineDialog from '@atlaskit/inline-dialog';
+  `}
+
+  ${(
+    <Example
+      title="Basic"
+      Component={require('../examples/01-basic').default}
+      source={require('!!raw-loader!../examples/01-basic')}
+    />
+  )}
 
   ${(
     <Props
