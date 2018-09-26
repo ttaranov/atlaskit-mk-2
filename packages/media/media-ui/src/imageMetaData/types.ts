42e8dfad31e30d6ac34a8ca8d8c3f9e11fdd2ed1
Ali Chamas <achamas@atlassian.com>
2018-09-26 13:51:59 +1000

    remove orientation types

--- packages/media/media-ui/src/imageMetaData/types.ts
+++ packages/media/media-ui/src/imageMetaData/types.ts
@@ -30,15 +30,3 @@ export type FileInfo = {
   file: File;
   src: string;
 };
-
-// https://www.impulseadventure.com/photo/exif-orientation.html
-export const OrientationTransforms: { [key: number]: string } = {
-  1: '',
-  2: 'rotateY(180deg)',
-  3: 'rotate(180deg)',
-  4: 'rotate(180deg) rotateY(180deg)',
-  5: 'rotate(270deg) rotateY(180deg)',
-  6: 'rotate(90deg)',
-  7: 'rotate(90deg) rotateY(180deg)',
-  8: 'rotate(270deg)',
-};
