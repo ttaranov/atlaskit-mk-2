ad655de569f11bc0fc5ccdd4f64034488f7aeb38
Alex Hixon <ahixon@atlassian.com>
2018-09-26 17:47:59 +1000

    support predefined layouts

--- packages/editor/editor-core/src/plugins/layout/actions.ts
+++ packages/editor/editor-core/src/plugins/layout/actions.ts
@@ -1,25 +1,104 @@
 import { safeInsert } from 'prosemirror-utils';
-import { Node } from 'prosemirror-model';
+import { Node, Fragment, Slice } from 'prosemirror-model';
 import { Command } from '../../types';
 import { pluginKey, LayoutState } from './pm-plugins/main';
+import { EditorState, Transaction } from 'prosemirror-state';
+import { mapChildren } from '../../utils/slice';
+import { isEmptyDocument } from '../../utils';
+
+export type PredefinedLayout = 'two_equal' | 'three_equal';
+
+const PredefinedLayoutFilters = (
+  widths,
+): { [key in PredefinedLayout]: boolean } => {
+  return {
+    two_equal: widths.length === 2 && widths.every(width => width === 50),
+    three_equal: widths.length === 3 && widths.every(width => width === 33.33),
+  };
+};
+
+export const getPredefinedLayout = (section: Node) => {
+  const widths = mapChildren(section, column => column.attrs.width);
+  const matchingFilters = PredefinedLayoutFilters(widths);
+  return Object.keys(matchingFilters).find(name => matchingFilters[name]);
+};
+
+export const createDefaultLayoutSection = (state: EditorState) => {
+  const { layoutSection, layoutColumn } = state.schema.nodes;
+
+  // create a 50-50 layout by default
+  const columns = Fragment.fromArray([
+    layoutColumn.createAndFill({ width: 50 }) as Node,
+    layoutColumn.createAndFill({ width: 50 }) as Node,
+  ]);
+
+  return layoutSection.createAndFill(undefined, columns) as Node;
+};
 
 export const insertLayoutColumns: Command = (state, dispatch) => {
-  const { layoutSection } = state.schema.nodes;
-  dispatch(safeInsert(layoutSection.createAndFill() as Node)(state.tr));
+  dispatch(safeInsert(createDefaultLayoutSection(state))(state.tr));
   return true;
 };
 
-// We just change the attribute value here
-// Merging the layouts is handled by the appendTransaction
-export const setActiveLayoutType = (layoutType: string): Command => (
+// FIXME: update column widths
+export function forceSectionToPredefinedLayout(
+  state: EditorState,
+  node: Node,
+  pos: number,
+  predefinedLayout: PredefinedLayout,
+): Transaction | undefined {
+  const tr = state.tr;
+  if (predefinedLayout === 'two_equal' && node.childCount === 3) {
+    const thirdColumn = node.content.child(2);
+    const insideRightEdgeOfLayoutSection = pos + node.nodeSize - 1;
+    const thirdColumnPos =
+      insideRightEdgeOfLayoutSection - thirdColumn.nodeSize;
+    if (isEmptyDocument(thirdColumn)) {
+      tr.replaceRange(
+        // end pos of second column
+        tr.mapping.map(thirdColumnPos - 1),
+        tr.mapping.map(insideRightEdgeOfLayoutSection),
+        Slice.empty,
+      );
+    } else {
+      tr.replaceRange(
+        // end pos of second column
+        tr.mapping.map(thirdColumnPos - 1),
+        // start pos of third column
+        tr.mapping.map(thirdColumnPos + 1),
+        Slice.empty,
+      );
+    }
+  } else if (predefinedLayout === 'three_equal' && node.childCount === 2) {
+    const insideRightEdgeOfLayoutSection = pos + node.nodeSize - 1;
+    tr.replaceWith(
+      tr.mapping.map(insideRightEdgeOfLayoutSection),
+      tr.mapping.map(insideRightEdgeOfLayoutSection),
+      state.schema.nodes.layoutColumn.createAndFill() as Node,
+    );
+  }
+  return tr.docChanged ? tr : undefined;
+}
+
+export const setPredefinedLayout = (layout: PredefinedLayout): Command => (
   state,
   dispatch,
 ) => {
   const { pos } = pluginKey.getState(state) as LayoutState;
-  if (pos !== null) {
-    dispatch(state.tr.setNodeMarkup(pos, undefined, { layoutType }));
+  if (pos === null) {
+    return false;
+  }
+
+  const node = state.doc.nodeAt(pos);
+  if (!node) {
+    return false;
+  }
+  const tr = forceSectionToPredefinedLayout(state, node, pos, layout);
+  if (tr) {
+    dispatch(tr);
     return true;
   }
+
   return false;
 };
 
--- packages/editor/editor-core/src/plugins/layout/toolbar.ts
+++ packages/editor/editor-core/src/plugins/layout/toolbar.ts
@@ -1,5 +1,4 @@
 import { defineMessages, InjectedIntl } from 'react-intl';
-import { Node } from 'prosemirror-model';
 import { EditorState } from 'prosemirror-state';
 import { findDomRefAtPos } from 'prosemirror-utils';
 import LayoutTwoEqualIcon from '@atlaskit/icon/glyph/editor/layout-two-equal';
@@ -14,7 +13,12 @@ import {
   FloatingToolbarItem,
   Icon,
 } from '../../../src/plugins/floating-toolbar/types';
-import { setActiveLayoutType, deleteActiveLayoutNode } from './actions';
+import {
+  setPredefinedLayout,
+  deleteActiveLayoutNode,
+  getPredefinedLayout,
+  PredefinedLayout,
+} from './actions';
 
 export const messages = defineMessages({
   twoColumns: {
@@ -29,7 +33,13 @@ export const messages = defineMessages({
   },
 });
 
-const LAYOUT_TYPES = [
+type PredefinedLayoutButtonItem = {
+  type: PredefinedLayout;
+  title: MessageDescriptor;
+  icon: Icon;
+};
+
+const LAYOUT_TYPES: PredefinedLayoutButtonItem[] = [
   { type: 'two_equal', title: messages.twoColumns, icon: LayoutTwoEqualIcon },
   {
     type: 'three_equal',
@@ -43,14 +53,14 @@ const LAYOUT_TYPES = [
 
 const buildLayoutButton = (
   intl: InjectedIntl,
-  item: { type: string; title: MessageDescriptor; icon: Icon },
-  active: Node,
+  item: PredefinedLayoutButtonItem,
+  currentLayout: string | undefined,
 ): FloatingToolbarItem<Command> => ({
   type: 'button',
   icon: item.icon,
   title: intl.formatMessage(item.title),
-  onClick: setActiveLayoutType(item.type),
-  selected: active.attrs.layoutType === item.type,
+  onClick: setPredefinedLayout(item.type),
+  selected: !!currentLayout && currentLayout === item.type,
 });
 
 export const buildToolbar = (
@@ -60,13 +70,14 @@ export const buildToolbar = (
 ): FloatingToolbarConfig | undefined => {
   const node = state.doc.nodeAt(pos);
   if (node) {
+    const currentLayout = getPredefinedLayout(node);
     return {
       title: 'Columns floating controls',
       getDomRef: view =>
         findDomRefAtPos(pos, view.domAtPos.bind(view)) as HTMLElement,
       nodeType: state.schema.nodes.layoutSection,
       items: [
-        ...LAYOUT_TYPES.map(i => buildLayoutButton(intl, i, node)),
+        ...LAYOUT_TYPES.map(i => buildLayoutButton(intl, i, currentLayout)),
         { type: 'separator' },
         {
           type: 'button',
