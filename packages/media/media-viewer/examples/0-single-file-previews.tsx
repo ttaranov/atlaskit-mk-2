20d8dcec036697aaf1c16fc22956ee21542947cf
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-09-27 17:29:02 +1000

    Use Card in MediaViewer examples

--- packages/media/media-viewer/examples/0-single-file-previews.tsx
+++ packages/media/media-viewer/examples/0-single-file-previews.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import Button from '@atlaskit/button';
 import { createStorybookContext } from '@atlaskit/media-test-helpers';
+import { FileIdentifier, Card } from '@atlaskit/media-card';
 import { ButtonList, Container, Group } from '../example-helpers/styled';
 import {
   archiveItem,
@@ -33,88 +33,64 @@ export default class Example extends React.Component<{}, State> {
     this.setState({ selectedItem });
   };
 
+  createItem = (item: MediaViewerItem, title: string) => {
+    const identifier: FileIdentifier = {
+      id: item.id,
+      mediaItemType: 'file',
+    };
+    const onClick = this.setItem(item);
+
+    return (
+      <div>
+        <h4>{title}</h4>
+        <Card identifier={identifier} context={context} onClick={onClick} />
+      </div>
+    );
+  };
+
   render() {
     return (
       <Container>
         <Group>
           <h2>Image</h2>
           <ButtonList>
-            <li>
-              <Button onClick={this.setItem(imageItem)}>Picture</Button>
-            </li>
-            <li>
-              <Button onClick={this.setItem(smallImageItem)}>Icon</Button>
-            </li>
-            <li>
-              <Button onClick={this.setItem(wideImageItem)}>Wide image</Button>
-            </li>
-            <li>
-              <Button onClick={this.setItem(largeImageItem)}>Tall image</Button>
-            </li>
+            <li>{this.createItem(imageItem, 'Picture')}</li>
+            <li>{this.createItem(smallImageItem, 'Icon')}</li>
+            <li>{this.createItem(wideImageItem, 'Wide')}</li>
+            <li>{this.createItem(largeImageItem, 'Large')}</li>
           </ButtonList>
         </Group>
-
         <Group>
           <h2>Document</h2>
           <ButtonList>
-            <li>
-              <Button onClick={this.setItem(docItem)}>Spreadsheet</Button>
-            </li>
+            <li>{this.createItem(docItem, 'Normal')}</li>
           </ButtonList>
         </Group>
-
         <Group>
           <h2>Video</h2>
           <ButtonList>
-            <li>
-              <Button onClick={this.setItem(videoHorizontalFileItem)}>
-                Video horizontal
-              </Button>
-            </li>
-            <li>
-              <Button onClick={this.setItem(videoLargeFileItem)}>
-                Video large
-              </Button>
-            </li>
-            <li>
-              <Button onClick={this.setItem(videoItem)}>Video vertical</Button>
-            </li>
+            <li>{this.createItem(videoHorizontalFileItem, 'Horizontal')}</li>
+            <li>{this.createItem(videoLargeFileItem, 'Large')}</li>
+            <li>{this.createItem(videoItem, 'Normal')}</li>
           </ButtonList>
         </Group>
-
         <Group>
           <h2>Audio</h2>
           <ButtonList>
-            <li>
-              <Button onClick={this.setItem(audioItem)}>Song with cover</Button>
-            </li>
-            <li>
-              <Button onClick={this.setItem(audioItemNoCover)}>
-                Song without cover
-              </Button>
-            </li>
+            <li>{this.createItem(audioItem, 'Normal')}</li>
+            <li>{this.createItem(audioItemNoCover, 'Song without cover')}</li>
           </ButtonList>
         </Group>
-
         <Group>
           <h2>Errors</h2>
           <ButtonList>
+            <li>{this.createItem(unsupportedItem, 'Unsupported item')}</li>
+            <li>{this.createItem(archiveItem, 'Archive has no preview')}</li>
             <li>
-              <Button onClick={this.setItem(unsupportedItem)}>
-                Unsupported item
-              </Button>
-            </li>
-            <li>
-              <Button onClick={this.setItem(archiveItem)}>Archive</Button>
-            </li>
-            <li>
-              <Button onClick={this.setItem(videoProcessingFailedItem)}>
-                Failed video processing
-              </Button>
+              {this.createItem(videoProcessingFailedItem, 'Failed processing')}
             </li>
           </ButtonList>
         </Group>
-
         {this.state.selectedItem && (
           <MediaViewer
             featureFlags={{ customVideoPlayer: true }}
