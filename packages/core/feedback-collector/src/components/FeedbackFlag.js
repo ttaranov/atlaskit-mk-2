f1e014aa835a06dd3277e4fa7510a10fda628c93
Nadia Makarevich <nmakarevich@atlassian.com>
2018-09-28 17:57:38 +1000

    fix flow

--- packages/core/feedback-collector/src/components/FeedbackFlag.js
+++ packages/core/feedback-collector/src/components/FeedbackFlag.js
@@ -4,7 +4,11 @@ import { AutoDismissFlag } from '@atlaskit/flag';
 import { colors } from '@atlaskit/theme';
 import SuccessIcon from '@atlaskit/icon/glyph/check-circle';
 
-const FeedbackFlag = props => (
+type AkProps = {
+  isDismissAllowed?: boolean,
+  onDismissed?: (...args: Array<any>) => mixed,
+};
+const FeedbackFlag = (props: AkProps) => (
   <AutoDismissFlag
     icon={<SuccessIcon primaryColor={colors.G300} label="Success" />}
     id="feedbackSent"
