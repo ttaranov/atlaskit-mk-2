c721a0fd3ca0faefa50716ec955dcfb537bdc624
Sean Curtis <scurtis@atlassian.com>
2018-10-02 14:59:31 +1000

    Fixing test.

--- packages/core/inline-dialog/src/InlineDialog/__tests__/unit/index.js
+++ packages/core/inline-dialog/src/InlineDialog/__tests__/unit/index.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import { mount, shallow } from 'enzyme';
+import { Popper } from '@atlaskit/popper';
 
 import InlineDialogWithAnalytics from '../../..';
 import { InlineDialogWithoutAnalytics as InlineDialog } from '../..';
