6a2d80afe2b61bdb4bff0c25f22b191e0344207d
Will Mendes <willmendesneto@gmail.com>
2018-10-03 16:23:55 +1000

    chore(quick-search): creating basic quick search file

--- packages/search/quick-search/examples/7-Using-inside-Drawers.js
+++ packages/search/quick-search/examples/7-Using-inside-Drawers.js
@@ -1,149 +1,8 @@
 // @flow
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Drawer from '@atlaskit/drawer';
 
-import { objectData, personData, containerData } from './utils/mockData';
-import {
-  QuickSearch,
-  ResultItemGroup,
-  ContainerResult,
-  PersonResult,
-  ObjectResult,
-} from '../src';
-
-type DataShape = {
-  title: string,
-  items: Array<any>,
-};
-
-const data: DataShape[] = [
-  {
-    title: 'Objects',
-    items: objectData(5),
-  },
-  {
-    title: 'People',
-    items: personData(5),
-  },
-  {
-    title: 'Containers',
-    items: containerData(5),
-  },
-];
-
-const availableResultTypes = {
-  person: PersonResult,
-  object: ObjectResult,
-  container: ContainerResult,
-};
-
-const mapResultsDataToComponents = resultData => {
-  if (!resultData || !resultData.length) {
-    return 'Nothin` to see here';
-  }
-
-  return resultData.map(group => (
-    <ResultItemGroup title={group.title} key={group.title}>
-      {group.items.map(props => {
-        const Result = availableResultTypes[props.type];
-        return Result ? (
-          <Result key={props.resultId} {...props} isSelected={false} />
-        ) : null;
-      })}
-    </ResultItemGroup>
-  ));
-};
-
-function contains(string, query) {
-  return string.toLowerCase().indexOf(query.toLowerCase()) > -1;
-}
-
-function searchData(query: string): DataShape[] {
-  const results = data
-    .map(({ title, items }) => {
-      const filteredItems = items.filter(item => contains(item.name, query));
-      return { title, items: filteredItems };
-    })
-    .filter(group => group.items.length);
-  return results;
-}
-
-// a little fake store for holding the query after a component unmounts
-const store = {};
-
-type BasicQuickSearchState = {
-  query: string,
-  results: DataShape[],
-  isLoading: boolean,
-};
-
-// eslint-disable-next-line react/no-multi-comp
-class BasicQuickSearch extends Component<*, BasicQuickSearchState> {
-  static propTypes = {
-    fakeNetworkLatency: PropTypes.number,
-  };
-
-  static defaultProps = {
-    fakeNetworkLatency: 0,
-  };
-
-  state = {
-    query: store.query || '',
-    results: searchData(''),
-    isLoading: false,
-  };
-
-  searchTimeoutId: ?TimeoutID;
-
-  setQuery(query) {
-    store.query = query;
-    this.setState({
-      query,
-    });
-  }
-
-  search = (query: string) => {
-    if (this.searchTimeoutId) {
-      clearTimeout(this.searchTimeoutId);
-    }
-    this.setState({
-      isLoading: true,
-    });
-    this.setQuery(query);
-    const results = searchData(query);
-    this.searchTimeoutId = setTimeout(() => {
-      this.setState({
-        results,
-        isLoading: false,
-      });
-    }, this.props.fakeNetworkLatency);
-  };
-
-  quickSearchInnerRef: mixed;
-
-  setQuickSearchRef = (ref: any) => {
-    this.quickSearchInnerRef = ref;
-  };
-
-  render() {
-    return (
-      <QuickSearch
-        isLoading={this.state.isLoading}
-        onSearchInput={({ target }) => {
-          this.search(target.value);
-        }}
-        onSearchSubmit={() => console.log('onSearchSubmit', this.state.query)}
-        value={this.state.query}
-        innerRef={this.setQuickSearchRef}
-      >
-        <div style={{ paddingLeft: '10px' }}>
-          {mapResultsDataToComponents(this.state.results)}
-        </div>
-      </QuickSearch>
-    );
-  }
-}
+import BasicQuickSearch from './utils/BasicQuickSearch';
 
 type State = {
   isDrawerOpen: boolean,
@@ -183,7 +42,9 @@ export default class DrawersExample extends Component<{}, State> {
   };
 
   setQuickSearchRef = (ref: any) => {
-    this.quickSearchRef = ref.quickSearchInnerRef;
+    if (ref) {
+      this.quickSearchRef = ref.quickSearchInnerRef;
+    }
   };
 
   render() {
