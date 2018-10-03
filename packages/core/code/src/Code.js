2f35147aec857dc83148bbfc408ac3cb1ef6aa88
Marco De Jongh <mdejongh@atlassian.com>
2018-10-03 16:43:54 +1000

    Switch to async loaded refractor

--- packages/core/code/src/Code.js
+++ packages/core/code/src/Code.js
@@ -1,6 +1,6 @@
 // @flow
 import React, { PureComponent } from 'react';
-import SyntaxHighlighter from 'react-syntax-highlighter/prism-light';
+import SyntaxHighlighter from 'react-syntax-highlighter/prism-async';
 import { withTheme, ThemeProvider } from 'styled-components';
 import {
   normalizeLanguage,
