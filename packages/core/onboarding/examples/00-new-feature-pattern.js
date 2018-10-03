0f875c1483b9088775bce1bcd9ad501103b522cb
Pete Gleeson <pgleeson@atlassian.com>
2018-10-03 16:55:28 +1000

    adds windowed blanket and API rework spike

--- packages/core/onboarding/examples/00-new-feature-pattern.js
+++ packages/core/onboarding/examples/00-new-feature-pattern.js
@@ -27,35 +27,56 @@ class NewFeature extends React.Component<{}, { spotlightVisible: boolean }> {
     const { spotlightVisible } = this.state;
     return (
       <Wrapper>
-        <SpotlightManager blanketIsTinted={false}>
-          <Button>Another element</Button>
-          <SpotlightTarget name="add-btn">
-            <SpotlightPulse>
-              <Button
-                iconBefore={<AddIcon />}
-                onClick={() =>
-                  this.setState({ spotlightVisible: !spotlightVisible })
-                }
-              />
-            </SpotlightPulse>
-          </SpotlightTarget>
-          <SpotlightTransition>
-            {spotlightVisible && (
-              <Spotlight
-                image="https://www.w3schools.com/w3css/img_lights.jpg"
-                target="add-btn"
-                targetBgColor="#fff"
-                targetOnClick={() =>
-                  this.setState({ spotlightVisible: !spotlightVisible })
-                }
-                footer="1/3"
-              >
-                It is now easier to create an issue. Click on the plus button to
-                create a new issue.
-              </Spotlight>
-            )}
-          </SpotlightTransition>
-        </SpotlightManager>
+        <Spotlight blanketIsTinted={false}>
+          {({ targetRef }) => (
+            <React.Fragment>
+              <SpotlightPulse ref={targetRef}>
+                <Button
+                  iconBefore={<AddIcon />}
+                  onClick={() =>
+                    this.setState({ spotlightVisible: !spotlightVisible })
+                  }
+                />
+              </SpotlightPulse>
+              <SpotlightTransition>
+                {spotlightVisible && (
+                  <SpotlightDialog
+                    image="https://www.w3schools.com/w3css/img_lights.jpg"
+                    targetBgColor="#fff"
+                    targetOnClick={() =>
+                      this.setState({ spotlightVisible: !spotlightVisible })
+                    }
+                    footer="1/3"
+                  >
+                    It is now easier to create an issue. Click on the plus
+                    button to create a new issue.
+                  </SpotlightDialog>
+                )}
+              </SpotlightTransition>
+            </React.Fragment>
+          )}
+        </Spotlight>
+
+        <Spotlight>
+          {({ targetRef }) => (
+            <React.Fragment>
+              <Button innerRef={targetRef}>I am a new feature</Button>
+              <SpotlightTransition>
+                {spotlightVisible && (
+                  <SpotlightDialog
+                    image="https://www.w3schools.com/w3css/img_lights.jpg"
+                    footer="1/3"
+                  >
+                    It is now easier to create an issue. Click on the plus
+                    button to create a new issue.
+                  </SpotlightDialog>
+                )}
+              </SpotlightTransition>
+            </React.Fragment>
+          )}
+        </Spotlight>
+
+        <Button>Another element</Button>
       </Wrapper>
     );
   }
---
+++ packages/core/onboarding/src/styled/WindowBlanket.js
@@ -0,0 +1,100 @@
+// @flow
+import React, { Component } from 'react';
+import { colors } from '@atlaskit/theme';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: fixed;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  pointer-events: none;
+`;
+
+const Top = styled.div`
+  flex: 0 0 ${({ top }) => top}px;
+  width: ${({ width }) => width}px;
+  background: rgba(9, 30, 66, 0.54);
+  pointer-events: auto;
+`;
+
+const Bottom = styled.div`
+  flex: 0 0 calc(100% - ${({ bottom }) => bottom}px);
+  width: ${({ width }) => width}px;
+  background: rgba(9, 30, 66, 0.54);
+  pointer-events: auto;
+`;
+
+const Left = styled.div`
+  width: ${({ width }) => width}px;
+  flex: 0 0 100%;
+  background: rgba(9, 30, 66, 0.54);
+  pointer-events: auto;
+`;
+
+const Right = styled.div`
+  flex: 0 0 100%;
+  background: rgba(9, 30, 66, 0.54);
+  width: calc(100% - ${({ right }) => right}px);
+  pointer-events: auto;
+`;
+
+type Props = {
+  bottom: number,
+  left: number,
+  right: number,
+  top: number,
+  width: number,
+  height: number,
+  radius: number,
+};
+
+export default class WindowedBlanket extends Component<Props> {
+  // targetRef = ref => {
+  //   if (ref) {
+  //     const {
+  //       left,
+  //       right,
+  //       top,
+  //       bottom,
+  //       height,
+  //       width,
+  //     } = ref.getBoundingClientRect();
+  //     const radius = parseInt(
+  //       window.getComputedStyle(ref).getPropertyValue('border-radius'),
+  //       10,
+  //     );
+  //     this.setState({ left, right, top, bottom, height, width, radius });
+  //   }
+  // };
+  render() {
+    const { left, right, top, bottom, height, width, radius } = this.props;
+    return (
+      <Container>
+        {left > 0 && <Left width={left} />}
+        {top > 0 && <Top top={top} width={width} />}
+        <svg height={height} width={width}>
+          <path
+            fill={colors.N100A}
+            fillRule="evenodd"
+            d={`M 0,0 H ${width} V ${height} H ${-width} z
+                  M 0,${radius} V ${height - radius}
+                  A ${radius},${radius} 0 0 0 ${radius},${radius}
+                  H ${width - radius}
+                  A ${radius},${radius} 0 0 0 ${radius},${-radius}
+                  V ${radius}
+                  A ${radius},${radius} 0 0 0 ${-radius},${-radius}
+                  H ${radius}
+                  A ${radius},${radius} 0 0 0 ${-radius},${radius} z`}
+          />
+        </svg>
+        {bottom > 0 && <Bottom width={width} bottom={bottom} />}
+        {right > 0 && <Right right={right} />}
+      </Container>
+    );
+  }
+}
