2d848cd6ceb8f20eff78dd5673d52c11f45554aa
Marco De Jongh <mdejongh@atlassian.com>
2018-10-03 12:11:45 +1000

    CHANGESET: Make hasherCreator load async and remove public export of createHasher
    Summary: Make hasherCreator load async and remove public export of createHasher
    
    Release notes: <none>
    
    Releases: @atlaskit/media-store@major
    
    Dependents: @atlaskit/media-core@patch, @atlaskit/media-picker@patch, @atlaskit/media-test-helpers@patch
    
    ---
    {"summary":"Make hasherCreator load async and remove public export of createHasher","releases":[{"name":"@atlaskit/media-store","type":"major"}],"dependents":[{"name":"@atlaskit/media-core","type":"patch","dependencies":["@atlaskit/media-test-helpers","@atlaskit/media-store"]},{"name":"@atlaskit/media-picker","type":"patch","dependencies":["@atlaskit/media-core","@atlaskit/media-test-helpers","@atlaskit/media-store"]},{"name":"@atlaskit/media-test-helpers","type":"patch","dependencies":["@atlaskit/media-core","@atlaskit/media-store"]}]}
    ---

--- packages/media/media-store/src/index.ts
+++ packages/media/media-store/src/index.ts
@@ -9,6 +9,3 @@ export {
   UploadFileCallbacks,
   UploadFileResult,
 } from './uploader';
-
-// Remove this export as soon as we deprecate oldUploadService
-export { createHasher } from './utils/hashing/hasherCreator';
