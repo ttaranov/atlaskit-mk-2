d349ce62d5a2a1b0f723cf093ee5038a0bdd5b04
Ajay Narain Mathur <amathur@atlassian.com>
2018-10-04 13:13:46 +1000

    flow fix

--- packages/core/pagination/src/types.js
+++ packages/core/pagination/src/types.js
@@ -3,14 +3,17 @@ import type { Node } from 'react';
 import type { ButtonProps, ButtonAppearances } from '@atlaskit/button';
 
 export type NavigatorPropsType = {
+  /** This will be passed in as ariaLabel to button. This is what screen reader will read */
+  ariaLabel?: string,
   /** React node to render in the button, pass the text you want use to view on pagination button */
   children?: Node,
   /** Is the navigator disabled */
   isDisabled?: boolean,
   /** This function is called with the when user clicks on navigator */
   onClick?: Function,
-  /** This will be passed in as ariaLabel to button. This is what screen reader will read */
-  ariaLabel?: string,
+  /** Add the padding styles to the navigator
+   * This can we used to add padding when displaying a icon
+   */
   styles?: Object,
 };
 
