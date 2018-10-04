f3b42814e0d21ddfa028159a455a57f85cc7a05f
Jake Coppinger <jcoppinger@atlassian.com>
2018-10-04 11:20:42 +1000

    Added buttonGroup class

---
+++ packages/editor/editor-core/src/ui/ButtonGroup/index.tsx
@@ -0,0 +1,89 @@
+import * as React from 'react';
+import { HTMLAttributes, ComponentClass, PureComponent } from 'react';
+import styled from 'styled-components';
+// import { ToolbarContext, ToolbarContextValue } from '../Toolbar/ToolbarContext';
+// import ToolbarButton from '../ToolbarButton';
+
+const ButtonGroupSpan: ComponentClass<
+  HTMLAttributes<{}> & { width?: 'small' | 'large' }
+> = styled.span`
+  display: inline-flex;
+  align-items: center;
+
+  & > div {
+    display: flex;
+  }
+`;
+
+export interface Props {
+  width?: 'small' | 'large';
+}
+
+export interface State {
+  selectedButton?: React.ReactChild;
+}
+
+export default class ButtonGroup extends PureComponent<
+  Props,
+  { selectedButton }
+> {
+  state = {
+    selectedButton: undefined,
+  };
+  constructor(props) {
+    super(props);
+  }
+  // private buttonClicked = consumerValue => (
+  //   button: ToolbarButton,
+  //   delta: number,
+  // ) => {
+  //   const children: React.ReactChild[] = React.Children.toArray(
+  //     this.props.children,
+  //   );
+
+  //   const buttonProps = button.props;
+  //   // @ts-ignore
+  //   const allButtonProps = children.map(item => item.props);
+  //   const buttonKeypressOriginIndex = allButtonProps.indexOf(buttonProps);
+  //   if (buttonKeypressOriginIndex === -1) {
+  //     console.log("Our button wasn't clicked, bailing from buttonClicked");
+  //     return null;
+  //   }
+  //   console.log('Keypress by button', buttonKeypressOriginIndex);
+  //   console.log('Delta is ', delta);
+  //   const selectedIndex = buttonKeypressOriginIndex + delta;
+  //   console.log('selectedIndex', selectedIndex);
+  //   if (selectedIndex >= 0 && selectedIndex < children.length) {
+  //     console.log('setting state');
+  //     this.setState({
+  //       selectedButton: children[selectedIndex],
+  //     });
+  //   }
+
+  //   if (selectedIndex < 0) {
+  //     consumerValue.buttonClickCallback(this, -1);
+  //   }
+  //   if (selectedIndex >= children.length) {
+  //     consumerValue.buttonClickCallback(this, -1);
+  //   }
+  //   return null;
+  // };
+
+  render() {
+    const { width } = this.props;
+    console.log('Buttongroup children:', this.props.children);
+
+    return (
+      // <ToolbarContext.Provider
+      //   value={{
+      //     // buttonClickCallback: this.buttonClicked(value),
+      //     // selectedButton,
+      //   }}
+      // >
+      <ButtonGroupSpan width={width}>{this.props.children}</ButtonGroupSpan>
+      // </ToolbarContext.Provider>
+    );
+  }
+}
+
+// value.buttonClickCallback
