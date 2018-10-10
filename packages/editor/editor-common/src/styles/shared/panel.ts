ea3b52274b097e9b42e1ee0f47a163a6433569c6
Stanislav Sysoev <ssysoev@atlassian.com>
2018-10-10 16:59:03 +1100

    CHANGESET: Dynamic font size for panels
    Summary: Dynamic font size for panels
    
    Release notes: <none>
    
    Releases: @atlaskit/editor-common@minor
    
    Dependents: []
    
    ---
    {"summary":"Dynamic font size for panels","releases":[{"name":"@atlaskit/editor-common","type":"minor"}],"dependents":[]}
    ---

--- packages/editor/editor-common/src/styles/shared/panel.ts
+++ packages/editor/editor-common/src/styles/shared/panel.ts
@@ -1,25 +1,32 @@
 // @ts-ignore: unused variable
 // prettier-ignore
 import { css, Styles, StyledComponentClass, InterpolationFunction, ThemeProps } from 'styled-components';
-import { gridSize, fontSize, borderRadius } from '@atlaskit/theme';
+import { gridSize, borderRadius } from '@atlaskit/theme';
 import { relativeSize, akEditorElementMinWidth } from '../consts';
 
 export const panelSharedStyles = css`
   & .ak-editor-panel {
     border-radius: ${borderRadius()}px;
     margin: ${relativeSize(1.142)}px 0;
-    font-size: ${fontSize()}px;
     padding: ${gridSize()}px;
     min-width: ${akEditorElementMinWidth}px;
+    display: flex;
+    align-items: baseline;
 
     .ak-editor-panel__icon {
-      position: absolute;
+      display: block;
       height: ${gridSize() * 3}px;
       width: ${gridSize() * 3}px;
+      padding-right: ${gridSize()}px;
+
+      > span {
+        vertical-align: middle;
+        display: inline;
+      }
     }
 
     .ak-editor-panel__content {
-      margin: 1px 0 1px ${gridSize() * 4}px;
+      margin: 1px 0 1px;
     }
   }
 `;
