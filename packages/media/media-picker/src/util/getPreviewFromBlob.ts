94293a7de71eb3318d412d79b98290b2234d7503
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-10-10 15:17:03 +1100

    Remove image src after using it

--- packages/media/media-picker/src/util/getPreviewFromBlob.ts
+++ packages/media/media-picker/src/util/getPreviewFromBlob.ts
@@ -16,6 +16,7 @@ export const getPreviewFromBlob = (
       img.onload = () => {
         const dimensions = { width: img.width, height: img.height };
 
+        URL.revokeObjectURL(src);
         resolve({ file, dimensions } as ImagePreview);
       };
       img.onerror = reject;
