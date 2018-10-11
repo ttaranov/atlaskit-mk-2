6c3b2a5867c3a862a1eae4c2853e43a668a78ae3
Jake Coppinger <jcoppinger@atlassian.com>
2018-10-11 15:58:25 +1100

    Inserting links into panels

---
+++ packages/editor/editor-core/src/__tests__/integration/panel/links-in-panels.ts
@@ -0,0 +1,29 @@
+import { BrowserTestCase } from '@atlaskit/webdriver-runner/runner';
+import Page from '@atlaskit/webdriver-runner/wd-wrapper';
+import {
+  editable,
+  getDocFromElement,
+  fullpage,
+  quickInsert,
+} from '../_helpers';
+
+BrowserTestCase(
+  'Inserts a link into a panel by typing Markdown',
+  { skip: ['edge', 'ie', 'safari'] },
+  async client => {
+    const browser = new Page(client);
+
+    await browser.goto(fullpage.path);
+
+    await browser.waitForSelector(fullpage.placeholder);
+    await browser.click(fullpage.placeholder);
+
+    await quickInsert(browser, 'Panel');
+
+    // type some text
+    await browser.type(editable, '[Atlassian](https://www.atlassian.com/)');
+
+    const doc = await browser.$eval(editable, getDocFromElement);
+    expect(doc).toMatchDocSnapshot();
+  },
+);
