fe38245bc43793bce453f1d118217f3626bfa216
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-10-11 19:25:10 +1100

    Update locales

--- packages/media/media-ui/src/i18n/it.ts
+++ packages/media/media-ui/src/i18n/it.ts
@@ -14,9 +14,13 @@ export default {
   'fabric.media.check_your_network': 'Check your network connection',
   'fabric.media.try_again': 'Try again',
   'fabric.media.no_gifs_found': "Hello? Was it me you're looking for?",
+  'fabric.media.no_gifs_found_suggestion':
+    'We couldn\'t find anything for "{query}"',
   'fabric.media.load_more_gifs': 'Load more GIFs',
   'fabric.media.add_account': 'Add account',
   'fabric.media.unlink_account': 'Unlink Account',
   'fabric.media.upload_file_from': 'Upload a file from {name}',
   'fabric.media.connect_to': 'Connect to {name}',
+  'fabric.media.connect_account_description':
+    "We'll open a new page to help you connect your {name} account",
 };
