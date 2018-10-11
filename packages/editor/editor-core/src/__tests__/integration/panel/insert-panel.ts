54bb06e4f8342c5997fce987f4064d702cee33ff
Jake Coppinger <jcoppinger@atlassian.com>
2018-10-11 15:17:30 +1100

    Integration test inserts panel via quickinsert

---
+++ packages/editor/editor-core/src/__tests__/integration/panel/insert-panel.ts
@@ -0,0 +1,33 @@
+import { BrowserTestCase } from '@atlaskit/webdriver-runner/runner';
+import Page from '@atlaskit/webdriver-runner/wd-wrapper';
+import {
+  editable,
+  getDocFromElement,
+  fullpage,
+  quickInsert,
+} from '../_helpers';
+
+BrowserTestCase(
+  'Inserts a panel on fullpage',
+  { skip: ['edge', 'ie', 'safari'] },
+  async client => {
+    const browser = new Page(client);
+
+    await browser.goto(fullpage.path);
+
+    // await browser.waitForSelector(editable);
+    // await browser.click(editable);
+    await browser.waitForSelector(fullpage.placeholder);
+    await browser.click(fullpage.placeholder);
+
+    await quickInsert(browser, 'Panel');
+    // type some text
+
+    await browser.type(editable, 'hello have a link ');
+
+    // expect(await browser.isVisible('.media-single')).toBe(true);
+
+    const doc = await browser.$eval(editable, getDocFromElement);
+    expect(doc).toMatchDocSnapshot();
+  },
+);
