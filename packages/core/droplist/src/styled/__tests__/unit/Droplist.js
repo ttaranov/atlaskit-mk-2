6704b2905f77e2a41173c61eeeeed31e011f58f0
Lucas <llago@atlassian.com>
2018-10-11 17:21:08 +1100

    Use should prefix in test scenarios to keep consistency

--- packages/core/droplist/src/styled/__tests__/unit/Droplist.js
+++ packages/core/droplist/src/styled/__tests__/unit/Droplist.js
@@ -4,16 +4,16 @@ import { shallow } from 'enzyme';
 import { Content } from '../../Droplist';
 
 describe('Dropbox content', () => {
-  it('renders correctly with default styles', () => {
+  it('should render correctly with default styles', () => {
     expect(shallow(<Content />)).toMatchSnapshot();
   });
 
-  it('renders max-height "90vh" when isTall is true', () => {
+  it('should render max-height "90vh" when isTall is true', () => {
     // $FlowFixMe - https://github.com/facebook/flow/issues/396
     expect(shallow(<Content isTall />)).toHaveStyleRule('max-height', '90vh');
   });
 
-  it('renders max-height "317.5px" when isTall is false', () => {
+  it('should render max-height "317.5px" when isTall is false', () => {
     // $FlowFixMe - https://github.com/facebook/flow/issues/396
     expect(shallow(<Content isTall={false} />)).toHaveStyleRule(
       'max-height',
@@ -21,7 +21,7 @@ describe('Dropbox content', () => {
     );
   });
 
-  it('renders max-height "350px" when maxHeight is 350', () => {
+  it('should render max-height "350px" when maxHeight is 350', () => {
     // $FlowFixMe - https://github.com/facebook/flow/issues/396
     expect(shallow(<Content maxHeight={350} />)).toHaveStyleRule(
       'max-height',
