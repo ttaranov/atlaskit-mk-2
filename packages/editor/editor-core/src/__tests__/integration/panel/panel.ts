e99c9624146af253841a9ee06cc0eca4bc176be8
Jake Coppinger <jcoppinger@atlassian.com>
2018-10-15 13:29:22 +1100

    Added mention test in panel

--- packages/editor/editor-core/src/__tests__/integration/panel/panel.ts
+++ packages/editor/editor-core/src/__tests__/integration/panel/panel.ts
@@ -6,14 +6,13 @@ import {
   fullpage,
   quickInsert,
 } from '../_helpers';
-// import {
-//   clipboardHelper,
-//   clipboardInput,
-//   copyAsHTMLButton,
-//   copyAsPlaintextButton,
-// } from '../_helpers';
-
-import { getExampleUrl } from '@atlaskit/webdriver-runner/utils/example';
+import {
+  clipboardHelper,
+  clipboardInput,
+  copyAsHTMLButton,
+  copyAsPlaintextButton,
+} from '../_helpers';
+import { insertMentionUsingClick } from '../message-renderer/_mention-helpers';
 
 BrowserTestCase(
   'Inserts a panel on fullpage',
@@ -22,17 +21,13 @@ BrowserTestCase(
     const browser = new Page(client);
 
     await browser.goto(fullpage.path);
+    await browser.waitForSelector(editable);
 
-    // await browser.waitForSelector(editable);
-    // await browser.click(editable);
-    await browser.waitForSelector(fullpage.placeholder);
-
-    await browser.click(fullpage.placeholder);
+    await browser.click(editable);
     await quickInsert(browser, 'Panel');
+
     // type some text
     await browser.type(editable, 'this text should be in the panel');
-
-    // expect(await browser.isVisible('.media-single')).toBe(true);
 
     const doc = await browser.$eval(editable, getDocFromElement);
     expect(doc).toMatchDocSnapshot();
@@ -86,29 +81,70 @@ BrowserTestCase(
 );
 
 // Cannot paste rich text in IE/Edge
-// BrowserTestCase(
-//   'Can paste rich text',
-//   { skip: ['ie', 'safari', 'edge'] },
-//   async client => {
-//     const browser = new Page(client);
-//     await browser.goto(clipboardHelper);
-//     await browser.isVisible(clipboardInput);
-//     await browser.type(
-//       clipboardInput,
-//       '<p>this is a link <a href="http://www.google.com">www.google.com</a></p><p>more elements with some <strong>format</strong></p><p>some addition<em> formatting</em></p>',
-//     );
-//     await browser.click(copyAsHTMLButton);
+BrowserTestCase(
+  'Can paste rich text',
+  { skip: ['ie', 'safari', 'edge'] },
+  async client => {
+    const browser = new Page(client);
+    await browser.goto(clipboardHelper);
+    await browser.isVisible(clipboardInput);
+    await browser.type(
+      clipboardInput,
+      '<p>this is a link <a href="http://www.google.com">www.google.com</a></p><p>more elements with some <strong>format</strong></p><p>some addition<em> formatting</em></p>',
+    );
+    await browser.click(copyAsHTMLButton);
 
-//     const fullPageEditor = getExampleUrl('editor', 'editor-core', 'full-page');
-//     await browser.goto(fullPageEditor);
-//     await browser.waitFor(editable);
+    await browser.goto(fullpage.path);
+    await browser.waitForSelector(editable);
 
-//     await browser.type(editable, '[] ');
-//     await browser.waitForSelector('ol');
+    await browser.click(editable);
+    await quickInsert(browser, 'Panel');
 
-//     await browser.paste(editable);
+    await browser.paste(editable);
 
-//     const doc = await browser.$eval(editable, getDocFromElement);
-//     expect(doc).toMatchDocSnapshot();
-//   },
-// );
+    const doc = await browser.$eval(editable, getDocFromElement);
+    expect(doc).toMatchDocSnapshot();
+  },
+);
+
+BrowserTestCase(
+  'can paste plain text',
+  { skip: ['ie', 'safari', 'edge'] },
+  async client => {
+    const browser = new Page(client);
+    await browser.goto(clipboardHelper);
+    await browser.isVisible(clipboardInput);
+    await browser.type(
+      clipboardInput,
+      'this is a link http://www.google.com more elements with some **format** some addition *formatting*',
+    );
+    await browser.click(copyAsPlaintextButton);
+
+    await browser.goto(fullpage.path);
+    await browser.waitForSelector(editable);
+
+    await browser.click(editable);
+    await quickInsert(browser, 'Panel');
+
+    await browser.paste(editable);
+    const doc = await browser.$eval(editable, getDocFromElement);
+    expect(doc).toMatchDocSnapshot();
+  },
+);
+
+BrowserTestCase(
+  'can insert mention into panel using click',
+  { skip: ['ie', 'safari', 'edge'] },
+  async client => {
+    const browser = new Page(client);
+
+    await browser.goto(fullpage.path);
+    await browser.waitFor(editable);
+    await browser.click(editable);
+    await quickInsert(browser, 'Panel');
+
+    await insertMentionUsingClick(browser, '0');
+    const doc = await browser.$eval(editable, getDocFromElement);
+    expect(doc).toMatchDocSnapshot();
+  },
+);
