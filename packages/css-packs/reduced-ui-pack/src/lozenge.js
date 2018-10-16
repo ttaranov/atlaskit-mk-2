0e9a221f1891d2ec129ae55f42020bef273522e5
Ben Conolly <ben@thinkmill.com.au>
2018-10-16 15:07:59 +1100

    CHANGESET: Update build process to use babel-7
    Summary: Update build process to use babel-7
    
    Release notes: <none>
    
    Releases: @atlaskit/reduced-ui-pack@patch
    
    Dependents: []
    
    ---
    {"summary":"Update build process to use babel-7","releases":[{"name":"@atlaskit/reduced-ui-pack","type":"patch"}],"dependents":[]}
    ---

--- packages/css-packs/reduced-ui-pack/src/lozenge.js
+++ packages/css-packs/reduced-ui-pack/src/lozenge.js
@@ -1,5 +1,5 @@
 //@flow
-import * as colors from '@atlaskit/theme/src/colors';
+import { colors } from '@atlaskit/theme';
 import evaluateInner from './utils/evaluate-inner';
 
 const lozengeBorderRadius = '3px';
