ea9af7d60d792306fce2d2e504d8692c7ee166ea
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-10-17 12:49:03 +1100

    Simplify getPreview middleware

--- packages/media/media-picker/src/popup/middleware/getPreview.ts
+++ packages/media/media-picker/src/popup/middleware/getPreview.ts
@@ -2,7 +2,11 @@ import { Store, Dispatch, Middleware } from 'redux';
 import { GetPreviewAction, isGetPreviewAction } from '../actions/getPreview';
 import { State } from '../domain';
 import { sendUploadEvent } from '../actions/sendUploadEvent';
-import { getPreviewFromMetadata, NonImagePreview } from '../../domain/preview';
+import {
+  getPreviewFromMetadata,
+  NonImagePreview,
+  Preview,
+} from '../../domain/preview';
 
 export default function(): Middleware {
   return store => (next: Dispatch<State>) => action => {
@@ -13,56 +17,54 @@ export default function(): Middleware {
   };
 }
 
+const dispatchPreviewUpdate = (
+  store: Store<State>,
+  { uploadId, file }: GetPreviewAction,
+  preview: Preview,
+) => {
+  store.dispatch(
+    sendUploadEvent({
+      event: {
+        name: 'upload-preview-update',
+        data: {
+          file,
+          preview,
+        },
+      },
+      uploadId,
+    }),
+  );
+};
+
 export async function getPreview(
   store: Store<State>,
-  { uploadId, file, collection }: GetPreviewAction,
+  action: GetPreviewAction,
 ) {
+  const { file, collection } = action;
   const { userContext } = store.getState();
   const subscription = userContext
     .getFile(file.id, { collectionName: collection })
     .subscribe({
       async next(state) {
-        if (state.status !== 'error') {
-          const { mediaType } = state;
-          // We need to wait for the next tick since rxjs might call "next" before returning from "subscribe"
-          setImmediate(() => subscription.unsubscribe());
+        if (state.status === 'error') {
+          return;
+        }
 
-          if (mediaType === 'image') {
-            const metadata = await userContext.getImageMetadata(file.id, {
-              collection,
-            });
-            const preview = getPreviewFromMetadata(metadata);
+        const { mediaType } = state;
+        // We need to wait for the next tick since rxjs might call "next" before returning from "subscribe"
+        setImmediate(() => subscription.unsubscribe());
 
-            store.dispatch(
-              sendUploadEvent({
-                event: {
-                  name: 'upload-preview-update',
-                  data: {
-                    file,
-                    preview,
-                  },
-                },
-                uploadId,
-              }),
-            );
-          } else {
-            const preview: NonImagePreview = {
-              file: state.preview ? state.preview.blob : undefined,
-            };
-
-            store.dispatch(
-              sendUploadEvent({
-                event: {
-                  name: 'upload-preview-update',
-                  data: {
-                    file,
-                    preview,
-                  },
-                },
-                uploadId,
-              }),
-            );
-          }
+        if (mediaType === 'image') {
+          const metadata = await userContext.getImageMetadata(file.id, {
+            collection,
+          });
+          const preview = getPreviewFromMetadata(metadata);
+          dispatchPreviewUpdate(store, action, preview);
+        } else {
+          const preview: NonImagePreview = {
+            file: state.preview ? state.preview.blob : undefined,
+          };
+          dispatchPreviewUpdate(store, action, preview);
         }
       },
     });
