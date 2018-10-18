d3c512ce6a1182f3ccea67f10db9c587d6fb2361
Jonathan Yeo <jyeo@atlassian.com>
2018-10-18 15:30:45 +1100

    remove unused avatar util

--- packages/people-and-teams/right-to-be-forgotten-drawers/src/util/getAvatarUrlByUserId.ts
+++
@@ -1,9 +0,0 @@
-function getAvatarUrlByUserId(
-  avatarCdnUrl: string,
-  id: string,
-  sizePx: number = 48,
-): string {
-  return `${avatarCdnUrl}/${id}?by=id&s=${sizePx}`;
-}
-
-export default getAvatarUrlByUserId;
