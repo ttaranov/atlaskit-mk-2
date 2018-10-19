91f6c4c7536fa19f7fc9f16ab504263ae8439599
Marco De Jongh <mdejongh@atlassian.com>
2018-10-19 15:17:10 +1100

    Fix import

--- packages/media/smart-card/src/Card/__tests__/unit/index.test.tsx
+++ packages/media/smart-card/src/Card/__tests__/unit/index.test.tsx
@@ -375,7 +375,7 @@ describe('Card', () => {
   });
 
   describe('Code split loading state', () => {
-    jest.mock('./CardContent', () => null);
+    jest.mock('../CardContent', () => null);
 
     it('should render the block resolving view when resolving', () => {
       const client = createClient([{ status: 'not-found' } as ObjectState]);
