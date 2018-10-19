69147c4e2e7b18a362ecfb9f929ec040a9655c8b
Ivan Demchenko <idemchenko@atlassian.com>
2018-10-19 16:21:21 +1100

    fixed imports again

--- packages/media/media-ui/src/BlockCard/index.ts
+++ packages/media/media-ui/src/BlockCard/index.ts
@@ -1,9 +1,5 @@
 export { ResolvingView, ResolvingViewProps } from './ResolvingView';
-export {
-  ResolvedView,
-  ResolvedViewProps,
-  LozengeViewModel,
-} from './ResolvedView';
+export { ResolvedView, ResolvedViewProps } from './ResolvedView';
 export { ErroredView, ErroredViewProps } from './ErroredView';
 export { ForbiddenView, ForbiddenViewProps } from './ForbiddenView';
 export { UnauthorisedView, UnauthorisedViewProps } from './UnauthorisedView';
