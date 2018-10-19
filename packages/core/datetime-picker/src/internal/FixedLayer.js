41036fd9270862c5df423242f3ce9f6da63d7320
Vito Belgiorno-Zegna <vbelgiornozegna@atlassian.com>
2018-10-19 11:11:38 +1100

    TimePicker modal now sticks to bottom ref element if positioned lower in viewport

--- packages/core/datetime-picker/src/internal/FixedLayer.js
+++ packages/core/datetime-picker/src/internal/FixedLayer.js
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { type Node } from 'react';
+import React, { Component, type Node } from 'react';
 import { layers } from '@atlaskit/theme';
 import ScrollLock from 'react-scrolllock';
 import { Popper, Manager, Reference } from '@atlaskit/popper';
@@ -14,6 +14,10 @@ type Props = {
    * The content to render in the layer.
    */
   content: Node,
+  /**
+   * input value from the menu.
+   */
+  inputValue: string,
 };
 
 /**
@@ -24,51 +28,61 @@ type Props = {
  * TODO: Fix a bug in layer/popper.js where auto flip isn't occuring when it should because of vertical scroll distance incorrectly
  * making the library think the component is further up the page than it is.
  */
-const FixedLayer = ({ containerRef, content }: Props) => {
-  if (!containerRef) {
-    // Wait for containerRef callback to cause a re-render
-    return <div />;
+class FixedLayer extends Component<Props> {
+  scheduleUpdate: () => {};
+
+  componentDidUpdate(prevProps: any) {
+    if (prevProps.inputValue !== this.props.inputValue) {
+      this.scheduleUpdate();
+    }
   }
-  const containerRect = containerRef.getBoundingClientRect();
-  return (
-    /* Need to wrap layer in a fixed position div so that it will render its content as fixed
-     * We need to set the intial top value to where the container is and zIndex so that it still
-     * applies since we're creating a new stacking context. */
-    <Manager>
-      <ScrollLock />
-      <Reference>
-        {({ ref }) => (
-          <div
-            ref={ref}
-            data-layer-child
-            style={{
-              position: 'absolute',
-              top: 0,
-              height: containerRect.height,
-              width: containerRect.width,
-              background: 'transparent',
-            }}
-          />
-        )}
-      </Reference>
-      <Popper>
-        {({ ref, style, placement }) => {
-          return (
+
+  render() {
+    const { containerRef, content } = this.props;
+
+    // Wait for containerRef callback to cause a re-render
+    if (!containerRef) return <div />;
+    const containerRect = containerRef.getBoundingClientRect();
+
+    return (
+      /* Need to wrap layer in a fixed position div so that it will render its content as fixed
+       * We need to set the intial top value to where the container is and zIndex so that it still
+       * applies since we're creating a new stacking context. */
+      <Manager>
+        <ScrollLock />
+        <Reference>
+          {({ ref }) => (
             <div
               ref={ref}
+              data-layer-child
               style={{
-                ...style,
-                zIndex: layers.dialog(),
+                position: 'absolute',
+                top: 0,
+                height: containerRect.height,
+                width: containerRect.width,
+                background: 'transparent',
               }}
-              placement={placement}
-            >
-              {content}
-            </div>
-          );
-        }}
-      </Popper>
-    </Manager>
-  );
-};
+            />
+          )}
+        </Reference>
+        <Popper>
+          {({ ref, style, placement, scheduleUpdate }) => {
+            this.scheduleUpdate = scheduleUpdate;
+
+            return (
+              <div
+                ref={ref}
+                style={{ ...style, zIndex: layers.dialog() }}
+                placement={placement}
+              >
+                {content}
+              </div>
+            );
+          }}
+        </Popper>
+      </Manager>
+    );
+  }
+}
 
 export default FixedLayer;
