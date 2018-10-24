eb7697c5ee468065448007457626061c0d0b27ad
Luke Batchelor <lbatchelor@atlassian.com>
2018-10-24 16:20:15 +1100

    Fixes bb-release-addon

--- build/bitbucket-release-addon/src/get-fs-changesets.js
+++ build/bitbucket-release-addon/src/get-fs-changesets.js
@@ -6,6 +6,14 @@ function fetchFolderInfo(user, repo, node, folderPath) {
   return `/2.0/repositories/${user}/${repo}/src/${node}/${folderPath}`;
 }
 
+function getDiffStatFromMasterUrl(user, repo, hash, page) {
+  return `/2.0/repositories/${user}/${repo}/diffstat/${hash}..master?page=${page}`;
+}
+
+function getFileUrl(user, repo, hash, filePath) {
+  return `/2.0/repositories/${user}/${repo}/src/${hash}/${filePath}`;
+}
+
 function promisifyAPRequest(url) {
   return new Promise((resolve, reject) => {
     window.AP.require('request', request => {
@@ -19,6 +27,20 @@ function promisifyAPRequest(url) {
         },
       });
     });
+  });
+}
+
+function getFullDiffStat(user, repo, hash, page, allValues = []) {
+  return promisifyAPRequest(
+    getDiffStatFromMasterUrl(user, repo, hash, page),
+  ).then(res => {
+    if (res.next) {
+      return getFullDiffStat(user, repo, hash, page + 1, [
+        ...allValues,
+        ...res.values,
+      ]);
+    }
+    return allValues;
   });
 }
 
@@ -37,37 +59,14 @@ export default function getChangesetInfo(user, repo, pullrequestid) {
     return response.values[0].hash;
   });
 
-  return hashPromise
-    .then(hash =>
-      promisifyAPRequest(fetchFolderInfo(user, repo, hash, '.changeset')).catch(
-        e => {
-          if (e.code === 'request_failed') throw 'resolve';
-          else throw e;
-        },
-      ),
-    )
-    .then(response =>
-      response.values
-        .filter(({ type }) => type === 'commit_directory')
-        .map(({ path }) => path),
-    )
-    .then(paths =>
-      Promise.all(
-        paths.map(path => getSomething(user, repo, hashPromise, path)),
-      ),
-    )
-    .then(folders =>
-      folders
-        .map(folder =>
-          folder.values.find(file =>
-            file.links.self.href.match(/.*\/changes\.json/),
-          ),
-        )
-        .map(file => file.links.self.href),
-    )
-    .then(links => Promise.all(links.map(promisifyAPRequest)))
-    .catch(e => {
-      if (e === 'resolve') return [];
-      else throw e;
-    });
+  return hashPromise.then(hash =>
+    getFullDiffStat(user, repo, hash, 1).then(allDiffStats => {
+      const relevantDiffs = allDiffStats
+        .filter(diff => diff.status !== 'removed')
+        .filter(diff => diff.new.path.match(/\.changeset\/.+?\/changes.json$/))
+        .map(diff => getFileUrl(user, repo, hash, diff.new.path))
+        .map(promisifyAPRequest);
+      return Promise.all(relevantDiffs);
+    }),
+  );
 }
