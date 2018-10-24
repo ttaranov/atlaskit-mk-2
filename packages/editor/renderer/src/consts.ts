b1a97426b91dabe1a46d063e11f4fbccdb9eb39e
Eduard Shvedai <eshvedai@atlassian.com>
2018-10-25 10:09:22 +1100

    ED-5518: fix numbered column with merged rows

--- packages/editor/renderer/src/consts.ts
+++ packages/editor/renderer/src/consts.ts
@@ -3,4 +3,5 @@ const clPrefix = 'ak-renderer-';
 export const RendererCssClassName = {
   DOCUMENT: `${clPrefix}document`,
   EXTENSION: `${clPrefix}extension`,
+  NUMBER_COLUMN: `${clPrefix}table-number-column`,
 };
