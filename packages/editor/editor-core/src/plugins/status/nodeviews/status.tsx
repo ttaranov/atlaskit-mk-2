2e8fb074a104f87d1f89e5128fe8e3fbda4b78a4
Craig Petchell <cpetchell@atlassian.com>
2018-10-25 18:30:43 +1100

    FS-3117 - show status selected in UI

--- packages/editor/editor-core/src/plugins/status/nodeviews/status.tsx
+++ packages/editor/editor-core/src/plugins/status/nodeviews/status.tsx
@@ -1,27 +1,87 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { Node as PMNode } from 'prosemirror-model';
+import { Selection } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { Status } from '@atlaskit/status';
+import { pluginKey, StatusState } from '../plugin';
 import { setStatusPickerAt } from '../actions';
+import { colors } from '@atlaskit/theme';
+
+const { B100 } = colors;
+
+interface StatusContainerProps {
+  selected: boolean;
+}
 
 const StatusContainer = styled.span`
   cursor: pointer;
+
+  * ::selection {
+    background-color: transparent;
+  }
+
+  > * {
+    border: 1px solid;
+    border-color: ${(props: StatusContainerProps) =>
+      props.selected ? B100 : 'transparent'};
+  }
 `;
 
 export interface Props {
   node: PMNode;
   view: EditorView;
+  getPos: () => number;
 }
 
-export default class StatusNodeView extends React.Component<Props> {
+export interface State {
+  selected: boolean;
+}
+
+export default class StatusNodeView extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      selected: false,
+    };
+  }
+
+  componentDidMount() {
+    const { view } = this.props;
+    const pluginState: StatusState = pluginKey.getState(view.state);
+    pluginState.selectionChanges.subscribe(this.handleSelectionChange);
+  }
+
+  componentWillUnmount() {
+    const { view } = this.props;
+    const pluginState: StatusState = pluginKey.getState(view.state);
+    pluginState.selectionChanges.unsubscribe(this.handleSelectionChange);
+  }
+
+  private handleSelectionChange = (
+    newSelection: Selection,
+    prevSelection: Selection,
+  ) => {
+    const { getPos } = this.props;
+    const { from, to } = newSelection;
+    const statusPos = getPos();
+    const selected = from <= statusPos && to > statusPos;
+
+    if (this.state.selected !== selected) {
+      this.setState({
+        selected,
+      });
+    }
+  };
+
   render() {
     const {
       attrs: { text, color, localId },
     } = this.props.node;
+    const { selected } = this.state;
 
     return (
-      <StatusContainer onClick={this.handleClick}>
+      <StatusContainer onClick={this.handleClick} selected={selected}>
         <Status text={text} color={color} localId={localId} />
       </StatusContainer>
     );
--- packages/editor/editor-core/src/plugins/status/plugin.ts
+++ packages/editor/editor-core/src/plugins/status/plugin.ts
@@ -1,4 +1,5 @@
-import { Plugin, PluginKey } from 'prosemirror-state';
+import { EditorState, Plugin, PluginKey, Selection } from 'prosemirror-state';
+import { EditorView } from 'prosemirror-view';
 import StatusNodeView from './nodeviews/status';
 import { ReactNodeView } from '../../nodeviews';
 import { PMPluginFactory } from '../../types';
@@ -7,12 +8,41 @@ export const pluginKey = new PluginKey('statusPlugin');
 
 export type StatusState = {
   showStatusPickerAt: number | null;
+  selectionChanges: SelectionChange;
 };
+
+export type SelectionChangeHandler = (
+  newSelection: Selection,
+  prevSelection: Selection,
+) => any;
+
+export class SelectionChange {
+  private changeHandlers: SelectionChangeHandler[] = [];
+
+  constructor() {
+    this.changeHandlers = [];
+  }
+
+  subscribe(cb: SelectionChangeHandler) {
+    this.changeHandlers.push(cb);
+  }
+
+  unsubscribe(cb: SelectionChangeHandler) {
+    this.changeHandlers = this.changeHandlers.filter(ch => ch !== cb);
+  }
+
+  notifyNewSelection(newSelection: Selection, prevSelection: Selection) {
+    this.changeHandlers.forEach(cb => cb(newSelection, prevSelection));
+  }
+}
 
 const createPlugin: PMPluginFactory = ({ dispatch, portalProviderAPI }) =>
   new Plugin({
     state: {
-      init: () => ({ showStatusPickerAt: null }),
+      init: () => ({
+        showStatusPickerAt: null,
+        selectionChanges: new SelectionChange(),
+      }),
       apply(tr, state: StatusState) {
         const meta = tr.getMeta(pluginKey);
 
@@ -45,6 +75,20 @@ const createPlugin: PMPluginFactory = ({ dispatch, portalProviderAPI }) =>
       nodeViews: {
         status: ReactNodeView.fromComponent(StatusNodeView, portalProviderAPI),
       },
+    },
+    view: (view: EditorView) => {
+      return {
+        update: (view: EditorView, prevState: EditorState) => {
+          const newSelection = view.state.selection;
+          const prevSelection = prevState.selection;
+          if (!prevSelection.eq(newSelection)) {
+            // selection changed
+            const pluginState: StatusState = pluginKey.getState(view.state);
+            const { selectionChanges } = pluginState;
+            selectionChanges.notifyNewSelection(newSelection, prevSelection);
+          }
+        },
+      };
     },
   });
 
