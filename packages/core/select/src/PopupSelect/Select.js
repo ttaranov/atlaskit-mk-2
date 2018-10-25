951ce579ebff5ef3e8a08bec8f62aeef6bce9ca4
Vito Belgiorno-Zegna <vbelgiornozegna@atlassian.com>
2018-10-25 15:32:22 +1100

    Make sure portal binds to DOM only if it exists

--- packages/core/select/src/PopupSelect/Select.js
+++ packages/core/select/src/PopupSelect/Select.js
@@ -23,6 +23,15 @@ import { colors } from '@atlaskit/theme';
 
 import { MenuDialog, DummyControl, defaultComponents } from './components';
 
+/** Are we rendering on the client or server? */
+function canUseDOM() {
+  return !!(
+    typeof window !== 'undefined' &&
+    window.document &&
+    window.document.createElement
+  );
+}
+
 // ==============================
 // Types
 // ==============================
@@ -249,7 +258,7 @@ export default class PopupSelect extends PureComponent<Props, State> {
     const { footer, maxMenuWidth, minMenuWidth, target, ...props } = this.props;
     const { isOpen, mergedComponents, mergedPopperProps } = this.state;
     const showSearchControl = this.showSearchControl();
-    const portalDestination = document.body;
+    const portalDestination = canUseDOM() ? document.body : null;
     const components = {
       ...mergedComponents,
       Control: showSearchControl ? mergedComponents.Control : DummyControl,
