d7939998a8524c4737adacaa98b6473e4109588c
Nathan Flew <nflew@atlassian.com>
2018-10-25 15:33:44 +1100

    Support link handling in the renderer.

---
+++ .changeset/3c57524b/changes.md
@@ -0,0 +1,4 @@
+- ED-5583: Add support for more EventHandlers in the renderer
+
+* Added event handlers for `Link` mark, `BlockCard` node and `InlineCard` node.
+* Removed `applicationCard` event handlers as this node no longer exists in the renderer.
--- packages/editor/renderer/src/react/nodes/inlineCard.tsx
+++ packages/editor/renderer/src/react/nodes/inlineCard.tsx
@@ -1,6 +1,26 @@
 import * as React from 'react';
 import { Card } from '@atlaskit/smart-card';
+import { EventHandlers } from '@atlaskit/editor-common';
 
-export default function InlineCard(props: { url?: string; data?: object }) {
-  return <Card appearance="inline" url={props.url} data={props.data} />;
+import { getEventHandler } from '../../utils';
+
+export default function InlineCard(props: {
+  url?: string;
+  data?: object;
+  eventHandlers?: EventHandlers;
+}) {
+  const { url, data, eventHandlers } = props;
+  const handler = getEventHandler(eventHandlers, 'smartCard');
+  return (
+    <Card
+      appearance="inline"
+      url={url}
+      data={data}
+      onClick={() => {
+        if (url && handler) {
+          handler(url);
+        }
+      }}
+    />
+  );
 }
