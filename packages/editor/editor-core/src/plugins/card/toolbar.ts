37daa151f669f9acc7ef3898a0bdb65a2cbcd4ec
Alex Hixon <ahixon@atlassian.com>
2018-10-26 12:34:58 +1100

    only show conversion options for card to what's allowed by schema

--- packages/editor/editor-core/src/plugins/card/toolbar.ts
+++ packages/editor/editor-core/src/plugins/card/toolbar.ts
@@ -19,6 +19,7 @@ import {
 } from './pm-plugins/doc';
 import { appearanceForNodeType } from './utils';
 import { CardAppearance } from './types';
+import { Fragment } from 'prosemirror-model';
 
 export const messages = defineMessages({
   block: {
@@ -70,17 +71,41 @@ const buildDropdown = (
   state: EditorState,
   intl: InjectedIntl,
 ): FloatingToolbarItem<Command> => {
-  const selectedCard =
-    state.selection instanceof NodeSelection && state.selection.node;
+  const { selection } = state;
+  const selectedNode = selection instanceof NodeSelection && selection.node;
   const options: SelectOption[] = [];
+  const { inlineCard, blockCard } = state.schema.nodes;
 
-  if (selectedCard) {
-    const currentAppearance = appearanceForNodeType(selectedCard.type);
+  if (selectedNode && [inlineCard, blockCard].indexOf(selectedNode.type) > -1) {
+    const currentAppearance = appearanceForNodeType(selectedNode.type);
 
     ['block', 'inline', 'link'].forEach(value => {
       // don't allow conversion to link if it has no url attached
-      if (value === 'link' && !selectedCard.attrs.url) {
+      if (value === 'link' && !selectedNode.attrs.url) {
         return;
+      }
+
+      if (value === 'block') {
+        // don't allow conversion if the parent node doesn't allow it
+        const { $from } = selection;
+        const containerDepth =
+          currentAppearance === 'block' ? $from.depth : $from.depth - 1;
+
+        const allowed = $from
+          .node(containerDepth)
+          .type.validContent(
+            Fragment.from(
+              blockCard.createChecked(
+                selectedNode.attrs,
+                undefined,
+                selectedNode.marks,
+              ),
+            ),
+          );
+
+        if (!allowed) {
+          return;
+        }
       }
 
       options.push({
