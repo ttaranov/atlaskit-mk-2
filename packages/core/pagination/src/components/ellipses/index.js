283a7837c238e3b20714384e15dde0cb52dd3c17
Ajay Narain Mathur <amathur@atlassian.com>
2018-10-26 21:11:05 +1100

    stylint fix

--- packages/core/pagination/src/components/ellipses/index.js
+++ packages/core/pagination/src/components/ellipses/index.js
@@ -6,7 +6,7 @@ const StyledEllipses = styled.span`
   display: flex;
   text-align: center;
   align-items: center;
-  padding: 0px 8px;
+  padding: 0 8px;
 `;
 
 export default function Ellipses() {
