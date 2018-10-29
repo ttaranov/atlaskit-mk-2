e5e662b4c972e46de2e632df479e3dfcc6e62a35
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-10-29 17:04:52 +1100

    Toggle MediaViewer visibility from i18n example

--- packages/media/media-viewer/examples/4-i18n.tsx
+++ packages/media/media-viewer/examples/4-i18n.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Component } from 'react';
 import {
   I18NWrapper,
   createStorybookContext,
@@ -9,14 +10,42 @@ import { imageItem, errorItem } from '../example-helpers';
 
 const context = createStorybookContext();
 
-export default () => (
-  <I18NWrapper>
-    <MediaViewer
-      featureFlags={{ customVideoPlayer: true }}
-      context={context}
-      selectedItem={errorItem}
-      dataSource={{ list: [imageItem, errorItem] }}
-      collectionName={defaultCollectionName}
-    />
-  </I18NWrapper>
-);
+export interface ExampleState {
+  isOpen: boolean;
+}
+
+class Example extends Component {
+  state: ExampleState = {
+    isOpen: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen,
+    });
+  };
+
+  render() {
+    const { isOpen } = this.state;
+
+    return (
+      <I18NWrapper>
+        <div>
+          <button onClick={this.toggle}>Toggle MediaViewer</button>
+          {isOpen && (
+            <MediaViewer
+              featureFlags={{ customVideoPlayer: true }}
+              context={context}
+              selectedItem={errorItem}
+              dataSource={{ list: [imageItem, errorItem] }}
+              collectionName={defaultCollectionName}
+              onClose={this.toggle}
+            />
+          )}
+        </div>
+      </I18NWrapper>
+    );
+  }
+}
+
+export default () => <Example />;
