9a6b5212087280f3990a5c291991f1a412bc9766
Vito Belgiorno-Zegna <vbelgiornozegna@atlassian.com>
2018-10-30 14:29:29 +1100

    Add docs

---
+++ packages/core/text-field/examples/00-basic.js
@@ -0,0 +1,12 @@
+// @flow
+
+import React from 'react';
+import TextField from '../src';
+
+const TextFieldExample = () => (
+  <div>
+    <TextField />
+  </div>
+);
+
+export default TextFieldExample;
