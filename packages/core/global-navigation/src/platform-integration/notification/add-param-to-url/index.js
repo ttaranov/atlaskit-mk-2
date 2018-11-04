00da91b6db155cb16ffdea77e2851fb041b8f19d
Harsha Ivaturi <isriharsha@atlassian.com>
2018-11-05 10:32:22 +1100

    Add tests for add-param-to-url

--- packages/core/global-navigation/src/platform-integration/notification/add-param-to-url/index.js
+++ packages/core/global-navigation/src/platform-integration/notification/add-param-to-url/index.js
@@ -3,7 +3,10 @@
 // TODO: Check if we really need to use this package below
 import UrlParse from 'url-parse';
 
-export default (url: string, key: string, value: string) => {
+export default (url: string, key?: string, value?: string) => {
+  if (!key || !value) {
+    return url;
+  }
   const parsedUrl = UrlParse(url);
   const seperator = parsedUrl.query.length ? '&' : '?';
   parsedUrl.set(
