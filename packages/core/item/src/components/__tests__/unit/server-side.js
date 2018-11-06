f6a6bdd118de1617622b366e9856e401115c397b
raja07 <rbellebon@atlassian.com>
2018-11-07 08:01:15 +1100

    Fix ssr tests

--- packages/core/item/src/components/__tests__/unit/server-side.js
+++ packages/core/item/src/components/__tests__/unit/server-side.js
@@ -6,12 +6,16 @@ import React from 'react';
 import { getExamplesFor } from '@atlaskit/build-utils/getExamples';
 import ReactDOMServer from 'react-dom/server';
 
-/* AK-5591: SSR tests failing because of Hash history needs a DOM */
-/* eslint-disable jest/no-disabled-tests */
-test.skip('Item server side rendering', async () => {
+test('Item server side rendering', async () => {
   (await getExamplesFor('item')).forEach(examples => {
     // $StringLitteral
-    const Example = require(examples.filePath).default; // eslint-disable-line import/no-dynamic-require
-    expect(() => ReactDOMServer.renderToString(<Example />)).not.toThrowError();
+    if (!examples.filePath.includes('item-story')) {
+      /* item story example contains import on react-router-dom */
+      // $StringLitteral
+      const Example = require(examples.filePath).default; // eslint-disable-line import/no-dynamic-require
+      expect(() =>
+        ReactDOMServer.renderToString(<Example />),
+      ).not.toThrowError();
+    }
   });
 });
